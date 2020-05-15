bd03bccf10984880b8e07387e6ebde1330165b80
horyu <28817604+horyu@users.noreply.github.com>
2020-05-15 09:08:54 +0900

    abc003 c

--- atcoder/abc003/src/bin/c.rs
+++ atcoder/abc003/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        k: usize,
+        mut rr: [usize; n]
+    };
+    rr.sort();
+    let mut c = 0f64;
+    let len = rr.len();
+    for &r in &rr[(len - k)..len] {
+        c = (c + r as f64) / 2.0;
+    }
+    println!("{}", c);
 }
