e457d26da45f7c601ec3b96a7a6e3dee5137aa7a
horyu <28817604+horyu@users.noreply.github.com>
2020-05-17 19:48:47 +0900

    abc004 bc

--- atcoder/abc004/src/bin/b.rs
+++ atcoder/abc004/src/bin/b.rs
@@ -1,8 +1,12 @@
-#![allow(unused_imports)]
-// use itertools::Itertools;
-use proconio::{input, marker::*};
+// cargo atcoder submit -f b
+// 実際の標準入力の最後にあるLFがtestだとない
+use std::io::{stdin, stdout, Read, Write};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    let mut buf = [0; 32];
+    let mut stdin = stdin();
+    stdin.lock().read(&mut buf[1..32]);
+    buf.reverse();
+    buf[31] = b'\n';
+    stdout().write(&buf);
 }
--- atcoder/abc004/src/bin/c.rs
+++ atcoder/abc004/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        mut n: usize
+    };
+    n %= 30;
+    let mut arr = [1, 2, 3, 4, 5, 6];
+    for i in 0..n {
+        arr.swap(i % 5, i % 5 + 1);
+    }
+    for x in &arr {
+        print!("{}", x)
+    }
+    print!("\n");
 }
