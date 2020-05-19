12b9b485e9245cc6f4ed02e809c980c2a1d19846
horyu <28817604+horyu@users.noreply.github.com>
2020-05-20 08:56:54 +0900

    /abc006 ab

---
+++ atcoder/abc006/src/bin/b.rs
@@ -0,0 +1,17 @@
+#![allow(unused_imports)]
+// use itertools::Itertools;
+use proconio::{input, marker::*};
+
+fn main() {
+    input! {
+        n: Usize1
+    };
+    let mut arr = [0; 1_000_000];
+    arr[2] = 1;
+    if n >= 3 {
+        for i in 3..=n {
+            arr[i] = (arr[i-1] + arr[i-2] + arr[i-3]) % 10007;
+        }
+    }
+    println!("{}", arr[n]);
+}
