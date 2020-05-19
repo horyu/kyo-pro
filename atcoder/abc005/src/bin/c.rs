56e911a7e111acabd7b26b92757355e1e0174f33
horyu <28817604+horyu@users.noreply.github.com>
2020-05-19 09:27:07 +0900

    abc005 c

--- atcoder/abc005/src/bin/c.rs
+++ atcoder/abc005/src/bin/c.rs
@@ -3,6 +3,25 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        t: usize,
+        n: usize,
+        mut aa: [usize; n],
+        m: usize,
+        mut bb: [usize; m]
+    };
+    for _i in 0..m {
+        let b = bb.remove(0);
+        loop {
+            if aa.is_empty() {
+                println!("no");
+                return;
+            }
+            let a = aa.remove(0);
+            if (a <= b) && (a + t >= b) {
+                break;
+            }
+        }
+    }
+    println!("yes");
 }
