263e3db50fbfa67a0ccae11e3db25ce9cb443b77
horyu <28817604+horyu@users.noreply.github.com>
2020-05-27 08:47:01 +0900

    abc010 c

--- atcoder/abc010/src/bin/c.rs
+++ atcoder/abc010/src/bin/c.rs
@@ -3,6 +3,23 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        txa : f64,
+        tya : f64,
+        txb : f64,
+        tyb : f64,
+        t: f64,
+        v: f64,
+        n: usize,
+        xxyy: [(f64, f64); n]
+    };
+    for &(x, y) in xxyy.iter() {
+        let distance = ((txa - x).powf(2.) + (tya - y).powf(2.)).sqrt()
+            + ((txb - x).powf(2.) + (tyb - y).powf(2.)).sqrt();
+        if distance <= t * v {
+            println!("YES");
+            return;
+        }
+    }
+    println!("NO");
 }
