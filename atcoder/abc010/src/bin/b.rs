0086438feac3daaac09b16e39cb60f34f6e961c9
horyu <28817604+horyu@users.noreply.github.com>
2020-05-26 09:04:29 +0900

    abc010 b

--- atcoder/abc010/src/bin/b.rs
+++ atcoder/abc010/src/bin/b.rs
@@ -7,19 +7,17 @@ fn main() {
         n: usize,
         aa: [usize; n]
     };
-    let mut sum = 0;
     // 1 2 3 4 5 0
     // o x o x o x
     // o x o o x o
-    for a in aa.iter() {
-        sum += match a % 6 {
-            1 => 0,
-            2 => 1,
-            3 => 0,
-            4 => 1,
+    let sum: usize = aa
+        .iter()
+        .map(|&a| match a % 6 {
+            1 | 3 => 0,
+            2 | 4 => 1,
             5 => 2,
-            _ => 3
-        };
-    }
+            _ => 3,
+        })
+        .sum();
     println!("{}", sum);
 }
