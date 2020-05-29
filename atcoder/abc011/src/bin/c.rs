0ed56086c3a55e7da27a49959a28a8aa2cc54b00
horyu <28817604+horyu@users.noreply.github.com>
2020-05-29 09:48:15 +0900

    abc011 c

--- atcoder/abc011/src/bin/c.rs
+++ atcoder/abc011/src/bin/c.rs
@@ -3,6 +3,30 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        mut n: isize,
+        ngs: [isize; 3]
+    };
+    if ngs.contains(&n) {
+        println!("NO");
+        return;
+    }
+    let mut count = 100;
+    'outer: while count > 0 {
+        for &diff in [3, 2, 1].iter() {
+            let next = n - diff;
+            if next <= 0 {
+                println!("YES");
+                return;
+            }
+            if !ngs.contains(&next) {
+                n = next;
+                count -= 1;
+                continue 'outer;
+            }
+        }
+        println!("NO");
+        return;
+    }
+    println!("NO");
 }
