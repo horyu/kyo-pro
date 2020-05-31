6a8f83a0b7832d6d38395eace3c3710a4748f033
horyu <28817604+horyu@users.noreply.github.com>
2020-05-31 09:35:23 +0900

    abc012 c

--- atcoder/abc012/src/bin/c.rs
+++ atcoder/abc012/src/bin/c.rs
@@ -3,6 +3,15 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize
+    };
+    let k = 2025 - n;
+    for i in 1..=9 {
+        for j in 1..=9 {
+            if k == i * j {
+                println!("{} x {}", i, j);
+            }
+        }
+    }
 }
