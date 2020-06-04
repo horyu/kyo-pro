dd1cfbacf23195b6067082afaa54678502d67e5d
horyu <28817604+horyu@users.noreply.github.com>
2020-06-04 20:50:20 +0900

    abc015 c

--- atcoder/abc015/src/bin/c.rs
+++ atcoder/abc015/src/bin/c.rs
@@ -1,8 +1,25 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        k: usize,
+        mut ttt: [[usize; k]; n]
+    };
+    for indexes in (0..k).combinations_with_replacement(n) {
+        for indexes in indexes.into_iter().permutations(n) {
+            // println!("{:?}", indexes);
+            let mut num = 0;
+            for (tt_index, t_index) in indexes.into_iter().enumerate() {
+                num ^= ttt[tt_index][t_index];
+            }
+            if num == 0 {
+                println!("Found");
+                return;
+            }
+        }
+    }
+    println!("Nothing");
 }
