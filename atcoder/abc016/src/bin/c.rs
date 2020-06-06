d4c81aa66ba122de9209f6883dc0c2d5f1404eb3
horyu <28817604+horyu@users.noreply.github.com>
2020-06-06 10:04:24 +0900

    abc016 c

--- atcoder/abc016/src/bin/c.rs
+++ atcoder/abc016/src/bin/c.rs
@@ -1,8 +1,23 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::HashSet;
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(Usize1, Usize1); m]
+    };
+    let mut g = vec![Vec::with_capacity(n); n];
+    for (a, b) in aabb {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    for i in 0..n {
+        let mut directs: HashSet<_> = (&g[i]).iter().collect();
+        let indirects: HashSet<_> = directs.iter().flat_map(|&&j| &g[j]).collect();
+        directs.insert(&i);
+        println!("{}", indirects.difference(&directs).count());
+    }
 }
