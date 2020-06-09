ad3fab0b3c02074e82804cff0dbc5be40ee5abd8
horyu <28817604+horyu@users.noreply.github.com>
2020-06-10 08:48:20 +0900

    abc019 c

--- atcoder/abc019/src/bin/c.rs
+++ atcoder/abc019/src/bin/c.rs
@@ -3,6 +3,17 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut set = std::collections::HashSet::new();
+    for a in aa {
+        let mut up = a;
+        while up < 1_000_000_000 {
+            up *= 2;
+        }
+        set.insert(up);
+    }
+    println!("{}", set.len());
 }
