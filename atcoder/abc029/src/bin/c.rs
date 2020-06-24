da776d6e366f332b78749a7541519d160af7f9c0
horyu <28817604+horyu@users.noreply.github.com>
2020-06-25 08:56:17 +0900

    abc029 c

--- atcoder/abc029/src/bin/c.rs
+++ atcoder/abc029/src/bin/c.rs
@@ -1,8 +1,27 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize
+    };
+    let abc = vec!['a', 'b', 'c'];
+    let mut s = String::new();
+    func(&mut s, &abc, n, "");
+    print!("{}", s);
+}
+
+// 文字を受け取って n>0 なら abc それぞれに対して再帰、 n==0 ならsにpush
+fn func(s: &mut String, abc: &Vec<char>, n: usize, tmp: &str) {
+    if n == 0 {
+        s.push_str(tmp);
+        s.push('\n');
+    } else {
+        for &char in abc {
+            let mut tmp = String::from(tmp);
+            tmp.push(char);
+            func(s, abc, n - 1, &tmp);
+        }
+    }
 }
