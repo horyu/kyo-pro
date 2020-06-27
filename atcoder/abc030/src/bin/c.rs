72b748744a36d27702bf7b96e86b9d603821b5b3
horyu <28817604+horyu@users.noreply.github.com>
2020-06-27 10:49:20 +0900

    abc030 c

--- atcoder/abc030/src/bin/c.rs
+++ atcoder/abc030/src/bin/c.rs
@@ -13,17 +13,21 @@ fn main() {
     let mut flight = 0;
     let mut from_to = &aa[..];
     let mut to_from = &bb[..];
-    loop {
-        if let Some(time) = from_to.first() {
-            let now = time + xy[flight % 2];
-            flight += 1;
-            if let Some(left) = to_from.iter().position(|&t| t >= now) {
-                to_from = &to_from[left..];
-                std::mem::swap(&mut from_to, &mut to_from);
-                continue;
+    while let Some(time) = from_to.first() {
+        let now = time + xy[flight % 2];
+        flight += 1;
+        let left = match to_from.binary_search_by(|t| t.cmp(&now)) {
+            Ok(i) => i,
+            Err(j) => {
+                if j >= to_from.len() {
+                    break;
+                } else {
+                    j
+                }
             }
-        }
-        break;
+        };
+        to_from = &to_from[left..];
+        std::mem::swap(&mut from_to, &mut to_from);
     }
     println!("{}", flight / 2);
 }
