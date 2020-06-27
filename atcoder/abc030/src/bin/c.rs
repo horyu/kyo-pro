717b0788061c9e9cdfedfcb712066768339378d3
horyu <28817604+horyu@users.noreply.github.com>
2020-06-27 10:01:31 +0900

    abc030 c

--- atcoder/abc030/src/bin/c.rs
+++ atcoder/abc030/src/bin/c.rs
@@ -3,6 +3,27 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        m: usize,
+        xy: [usize; 2],
+        aa: [usize; n],
+        bb: [usize; m]
+    };
+    let mut flight = 0;
+    let mut from_to = &aa[..];
+    let mut to_from = &bb[..];
+    loop {
+        if let Some(time) = from_to.first() {
+            let now = time + xy[flight % 2];
+            flight += 1;
+            if let Some(left) = to_from.iter().position(|&t| t >= now) {
+                to_from = &to_from[left..];
+                std::mem::swap(&mut from_to, &mut to_from);
+                continue;
+            }
+        }
+        break;
+    }
+    println!("{}", flight / 2);
 }
