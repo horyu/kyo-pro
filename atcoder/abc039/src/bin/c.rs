de8797d0494a867d52bcf6df292e4c121806951f
horyu <28817604+horyu@users.noreply.github.com>
2020-07-08 09:00:04 +0900

    abc039 c

--- atcoder/abc039/src/bin/c.rs
+++ atcoder/abc039/src/bin/c.rs
@@ -3,6 +3,17 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        s: String
+    };
+    // WBWBWWBWBWBW
+    let oto = ["Do", "", "Re", "", "Mi", "Fa", "", "So", "", "La", "", "Si"];
+    let kenban3 = "WBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBW";
+    for left in 0.. {
+        if &kenban3[left..(left + s.len())] == &s {
+            let index = left % oto.len();
+            println!("{}", oto[index]);
+            return;
+        }
+    }
 }
