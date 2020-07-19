e7a85c7582425f203883785381bb9317640ff09d
horyu <28817604+horyu@users.noreply.github.com>
2020-07-19 09:39:40 +0900

    abc047 c

--- atcoder/abc047/src/bin/c.rs
+++ atcoder/abc047/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        s: Chars
+    };
+    let mut pre = s[0];
+    let mut count = 0;
+    for c in s {
+        if c != pre {
+            count += 1;
+            pre = c;
+        }
+    }
+    print!("{}", count);
 }
