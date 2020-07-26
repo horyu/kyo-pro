f35d10aaaaed3356f27697f34134f83d264882dc
horyu <28817604+horyu@users.noreply.github.com>
2020-07-26 10:09:58 +0900

    abc053 c

--- atcoder/abc053/src/bin/c.rs
+++ atcoder/abc053/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        x: usize
+    };
+    let div: usize = x / 11;
+    let diff: usize = x % 11;
+    if diff == 0 {
+        println!("{}", div * 2);
+    } else if diff <= 6 {
+        println!("{}", div * 2 + 1);
+    } else {
+        println!("{}", div * 2 + 2);
+    }
 }
