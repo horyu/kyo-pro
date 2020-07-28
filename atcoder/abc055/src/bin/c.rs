0129c69df27d70aab6fe1096b66d9fefbcf3e1d1
horyu <28817604+horyu@users.noreply.github.com>
2020-07-29 08:42:05 +0900

    abc055 c

--- atcoder/abc055/src/bin/c.rs
+++ atcoder/abc055/src/bin/c.rs
@@ -3,6 +3,14 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut m: usize
+    };
+    let mut sum = 0;
+    let num = std::cmp::min(n, m / 2);
+    sum += num;
+    m -= 2 * num;
+    sum += m / 4;
+    println!("{}", sum);
 }
