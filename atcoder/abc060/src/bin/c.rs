f31870740519293eba563b436521de05dccf7251
horyu <28817604+horyu@users.noreply.github.com>
2020-08-06 08:32:26 +0900

    abc060 c

--- atcoder/abc060/src/bin/c.rs
+++ atcoder/abc060/src/bin/c.rs
@@ -1,8 +1,17 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        t: usize,
+        mut tt: [usize; n]
+    };
+    tt.push(std::usize::MAX);
+    let mut sum = 0;
+    for (&a, &b) in tt.iter().tuple_windows() {
+        sum += if a + t < b { t } else { b - a };
+    }
+    println!("{}", sum);
 }
