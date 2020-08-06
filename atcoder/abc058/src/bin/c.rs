a28467e97c1862e851ede10156d234b16c1a3869
horyu <28817604+horyu@users.noreply.github.com>
2020-08-07 08:37:10 +0900

    abc058 c

--- atcoder/abc058/src/bin/c.rs
+++ atcoder/abc058/src/bin/c.rs
@@ -3,6 +3,21 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        ss: [Chars; n]
+    };
+    let mut vv = vec![vec![0; n]; 26];
+    for (index, s) in ss.iter().enumerate() {
+        for &c in s {
+            vv[((c as u8) - b'a') as usize][index] += 1;
+        }
+    }
+    let mut s = String::new();
+    for (index, v) in vv.iter().enumerate() {
+        for _ in 0..*v.iter().min().unwrap() {
+            s.push((b'a' + index as u8) as char);
+        }
+    }
+    println!("{}", s);
 }
