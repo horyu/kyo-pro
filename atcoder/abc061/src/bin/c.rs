d30282b925c0faefa90bc54d8a392298836414d2
horyu <28817604+horyu@users.noreply.github.com>
2020-08-08 09:57:53 +0900

    abc061 c

--- atcoder/abc061/src/bin/c.rs
+++ atcoder/abc061/src/bin/c.rs
@@ -3,6 +3,18 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        k: usize,
+        mut aabb: [(usize, usize); n]
+    };
+    aabb.sort_by_key(|ab| ab.0);
+    let mut num_sum = 0;
+    for (a, b) in aabb {
+        if k <= num_sum + b {
+            println!("{}", a);
+            return;
+        }
+        num_sum += b;
+    }
 }
