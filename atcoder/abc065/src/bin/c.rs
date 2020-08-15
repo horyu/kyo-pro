4435bb9422d7867420a1222cfd7d1a7614535e65
horyu <28817604+horyu@users.noreply.github.com>
2020-08-15 09:22:42 +0900

    abc065 c

--- atcoder/abc065/src/bin/c.rs
+++ atcoder/abc065/src/bin/c.rs
@@ -2,7 +2,23 @@
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
+const MOD: isize = 1_000_000_007;
+
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: isize,
+        m: isize
+    };
+    let diff = (n - m).abs();
+    if diff == 0 {
+        println!("{}", product(n) * product(m) * 2 % MOD);
+    } else if diff == 1 {
+        println!("{}", product(n) * product(m) % MOD);
+    } else {
+        println!("0");
+    }
+}
+
+fn product(num: isize) -> isize {
+    (1..=num).fold(1, |acc, x| acc * x % MOD)
 }
