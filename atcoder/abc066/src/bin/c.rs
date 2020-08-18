3fe3209c1395c088c6eae5a6f60aab09db92cfb4
horyu <28817604+horyu@users.noreply.github.com>
2020-08-18 13:38:28 +0900

    abc066 c

--- atcoder/abc066/src/bin/c.rs
+++ atcoder/abc066/src/bin/c.rs
@@ -3,6 +3,38 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    if n == 1 {
+        println!("{}", aa[0]);
+        return;
+    }
+    // 1 -> 0
+    // 2 -> 1 0
+    // 3 -> 2 0 1
+    // 4 -> 3 1 0 2
+    // 5 -> 4 2 0 1 3
+    // 6 -> 5 3 1 0 2 4
+    let evens = (0..(n + 1) / 2)
+        .map(|i: usize| aa[2 * i])
+        .collect::<Vec<usize>>();
+    let odds = (0..n / 2)
+        .map(|i: usize| aa[2 * i + 1])
+        .collect::<Vec<usize>>();
+    let (mae, ato) = if n % 2 == 0 {
+        (odds, evens)
+    } else {
+        (evens, odds)
+    };
+    println!(
+        "{}",
+        mae.iter()
+            .rev()
+            .chain(ato.iter())
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
