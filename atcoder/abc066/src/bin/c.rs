202f56fa1bb1946cfd1c830c898ab83a9253ac46
horyu <28817604+horyu@users.noreply.github.com>
2020-08-19 08:30:38 +0900

    abc066 c

--- atcoder/abc066/src/bin/c.rs
+++ atcoder/abc066/src/bin/c.rs
@@ -5,12 +5,8 @@ use proconio::{input, marker::*};
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n]
+        aa: [String; n]
     };
-    if n == 1 {
-        println!("{}", aa[0]);
-        return;
-    }
     // 1 -> 0
     // 2 -> 1 0
     // 3 -> 2 0 1
@@ -18,11 +14,11 @@ fn main() {
     // 5 -> 4 2 0 1 3
     // 6 -> 5 3 1 0 2 4
     let evens = (0..(n + 1) / 2)
-        .map(|i: usize| aa[2 * i])
-        .collect::<Vec<usize>>();
+        .map(|i: usize| aa[2 * i].as_str())
+        .collect::<Vec<&str>>();
     let odds = (0..n / 2)
-        .map(|i: usize| aa[2 * i + 1])
-        .collect::<Vec<usize>>();
+        .map(|i: usize| aa[2 * i + 1].as_str())
+        .collect::<Vec<&str>>();
     let (mae, ato) = if n % 2 == 0 {
         (odds, evens)
     } else {
@@ -30,10 +26,9 @@ fn main() {
     };
     println!(
         "{}",
-        mae.iter()
+        mae.into_iter()
             .rev()
-            .chain(ato.iter())
-            .map(|a| a.to_string())
+            .chain(ato.into_iter())
             .collect::<Vec<_>>()
             .join(" ")
     );
