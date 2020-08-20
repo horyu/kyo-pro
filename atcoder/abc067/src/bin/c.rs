46a0e3bdcc4371843cacaadf685f1d205433740c
horyu <28817604+horyu@users.noreply.github.com>
2020-08-21 08:23:40 +0900

    abc067 c

--- atcoder/abc067/src/bin/c.rs
+++ atcoder/abc067/src/bin/c.rs
@@ -3,6 +3,18 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [isize; n]
+    };
+    let mut min = std::isize::MAX;
+    let mut x = 0isize;
+    let mut y: isize = aa.iter().sum();
+    for i in 0..(n - 1) {
+        let a = aa[i];
+        x += a;
+        y -= a;
+        min = std::cmp::min(min, (x - y).abs());
+    }
+    println!("{}", min);
 }
