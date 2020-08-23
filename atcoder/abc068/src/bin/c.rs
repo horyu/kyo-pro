57fd89865cb20c6e2597a49a56b86fbb5a9a61bd
horyu <28817604+horyu@users.noreply.github.com>
2020-08-24 08:42:26 +0900

    abc068 c

--- atcoder/abc068/src/bin/c.rs
+++ atcoder/abc068/src/bin/c.rs
@@ -1,8 +1,26 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::HashSet;
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(usize, usize); m]
+    };
+    let mut first = HashSet::new();
+    let mut second = HashSet::new();
+    for (a, b) in aabb {
+        if a == 1 {
+            first.insert(b);
+        }
+        if b == n {
+            second.insert(a);
+        }
+    }
+    println!(
+        "{}POSSIBLE",
+        ["", "IM"][(first.intersection(&second).count() == 0) as usize]
+    );
 }
