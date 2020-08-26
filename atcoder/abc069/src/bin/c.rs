04f98956352908b5b0cc26e829e618f7a7131510
horyu <28817604+horyu@users.noreply.github.com>
2020-08-27 08:06:48 +0900

    abc069 c

--- atcoder/abc069/src/bin/c.rs
+++ atcoder/abc069/src/bin/c.rs
@@ -3,6 +3,28 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut odds = 0;
+    let mut amari2 = 0;
+    let mut space4 = 0;
+    for a in aa {
+        match a % 4 {
+            0 => space4 += 1,
+            2 => amari2 += 1,
+            _ => odds += 1,
+        }
+    }
+    if amari2 == 0 {
+        println!("{}", ["No", "Yes"][(odds <= (space4 + 1)) as usize])
+    } else {
+        // 2あまりは2個以上あれば打ち消しあえて一端は4の隣
+        amari2 = 1;
+        println!(
+            "{}",
+            ["No", "Yes"][((odds + amari2) <= (space4 + 1)) as usize]
+        )
+    }
 }
