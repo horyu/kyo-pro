94894a78e54372dfad78960dab355c76724daa37
horyu <28817604+horyu@users.noreply.github.com>
2020-08-29 09:49:51 +0900

    abc070 c

--- atcoder/abc070/src/bin/c.rs
+++ atcoder/abc070/src/bin/c.rs
@@ -3,6 +3,24 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut tt: [usize; n]
+    };
+    tt.sort();
+    tt.dedup();
+    if tt.len() == 1 {
+        println!("{}", tt[0]);
+        return;
+    }
+    let ans = tt.iter().fold(1usize, |acc, &t| lcm(acc, t));
+    println!("{}", ans);
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    return a / gcd(a, b) * b;
+}
+
+fn gcd(m: usize, n: usize) -> usize {
+    return if n == 0 { m } else { gcd(n, m % n) };
 }
