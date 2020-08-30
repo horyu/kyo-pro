d5d80dca15f3b291b1d5f6327f405e7be5562501
horyu <28817604+horyu@users.noreply.github.com>
2020-08-31 08:40:24 +0900

    abc071 c

--- atcoder/abc071/src/bin/c.rs
+++ atcoder/abc071/src/bin/c.rs
@@ -3,6 +3,31 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut aa: [usize; n]
+    };
+    aa.sort();
+    aa.reverse();
+
+    let mut pre_a = 0;
+    let mut count = 0;
+    let mut pairs = vec![];
+    for a in aa {
+        if a == pre_a {
+            count += 1;
+            if count == 2 {
+                pairs.push(a);
+                if pairs.len() == 2 {
+                    println!("{}", pairs[0] * pairs[1]);
+                    return;
+                }
+                count = 0;
+            }
+        } else {
+            pre_a = a;
+            count = 1;
+        }
+    }
+    println!("0");
 }
