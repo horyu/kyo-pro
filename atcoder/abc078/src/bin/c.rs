cbbd22616a9662e898214e48e4981930aa6b7989
horyu <28817604+horyu@users.noreply.github.com>
2020-09-10 20:14:28 +0900

    abc078 c

--- atcoder/abc078/src/bin/c.rs
+++ atcoder/abc078/src/bin/c.rs
@@ -3,6 +3,14 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: i64,
+        m: i64
+    };
+    // k 番目までの期待値
+    // Sum[i=1..k] (1 / (2^m)^i) * { 100*(n-m) + 1900*m }
+    println!(
+        "{}",
+        (100 * (n - m) + 1900 * m) * 2i64.pow(m as u32)
+    );
 }
