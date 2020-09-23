618797cbca14e59fb556a35d8667fa4f588e6ad0
horyu <28817604+horyu@users.noreply.github.com>
2020-09-24 08:32:45 +0900

    abc085 c

--- atcoder/abc085/src/bin/c.rs
+++ atcoder/abc085/src/bin/c.rs
@@ -3,6 +3,20 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: isize,
+        a: isize
+    };
+    for x in 0..=n {
+        let xx = 10000 * x;
+        for y in 0..=(n - x) {
+            let z = n - x - y;
+            let sum = xx + 5000 * y + 1000 * z;
+            if sum == a {
+                println!("{} {} {}", x, y, z);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1");
 }
