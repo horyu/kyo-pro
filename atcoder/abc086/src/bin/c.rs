ee97da2224791d8d752ec289e489c92fa8e046b4
horyu <28817604+horyu@users.noreply.github.com>
2020-09-26 10:01:32 +0900

    abc086 c

--- atcoder/abc086/src/bin/c.rs
+++ atcoder/abc086/src/bin/c.rs
@@ -3,6 +3,22 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut ttxxyy: [(isize, isize, isize); n]
+    };
+    ttxxyy.insert(0, (0, 0, 0));
+    for tmp in ttxxyy.windows(2) {
+        let a = tmp[0];
+        let b = tmp[1];
+        let (at, ax, ay) = a;
+        let (bt, bx, by) = b;
+        let time = bt - at;
+        let distance = (bx - ax).abs() + (by - ay).abs();
+        if (time < distance) || ((time - distance) % 2 != 0) {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
