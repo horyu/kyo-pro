b24f9110c09ca036a9bcca110fb090f7c53a5851
horyu <28817604+horyu@users.noreply.github.com>
2020-09-28 08:26:40 +0900

    abc087 c

--- atcoder/abc087/src/bin/c.rs
+++ atcoder/abc087/src/bin/c.rs
@@ -3,6 +3,15 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa1: [usize; n],
+        aa2: [usize; n],
+    };
+    let mut max = 0;
+    for i in 0..n {
+        let sum: usize = aa1[0..=i].iter().sum::<usize>() + aa2[i..n].iter().sum::<usize>();
+        max = std::cmp::max(max, sum);
+    }
+    println!("{}", max);
 }
