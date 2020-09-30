2dc542e3189b47cd48597265ff52f25df89841ee
horyu <28817604+horyu@users.noreply.github.com>
2020-09-30 20:23:00 +0900

    abc088 c

--- atcoder/abc088/src/bin/c.rs
+++ atcoder/abc088/src/bin/c.rs
@@ -3,6 +3,24 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        c: [[isize; 3]; 3]
+    };
+    let mut a = [0; 3];
+    let mut b = [0; 3];
+    b[0] = c[0][0];
+    b[1] = c[0][1];
+    b[2] = c[0][2];
+    a[1] = c[1][0] - b[0];
+    a[2] = c[2][0] - b[0];
+
+    for i in 0..3 {
+        for j in 0..3 {
+            if a[i] + b[j] != c[i][j] {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
 }
