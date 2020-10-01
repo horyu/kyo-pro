8fd96e1b62a0fe9fed8202e9a5d3e890f960351a
horyu <28817604+horyu@users.noreply.github.com>
2020-10-02 08:55:04 +0900

    abc089 c

--- atcoder/abc089/src/bin/c.rs
+++ atcoder/abc089/src/bin/c.rs
@@ -1,8 +1,22 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut ss: [Chars; n]
+    };
+    let arr = ['M', 'A', 'R', 'C', 'H'];
+    let mut vv = vec![0usize; 5];
+    for s in ss {
+        if let Some(pos) = arr.iter().position(|x| x == &s[0]) {
+            vv[pos] += 1;
+        }
+    }
+    let rs = vv
+        .iter()
+        .combinations(3)
+        .fold(0, |acc, xxx| acc + xxx[0] * xxx[1] * xxx[2]);
+    println!("{}", rs);
 }
