375e12b734dfca11ad830ec2205febcadd26d2a5
horyu <28817604+horyu@users.noreply.github.com>
2020-10-07 09:13:48 +0900

    abc092 c

--- atcoder/abc092/src/bin/c.rs
+++ atcoder/abc092/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut aa: [isize; n]
+    };
+    aa.insert(0, 0);
+    aa.push(0);
+    let sum = aa.windows(2).fold(0, |acc, xy| acc + (xy[0] - xy[1]).abs());
+    for i in 1..=n {
+        let rs = sum - (aa[i - 1] - aa[i]).abs() - (aa[i] - aa[i + 1]).abs()
+            + (aa[i - 1] - aa[i + 1]).abs();
+        println!("{}", rs);
+    }
 }
