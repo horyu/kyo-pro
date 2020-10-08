14a8859555b8113e52a43413af0298d79012c0db
horyu <28817604+horyu@users.noreply.github.com>
2020-10-09 08:40:06 +0900

    abc093 c

--- atcoder/abc093/src/bin/c.rs
+++ atcoder/abc093/src/bin/c.rs
@@ -3,6 +3,15 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        mut abc: [usize; 3]
+    };
+    abc.sort();
+    let diff = (abc[2] - abc[1]) + (abc[2] - abc[0]);
+    let rs = if diff % 2 == 0 {
+        diff / 2
+    } else {
+        diff / 2 + 2
+    };
+    println!("{}", rs);
 }
