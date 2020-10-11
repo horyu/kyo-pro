9a54ec89cc6edf3797531dc7b9c0248cd04095c2
horyu <28817604+horyu@users.noreply.github.com>
2020-10-11 09:29:28 +0900

    abc094 c

--- atcoder/abc094/src/bin/c.rs
+++ atcoder/abc094/src/bin/c.rs
@@ -3,6 +3,14 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        xx: [usize; n]
+    };
+    let mut yy = xx.clone();
+    yy.sort();
+    let border = yy[(n - 1) / 2];
+    for x in xx {
+        println!("{}", yy[[n / 2, n / 2 - 1][(x > border) as usize]]);
+    }
 }
