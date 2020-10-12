7ec44ce7b4c95abff8371725d81e64ef0f53bec4
horyu <28817604+horyu@users.noreply.github.com>
2020-10-13 08:46:02 +0900

    abc095 c

--- atcoder/abc095/src/bin/c.rs
+++ atcoder/abc095/src/bin/c.rs
@@ -3,6 +3,26 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        a: isize,
+        b: isize,
+        c: isize,
+        x: isize,
+        y: isize,
+    };
+    use std::cmp::{max, min};
+    let mut v = vec![];
+    // 全部AB
+    v.push(2 * c * max(x, y));
+    // AとBのみ
+    v.push(a * x + b * y);
+    // (x < y) AをABで残りB
+    if x < y {
+        v.push(2 * c * x + b * (y - x));
+    }
+    // (x > y) BをABで残りA
+    if x > y {
+        v.push(2 * c * y + a * (x - y));
+    }
+    println!("{}", v.iter().min().unwrap());
 }
