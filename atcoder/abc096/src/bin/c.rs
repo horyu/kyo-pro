d3e45fe395d4f116857e6d43fec940053b3be38d
horyu <28817604+horyu@users.noreply.github.com>
2020-10-15 08:23:16 +0900

    abc096 c

--- atcoder/abc096/src/bin/c.rs
+++ atcoder/abc096/src/bin/c.rs
@@ -3,6 +3,28 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        h: usize,
+        w: usize,
+        sss: [Chars; h]
+    };
+    for y in 0..h {
+        for x in 0..w {
+            if !ok_urdl(&sss, y, x, h, w) {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
+}
+
+fn ok_urdl(sss: &[Vec<char>], i: usize, j: usize, h: usize, w: usize) -> bool {
+    if sss[i][j] == '.' {
+        return true;
+    }
+    return (i > 0 && sss[i - 1][j] == '#')
+        || (i < h - 1 && sss[i + 1][j] == '#')
+        || (j > 0 && sss[i][j - 1] == '#')
+        || (j < w - 1 && sss[i][j + 1] == '#');
 }
