12aa1a5dc9fb10ddf3424aa9d81730d69c0769b8
horyu <28817604+horyu@users.noreply.github.com>
2020-10-19 08:53:25 +0900

    abc098 c

--- atcoder/abc098/src/bin/c.rs
+++ atcoder/abc098/src/bin/c.rs
@@ -3,6 +3,17 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        s: Chars
+    };
+    let mut left = 0usize;
+    let mut right = (&s[1..]).iter().filter(|&&c| c == 'E').count();
+    let mut rs = left + right;
+    for i in 1..n {
+        left += (s[i - 1] == 'W') as usize;
+        right -= (s[i] == 'E') as usize;
+        rs = std::cmp::min(rs, left + right);
+    }
+    println!("{}", rs);
 }
