15d083b0889265b852ca0dc4ccdb348f863dc1ae
horyu <28817604+horyu@users.noreply.github.com>
2020-10-22 08:28:32 +0900

    abc100 c

--- atcoder/abc100/src/bin/c.rs
+++ atcoder/abc100/src/bin/c.rs
@@ -3,6 +3,20 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut aa: [usize; n]
+    };
+    let rs = aa
+        .into_iter()
+        .map(|mut a| {
+            let mut num = 0;
+            while a % 2 == 0 {
+                num += 1;
+                a /= 2;
+            }
+            num
+        })
+        .sum::<i32>();
+    println!("{}", rs);
 }
