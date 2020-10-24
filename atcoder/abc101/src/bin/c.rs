f09b8cfe1a2b35cfa445e900fbd3b4be1774a384
horyu <28817604+horyu@users.noreply.github.com>
2020-10-24 10:05:07 +0900

    abc101 c

--- atcoder/abc101/src/bin/c.rs
+++ atcoder/abc101/src/bin/c.rs
@@ -3,6 +3,22 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        k: usize,
+        _aa: [usize; n]
+    };
+    if n == k {
+        println!("1");
+        return;
+    }
+    let mut min = 100000;
+    // 先頭 i から k-1 ずつ範囲を拡大していく
+    // iを 2 ~ k までずらしていく
+    for i in 2..=k {
+        let nokori = n - i;
+        let count = 1 + nokori / (k - 1) + ((nokori % (k - 1) != 0) as usize);
+        min = std::cmp::min(min, count);
+    }
+    println!("{}", min);
 }
