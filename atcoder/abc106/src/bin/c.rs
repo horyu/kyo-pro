b8289b9830ac6291e15c5a0aead443eda82b0e2a
horyu <28817604+horyu@users.noreply.github.com>
2020-10-31 09:45:08 +0900

    abc106 c

--- atcoder/abc106/src/bin/c.rs
+++ atcoder/abc106/src/bin/c.rs
@@ -3,6 +3,29 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        s: Chars,
+        k: u128
+    };
+    let s: Vec<_> = s.iter().map(|&c| c.to_digit(10).unwrap() as u128).collect();
+    let mut sum = 0u128;
+    for c in s {
+        if c == 1 {
+            sum += 1;
+            if sum >= k {
+                println!("{}", c);
+                return;
+            }
+        } else {
+            let mut csize = 1;
+            for _ in 0..k {
+                csize *= c;
+                if sum + csize >= k {
+                    println!("{}", c);
+                    return;
+                }
+            }
+            sum += csize;
+        }
+    }
 }
