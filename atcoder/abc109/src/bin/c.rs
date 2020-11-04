6a2ee9795acb8f912c0a901b2dd89af5a5422b96
horyu <28817604+horyu@users.noreply.github.com>
2020-11-05 08:31:45 +0900

    abc109 c

--- atcoder/abc109/src/bin/c.rs
+++ atcoder/abc109/src/bin/c.rs
@@ -3,6 +3,22 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        x0: isize,
+        xx: [isize; n]
+    };
+    let rs = xx
+        .iter()
+        .map(|x| (x0 - x).abs())
+        .fold((x0 - xx[0]).abs(), gcd);
+    println!("{}", rs);
+}
+
+fn gcd(a: isize, b: isize) -> isize {
+    if a % b == 0 {
+        b
+    } else {
+        gcd(b, a % b)
+    }
 }
