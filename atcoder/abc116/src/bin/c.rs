f73f231765139bdf1739f1a0ce5f609d9c69c5a8
horyu <28817604+horyu@users.noreply.github.com>
2020-11-14 09:17:43 +0900

    abc116 c

--- atcoder/abc116/src/bin/c.rs
+++ atcoder/abc116/src/bin/c.rs
@@ -3,6 +3,21 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut hh: [usize; n]
+    };
+    let mut count = 0;
+    for _ in 0..(*hh.iter().max().unwrap()) {
+        for group in hh.split_mut(|&h| h == 0) {
+            if group.is_empty() {
+                continue;
+            }
+            for h in group {
+                *h -= 1;
+            }
+            count += 1;
+        }
+    }
+    println!("{}", count);
 }
