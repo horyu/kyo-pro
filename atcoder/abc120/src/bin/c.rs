31e14c0c556adfe680fb189e5d7f9cae22d3babd
horyu <28817604+horyu@users.noreply.github.com>
2020-11-26 20:24:05 +0900

    abc120 c

--- atcoder/abc120/src/bin/c.rs
+++ atcoder/abc120/src/bin/c.rs
@@ -3,6 +3,25 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        mut s: Chars
+    };
+    let n = s.len();
+    if n == 1 {
+        println!("0");
+        return;
+    }
+    let mut i = 0usize;
+    while i + 1 < s.len() {
+        if s[i] != s[i + 1] {
+            s.remove(i + 1);
+            s.remove(i);
+            if i > 0 {
+                i -= 1;
+            }
+            continue;
+        }
+        i += 1;
+    }
+    println!("{}", n - s.len());
 }
