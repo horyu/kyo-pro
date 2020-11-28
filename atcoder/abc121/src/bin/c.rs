fd7c322eb6fc06e4ee6bcb079d2a91f8b4c12271
horyu <28817604+horyu@users.noreply.github.com>
2020-11-28 10:00:32 +0900

    abc121 c

--- atcoder/abc121/src/bin/c.rs
+++ atcoder/abc121/src/bin/c.rs
@@ -3,6 +3,27 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        mut abab: [(usize, usize); n]
+    };
+    abab.sort_unstable_by_key(|ab| ab.0);
+    let mut sum = 0;
+    let mut count = 0;
+    while count < m {
+        if abab[0].1 == 0 {
+            abab.remove(0);
+        }
+        if count + abab[0].1 <= m {
+            sum += abab[0].0 * abab[0].1;
+            count += abab[0].1;
+            abab[0].1 = 0;
+        } else {
+            sum += abab[0].0;
+            count += 1;
+            abab[0].1 -= 1;
+        }
+    }
+    println!("{}", sum);
 }
