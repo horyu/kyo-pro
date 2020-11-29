4a08e4bcc5186eb951483ac02674868a0123118d
horyu <28817604+horyu@users.noreply.github.com>
2020-11-30 08:53:16 +0900

    abc122 c

--- atcoder/abc122/src/bin/c.rs
+++ atcoder/abc122/src/bin/c.rs
@@ -3,6 +3,19 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        q: usize,
+        s: String,
+        lrlr: [(Usize1, Usize1); q]
+    };
+    let mut ac_counts = vec![0];
+    let mut count = 0;
+    for i in 0usize..(n - 1) {
+        count += (&s[i..=(i + 1)] == "AC") as i32;
+        ac_counts.push(count);
+    }
+    for (l, r) in lrlr {
+        println!("{}", ac_counts[r] - ac_counts[l]);
+    }
 }
