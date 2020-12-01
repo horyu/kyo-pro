a38b271d038840ba6eb4f1b0aac5bc6337ceaa9a
horyu <28817604+horyu@users.noreply.github.com>
2020-12-02 08:24:24 +0900

    abc123 c

--- atcoder/abc123/src/bin/c.rs
+++ atcoder/abc123/src/bin/c.rs
@@ -3,6 +3,15 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        xx: [usize; 5]
+        // a: usize,
+        // b: usize,
+        // c: usize,
+        // d: usize,
+        // e: usize,
+    };
+    let min = xx.iter().min().unwrap();
+    println!("{}", n / min + ((n % min != 0) as usize) + 4);
 }
