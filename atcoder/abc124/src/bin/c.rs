1a893499cdb8808c79e742a49817782a0ef7e9b3
horyu <28817604+horyu@users.noreply.github.com>
2020-12-04 08:31:09 +0900

    abc124 c

--- atcoder/abc124/src/bin/c.rs
+++ atcoder/abc124/src/bin/c.rs
@@ -3,6 +3,21 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        s: Chars
+    };
+    let mut black_start = 0;
+    let mut white_start = 0;
+    for i in 0..s.len() {
+        let is_black = (s[i] == '0') as i32;
+        let is_white = (s[i] == '1') as i32;
+        if i % 2 == 0 {
+            black_start += is_white;
+            white_start += is_black;
+        } else {
+            black_start += is_black;
+            white_start += is_white;
+        }
+    }
+    println!("{}", std::cmp::min(black_start, white_start));
 }
