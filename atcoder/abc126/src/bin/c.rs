9d1cfde3dbd741548e27e164e5e1f059e7312405
horyu <28817604+horyu@users.noreply.github.com>
2020-12-07 08:44:11 +0900

    abc126 c

--- atcoder/abc126/src/bin/c.rs
+++ atcoder/abc126/src/bin/c.rs
@@ -1,8 +1,27 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
+use std::collections::btree_map::Keys;
+
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize
+    };
+    let base = 1.0 / (n as f64);
+    let rs = (1usize..=n).fold(0f64, |acc, i| {
+        if i >= k {
+            acc + base
+        } else {
+            let mut score = i;
+            let mut p = base;
+            while score < k {
+                score *= 2;
+                p /= 2f64;
+            }
+            acc + p
+        }
+    });
+    println!("{}", rs);
 }
