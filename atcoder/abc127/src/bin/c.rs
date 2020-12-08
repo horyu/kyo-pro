187061e91d0e3928465e7b29220fb783480537e9
horyu <28817604+horyu@users.noreply.github.com>
2020-12-09 08:30:44 +0900

    abc127 c

--- atcoder/abc127/src/bin/c.rs
+++ atcoder/abc127/src/bin/c.rs
@@ -3,6 +3,18 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        lrlr: [(usize, usize); m]
+    };
+    let mut left = 0;
+    let mut right = n;
+    for (l, r) in lrlr {
+        use std::cmp;
+        left = cmp::max(l, left);
+        right = cmp::min(r, right);
+    }
+    let rs = if right >= left { right - left + 1 } else { 0 };
+    println!("{}", rs);
 }
