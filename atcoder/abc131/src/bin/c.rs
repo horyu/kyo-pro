87c61a0148d61cd6d6e2d521e766cc1c3786940d
horyu <28817604+horyu@users.noreply.github.com>
2020-12-16 08:45:53 +0900

    abc131 c

--- atcoder/abc131/src/bin/c.rs
+++ atcoder/abc131/src/bin/c.rs
@@ -3,6 +3,24 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+    };
+    let rs = (b - a + 1) - (b / c - (a - 1) / c) - (b / d - (a - 1) / d) + (b / lcm(c, d) - (a - 1) / lcm(c, d));
+    println!("{}", rs);
+}
+
+// http://www.nct9.ne.jp/m_hiroi/linux/rust01.html
+// 最小公倍数
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
