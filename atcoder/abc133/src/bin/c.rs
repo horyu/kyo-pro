59091aaa660e71f58c402fa9382faae6ebbfc608
horyu <28817604+horyu@users.noreply.github.com>
2020-12-23 08:23:59 +0900

    abc133 c

--- atcoder/abc133/src/bin/c.rs
+++ atcoder/abc133/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        l: usize,
+        r: usize,
+    };
+    use std::cmp::min;
+    let mut rs = std::usize::MAX;
+    for i in l..=min(r - 1, l + 2019 - 1) {
+        for j in (i + 1)..=min(r, l + 2019) {
+            rs = min(rs, i * j % 2019)
+        }
+    }
+    println!("{}", rs);
 }
