372494bdecd5431d8ea7fe4278c331dc99c3cbc4
horyu <28817604+horyu@users.noreply.github.com>
2020-12-24 08:22:05 +0900

    abc134 c

--- atcoder/abc134/src/bin/c.rs
+++ atcoder/abc134/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut bb = aa.clone();
+    bb.sort_unstable();
+    bb.reverse();
+    let x = bb[0];
+    let y = bb[1];
+    for a in aa {
+        println!("{}", [x, y][(a == x) as usize]);
+    }
 }
