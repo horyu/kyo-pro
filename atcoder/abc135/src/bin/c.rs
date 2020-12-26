5a13150b97584d59d43248efb61b7785e22e038e
horyu <28817604+horyu@users.noreply.github.com>
2020-12-26 09:42:27 +0900

    abc135 c

--- atcoder/abc135/src/bin/c.rs
+++ atcoder/abc135/src/bin/c.rs
@@ -3,6 +3,21 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aa: [usize; n + 1],
+        bb: [usize; n]
+    };
+    let mut sum = 0;
+    for i in 0usize..n {
+        let b = bb[i];
+        if b >= aa[i] + aa[i + 1] {
+            sum += aa[i] + aa[i + 1];
+            aa[i + 1] = 0;
+        } else {
+            sum += b;
+            aa[i + 1] -= b.checked_sub(aa[i]).unwrap_or(0);
+        }
+    }
+    println!("{}", sum);
 }
