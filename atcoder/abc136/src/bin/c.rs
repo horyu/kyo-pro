6b4c89f7357b36917a0a65f8bdbc754e84eaeac5
horyu <28817604+horyu@users.noreply.github.com>
2020-12-29 16:46:37 +0900

    abc136 c

--- atcoder/abc136/src/bin/c.rs
+++ atcoder/abc136/src/bin/c.rs
@@ -3,6 +3,23 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        hh: [usize; n]
+    };
+    if n == 1 {
+        println!("Yes");
+        return;
+    }
+    let mut pre = hh[0] - 1;
+    for &h in &hh[1..] {
+        if pre < h {
+            pre = h - 1;
+        } else if pre == h {
+        } else {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
