05e80fef630c2b7f3dd9d8119cc3796f1f76251e
horyu <28817604+horyu@users.noreply.github.com>
2020-12-30 16:38:31 +0900

    abc136 d

--- atcoder/abc136/src/bin/d.rs
+++ atcoder/abc136/src/bin/d.rs
@@ -3,6 +3,43 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        mut s: Chars
+    };
+    s.push('R');
+    // LR となったらそれまでを集計
+    let mut counts = vec![0; s.len()];
+    let mut zure_even = 0;
+    let mut zure_odd = 0;
+    let mut close_r = 0;
+    let mut close_l = 0;
+    let mut base_r = 0usize;
+    for i in 0..(s.len() - 1) {
+        if (i - base_r) % 2 == 0 {
+            zure_even += 1;
+        } else {
+            zure_odd += 1;
+        }
+        if (s[i], s[i + 1]) == ('R', 'L') {
+            close_r = i;
+            close_l = i + 1;
+        } else if (s[i], s[i + 1]) == ('L', 'R') {
+            if (i - close_l) % 2 == 0 {
+                counts[close_r] += zure_odd;
+                counts[close_l] += zure_even;
+            } else {
+                counts[close_r] += zure_even;
+                counts[close_l] += zure_odd;
+            }
+            zure_even = 0;
+            zure_odd = 0;
+            base_r = i + 1;
+        }
+    }
+    let mut rs = counts[0].to_string();
+    for count in &counts[1..(counts.len() - 1)] {
+        rs.push(' ');
+        rs.push_str(&count.to_string())
+    }
+    println!("{}", rs);
 }
