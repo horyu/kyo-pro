61f875d8ca16c5932e512bb1aa773a557d7c1876
horyu <28817604+horyu@users.noreply.github.com>
2021-01-03 15:05:40 +0900

    abc138 c

--- atcoder/abc138/src/bin/c.rs
+++ atcoder/abc138/src/bin/c.rs
@@ -3,6 +3,11 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut vv: [usize; n]
+    };
+    vv.sort_unstable();
+    let vv: Vec<f64> = vv.into_iter().map(|v| v as f64).collect();
+    println!("{}", vv[1..].iter().fold(vv[0], |acc, v| (acc + v) / 2f64));
 }
