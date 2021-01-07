a7e9dd125b1e6c5c6b3efd786e38d89b3d7cfdd1
horyu <28817604+horyu@users.noreply.github.com>
2021-01-08 08:49:01 +0900

    abc140 c

--- atcoder/abc140/src/bin/c.rs
+++ atcoder/abc140/src/bin/c.rs
@@ -3,6 +3,16 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        bb: [usize; n - 1]
+    };
+    let mut aa = vec![0; n];
+    aa[0] = bb[0];
+    for i in 1..(n - 1) {
+        aa[i] = bb[i - 1].min(bb[i]);
+    }
+    aa[n - 1] = bb[n - 2];
+    println!("{}", aa.iter().sum::<usize>());
+    dbg!(aa);
 }
