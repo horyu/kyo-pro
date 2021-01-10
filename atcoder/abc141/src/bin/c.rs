18c9c9f09e0a528a11492e0fecb3a7e73a53ffde
horyu <28817604+horyu@users.noreply.github.com>
2021-01-10 10:24:25 +0900

    abc141 c

--- atcoder/abc141/src/bin/c.rs
+++ atcoder/abc141/src/bin/c.rs
@@ -3,6 +3,17 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: isize,
+        q: isize,
+        aa: [Usize1; q]
+    };
+    let mut kk = vec![k - q; n];
+    for a in aa {
+        kk[a] += 1;
+    }
+    for k in kk {
+        println!("{}", ["No", "Yes"][(k > 0) as usize]);
+    }
 }
