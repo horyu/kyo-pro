d9b8d6ebb29ab874814f41b5ee81578c7437e3d9
horyu <28817604+horyu@users.noreply.github.com>
2021-01-13 08:28:58 +0900

    abc142 c

--- atcoder/abc142/src/bin/c.rs
+++ atcoder/abc142/src/bin/c.rs
@@ -1,8 +1,13 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut vv: Vec<(usize, usize)> = aa.into_iter().enumerate().collect();
+    vv.sort_by_key(|&(_i, a)| a);
+    println!("{}", vv.iter().map(|&(i, _a)| i + 1).join(" "));
 }
