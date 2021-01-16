5eed0e7905aeaf2f816ed930b214f008e42b8f91
horyu <28817604+horyu@users.noreply.github.com>
2021-01-16 10:01:20 +0900

    abc143 d

--- atcoder/abc143/src/bin/d.rs
+++ atcoder/abc143/src/bin/d.rs
@@ -3,6 +3,22 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut ll: [usize; n]
+    };
+    ll.sort_unstable();
+    let mut count = 0;
+    for l in 0..(n - 2) {
+        for c in (l + 1)..(n - 1) {
+            for r in (c + 1)..n {
+                if ll[r] < ll[l] + ll[c] {
+                    count += 1;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    println!("{}", count);
 }
