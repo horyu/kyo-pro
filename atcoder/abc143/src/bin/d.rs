439f1715ea89279b847fff643555430436a03984
horyu <28817604+horyu@users.noreply.github.com>
2021-01-17 10:01:21 +0900

    abc143 d

--- atcoder/abc143/src/bin/d.rs
+++ atcoder/abc143/src/bin/d.rs
@@ -11,13 +11,11 @@ fn main() {
     let mut count = 0;
     for l in 0..(n - 2) {
         for c in (l + 1)..(n - 1) {
-            for r in (c + 1)..n {
-                if ll[r] < ll[l] + ll[c] {
-                    count += 1;
-                } else {
-                    break;
-                }
-            }
+            let rr = ll[(c + 1)..].binary_search(&(ll[l] + ll[c] - 1));
+            count += match rr {
+                Ok(r) => r + 1,
+                Err(r) => r,
+            };
         }
     }
     println!("{}", count);
