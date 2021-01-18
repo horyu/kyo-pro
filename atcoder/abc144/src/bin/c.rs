287291750a9a70bc1c3ab38bde1aea1a8bf9a8e7
horyu <28817604+horyu@users.noreply.github.com>
2021-01-19 08:21:04 +0900

    abc144 c

--- atcoder/abc144/src/bin/c.rs
+++ atcoder/abc144/src/bin/c.rs
@@ -3,6 +3,14 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize
+    };
+    let mut min = n - 1;
+    for i in (2usize..).take_while(|x| x * x <= n) {
+        if n % i == 0 {
+            min = min.min((n / i - 1) + (i - 1));
+        }
+    }
+    println!("{}", min);
 }
