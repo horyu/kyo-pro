6c24e6d5b20d869e06e9f2c882a84e512792bee3
horyu <28817604+horyu@users.noreply.github.com>
2021-01-22 08:30:03 +0900

    abc145 c

--- atcoder/abc145/src/bin/c.rs
+++ atcoder/abc145/src/bin/c.rs
@@ -1,8 +1,28 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        xxyy: [(isize, isize); n]
+    };
+    let mut sum = 0.0;
+    let mut count = 0;
+    let mut memo = vec![vec![0.0; n]; n];
+    for indexes in (0..n).permutations(n) {
+        count += 1;
+        sum += indexes.windows(2).fold(0.0, |acc, pair| {
+            let mae = pair[0];
+            let ato = pair[1];
+            if memo[mae][ato] == 0.0 {
+                let m = xxyy[mae];
+                let a = xxyy[ato];
+                memo[mae][ato] = (((m.0 - a.0).pow(2) + (m.1 - a.1).pow(2)) as f64).sqrt();
+                memo[ato][mae] = memo[mae][ato];
+            }
+            acc + memo[mae][ato]
+        });
+    }
+    println!("{}", (sum / (count as f64)));
 }
