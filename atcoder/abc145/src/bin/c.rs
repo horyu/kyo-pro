3996fcbe94724ab5b7598f2d2de041d55fe081e9
horyu <28817604+horyu@users.noreply.github.com>
2021-01-23 09:37:31 +0900

    abc145 c

--- atcoder/abc145/src/bin/c.rs
+++ atcoder/abc145/src/bin/c.rs
@@ -7,22 +7,12 @@ fn main() {
         n: usize,
         xxyy: [(isize, isize); n]
     };
-    let mut sum = 0.0;
-    let mut count = 0;
-    let mut memo = vec![vec![0.0; n]; n];
-    for indexes in (0..n).permutations(n) {
-        count += 1;
-        sum += indexes.windows(2).fold(0.0, |acc, pair| {
-            let mae = pair[0];
-            let ato = pair[1];
-            if memo[mae][ato] == 0.0 {
-                let m = xxyy[mae];
-                let a = xxyy[ato];
-                memo[mae][ato] = (((m.0 - a.0).pow(2) + (m.1 - a.1).pow(2)) as f64).sqrt();
-                memo[ato][mae] = memo[mae][ato];
-            }
-            acc + memo[mae][ato]
-        });
-    }
-    println!("{}", (sum / (count as f64)));
+    let sum = (0..n).combinations(2).fold(0.0, |acc, pair| {
+        let mae = pair[0];
+        let ato = pair[1];
+        let m = xxyy[mae];
+        let a = xxyy[ato];
+        acc + (((m.0 - a.0).pow(2) + (m.1 - a.1).pow(2)) as f64).sqrt()
+    });
+    println!("{}", sum * 2.0 / (n as f64));
 }
