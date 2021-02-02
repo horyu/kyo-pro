b680af6bd864c09c162e94780e723368503ec4c6
horyu <28817604+horyu@users.noreply.github.com>
2021-02-03 08:16:44 +0900

    abc148 c

--- atcoder/abc148/src/bin/c.rs
+++ atcoder/abc148/src/bin/c.rs
@@ -4,6 +4,21 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        a: usize,
+        b: usize,
+    };
+    println!("{}", lcm(a, b));
+}
+
+
+// http://www.nct9.ne.jp/m_hiroi/linux/rust01.html#chap06
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b)
+    }
+}
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
 }
