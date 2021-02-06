69c42558e76f76ea15a7cc5d1bf257020dcce4ff
horyu <28817604+horyu@users.noreply.github.com>
2021-02-06 11:09:37 +0900

    abc149 d

--- atcoder/abc149/src/bin/d.rs
+++ atcoder/abc149/src/bin/d.rs
@@ -4,6 +4,31 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        k: usize,
+        r: usize,
+        s: usize,
+        p: usize,
+        t: Chars,
+    };
+    let mut sum = 0;
+    // [0,k,2k,...], [1,k+1,2k+1,...], [...] 分けて考える
+    for start in 0..k {
+        let mut pre = '_';
+        for c in t.iter().skip(start).step_by(k) {
+            if c == &pre {
+                pre = '_';
+                continue;
+            }
+            sum += match c {
+                'r' => p,
+                's' => r,
+                'p' => s,
+                _ => unreachable!(),
+            };
+            pre = *c;
+        }
+    }
+    println!("{}", sum);
 }
