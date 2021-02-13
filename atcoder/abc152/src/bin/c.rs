3d59df96d21be5a8b95a3f007662dc1dc46823d8
horyu <28817604+horyu@users.noreply.github.com>
2021-02-13 09:47:02 +0900

    abc152 c

--- atcoder/abc152/src/bin/c.rs
+++ atcoder/abc152/src/bin/c.rs
@@ -4,6 +4,17 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        pp: [usize; n]
+    };
+    let mut min = pp[0];
+    let mut rs = 0;
+    for p in pp {
+        if p <= min {
+            rs += 1;
+        }
+        min = min.min(p);
+    }
+    println!("{}", rs);
 }
