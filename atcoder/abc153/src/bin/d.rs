73513e6b3bd182e6330f8269481b4cdf4b0dfa00
horyu <28817604+horyu@users.noreply.github.com>
2021-02-19 08:21:53 +0900

    abc153 d

--- atcoder/abc153/src/bin/d.rs
+++ atcoder/abc153/src/bin/d.rs
@@ -4,6 +4,15 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        mut h: usize
+    };
+    let mut num = 1usize;
+    let mut rs = 0usize;
+    while h >= 1 {
+        rs += num;
+        num *= 2;
+        h /= 2;
+    }
+    println!("{}", rs);
 }
