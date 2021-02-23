d0dcbd35e8e46ca7ba189e9c1546eadd1b48a420
horyu <28817604+horyu@users.noreply.github.com>
2021-02-24 08:12:16 +0900

    abc155 c

--- atcoder/abc155/src/bin/c.rs
+++ atcoder/abc155/src/bin/c.rs
@@ -1,9 +1,27 @@
 #![allow(clippy::many_single_char_names)]
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ss: [String; n]
+    };
+    let mut hm = std::collections::HashMap::new();
+    let mut max = 0;
+    for s in ss {
+        let count = hm.entry(s).or_insert(0);
+        *count += 1;
+        max = max.max(*count);
+    }
+    let mut keys = hm
+        .iter()
+        .filter(|(_k, v)| **v == max)
+        .map(|(k, _v)| k)
+        .collect_vec();
+    keys.sort_unstable();
+    for s in keys {
+        println!("{}", s);
+    }
 }
