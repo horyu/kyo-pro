49900481c82170305485a496e8206be7b923a90f
horyu <28817604+horyu@users.noreply.github.com>
2021-02-27 11:15:21 +0900

    abc072 d

--- atcoder/abc072/src/bin/d.rs
+++ atcoder/abc072/src/bin/d.rs
@@ -3,6 +3,22 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        pp: [Usize1; n],
+    };
+    let mut rs = 0;
+    // 連続した要素が index と被っていたら +1
+    // 1個だけ index と同じでも +1
+    let mut i = 0;
+    while i < n {
+        if i == pp[i] {
+            rs += 1;
+            if (i + 1 < n) && ((i + 1) == pp[i + 1]) {
+                i += 1;
+            }
+        }
+        i += 1;
+    }
+    println!("{}", rs);
 }
