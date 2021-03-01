f16a92eca9a75745f5defb9039ff7a71a7781263
horyu <28817604+horyu@users.noreply.github.com>
2021-03-02 08:47:50 +0900

    abc157 c

--- atcoder/abc157/src/bin/c.rs
+++ atcoder/abc157/src/bin/c.rs
@@ -5,6 +5,33 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        scsc: [(Usize1, usize); m]
+    };
+    let mut v = vec![None; n];
+    for (s, c) in scsc {
+        if let Some(vc) = v[s] {
+            if vc != c {
+                println!("-1");
+                return;
+            }
+        } else {
+            v[s] = Some(c);
+        }
+    }
+    if n > 1 {
+        if let Some(0) = v[0] {
+            println!("-1");
+            return;
+        } else if v[0].is_none() {
+            v[0] = Some(1);
+        }
+    }
+    let rs = v
+        .into_iter()
+        .map(|opt| opt.unwrap_or_default().to_string())
+        .join("");
+    println!("{}", rs);
 }
