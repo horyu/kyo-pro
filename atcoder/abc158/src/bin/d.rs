44fc801e2ecb89ea2313a37c6724ed0930155d08
horyu <28817604+horyu@users.noreply.github.com>
2021-03-05 09:46:58 +0900

    abc158 d

--- atcoder/abc158/src/bin/d.rs
+++ atcoder/abc158/src/bin/d.rs
@@ -2,9 +2,45 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    mem::swap,
+};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        mut s: Chars,
+        q: usize,
+    };
+    let mut mae = std::collections::VecDeque::new();
+    let mut ato = std::collections::VecDeque::new();
+    let mut rev = false;
+    for _ in 0..q {
+        input! { t: usize };
+        if t == 1 {
+            rev = !rev;
+        } else {
+            input! { f: usize, c: char }
+            if ((f == 1) && !rev) || ((f != 1) && rev) {
+                mae.push_front(c);
+            } else {
+                ato.push_back(c);
+            }
+        }
+    }
+    if rev {
+        println!(
+            "{}{}{}",
+            ato.iter().rev().join(""),
+            s.iter().rev().join(""),
+            mae.iter().rev().join("")
+        );
+    } else {
+        println!(
+            "{}{}{}",
+            mae.iter().join(""),
+            s.iter().join(""),
+            ato.iter().join("")
+        );
+    }
 }
