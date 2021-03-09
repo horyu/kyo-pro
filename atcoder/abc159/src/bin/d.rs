f2a2291bacd56f65f4605833b0333762ffedcbf3
horyu <28817604+horyu@users.noreply.github.com>
2021-03-09 23:10:15 +0900

    abc159 d

--- atcoder/abc159/src/bin/d.rs
+++ atcoder/abc159/src/bin/d.rs
@@ -5,6 +5,17 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [Isize1; n]
+    };
+    let mut v = vec![0isize; n];
+    for &a in &aa {
+        v[a as usize] += 1;
+    }
+    let sum = v.iter().fold(0isize, |acc, &x| acc + x * (x - 1) / 2);
+    for &a in &aa {
+        let x = v[a as usize];
+        println!("{}", sum - x * (x - 1) / 2 + (x - 1) * (x - 2) / 2);
+    }
 }
