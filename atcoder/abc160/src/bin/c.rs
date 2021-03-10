7c61a314856e328efe10838c080ebd9bf7684c02
horyu <28817604+horyu@users.noreply.github.com>
2021-03-11 08:39:26 +0900

    abc160 c

--- atcoder/abc160/src/bin/c.rs
+++ atcoder/abc160/src/bin/c.rs
@@ -5,6 +5,17 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        k: usize,
+        n: usize,
+        aa: [usize; n]
+    };
+    let max_len = aa
+        .iter()
+        .chain(std::iter::once(&(aa[0] + k)))
+        .tuple_windows()
+        .map(|(al, ar)| ar - al)
+        .max()
+        .unwrap();
+    println!("{}", k - max_len);
 }
