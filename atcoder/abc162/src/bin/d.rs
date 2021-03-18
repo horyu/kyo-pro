a3199f9c1fe7cde63b7618f258fed5447cdc3c3a
horyu <28817604+horyu@users.noreply.github.com>
2021-03-18 14:11:49 +0900

    abc162 d

--- atcoder/abc162/src/bin/d.rs
+++ atcoder/abc162/src/bin/d.rs
@@ -5,7 +5,6 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    panic!("漏れ有り");
     input! {
         n: usize,
         s: Chars
@@ -14,15 +13,7 @@ fn main() {
         println!("0");
         return;
     }
-    fn c_to_i(c: &char) -> usize {
-        match c {
-            'R' => 0usize,
-            'G' => 1usize,
-            'B' => 2usize,
-            _ => unreachable!(),
-        }
-    }
-    let mut counts = vec![0; 3];
+    let mut counts = [0usize; 3];
     for c in &s {
         counts[c_to_i(&c)] += 1;
     }
@@ -30,30 +21,29 @@ fn main() {
         println!("0");
         return;
     }
-    let mut rs = 0;
+    let mut rs = counts.iter().product::<usize>();
     for i in 0..(n - 2) {
         let si = s[i];
-        counts[c_to_i(&si)] -= 1;
-        let mut viewed = vec![0; 3];
         for j in (i + 1)..(n - 1) {
-            let sj = s[j];
-            viewed[c_to_i(&sj)] += 1;
-            if si == sj {
-                continue;
+            let k = 2 * j - i;
+            if k >= n {
+                break;
             }
-            let nokori_index = *[0usize, 1, 2]
-                .iter()
-                .find(|&&i| (i != c_to_i(&si)) && (i != c_to_i(&sj)))
-                .unwrap();
-            rs += counts[nokori_index] - viewed[nokori_index];
-            // j - i == k - j を飛ばす
-            // 2j - i == k
-            if let Some(&sk) = s.get(2 * j - i) {
-                if (sk != si) && (sk != sj) {
-                    rs -= 1;
-                }
+            let sj = s[j];
+            let sk = s[k];
+            if (si != sj) && (sk != si) && (sk != sj) {
+                rs -= 1;
             }
         }
     }
     println!("{}", rs);
+}
+
+fn c_to_i(c: &char) -> usize {
+    match c {
+        'R' => 0usize,
+        'G' => 1usize,
+        'B' => 2usize,
+        _ => unreachable!(),
+    }
 }
