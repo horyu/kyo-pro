a7dc1b3c6eb80711e5919209511adcce36837493
horyu <28817604+horyu@users.noreply.github.com>
2021-03-27 10:00:51 +0900

    abc166 c

--- atcoder/abc166/src/bin/c.rs
+++ atcoder/abc166/src/bin/c.rs
@@ -5,6 +5,27 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        hh: [usize; n],
+        aabb: [(Usize1, Usize1); m]
+    };
+    let mut v = vec![true; n];
+    for (a, b) in aabb {
+        use std::cmp::Ordering;
+        match hh[a].cmp(&hh[b]) {
+            Ordering::Equal => {
+                v[a] = false;
+                v[b] = false;
+            }
+            Ordering::Greater => {
+                v[b] = false;
+            }
+            Ordering::Less => {
+                v[a] = false;
+            }
+        }
+    }
+    println!("{}", v.iter().filter(|&&x| x).count());
 }
