980abd3b958782131f9b6386c098c8f2b2091a01
horyu <28817604+horyu@users.noreply.github.com>
2021-03-30 08:30:42 +0900

    abc168 c

--- atcoder/abc168/src/bin/c.rs
+++ atcoder/abc168/src/bin/c.rs
@@ -5,6 +5,17 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        a: f64,
+        b: f64,
+        h: f64,
+        m: f64,
+    };
+    use std::f64::consts::PI;
+    let a_theta = (30.0 * h + m / 2.0) * PI / 180.0;
+    let b_theta = (6.0 * m) * PI / 180.0;
+    println!(
+        "{}",
+        (a.powi(2) + b.powi(2) - 2.0 * a * b * (b_theta - a_theta).cos()).sqrt()
+    );
 }
