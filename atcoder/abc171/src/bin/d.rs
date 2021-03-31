49dfddf4a0708914ab35d92df56e0890bf34dd7a
horyu <28817604+horyu@users.noreply.github.com>
2021-04-01 08:41:43 +0900

    abc171 d

--- atcoder/abc171/src/bin/d.rs
+++ atcoder/abc171/src/bin/d.rs
@@ -3,6 +3,24 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut aa: [usize; n],
+        q: usize,
+        bcbc: [(usize, usize); q],
+    };
+    use std::collections::HashMap;
+    let mut hm = HashMap::new();
+    let mut sum = 0;
+    for a in aa {
+        *hm.entry(a).or_insert(0) += 1;
+        sum += a;
+    }
+    for (b, c) in bcbc {
+        if let Some(num) = hm.remove(&b) {
+            *hm.entry(c).or_insert(0) += num;
+            sum = sum + num * c - num * b
+        };
+        println!("{}", sum);
+    }
 }
