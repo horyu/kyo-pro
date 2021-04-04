e1baab1e95dff2b63eb99c939e8483f222ed8362
horyu <28817604+horyu@users.noreply.github.com>
2021-04-04 12:06:48 +0900

    abc173 d

--- atcoder/abc173/src/bin/d.rs
+++ atcoder/abc173/src/bin/d.rs
@@ -1,8 +1,14 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::BTreeMap;
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut aa: [usize; n]
+    };
+    aa.sort_unstable();
+    let rs = (1..n).fold(0usize, |acc, k| acc + aa[n - 1 - k / 2]);
+    println!("{}", rs)
 }
