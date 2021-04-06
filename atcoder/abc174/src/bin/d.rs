ad9dfe5e20e2c9703fec71c0f6f1772626a3360f
horyu <28817604+horyu@users.noreply.github.com>
2021-04-06 09:01:06 +0900

    abc174 d

--- atcoder/abc174/src/bin/d.rs
+++ atcoder/abc174/src/bin/d.rs
@@ -5,6 +5,25 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        cc: Chars
+    };
+    let mut rs = 0;
+    let mut l = 0;
+    let mut r = n - 1;
+    while l < r {
+        if cc[l] == 'W' {
+            while (l < r) && (cc[r] == 'W') {
+                r -= 1;
+            }
+            if l == r {
+                break;
+            }
+            r -= 1;
+            rs += 1;
+        }
+        l += 1;
+    }
+    println!("{}", rs);
 }
