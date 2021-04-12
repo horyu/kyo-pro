efa2579b78965704a2d20f3b566950996c19ceca
horyu <28817604+horyu@users.noreply.github.com>
2021-04-13 08:40:37 +0900

    abc182 c

--- atcoder/abc182/src/bin/c.rs
+++ atcoder/abc182/src/bin/c.rs
@@ -5,6 +5,35 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: Chars
+    };
+    let n = n.iter().map(|&c| c.to_digit(10).unwrap() % 3).collect_vec();
+    let ones = n.iter().filter(|&&i| i == 1).count();
+    let twos = n.iter().filter(|&&i| i == 2).count();
+    let k = n.len();
+    let sum = n.iter().sum::<u32>();
+    let rs = match sum % 3 {
+        0 => 0,
+        1 => {
+            if (ones >= 1) && (k > 1) {
+                1
+            } else if (twos >= 2) && (k > 2) {
+                2
+            } else {
+                -1
+            }
+        }
+        2 => {
+            if (twos >= 1) && (k > 1) {
+                1
+            } else if (ones >= 2) && (k > 2) {
+                2
+            } else {
+                -1
+            }
+        }
+        _ => unreachable!(),
+    };
+    println!("{}", rs);
 }
