854c4c60c61503c82cf71a790464b316c9eef0f0
horyu <28817604+horyu@users.noreply.github.com>
2021-04-14 08:53:59 +0900

    abc182 d

--- atcoder/abc182/src/bin/d.rs
+++ atcoder/abc182/src/bin/d.rs
@@ -5,6 +5,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [isize; n]
+    };
+    let mut max = 0isize;
+    let mut max_add = 0;
+    let mut current_add = 0;
+    let mut last_pos = 0;
+    for a in aa {
+        current_add += a;
+        max_add = max_add.max(current_add);
+        max = max.max(last_pos + max_add);
+        last_pos += current_add;
+    }
+    println!("{}", max);
 }
