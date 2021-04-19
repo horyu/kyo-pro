b686bc8936be27ca4e1bbb64e0751ab0bb320d6b
horyu <28817604+horyu@users.noreply.github.com>
2021-04-20 08:47:04 +0900

    abc187 d

--- atcoder/abc187/src/bin/d.rs
+++ atcoder/abc187/src/bin/d.rs
@@ -5,6 +5,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aabb: [(usize, usize); n],
+    };
+    aabb.sort_unstable_by_key(|(a, b)| 2 * a + b);
+    let mut aa = aabb.iter().fold(0, |acc, (a, _b)| acc + a);
+    let mut bb = 0;
+    for (i, (a, b)) in aabb.into_iter().rev().enumerate() {
+        aa -= a;
+        bb += a + b;
+        if aa < bb {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
