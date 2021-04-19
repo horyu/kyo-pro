5ecd934a4a27858572fd56440c6c9ddf662e8bf9
horyu <28817604+horyu@users.noreply.github.com>
2021-04-19 09:25:42 +0900

    abc187 c

--- atcoder/abc187/src/bin/c.rs
+++ atcoder/abc187/src/bin/c.rs
@@ -5,6 +5,24 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ss: [String; n]
+    };
+    let mut hs1 = HashSet::new();
+    let mut hs2 = HashSet::new();
+    for s in ss {
+        if s.starts_with('!') {
+            hs1.insert(s);
+        } else {
+            hs2.insert(s);
+        }
+    }
+    for s in hs2 {
+        if hs1.contains(&format!("!{}", s)) {
+            println!("{}", s);
+            return;
+        }
+    }
+    println!("satisfiable");
 }
