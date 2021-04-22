c3c427952943cfd21bd6e68c3804ea768efa0cd2
horyu <28817604+horyu@users.noreply.github.com>
2021-04-23 08:25:25 +0900

    arc001 b

--- atcoder/arc001/src/bin/b.rs
+++ atcoder/arc001/src/bin/b.rs
@@ -5,6 +5,18 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        a: isize,
+        b: isize,
+    };
+    let mut diff = (a - b).abs();
+    let mut rs = diff / 10;
+    diff %= 10;
+    rs += match diff {
+        1 | 5 => 1,
+        2 | 4 | 6 | 9 => 2,
+        3 | 7 | 8 => 3,
+        _ => 0,
+    };
+    println!("{}", rs);
 }
