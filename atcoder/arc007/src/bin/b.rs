1bc5e3ce03dac8a7559141126e53e440c2affe39
horyu <28817604+horyu@users.noreply.github.com>
2021-05-04 10:36:10 +0900

    arc007 b

--- atcoder/arc007/src/bin/b.rs
+++ atcoder/arc007/src/bin/b.rs
@@ -5,6 +5,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+    };
+    if m == 0 {
+        println!("{}", (1..=n).join("\n"));
+        return;
+    }
+    input! {
+        dd: [usize; m]
+    }
+    let mut vv = (0usize..=n).collect_vec();
+    for d in dd {
+        let pos = vv.iter().position(|&v| v == d).unwrap();
+        vv.swap(0, pos);
+    }
+    println!("{}", vv[1..].iter().join("\n"));
 }
