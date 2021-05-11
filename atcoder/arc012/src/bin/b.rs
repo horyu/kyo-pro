942a5dbc7d7977fd3d09f0ee81dca94f31371a5e
horyu <28817604+horyu@users.noreply.github.com>
2021-05-12 08:16:23 +0900

    arc012 b

--- atcoder/arc012/src/bin/b.rs
+++ atcoder/arc012/src/bin/b.rs
@@ -5,6 +5,15 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        va: f64,
+        vb: f64,
+        mut l: f64,
+    };
+    for _ in 0..n {
+        let s = l / va;
+        l = vb * s;
+    }
+    println!("{}", l);
 }
