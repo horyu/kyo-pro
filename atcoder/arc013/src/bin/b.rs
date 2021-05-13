1d741647f92d8cfb217b781b12a60114082e1f0f
horyu <28817604+horyu@users.noreply.github.com>
2021-05-14 08:44:22 +0900

    arc013 b

--- atcoder/arc013/src/bin/b.rs
+++ atcoder/arc013/src/bin/b.rs
@@ -5,6 +5,16 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        c: usize,
+        mut nnmmll: [[usize; 3]; c]
+    };
+    let mut arr = [0usize; 3];
+    for mut nml in nnmmll {
+        nml.sort_unstable();
+        for i in 0usize..3 {
+            arr[i] = arr[i].max(nml[i]);
+        }
+    }
+    println!("{}", arr.iter().product::<usize>());
 }
