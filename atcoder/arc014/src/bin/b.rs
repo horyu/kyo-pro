9d15c535374a1c1a4fe74b327d9ed3add9082662
horyu <28817604+horyu@users.noreply.github.com>
2021-05-17 08:45:22 +0900

    arc014 b

--- atcoder/arc014/src/bin/b.rs
+++ atcoder/arc014/src/bin/b.rs
@@ -5,6 +5,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ww: [String; n],
+    };
+    if n > 1 {
+        let mut pre = &ww[0];
+        let mut hs = HashSet::new();
+        for (i, w) in ww.iter().skip(1).enumerate() {
+            hs.insert(pre);
+            if hs.contains(w) || !w.starts_with(pre.chars().last().unwrap()) {
+                println!("{}", ["LOSE", "WIN"][(i % 2 == 0) as usize]);
+                return;
+            }
+            pre = w;
+        }
+    }
+    println!("DRAW");
 }
