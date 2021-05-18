c33167c471ae4baba9368207478c2f1b2d2ad10b
horyu <28817604+horyu@users.noreply.github.com>
2021-05-19 08:19:49 +0900

    arc015 b

--- atcoder/arc015/src/bin/b.rs
+++ atcoder/arc015/src/bin/b.rs
@@ -5,6 +5,27 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ttt: [(f64, f64); n]
+    };
+    let mut arr = [0; 6];
+    for (t1, t2) in ttt {
+        if t1 >= 35.0 {
+            arr[0] += 1;
+        } else if t1 >= 30.0 {
+            arr[1] += 1;
+        } else if t1 >= 25.0 {
+            arr[2] += 1;
+        } else if t1 < 0.0 {
+            arr[5] += 1;
+        }
+        if t2 >= 25.0 {
+            arr[3] += 1;
+        }
+        if (t1 >= 0.0) && (t2 < 0.0) {
+            arr[4] += 1;
+        }
+    }
+    println!("{}", arr.iter().map(|count| count.to_string()).join(" "));
 }
