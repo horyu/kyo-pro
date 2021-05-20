b8baf9ab554e38255b0897827c22a7b02977a64d
horyu <28817604+horyu@users.noreply.github.com>
2021-05-21 08:17:38 +0900

    arc016 b

--- atcoder/arc016/src/bin/b.rs
+++ atcoder/arc016/src/bin/b.rs
@@ -5,6 +5,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut xxx: [Chars; n]
+    };
+    xxx.insert(0, vec!['.'; 9]);
+    let mut count = 0;
+    for (mae, ato) in xxx.iter().tuple_windows() {
+        for (m, a) in mae.iter().zip(ato.iter()) {
+            count += match (m, a) {
+                (_, 'x') => 1,
+                ('o', _) => 0,
+                (_, 'o') => 1,
+                _ => 0,
+            };
+        }
+    }
+    println!("{}", count);
 }
