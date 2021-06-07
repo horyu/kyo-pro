01d6ecf5d53e4a30f5c309ec81d6edab39186853
horyu <28817604+horyu@users.noreply.github.com>
2021-06-08 08:17:43 +0900

    /arc030 a

---
+++ atcoder/arc030/src/bin/a.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+    };
+    println!("{}", ["NO", "YES"][(n.div_floor(&2) >= k) as usize]);
+}
