7db877d8690198a586cdc8072ff00f2c117bd786
horyu <28817604+horyu@users.noreply.github.com>
2021-06-17 08:19:23 +0900

    /arc037 a

---
+++ atcoder/arc037/src/bin/a.rs
@@ -0,0 +1,15 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        n: usize,
+        mm: [usize; n]
+    };
+    let rs = mm.into_iter().fold(0, |acc, m| acc + 80usize.saturating_sub(m));
+    println!("{}", rs);
+}
