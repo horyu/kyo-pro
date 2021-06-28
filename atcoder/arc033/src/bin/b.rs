d2fc9ea1ca9be74a87188a31287f8eb4c3359a8f
horyu <28817604+horyu@users.noreply.github.com>
2021-06-29 08:34:49 +0900

    arc033 b

--- atcoder/arc033/src/bin/b.rs
+++ atcoder/arc033/src/bin/b.rs
@@ -6,6 +6,14 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        na: usize,
+        nb: usize,
+        aa: [usize; na],
+        bb: [usize; nb],
+    };
+    let sa: HashSet<_> = aa.iter().collect();
+    let sb: HashSet<_> = bb.iter().collect();
+    let rs = sa.intersection(&sb).count() as f64 / sa.union(&sb).count() as f64;
+    println!("{}", rs);
 }
