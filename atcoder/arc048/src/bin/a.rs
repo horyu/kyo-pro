ae781fc5f217fc89370e5e32bc06196f3db441bd
horyu <28817604+horyu@users.noreply.github.com>
2021-06-30 08:17:39 +0900

    /arc048 a

---
+++ atcoder/arc048/src/bin/a.rs
@@ -0,0 +1,17 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        mut a: isize,
+        b: isize,
+    };
+    if (a * b).is_negative() {
+        a += 1;
+    }
+    println!("{}", b - a);
+}
