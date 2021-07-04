7f1b75ec64ec4f05a11211f06ba41dbae806dc15
horyu <28817604+horyu@users.noreply.github.com>
2021-07-04 10:22:42 +0900

    /arc052 a

---
+++ atcoder/arc052/src/bin/a.rs
@@ -0,0 +1,15 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        mut s: Chars
+    };
+    s.retain(|c| c.is_numeric());
+    let rs = s.iter().collect::<String>();
+    println!("{}", rs);
+}
