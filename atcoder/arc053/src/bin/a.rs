1a79bd77ff9d9a52485c9ad8df3060577df46bfd
horyu <28817604+horyu@users.noreply.github.com>
2021-07-06 08:16:49 +0900

    /arc053 a

---
+++ atcoder/arc053/src/bin/a.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+    };
+    println!("{}", w.saturating_sub(1) * h + h.saturating_sub(1) * w);
+}
