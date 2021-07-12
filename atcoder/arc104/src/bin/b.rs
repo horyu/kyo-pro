f8b9aaf60f28b3b79543c8ca01454437cab03259
horyu <28817604+horyu@users.noreply.github.com>
2021-07-13 08:49:34 +0900

    arc104 b

--- atcoder/arc104/src/bin/b.rs
+++ atcoder/arc104/src/bin/b.rs
@@ -6,6 +6,30 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        s: Chars
+    };
+    if n == 1 {
+        println!("0");
+        return;
+    }
+    let mut rs = 0;
+    for i in 0..(n - 1) {
+        let mut at = 0;
+        let mut cg = 0;
+        for j in i..n {
+            match s[j] {
+                'A' => at += 1,
+                'T' => at -= 1,
+                'C' => cg += 1,
+                'G' => cg -= 1,
+                _ => unreachable!(),
+            }
+            if (at == 0) && (cg == 0) {
+                rs += 1;
+            }
+        }
+    }
+    println!("{}", rs);
 }
