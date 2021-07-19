0cd0c19235237261ac058ccfae1186c4198d515c
horyu <28817604+horyu@users.noreply.github.com>
2021-07-19 13:58:12 +0900

    arc107 b

--- atcoder/arc107/src/bin/b.rs
+++ atcoder/arc107/src/bin/b.rs
@@ -6,6 +6,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: isize,
+        k: isize,
+    };
+    let mut rs = 0isize;
+    let ab_range = if k <= 0 {
+        2..=(2 * n + k)
+    } else {
+        (2 + k)..=(2 * n)
+    };
+    for ab in ab_range {
+        let cd = ab - k;
+        let ab_comb = if ab <= n + 1 { ab - 1 } else { 2 * n + 1 - ab };
+        let cd_comb = if cd <= n + 1 { cd - 1 } else { 2 * n + 1 - cd };
+        rs += ab_comb * cd_comb;
+    }
+    println!("{}", rs);
 }
