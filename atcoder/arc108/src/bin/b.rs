811c8ab676f17ba8550f6aa21ee016fa3a36b951
horyu <28817604+horyu@users.noreply.github.com>
2021-07-21 08:43:14 +0900

    arc108 b

--- atcoder/arc108/src/bin/b.rs
+++ atcoder/arc108/src/bin/b.rs
@@ -3,9 +3,41 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        s: Chars
+    };
+    let mut count = 0;
+    let mut v = VecDeque::new();
+    for c in s {
+        match c {
+            'f' => {
+                v.push_back(c);
+            }
+            'o' => {
+                if Some(&'f') == v.back() {
+                    v.push_back(c);
+                } else {
+                    v.clear();
+                }
+            }
+            'x' => {
+                let len = v.len();
+                if len >= 2 && v[len - 2] == 'f' && v[len - 1] == 'o' {
+                    count += 1;
+                    v.pop_back();
+                    v.pop_back();
+                } else {
+                    v.clear();
+                }
+            }
+            _ => {
+                v.clear();
+            }
+        }
+    }
+    println!("{}", n - 3 * count);
 }
