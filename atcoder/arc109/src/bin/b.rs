878da4eb5caaee49a88c5198ddeaaec9df971a15
horyu <28817604+horyu@users.noreply.github.com>
2021-07-23 09:45:29 +0900

    arc109 b

--- atcoder/arc109/src/bin/b.rs
+++ atcoder/arc109/src/bin/b.rs
@@ -3,9 +3,23 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Div,
+};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+    };
+    if n == 1 {
+        println!("{}", n);
+        return;
+    }
+    for i in ((1 + 8 * n).sqrt() - 1).div(2)..=n {
+        if (i + 1) * (i + 2) / 2 > n + 1 {
+            println!("{}", n + 1 - i);
+            return;
+        }
+    }
 }
