342060ae17e2949d661542ad95c203bf5147a028
horyu <28817604+horyu@users.noreply.github.com>
2021-08-04 22:57:58 +0900

    arc118 a

---
+++ atcoder/arc118/src/bin/a.rs
@@ -0,0 +1,15 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        t: usize,
+        n: usize
+    };
+    let v = (n * 100).div_ceil(&t);
+    println!("{}", (100 + t) * v / 100 - 1)
+}
