32515e36da9117bab3d40dfdcd2e2934a438b711
horyu <28817604+horyu@users.noreply.github.com>
2021-08-07 19:24:34 +0900

    arc122 b

--- atcoder/arc122/src/bin/b.rs
+++ atcoder/arc122/src/bin/b.rs
@@ -6,6 +6,17 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aa: [isize; n]
+    };
+    aa.sort_unstable();
+    let aa = aa.into_iter().map(|a| a as f64).collect_vec();
+    let x = if n.is_odd() {
+        aa[n / 2] / 2.0
+    } else {
+        (aa[(n - 1) / 2] + aa[n / 2]) / 4.0
+    };
+    let rs = aa.iter().map(|&a| x + a - a.min(x * 2.0)).sum::<f64>() / n as f64;
+    println!("{}", rs);
 }
