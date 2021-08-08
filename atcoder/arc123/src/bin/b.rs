62f66a7ddedbdc6b09783993981d95ae43066a57
horyu <28817604+horyu@users.noreply.github.com>
2021-08-09 08:42:41 +0900

    arc123 b

--- atcoder/arc123/src/bin/b.rs
+++ atcoder/arc123/src/bin/b.rs
@@ -3,9 +3,35 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aa: [usize; n],
+        mut bb: [usize; n],
+        mut cc: [usize; n],
+    };
+    aa.sort_unstable();
+    bb.sort_unstable();
+    cc.sort_unstable();
+    let mut aa: VecDeque<_> = aa.iter().collect();
+    let mut bb: VecDeque<_> = bb.iter().collect();
+    let mut cc: VecDeque<_> = cc.iter().collect();
+    let mut cnt = 0;
+    'a: while let Some(a) = aa.pop_front() {
+        while let Some(b) = bb.pop_front() {
+            if a < b {
+                while let Some(c) = cc.pop_front() {
+                    if b < c {
+                        cnt += 1;
+                        continue 'a;
+                    }
+                }
+                break 'a;
+            }
+        }
+        break;
+    }
+    println!("{}", cnt);
 }
