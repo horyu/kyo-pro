66c854b283afcc348475bce85df047e9e9480991
horyu <28817604+horyu@users.noreply.github.com>
2021-08-14 08:30:35 +0900

    abc206 c

--- atcoder/abc206/src/bin/c.rs
+++ atcoder/abc206/src/bin/c.rs
@@ -6,6 +6,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut hm = HashMap::new();
+    for a in aa {
+        *hm.entry(a).or_insert(0usize) += 1;
+    }
+    let rs = n * (n - 1) / 2
+        - hm.values()
+            .into_iter()
+            .filter(|cnt| **cnt > 1)
+            .map(|cnt| cnt * (cnt - 1) / 2)
+            .sum::<usize>();
+    println!("{}", rs);
 }
