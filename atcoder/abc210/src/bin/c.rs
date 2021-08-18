5da14945fb0d571a3828d0039e4837ee1bf03d5b
horyu <28817604+horyu@users.noreply.github.com>
2021-08-19 08:18:07 +0900

    abc210 c

--- atcoder/abc210/src/bin/c.rs
+++ atcoder/abc210/src/bin/c.rs
@@ -6,6 +6,39 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        cc: [usize; n],
+    };
+    let mut hm = HashMap::new();
+    let mut cnt = 0;
+    for i in 0..k {
+        if let Some(v) = hm.get_mut(&cc[i]) {
+            *v += 1;
+        } else {
+            cnt += 1;
+            hm.insert(cc[i], 1);
+        }
+    }
+    let mut rs = cnt;
+    for i in k..n {
+        if let Some(v) = hm.get_mut(&cc[i - k]) {
+            *v -= 1;
+            if *v == 0 {
+                cnt -= 1;
+            }
+        }
+        if let Some(v) = hm.get_mut(&cc[i]) {
+            *v += 1;
+            if *v == 1 {
+                cnt += 1;
+            }
+        } else {
+            cnt += 1;
+            hm.insert(cc[i], 1);
+        }
+        rs = rs.max(cnt);
+    }
+    println!("{}", rs);
 }
