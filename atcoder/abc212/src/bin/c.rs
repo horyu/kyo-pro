519477c1571881ed48dffec7e2183f55cf9d1385
horyu <28817604+horyu@users.noreply.github.com>
2021-08-21 19:39:42 +0900

    abc212 c

--- atcoder/abc212/src/bin/c.rs
+++ atcoder/abc212/src/bin/c.rs
@@ -6,6 +6,24 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        mut aa: [isize; n],
+        mut bb: [isize; m],
+    };
+    aa.sort_unstable();
+    bb.sort_unstable();
+    let mut rs = std::isize::MAX;
+    let mut i = 0;
+    let mut j = 0;
+    while (i < n) && (j < m) {
+        rs = rs.min((aa[i] - bb[j]).abs());
+        if aa[i] > bb[j] {
+            j += 1;
+        } else {
+            i += 1;
+        }
+    }
+    println!("{}", rs);
 }
