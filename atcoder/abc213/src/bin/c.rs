e5796994028a7572152445c4e407f51fde542b07
horyu <28817604+horyu@users.noreply.github.com>
2021-08-24 09:29:56 +0900

    abc213 c

--- atcoder/abc213/src/bin/c.rs
+++ atcoder/abc213/src/bin/c.rs
@@ -6,6 +6,31 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _h: usize,
+        _w: usize,
+        n: usize,
+    };
+    let mut aabb = vec![];
+    let mut hh = HashSet::new();
+    let mut ww = HashSet::new();
+    for _ in 0..n {
+        input! {a: Usize1, b: Usize1};
+        aabb.push((a, b));
+        hh.insert(a);
+        ww.insert(b);
+    }
+    let hh = to_hm(hh);
+    let ww = to_hm(ww);
+    for (a, b) in aabb {
+        println!("{} {}", hh.get(&a).unwrap(), ww.get(&b).unwrap());
+    }
+}
+
+fn to_hm(hs: HashSet<usize>) -> HashMap<usize, usize> {
+    let mut hm = HashMap::new();
+    for (i, v) in hs.into_iter().sorted().enumerate() {
+        hm.insert(v, i + 1);
+    }
+    hm
 }
