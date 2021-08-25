1ae7186911909e81ea40fa5c1695e97dc5785961
horyu <28817604+horyu@users.noreply.github.com>
2021-08-25 10:40:49 +0900

    abc213 d

--- atcoder/abc213/src/bin/d.rs
+++ atcoder/abc213/src/bin/d.rs
@@ -3,9 +3,35 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{BTreeSet, HashMap, HashSet, VecDeque},
+    vec,
+};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aabb: [(Usize1, Usize1); n - 1]
+    };
+    let mut g = vec![vec![]; n];
+    for (a, b) in aabb {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    for v in g.iter_mut() {
+        v.sort_unstable();
+    }
+    let mut ans = vec![];
+    dfs(&mut ans, &g, 0, std::usize::MAX);
+    println!("{}", ans.into_iter().map(|i| (i + 1).to_string()).join(" "));
+}
+
+fn dfs(ans: &mut Vec<usize>, g: &Vec<Vec<usize>>, crr: usize, pre: usize) {
+    ans.push(crr);
+    for &nxt in &g[crr] {
+        if nxt != pre {
+            dfs(ans, g, nxt, crr);
+            ans.push(crr);
+        }
+    }
 }
