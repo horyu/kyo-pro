014884c588d5a7196e546e9f42902feeb5084dc4
horyu <28817604+horyu@users.noreply.github.com>
2021-08-27 08:46:44 +0900

    abc214 c

--- atcoder/abc214/src/bin/c.rs
+++ atcoder/abc214/src/bin/c.rs
@@ -6,6 +6,14 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ss: [usize; n],
+        mut tt: [usize; n],
+    };
+    for i in (0..n).chain(0..n) {
+        let next = (i + 1) % n;
+        tt[next] = tt[next].min(ss[i] + tt[i]);
+    }
+    println!("{}", tt.iter().join("\n"));
 }
