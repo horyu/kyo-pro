fbeb59ea854b446e75e10d07b50bfc6ef3b0cea3
horyu <28817604+horyu@users.noreply.github.com>
2021-09-01 08:36:03 +0900

    abc216 c

--- atcoder/abc216/src/bin/c.rs
+++ atcoder/abc216/src/bin/c.rs
@@ -6,6 +6,18 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        mut n: usize,
+    };
+    let mut rs = Vec::new();
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+            rs.push('B');
+        } else {
+            n -= 1;
+            rs.push('A');
+        }
+    }
+    println!("A{}", rs.into_iter().rev().collect::<String>());
 }
