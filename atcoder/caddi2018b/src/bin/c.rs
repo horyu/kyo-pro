e347c3483ee30b561990c38d533528d74b7c4db5
horyu <28817604+horyu@users.noreply.github.com>
2021-09-08 09:53:41 +0900

    caddi2018b c

--- atcoder/caddi2018b/src/bin/c.rs
+++ atcoder/caddi2018b/src/bin/c.rs
@@ -6,6 +6,38 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        p: usize,
+    };
+    let mut rs = 1;
+    for (k, v) in factors(p) {
+        if v >= n {
+            rs *= k.pow((v / n) as u32);
+        }
+    }
+    println!("{}", rs);
+}
+
+fn factors(mut n: usize) -> HashMap<usize, usize> {
+    let mut hm = HashMap::new();
+    if n <= 1 {
+        return hm;
+    }
+    while n % 2 == 0 {
+        n /= 2;
+        *hm.entry(2).or_insert(0) += 1;
+    }
+    let mut i = 3;
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+            *hm.entry(i).or_insert(0) += 1;
+        }
+        i += 2;
+    }
+    if n != 1 {
+        hm.insert(n, 1);
+    }
+    hm
 }
