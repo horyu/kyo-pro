91ff576be49a9614f4c996bad8327f9efac65881
horyu <28817604+horyu@users.noreply.github.com>
2021-09-11 09:02:00 +0900

    sumitrust2019 c

--- atcoder/sumitrust2019/src/bin/c.rs
+++ atcoder/sumitrust2019/src/bin/c.rs
@@ -6,6 +6,18 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        x: usize
+    };
+    let mut l = 100;
+    let mut r = 105;
+    while x >= l {
+        if (l..=r).contains(&x) {
+            println!("1");
+            return;
+        }
+        l += 100;
+        r += 105;
+    }
+    println!("0");
 }
