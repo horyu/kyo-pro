d8c68962ee80a73adf9e0d75fd26e90187fd214e
horyu <28817604+horyu@users.noreply.github.com>
2021-09-17 08:50:46 +0900

    m-solutions2020 d

--- atcoder/m-solutions2020/src/bin/d.rs
+++ atcoder/m-solutions2020/src/bin/d.rs
@@ -6,6 +6,32 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut cache = 1000usize;
+    let mut stock = 0usize;
+    for (&x, &y) in aa.iter().tuple_windows() {
+        use std::cmp::Ordering;
+        match x.cmp(&y) {
+            Ordering::Less => {
+                if cache >= x {
+                    stock += cache / x;
+                    cache %= x;
+                }
+            }
+            Ordering::Greater => {
+                if stock > 0 {
+                    cache += x * stock;
+                    stock = 0;
+                }
+            }
+            _ => (),
+        }
+    }
+    if stock != 0 {
+        cache += aa.into_iter().last().unwrap() * stock;
+    }
+    println!("{}", cache);
 }
