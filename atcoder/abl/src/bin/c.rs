6fc5e515c696d26d2846886a4e2f66bffff400f2
horyu <28817604+horyu@users.noreply.github.com>
2021-09-19 11:21:26 +0900

    abl c

--- atcoder/abl/src/bin/c.rs
+++ atcoder/abl/src/bin/c.rs
@@ -2,10 +2,22 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use num_integer::*;
+use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(Usize1, Usize1); m]
+    };
+    let mut uf = UnionFind::new(n);
+    for (a, b) in aabb {
+        uf.union(a, b);
+    }
+    let mut labels = uf.into_labeling();
+    labels.sort_unstable();
+    labels.dedup();
+    println!("{}", labels.len() - 1);
 }
