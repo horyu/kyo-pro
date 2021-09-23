0a8d4d1bb1c47ad18e5cb4081107d9ec608f479e
horyu <28817604+horyu@users.noreply.github.com>
2021-09-23 10:36:15 +0900

    jsc2021 c

--- atcoder/jsc2021/src/bin/c.rs
+++ atcoder/jsc2021/src/bin/c.rs
@@ -6,6 +6,17 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        a: usize,
+        b: usize
+    };
+    let mut rs = 1;
+    for i in 2..=(b - a) {
+        let l = i * a.div_ceil(&i);
+        let r = i * (a.div_ceil(&i) + 1);
+        if (a..=b).contains(&l) && (a..=b).contains(&r) {
+            rs = i;
+        }
+    }
+    println!("{}", rs);
 }
