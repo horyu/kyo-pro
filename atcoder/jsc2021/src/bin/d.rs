b8132ddd631bd5a5c36ea8b7910cd6739ec2b9fe
horyu <28817604+horyu@users.noreply.github.com>
2021-09-24 10:57:05 +0900

    jsc2021 d

--- atcoder/jsc2021/src/bin/d.rs
+++ atcoder/jsc2021/src/bin/d.rs
@@ -6,6 +6,24 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        p: usize,
+    };
+    const MOD: usize = 1_000_000_007;
+    let rs = (p - 1) * mod_pow(p - 2, n - 1, MOD) % MOD;
+    println!("{}", rs);
+}
+
+// https://blog.spiralray.net/cp/modulo#i-8
+fn mod_pow(mut x: usize, mut n: usize, m: usize) -> usize {
+    let mut ans = 1;
+    while n != 0 {
+        if n.is_odd() {
+            ans = ans * x % m;
+        }
+        x = x * x % m;
+        n >>= 1;
+    }
+    ans
 }
