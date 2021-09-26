bb9912773e782db4386e4eb8aa7b06c80dc628ea
horyu <28817604+horyu@users.noreply.github.com>
2021-09-27 08:35:56 +0900

    keyence2019 b

--- atcoder/keyence2019/src/bin/b.rs
+++ atcoder/keyence2019/src/bin/b.rs
@@ -6,6 +6,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        s: String
+    };
+    let mut tf = s.starts_with("keyence") || s.ends_with("keyence");
+    if !tf {
+        tf = [
+            ("k", "eyence"),
+            ("ke", "yence"),
+            ("key", "ence"),
+            ("keye", "nce"),
+            ("keyen", "ce"),
+            ("keyenc", "e"),
+        ]
+        .iter()
+        .any(|(l, r)| s.starts_with(l) && s.ends_with(r));
+    }
+    println!("{}", ["NO", "YES"][tf as usize]);
 }
