f7e2af4f811ae1141f0d04d710ca9ad00197b899
horyu <28817604+horyu@users.noreply.github.com>
2021-09-29 08:51:07 +0900

    keyence2019 c

--- atcoder/keyence2019/src/bin/c.rs
+++ atcoder/keyence2019/src/bin/c.rs
@@ -6,6 +6,37 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [isize; n],
+        bb: [isize; n]
+    };
+    let mut pp = vec![];
+    let mut nn = vec![];
+    for (a, b) in aa.into_iter().zip(bb.into_iter()) {
+        let d = a - b;
+        use std::cmp::Ordering;
+        match d.cmp(&0) {
+            Ordering::Greater => pp.push(d),
+            Ordering::Less => nn.push(d),
+            _ => (),
+        }
+    }
+    let n_sum = nn.iter().sum::<isize>();
+    if n_sum == 0 {
+        println!("0");
+        return;
+    }
+    pp.sort_unstable();
+    let mut p_sum = 0;
+    let mut p_cnt = 0;
+    while let Some(p) = pp.pop() {
+        p_sum += p;
+        p_cnt += 1;
+        if p_sum + n_sum >= 0 {
+            println!("{}", p_cnt + nn.len());
+            return;
+        }
+    }
+    println!("-1");
 }
