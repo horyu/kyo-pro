1c8e65b382ac2d4a5e60e8515faebed7c421ca69
horyu <28817604+horyu@users.noreply.github.com>
2021-10-01 08:19:55 +0900

    nikkei2019-qual b

--- atcoder/nikkei2019-qual/src/bin/b.rs
+++ atcoder/nikkei2019-qual/src/bin/b.rs
@@ -6,6 +6,25 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        a: Chars,
+        b: Chars,
+        c: Chars,
+    };
+    let mut cnt = 0;
+    for ((a, b), c) in a.into_iter().zip(b.into_iter()).zip(c.into_iter()) {
+        let mut sames = 0;
+        sames += (a == b) as i32;
+        sames += (b == c) as i32;
+        sames += (c == a) as i32;
+        cnt += if sames >= 2 {
+            0
+        } else if sames == 1 {
+            1
+        } else {
+            2
+        };
+    }
+    println!("{}", cnt);
 }
