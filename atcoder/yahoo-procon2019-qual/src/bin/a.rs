9f40b2b6a8165993c05a942e15af0cc742a623e8
horyu <28817604+horyu@users.noreply.github.com>
2021-10-02 10:24:14 +0900

    yahoo-procon2019-qual a

---
+++ atcoder/yahoo-procon2019-qual/src/bin/a.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::many_single_char_names)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize
+    };
+    println!("{}", ["NO", "YES"][(n >= (2 * k - 1)) as usize]);
+}
