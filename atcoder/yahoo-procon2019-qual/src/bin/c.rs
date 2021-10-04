78b594d5eb7164c462d6114db5bde10f333811c5
horyu <28817604+horyu@users.noreply.github.com>
2021-10-05 08:54:42 +0900

    yahoo-procon2019-qual c

--- atcoder/yahoo-procon2019-qual/src/bin/c.rs
+++ atcoder/yahoo-procon2019-qual/src/bin/c.rs
@@ -6,6 +6,18 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        mut k: usize,
+        a: usize,
+        b: usize,
+    };
+    // 叩き続けた場合
+    let mut max = k + 1;
+    // 交換できるだけ交換して1回余ったら叩く
+    if (a < b) && (a < k) {
+        k -= a + 1;
+        let n = b + (b - a) * (k / 2) + k % 2;
+        max = max.max(n);
+    }
+    println!("{}", max);
 }
