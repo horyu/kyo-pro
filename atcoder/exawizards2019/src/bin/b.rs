230fc3ebd0a765ca29d35b078b8e97ea615eef57
horyu <28817604+horyu@users.noreply.github.com>
2021-10-07 09:00:51 +0900

    exawizards2019 b

--- atcoder/exawizards2019/src/bin/b.rs
+++ atcoder/exawizards2019/src/bin/b.rs
@@ -6,6 +6,13 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        s: Chars
+    };
+    let tf = s
+        .into_iter()
+        .fold(0i32, |acc, c| acc + if c == 'R' { 1 } else { -1 })
+        > 0;
+    println!("{}", ["No", "Yes"][tf as usize]);
 }
