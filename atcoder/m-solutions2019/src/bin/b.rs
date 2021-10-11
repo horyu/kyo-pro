8a38028403ca3210604c2c437ca4583b8446e4c8
horyu <28817604+horyu@users.noreply.github.com>
2021-10-12 08:19:25 +0900

    m-solutions2019 b

--- atcoder/m-solutions2019/src/bin/b.rs
+++ atcoder/m-solutions2019/src/bin/b.rs
@@ -6,6 +6,11 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        s: Chars
+    };
+    let k = s.len();
+    let win = s.into_iter().filter(|&c| c == 'o').count();
+    let tf = 15 - k + win >= 8;
+    println!("{}", ["NO", "YES"][tf as usize]);
 }
