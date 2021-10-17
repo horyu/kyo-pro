7e8986fa04f146b8302bf62355e5eff7b91d7cbf
horyu <28817604+horyu@users.noreply.github.com>
2021-10-17 11:04:04 +0900

    ddcc2020-qual b

--- atcoder/ddcc2020-qual/src/bin/b.rs
+++ atcoder/ddcc2020-qual/src/bin/b.rs
@@ -6,6 +6,22 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n],
+    };
+    let mut r_sum = aa[0];
+    let mut l_sum = aa[n - 1];
+    let mut r = 1;
+    let mut l = n - 2;
+    while r <= l {
+        if r_sum <= l_sum {
+            r_sum += aa[r];
+            r += 1;
+        } else {
+            l_sum += aa[l];
+            l -= 1;
+        }
+    }
+    println!("{}", r_sum.max(l_sum) - r_sum.min(l_sum));
 }
