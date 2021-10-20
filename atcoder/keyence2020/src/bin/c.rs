96850a9a5ee95db862afccd00bf588be0cc0b159
horyu <28817604+horyu@users.noreply.github.com>
2021-10-20 09:13:23 +0900

    keyence2020 c

--- atcoder/keyence2020/src/bin/c.rs
+++ atcoder/keyence2020/src/bin/c.rs
@@ -6,6 +6,14 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        s: usize,
+    };
+    const MAX: usize = 1000000000;
+    let x = if s == MAX { 1 } else { MAX };
+    let aa = vec![s; k];
+    let bb = vec![x; n - k];
+    println!("{}", aa.iter().chain(bb.iter()).join(" "));
 }
