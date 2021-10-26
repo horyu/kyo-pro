03ce305683cee1d040d04a4c0ce6201b1130b613
horyu <28817604+horyu@users.noreply.github.com>
2021-10-27 08:42:07 +0900

    keyence2021 b

--- atcoder/keyence2021/src/bin/b.rs
+++ atcoder/keyence2021/src/bin/b.rs
@@ -6,6 +6,25 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n]
+    };
+    let mut cnts = vec![0; n + 1];
+    for a in aa {
+        cnts[a] += 1;
+    }
+    let mut cnt = cnts[0].min(k);
+    let mut rs = 0;
+    for (i, c) in cnts.into_iter().enumerate().skip(1) {
+        if cnt == 0 {
+            break;
+        }
+        if c < cnt {
+            rs += (cnt - c) * i;
+        }
+        cnt = cnt.min(c);
+    }
+    println!("{}", rs);
 }
