4f5888db1557ac8c5df10602a528e0535f2d501d
horyu <28817604+horyu@users.noreply.github.com>
2021-10-29 09:45:17 +0900

    abc219 c

--- atcoder/abc219/src/bin/c.rs
+++ atcoder/abc219/src/bin/c.rs
@@ -6,6 +6,17 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        x: Chars,
+        n: usize,
+        mut ss: [String; n]
+    };
+    ss.sort_by_cached_key(|s| {
+        s.chars()
+            .map(|c| x.iter().position(|&xc| xc == c).unwrap())
+            .collect_vec()
+    });
+    for s in ss {
+        println!("{}", s);
+    }
 }
