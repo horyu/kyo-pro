ad9a13b11d239af0bc6241d1e4a9236b65c9d3d4
horyu <28817604+horyu@users.noreply.github.com>
2021-11-01 08:38:40 +0900

    abc220 c

--- atcoder/abc220/src/bin/c.rs
+++ atcoder/abc220/src/bin/c.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: u128,
+        aa: [u128; n],
+        x: u128
+    };
+    let a_sum = aa.iter().sum::<u128>();
+    let mut cnt = x / a_sum * n;
+    let mut sum = x / a_sum * a_sum;
+    for &a in &aa {
+        if sum > x {
+            break;
+        }
+        cnt += 1;
+        sum += a;
+    }
+    println!("{}", cnt);
 }
