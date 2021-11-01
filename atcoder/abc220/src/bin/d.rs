fe3066ff03460953104cd3b8690b5736ba6923cd
horyu <28817604+horyu@users.noreply.github.com>
2021-11-02 08:59:59 +0900

    abc220 d

--- atcoder/abc220/src/bin/d.rs
+++ atcoder/abc220/src/bin/d.rs
@@ -6,6 +6,22 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut dpp = [0usize; 10];
+    dpp[aa[0]] = 1;
+    for &a in &aa[1..] {
+        let mut dpn = [0usize; 10];
+        for (i, p) in dpp.iter().enumerate() {
+            dpn[(i + a) % 10] += p;
+            dpn[(i * a) % 10] += p;
+        }
+        for n in dpn.iter_mut() {
+            *n %= 998244353;
+        }
+        dpp = dpn;
+    }
+    println!("{}", dpp.iter().join("\n"));
 }
