26f1478b9c0fb6808375fd744ef2a8522e106e72
horyu <28817604+horyu@users.noreply.github.com>
2021-11-04 13:46:15 +0900

    abc223 c

--- atcoder/abc223/src/bin/c.rs
+++ atcoder/abc223/src/bin/c.rs
@@ -3,9 +3,19 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aabb: [(f64, f64); n]
+    };
+    let mut t = aabb.iter().fold(0.0, |acc, (a, b)| acc + a / b) / 2.0;
+
+    let mut rs = 0.0;
+    for (a, b) in aabb {
+        rs += a.min(t * b);
+        t -= t.min(a / b);
+    }
+    println!("{}", rs);
 }
