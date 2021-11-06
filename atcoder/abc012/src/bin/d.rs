4848b16762fb9a763466f950d0e9e494aebb8520
horyu <28817604+horyu@users.noreply.github.com>
2021-11-06 10:28:02 +0900

    abc012 d

--- atcoder/abc012/src/bin/d.rs
+++ atcoder/abc012/src/bin/d.rs
@@ -1,10 +1,44 @@
 #![allow(unused_imports)]
+#![allow(clippy::needless_range_loop)]
 use itertools::Itertools;
 // use itertools::Itertools;
 use petgraph::graph::UnGraph;
 use proconio::{input, marker::*};
 
 fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        aabbtt: [(Usize1, Usize1, usize); m]
+    };
+    // ワーシャルフロイド法
+    let mut dist = vec![vec![44850usize * 10000usize; n]; n];
+    for i in 0..n {
+        dist[i][i] = 0;
+    }
+    for (a, b, t) in aabbtt {
+        dist[a][b] = t;
+        dist[b][a] = t;
+    }
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                dist[i][j] = dist[i][j].min(dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    let rs = dist
+        .into_iter()
+        .map(|d| d.into_iter().max().unwrap())
+        .min()
+        .unwrap();
+
+    println!("{}", rs);
+}
+
+// ダイクストラ法
+#[allow(dead_code)]
+fn main2() {
     input! {
         n: usize,
         m: usize,
