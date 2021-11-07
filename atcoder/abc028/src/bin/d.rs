f8e0070c169a63d9abd60fd13dd6c17dbdd2edcd
horyu <28817604+horyu@users.noreply.github.com>
2021-11-07 11:26:03 +0900

    abc028 d

--- atcoder/abc028/src/bin/d.rs
+++ atcoder/abc028/src/bin/d.rs
@@ -1,8 +1,22 @@
 #![allow(unused_imports)]
+#![allow(clippy::many_single_char_names)]
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: u128,
+        k: u128,
+    };
+    let mut cnt = 0u128;
+    // === *1
+    cnt += 1;
+    // <== *3
+    cnt += (k - 1) * 3;
+    // <=< *6
+    cnt += (k - 1) * (n - k) * 6;
+    // ==< *3
+    cnt += (n - k) * 3;
+
+    println!("{}", cnt as f64 / n.pow(3) as f64);
 }
