2c83f57bd4b29ac34c81b178227104675d38b99a
horyu <28817604+horyu@users.noreply.github.com>
2021-11-10 08:33:38 +0900

    abc036 c

--- atcoder/abc036/src/bin/c.rs
+++ atcoder/abc036/src/bin/c.rs
@@ -1,8 +1,20 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut xx = aa.clone();
+    xx.sort_unstable();
+    xx.dedup();
+    let mut hm = std::collections::HashMap::new();
+    for (i, x) in xx.into_iter().enumerate() {
+        hm.insert(x, i);
+    }
+    let s = aa.into_iter().map(|a| hm.get(&a).unwrap()).join("\n");
+    println!("{}", s);
 }
