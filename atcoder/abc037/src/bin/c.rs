afee27045a6339e7ed805a1ec0c09ca93156befc
horyu <28817604+horyu@users.noreply.github.com>
2021-11-11 09:47:40 +0900

    abc037 c

--- atcoder/abc037/src/bin/c.rs
+++ atcoder/abc037/src/bin/c.rs
@@ -8,6 +8,8 @@ fn main() {
         k: usize,
         aa: [u128; n]
     };
+    // 愚直だが普通に通る
+    // let rs = aa.windows(k).map(|v| v.iter().sum::<u128>()).sum::<u128>();
     let rs = aa.iter().sum::<u128>() * (k as u128)
         - (0..(k - 1))
             .map(|i| (k - 1 - i) as u128 * (aa[i] + aa[n - 1 - i]))
