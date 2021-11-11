66f78388512ef473f9da587abde863ab9c2fd481
horyu <28817604+horyu@users.noreply.github.com>
2021-11-12 08:41:37 +0900

    abc038 c

--- atcoder/abc038/src/bin/c.rs
+++ atcoder/abc038/src/bin/c.rs
@@ -1,8 +1,18 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut cnts = vec![1; n];
+    for (i, vv) in aa.windows(2).enumerate().rev() {
+        if vv[0] < vv[1] {
+            cnts[i] = cnts[i + 1] + 1;
+        }
+    }
+    println!("{}", cnts.iter().sum::<usize>());
 }
