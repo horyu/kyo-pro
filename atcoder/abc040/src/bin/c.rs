13066d2db4889775988c4a741d53bb8c96444c95
horyu <28817604+horyu@users.noreply.github.com>
2021-11-15 08:23:05 +0900

    abc040 c

--- atcoder/abc040/src/bin/c.rs
+++ atcoder/abc040/src/bin/c.rs
@@ -3,6 +3,15 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [isize; n]
+    };
+    let mut vv = vec![std::isize::MAX; n];
+    vv[0] = 0;
+    vv[1] = (aa[1] - aa[0]).abs();
+    for i in 2..n {
+        vv[i] = (vv[i - 1] + (aa[i] - aa[i - 1]).abs()).min(vv[i - 2] + (aa[i] - aa[i - 2]).abs());
+    }
+    println!("{}", vv[n - 1]);
 }
