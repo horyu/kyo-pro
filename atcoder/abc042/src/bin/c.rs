d30704439dd3a4d1c5561fb6bb20446c261a2f4c
horyu <28817604+horyu@users.noreply.github.com>
2021-11-16 21:28:21 +0900

    abc042 c

--- atcoder/abc042/src/bin/c.rs
+++ atcoder/abc042/src/bin/c.rs
@@ -5,34 +5,32 @@ use proconio::{input, marker::*};
 
 fn main() {
     input! {
-        n: usize,
+        mut n: usize,
         k: usize,
         dd: [usize; k]
     };
-    let usable_head = (1usize..=9).filter(|i| !dd.contains(i)).collect_vec();
-    if n < 10 {
-        if let Some(i) = usable_head.iter().find(|&&i| i >= n) {
-            println!("{}", i);
+    let outs = {
+        let mut arr = [false; 10];
+        for d in dd {
+            arr[d] = true;
+        }
+        arr
+    };
+    loop {
+        if all_ok(&outs, n) {
+            println!("{}", n);
             return;
         }
+        n += 1;
     }
-    let order = (1..std::u32::MAX)
-        .find(|&o| n / 10usize.pow(o) == 0)
-        .unwrap();
-    let usable_body = (0usize..=9).filter(|i| !dd.contains(i)).collect_vec();
-    for i in order..std::u32::MAX {
-        for &head in &usable_head {
-            for vv in (0..(i - 1))
-                .map(|_| usable_body.iter())
-                .multi_cartesian_product()
-            {
-                let body = vv.into_iter().fold(0usize, |acc, v| acc * 10 + v);
-                let num = body + head * 10usize.pow(i - 1);
-                if num % n == 0 {
-                    println!("{}", num);
-                    return;
-                }
-            }
+}
+
+fn all_ok(outs: &[bool; 10], mut n: usize) -> bool {
+    while n != 0 {
+        if outs[n % 10] {
+            return false;
         }
+        n /= 10;
     }
+    true
 }
