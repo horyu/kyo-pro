35485af2a35fc5a2c16f371386de2f29eff487ba
horyu <28817604+horyu@users.noreply.github.com>
2021-11-18 09:19:28 +0900

    abc048 c

--- atcoder/abc048/src/bin/c.rs
+++ atcoder/abc048/src/bin/c.rs
@@ -1,8 +1,26 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        x: usize,
+        mut aa: [usize; n]
+    };
+    let mut rs = 0;
+    for i in 0..(aa.len() - 1) {
+        if aa[i] == 0 {
+            continue;
+        }
+        let diff = (aa[i] + aa[i + 1]).saturating_sub(x);
+        if diff > 0 {
+            rs += diff;
+            // aa[i+1]から優先して引く
+            aa[i + 1] = aa[i + 1].saturating_sub(diff);
+        }
+    }
+    rs += aa.last().unwrap().saturating_sub(x);
+    println!("{}", rs);
 }
