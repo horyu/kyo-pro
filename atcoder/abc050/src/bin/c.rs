548d5bc50b5e17fcd15eb49ab418122974dcd8be
horyu <28817604+horyu@users.noreply.github.com>
2021-11-20 11:28:44 +0900

    abc050 c

--- atcoder/abc050/src/bin/c.rs
+++ atcoder/abc050/src/bin/c.rs
@@ -3,6 +3,43 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    const MOD: usize = 1000000007;
+    let mut hm = std::collections::HashMap::new();
+    for a in aa {
+        if n % 2 == a % 2 {
+            println!("0");
+            return;
+        }
+        *hm.entry(a).or_insert(0) += 1;
+    }
+    let mut rs = 1;
+    if n % 2 == 0 {
+        for i in (1..n).step_by(2) {
+            if let Some(&2) = hm.get(&i) {
+                rs = rs * 2 % MOD;
+            } else {
+                println!("0");
+                return;
+            }
+        }
+    } else {
+        if let Some(&1) = hm.get(&0) {
+        } else {
+            println!("0");
+            return;
+        }
+        for i in (2..n).step_by(2) {
+            if let Some(&2) = hm.get(&i) {
+                rs = rs * 2 % MOD;
+            } else {
+                println!("0");
+                return;
+            }
+        }
+    }
+    println!("{}", rs);
 }
