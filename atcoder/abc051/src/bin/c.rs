8c9df8b6e2048eb2af587906e729323f1247cf69
horyu <28817604+horyu@users.noreply.github.com>
2021-11-22 09:12:32 +0900

    abc051 c

--- atcoder/abc051/src/bin/c.rs
+++ atcoder/abc051/src/bin/c.rs
@@ -3,6 +3,59 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        sx: isize,
+        sy: isize,
+        tx: isize,
+        ty: isize,
+    };
+    let mut x = sx;
+    let mut y = sy;
+    let mut rs = String::new();
+    // 上*右*
+    while y != ty {
+        y += 1;
+        rs.push('U');
+    }
+    while x != tx {
+        x += 1;
+        rs.push('R');
+    }
+    // 下*左*
+    while y != sy {
+        y -= 1;
+        rs.push('D');
+    }
+    while x != sx {
+        x -= 1;
+        rs.push('L');
+    }
+    // 左上*右*下
+    x -= 1;
+    rs.push('L');
+    while y != ty + 1 {
+        y += 1;
+        rs.push('U');
+    }
+    while x != tx {
+        x += 1;
+        rs.push('R');
+    }
+    y -= 1;
+    rs.push('D');
+    // 右下*左*上
+    x += 1;
+    rs.push('R');
+    while y != sy - 1 {
+        y -= 1;
+        rs.push('D');
+    }
+    while x != sx {
+        x -= 1;
+        rs.push('L');
+    }
+    // y -= 1;
+    rs.push('U');
+
+    println!("{}", rs);
 }
