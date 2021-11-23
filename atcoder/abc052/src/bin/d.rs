4d55e592c46bbeac11a982f94addc64b9c8a8b7d
horyu <28817604+horyu@users.noreply.github.com>
2021-11-24 08:32:34 +0900

    abc052 d

--- atcoder/abc052/src/bin/d.rs
+++ atcoder/abc052/src/bin/d.rs
@@ -1,8 +1,18 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        xx: [usize; n]
+    };
+    let mut rs = 0;
+    for (xi, xj) in xx.into_iter().tuple_windows() {
+        rs += b.min(a * (xj - xi));
+    }
+    println!("{}", rs);
 }
