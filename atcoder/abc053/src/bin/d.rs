08d9bfb44b10ff05ad8d79b02a1fd93b9b29ac60
horyu <28817604+horyu@users.noreply.github.com>
2021-11-25 13:58:50 +0900

    abc053 d

--- atcoder/abc053/src/bin/d.rs
+++ atcoder/abc053/src/bin/d.rs
@@ -3,6 +3,33 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut hm = std::collections::HashMap::new();
+    for a in aa {
+        let e = hm.entry(a).or_insert(0usize);
+        if *e == 2 {
+            // 同じのが3個になったら1個に圧縮
+            *e = 1;
+        } else {
+            *e += 1;
+        }
+    }
+    let nums = hm.len();
+    let surplus = hm.into_iter().map(|(_, v)| v - 1).sum::<usize>();
+    if surplus == 0 {
+        println!("{}", nums);
+    } else {
+        // 1
+        // 1 2
+        // 1 2 3
+        let rs = match surplus % 2 {
+            0 => nums,
+            1 => nums - 1,
+            _ => unreachable!(),
+        };
+        println!("{}", rs)
+    }
 }
