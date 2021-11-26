96bc43d0d5b3f137ecd7c1e7eebb837aba6d2bee
horyu <28817604+horyu@users.noreply.github.com>
2021-11-26 09:15:41 +0900

    abc057 c

--- atcoder/abc057/src/bin/c.rs
+++ atcoder/abc057/src/bin/c.rs
@@ -1,8 +1,27 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
+use num_integer::*;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize
+    };
+    let rs = (1..=(n.sqrt() + 1))
+        .filter(|i| n % i == 0)
+        .map(|i| f(i, n / i))
+        .min()
+        .unwrap();
+
+    println!("{}", rs);
+}
+
+fn f(x: usize, y: usize) -> usize {
+    let mut z = x.max(y);
+    let mut rs = 0;
+    while z > 0 {
+        z /= 10;
+        rs += 1;
+    }
+    rs
 }
