92b56819dfa4ee1bef1e66ffcad216c01f7ab8b9
horyu <28817604+horyu@users.noreply.github.com>
2021-12-03 09:09:09 +0900

    abc077 c

--- atcoder/abc077/src/bin/a.rs
+++ atcoder/abc077/src/bin/a.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut buf = [b'\n'; 8];
     let stdin = stdin();
     let mut handle = stdin.lock();
-    handle.read(&mut buf).unwrap();
+    handle.read_exact(&mut buf).unwrap();
     if (buf[0] == buf[6]) && (buf[1] == buf[5]) && (buf[2] == buf[4]) {
         stdout().write_all(b"YES").unwrap();
     } else {
--- atcoder/abc077/src/bin/c.rs
+++ atcoder/abc077/src/bin/c.rs
@@ -3,6 +3,35 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        mut aa: [usize; n],
+        mut bb: [usize; n],
+        mut cc: [usize; n],
+    };
+    aa.sort_unstable();
+    bb.sort_unstable();
+    cc.sort_unstable();
+    let mut a_i = 0;
+    let mut c_i = 0;
+    // bを基準に自身未満のaa, 自身以下のcc(nから引けば自身超過)の数を求める
+    let mut rs = 0;
+    for b in bb {
+        while let Some(next_a) = aa.get(a_i) {
+            if *next_a < b {
+                a_i += 1;
+            } else {
+                break;
+            }
+        }
+        while let Some(next_c) = cc.get(c_i) {
+            if *next_c <= b {
+                c_i += 1;
+            } else {
+                break;
+            }
+        }
+        rs += a_i * (n - c_i);
+    }
+    println!("{}", rs);
 }
