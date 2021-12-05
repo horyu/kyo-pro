1c3f8c7745deb01ec26216b06ce12516cca2f783
horyu <28817604+horyu@users.noreply.github.com>
2021-12-05 11:03:30 +0900

    abc079 d

--- atcoder/abc079/src/bin/d.rs
+++ atcoder/abc079/src/bin/d.rs
@@ -1,8 +1,32 @@
 #![allow(unused_imports)]
+use std::collections::VecDeque;
+
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        h: usize,
+        w: usize,
+        mut ccc: [[usize; 10]; 10],
+        aaa: [[isize; w]; h]
+    };
+    // ワーシャルフロイド法
+    for k in 0..10 {
+        for i in 0..10 {
+            for j in 0..10 {
+                ccc[i][j] = ccc[i][j].min(ccc[i][k] + ccc[k][j]);
+            }
+        }
+    }
+
+    let mut rs = 0;
+    for aa in aaa {
+        for a in aa {
+            if a != -1 {
+                rs += ccc[a as usize][1];
+            }
+        }
+    }
+    println!("{}", rs);
 }
