4751971780d912ec94af726abcf34ba3546ebfa3
horyu <28817604+horyu@users.noreply.github.com>
2021-12-10 09:44:50 +0900

    abc085 d

--- atcoder/abc085/src/bin/d.rs
+++ atcoder/abc085/src/bin/d.rs
@@ -10,28 +10,17 @@ fn main() {
         mut aabb: [(usize, usize); n]
     };
     aabb.sort_unstable_by_key(|ab| ab.1);
-    let mut max_a = 0;
-    let mut max_a_index = 0;
-    for (i, ab) in aabb.iter().enumerate() {
-        if max_a < ab.0 {
-            max_a = ab.0;
-            max_a_index = i;
-        }
-    }
+    let max_a = aabb.iter().map(|ab| ab.0).max().unwrap();
+
     let mut rs = 0;
-    let max_a_min_b = aabb[max_a_index].1;
-    for (i, &(_a, b)) in aabb.iter().enumerate().rev() {
-        if max_a_index == i {
-            continue;
-        }
-        if h <= max_a_min_b || b <= max_a {
+    for &(_a, b) in aabb.iter().rev() {
+        if h == 0 || b <= max_a {
             break;
         }
         rs += 1;
         h = h.saturating_sub(b);
     }
-    if h != 0 {
-        rs += 1 + h.saturating_sub(max_a_min_b).div_ceil(&max_a);
-    }
+    rs += h.div_ceil(&max_a);
+
     println!("{}", rs);
 }
