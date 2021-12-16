677fd4d9de0ab507f2e5ca2ab60f2c4d178adffc
horyu <28817604+horyu@users.noreply.github.com>
2021-12-17 08:56:35 +0900

    abc097 c

--- atcoder/abc097/src/bin/c.rs
+++ atcoder/abc097/src/bin/c.rs
@@ -1,8 +1,18 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::HashSet;
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        s: String,
+        k: usize
+    };
+    let mut hs = HashSet::new();
+    for i in 0..s.len() {
+        for j in i..(s.len().min(i + k)) {
+            hs.insert(&s[i..=j]);
+        }
+    }
+    println!("{}", hs.into_iter().sorted().nth(k - 1).unwrap());
 }
