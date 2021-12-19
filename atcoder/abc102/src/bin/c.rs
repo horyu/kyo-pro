2c2821522cb320645c1da5e2b38896a05d8384ca
horyu <28817604+horyu@users.noreply.github.com>
2021-12-19 10:10:36 +0900

    abc102 c

--- atcoder/abc102/src/bin/c.rs
+++ atcoder/abc102/src/bin/c.rs
@@ -1,8 +1,23 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        aa: [isize; n]
+    };
+    let aa = aa
+        .iter()
+        .enumerate()
+        .map(|(i, a)| a - (i + 1) as isize)
+        .sorted()
+        .collect_vec();
+    let b = if n % 2 == 1 {
+        aa[n / 2]
+    } else {
+        (aa[n / 2 - 1] + aa[n / 2]) / 2
+    };
+    println!("{}", aa.iter().map(|a| (a - b).abs()).sum::<isize>());
 }
