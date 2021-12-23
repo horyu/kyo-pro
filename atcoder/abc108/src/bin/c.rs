d2f03deaec6406ea5765b95929eae22467a91f0e
horyu <28817604+horyu@users.noreply.github.com>
2021-12-23 21:27:53 +0900

    abc108 c

--- atcoder/abc108/src/bin/c.rs
+++ atcoder/abc108/src/bin/c.rs
@@ -1,8 +1,24 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
+use num_integer::*;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        k: usize,
+    };
+    let mut rs = 0;
+    // k..=n の中でkの倍数の候補から重複有りで3つ選ぶ
+    rs += (n / k).pow(3);
+    if k % 2 == 0 {
+        // i mod k == k/2 のものを足し合わせたらkの倍数になる
+        let half_k = k / 2;
+        if (n / k) * k + half_k <= n {
+            rs += (n / k + 1).pow(3);
+        } else {
+            rs += (n / k).pow(3);
+        }
+    }
+    println!("{}", rs);
 }
