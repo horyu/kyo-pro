66c04ba703655f35a199c9e9e2eee5dc5fa06c70
horyu <28817604+horyu@users.noreply.github.com>
2021-12-24 09:40:45 +0900

    abc110 c

--- atcoder/abc110/src/bin/c.rs
+++ atcoder/abc110/src/bin/c.rs
@@ -1,8 +1,21 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        s: Chars,
+        t: Chars,
+    };
+    let mut ts = [[false; 26]; 26];
+    let mut st = [[false; 26]; 26];
+    for (&sc, &tc) in s.iter().zip(t.iter()) {
+        let ti = (tc as u8 - b'a') as usize;
+        let si = (sc as u8 - b'a') as usize;
+        ts[ti][si] = true;
+        st[si][ti] = true;
+    }
+    let tf = ts.iter().all(|arr| arr.iter().filter(|&&x| x).count() <= 1)
+    && st.iter().all(|arr| arr.iter().filter(|&&x| x).count() <= 1);
+    println!("{}", ["No", "Yes"][tf as usize]);
 }
