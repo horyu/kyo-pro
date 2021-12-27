f634b4a799cfb5ff9330d92c3ff82bc1db19765a
horyu <28817604+horyu@users.noreply.github.com>
2021-12-27 12:22:58 +0900

    abc112 d

--- atcoder/abc112/src/bin/d.rs
+++ atcoder/abc112/src/bin/d.rs
@@ -3,6 +3,33 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: usize,
+        m: usize,
+    };
+    let mut rs = 1;
+    for k in divisors(m) {
+        if let Some(amari) = m.checked_sub(n * k) {
+            if amari % k == 0 {
+                rs = k;
+            }
+        }
+    }
+    println!("{}", rs);
+}
+
+// https://qiita.com/Cassin01/items/9bc63a4bde5526150681
+fn divisors(n: usize) -> Vec<usize> {
+    let mut divisors = Vec::new();
+    for i in 1..=(f64::sqrt(n as f64) + 1e-9) as usize {
+        if n % i == 0 {
+            divisors.push(i);
+            if i != n / i {
+                divisors.push(n / i);
+            }
+        }
+    }
+    divisors.sort_unstable();
+
+    divisors
 }
