8008c5e96b8fbeac02ae4d8842451759721052c7
horyu <28817604+horyu@users.noreply.github.com>
2021-12-28 09:37:57 +0900

    abc113 c

--- atcoder/abc113/src/bin/c.rs
+++ atcoder/abc113/src/bin/c.rs
@@ -1,8 +1,27 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::HashMap;
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        _n: usize,
+        m: usize,
+        ppyy: [(usize, usize); m]
+    };
+    let mut hm = HashMap::new();
+    for (p, y) in &ppyy {
+        hm.entry(*p).or_insert(vec![]).push(*y);
+    }
+    let mut new_hm = HashMap::new();
+    for (p, vv) in hm {
+        let vv = vv.into_iter().sorted().enumerate();
+        for (i, y) in vv {
+            new_hm.insert((p, y), i + 1);
+        }
+    }
+    for py in ppyy {
+        let i = new_hm.get(&py).unwrap();
+        println! {"{:06}{:06}", py.0, i};
+    }
 }
