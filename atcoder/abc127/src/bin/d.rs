57e7c3055038f9a705cc471f4814c29ca380c665
horyu <28817604+horyu@users.noreply.github.com>
2022-01-04 08:46:34 +0900

    abc127 d

--- atcoder/abc127/src/bin/d.rs
+++ atcoder/abc127/src/bin/d.rs
@@ -1,8 +1,27 @@
 #![allow(unused_imports)]
+use std::{collections::BTreeMap, iter::FromIterator};
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        mut aa: [usize; n],
+        mut bbcc: [(usize, usize); m]
+    };
+    aa.sort_unstable();
+    bbcc.sort_unstable_by_key(|bc| std::cmp::Reverse(bc.1));
+    let mut ai = 0;
+    'outer: for (mut b, c) in bbcc {
+        while b > 0 {
+            if ai >= n || aa[ai] >= c {
+                break 'outer;
+            }
+            aa[ai] = c;
+            ai += 1;
+            b -= 1;
+        }
+    }
+    println!("{}", aa.iter().sum::<usize>());
 }
