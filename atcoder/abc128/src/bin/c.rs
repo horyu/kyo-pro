26dc93a178ba8d264beb24222e24e7d126a47901
horyu <28817604+horyu@users.noreply.github.com>
2022-01-05 09:01:28 +0900

    abc128 c

--- atcoder/abc128/src/bin/c.rs
+++ atcoder/abc128/src/bin/c.rs
@@ -1,8 +1,28 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+    };
+    let mut ssss = vec![];
+    for _ in 0..m {
+        input! {k: usize};
+        input! {ss: [Usize1; k]};
+        ssss.push(ss);
+    }
+    input! {pp: [usize; m]};
+
+    let rs = (0..n)
+        .map(|_| [false, true])
+        .multi_cartesian_product()
+        .filter(|ttff| {
+            ssss.iter()
+                .zip(pp.iter())
+                .all(|(ss, p)| ss.iter().filter(|s| ttff[**s]).count() % 2 == *p)
+        })
+        .count();
+    println!("{}", rs);
 }
