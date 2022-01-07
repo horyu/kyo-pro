10ff89a3ef2e295597d9cfa69034c914f02950ff
horyu <28817604+horyu@users.noreply.github.com>
2022-01-07 09:46:10 +0900

    abc130 d

--- atcoder/abc130/src/bin/d.rs
+++ atcoder/abc130/src/bin/d.rs
@@ -3,6 +3,28 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n]
+    };
+    // 尺取り法
+    let mut r = 0;
+    let mut sum = 0;
+    let mut rs = 0;
+    for l in 0..n {
+        while sum < k {
+            if r == n {
+                break;
+            }
+            sum += aa[r];
+            r += 1;
+        }
+        if sum < k {
+            break;
+        }
+        rs += n - r + 1;
+        sum -= aa[l];
+    }
+    println!("{}", rs);
 }
