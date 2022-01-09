bb9a85850a5137ef17fdbd530ea1d286ea3d9ddb
horyu <28817604+horyu@users.noreply.github.com>
2022-01-09 09:53:05 +0900

    abc133 d

--- atcoder/abc133/src/bin/d.rs
+++ atcoder/abc133/src/bin/d.rs
@@ -1,8 +1,17 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let sum = aa.iter().sum::<usize>();
+    let b0 = (sum / 2 - aa[1..].iter().step_by(2).sum::<usize>()) * 2;
+    let bb = aa[..(n - 1)].iter().fold(vec![b0], |mut vv, a| {
+        vv.push((a - vv.last().unwrap() / 2) * 2);
+        vv
+    });
+    println!("{}", bb.into_iter().join(" "));
 }
