a8edfc182ab4ffad3915a6e9d9b7ca2842dd49f9
horyu <28817604+horyu@users.noreply.github.com>
2022-01-10 09:03:37 +0900

    abc134 d

--- atcoder/abc134/src/bin/d.rs
+++ atcoder/abc134/src/bin/d.rs
@@ -1,8 +1,28 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut bb = vec![0; n];
+    for (i, a) in aa.iter().enumerate().rev() {
+        if *a != bb[i..].iter().step_by(i + 1).sum::<usize>() % 2 {
+            bb[i] = 1;
+        }
+    }
+    let rs = bb
+        .iter()
+        .enumerate()
+        .filter(|ib| *ib.1 > 0)
+        .map(|ib| ib.0 + 1)
+        .collect_vec();
+    if rs.is_empty() {
+        println!("0");
+    } else {
+        println!("{}\n{}", rs.len(), rs.iter().join(" "));
+    }
 }
