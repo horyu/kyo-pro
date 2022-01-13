479ff33760d0c0c1dffafe4d260cf797c23650b3
horyu <28817604+horyu@users.noreply.github.com>
2022-01-13 13:22:24 +0900

    abc141 d

--- atcoder/abc141/src/bin/d.rs
+++ atcoder/abc141/src/bin/d.rs
@@ -1,8 +1,20 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        aa: [usize; n]
+    };
+    let mut bh: BinaryHeap<_> = aa.into_iter().collect();
+    for _ in 0..m {
+        if let Some(a) = bh.pop() {
+            bh.push(a / 2);
+        }
+    }
+    let rs = bh.into_iter().sum::<usize>();
+    println!("{}", rs);
 }
