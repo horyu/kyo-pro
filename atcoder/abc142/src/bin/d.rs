b129988007575cceb1fd82b29f27ac9c9d796e9c
horyu <28817604+horyu@users.noreply.github.com>
2022-01-14 08:31:57 +0900

    abc142 d

--- atcoder/abc142/src/bin/d.rs
+++ atcoder/abc142/src/bin/d.rs
@@ -1,8 +1,39 @@
 #![allow(unused_imports)]
 // use itertools::Itertools;
 use proconio::{input, marker::*};
+use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        a: usize,
+        b: usize,
+    };
+    let adivs = divisors(a);
+    let bdivs = divisors(b);
+    let rs = adivs.intersection(&bdivs).count();
+    println!("{}", rs);
+}
+
+fn divisors(mut n: usize) -> HashSet<usize> {
+    let mut hs = HashSet::new();
+    hs.insert(1);
+    if n <= 1 {
+        return hs;
+    }
+    while n % 2 == 0 {
+        n /= 2;
+        hs.insert(2);
+    }
+    let mut i = 3;
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+            hs.insert(i);
+        }
+        i += 2;
+    }
+    if n != 1 {
+        hs.insert(n);
+    }
+    hs
 }
