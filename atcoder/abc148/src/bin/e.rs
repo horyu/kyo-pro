f9cbd9ed4f538251b25edbe91cf26f25a3bc7a62
horyu <28817604+horyu@users.noreply.github.com>
2022-01-18 08:51:43 +0900

    abc148 e

--- atcoder/abc148/src/bin/e.rs
+++ atcoder/abc148/src/bin/e.rs
@@ -4,6 +4,19 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: u128
+    };
+    if n < 10 || n % 2 == 1 {
+        println!("0");
+        return;
+    }
+    let upper = 1e19 as u128;
+    let mut rs = 0;
+    let mut div = 10;
+    while div < upper {
+        rs += n / div;
+        div *= 5;
+    }
+    println!("{rs}");
 }
