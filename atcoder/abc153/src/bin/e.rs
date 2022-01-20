5343675d25823b1c505e1788cea22b73051107cd
horyu <28817604+horyu@users.noreply.github.com>
2022-01-20 09:07:07 +0900

    abc153 e

--- atcoder/abc153/src/bin/e.rs
+++ atcoder/abc153/src/bin/e.rs
@@ -4,6 +4,19 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        h: usize,
+        n: usize,
+        mut aabb: [(usize, usize); n]
+    };
+    aabb.sort_unstable_by_key(|ab| ab.0);
+    const N: usize = 10000;
+    let mut vv = vec![std::usize::MAX >> 4; N * 2 + 1];
+    vv[0] = 0;
+    for (a, b) in aabb {
+        for i in 0..=N {
+            vv[i + a] = vv[i + a].min(vv[i] + b);
+        }
+    }
+    println!("{}", vv[h..].iter().min().unwrap());
 }
