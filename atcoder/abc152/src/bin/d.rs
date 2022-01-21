eec23c350d8287fe31c64da17d67e5a30d9bf85e
horyu <28817604+horyu@users.noreply.github.com>
2022-01-21 23:36:12 +0900

    abc152 d

--- atcoder/abc152/src/bin/d.rs
+++ atcoder/abc152/src/bin/d.rs
@@ -4,6 +4,28 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+    };
+    let mut vv = [[0usize; 10]; 10];
+    for i in 1..=n {
+        let tail = i % 10;
+        let head = {
+            let mut h = i;
+            while h >= 10 {
+                h /= 10;
+            }
+            h
+        };
+
+        vv[head][tail] += 1;
+    }
+
+    let mut rs = 0;
+    for t in 0..=9 {
+        for h in 0..=9 {
+            rs += vv[t][h] * vv[h][t];
+        }
+    }
+    println!("{rs}");
 }
