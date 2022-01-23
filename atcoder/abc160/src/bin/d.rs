7f7c3cf5d3c3fd3f43445538e9af2a1724a62c87
horyu <28817604+horyu@users.noreply.github.com>
2022-01-23 10:51:09 +0900

    abc160 d

--- atcoder/abc160/src/bin/d.rs
+++ atcoder/abc160/src/bin/d.rs
@@ -5,6 +5,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: isize,
+        x: Isize1,
+        y: Isize1,
+    };
+    let mut rs = vec![0; 2 * 1000 + 1];
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let k = (j - i).abs()
+                .min((x - i).abs() + (y - j).abs() + 1)
+                .min((y - i).abs() + (x - j).abs() + 1);
+            rs[k as usize] += 1;
+        }
+    }
+    println!("{}", rs[1..(n as usize)].iter().join("\n"));
 }
