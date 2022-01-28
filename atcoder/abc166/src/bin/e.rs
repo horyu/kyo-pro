b204b01609d7c0bb75addfe1d825d16bbb000c81
horyu <28817604+horyu@users.noreply.github.com>
2022-01-28 22:05:52 +0900

    abc166 e

--- atcoder/abc166/src/bin/e.rs
+++ atcoder/abc166/src/bin/e.rs
@@ -5,6 +5,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aa: [usize; n]
+    };
+    // j - i = ai + aj
+    // j - aj = ai + i
+    let mut rs = 0usize;
+    let mut hm = HashMap::new();
+    for (i, &a) in aa.iter().enumerate() {
+        if let Some(v) = i.checked_sub(a) {
+            rs += hm.get(&v).unwrap_or(&0);
+        }
+        *hm.entry(a + i).or_insert(0) += 1;
+    }
+    println!("{rs}");
 }
