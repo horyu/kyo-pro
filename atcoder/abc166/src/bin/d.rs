1d2e4d7b6b36c22545f661157ec36edb10805c7e
horyu <28817604+horyu@users.noreply.github.com>
2022-01-30 12:46:10 +0900

    abc166 d

--- atcoder/abc166/src/bin/d.rs
+++ atcoder/abc166/src/bin/d.rs
@@ -5,6 +5,20 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        x: u128
+    };
+    let vv = (0u128..1000).map(|i| i.pow(5u32)).collect_vec();
+    for i in 1..1000 {
+        for j in 0..i {
+            if vv[i] - vv[j] == x {
+                println!("{i} {j}");
+                return;
+            }
+            if vv[i] + vv[j] == x {
+                println!("{i} -{j}");
+                return;
+            }
+        }
+    }
 }
