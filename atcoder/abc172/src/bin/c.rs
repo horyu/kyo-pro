1050f18356c378175f5225415b7293bd5d17222a
horyu <28817604+horyu@users.noreply.github.com>
2022-01-31 09:19:09 +0900

    abc172 c

--- atcoder/abc172/src/bin/c.rs
+++ atcoder/abc172/src/bin/c.rs
@@ -5,6 +5,51 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        k: u128,
+        aa: [u128; n],
+        bb: [u128; m],
+    };
+    // n,m <= 200_000
+    let mut i = 0;
+    let mut j = 0;
+    let mut sum = 0;
+    // aa優先で読めるだけ読みbも読む
+    while i < n {
+        let next_sum = sum + aa[i];
+        if next_sum <= k {
+            sum = next_sum;
+            i += 1;
+            continue;
+        }
+        break;
+    }
+    while j < m {
+        let next_sum = sum + bb[j];
+        if next_sum <= k {
+            sum = next_sum;
+            j += 1;
+            continue;
+        }
+        break;
+    }
+    let mut rs = i + j;
+    // bbの読む数を増やしていく→aaを減らす
+    while i > 0 && j < m {
+        i -= 1;
+        sum -= aa[i];
+        while j < m {
+            let next_sum = sum + bb[j];
+            if next_sum <= k {
+                sum = next_sum;
+                j += 1;
+                continue;
+            }
+            break;
+        }
+        rs = rs.max(i + j);
+    }
+    println!("{rs}");
 }
