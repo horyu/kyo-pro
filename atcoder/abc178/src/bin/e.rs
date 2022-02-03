87d1f54fd4f12e95cb0e4ae8fd4c7b5297bd8068
horyu <28817604+horyu@users.noreply.github.com>
2022-02-03 14:00:31 +0900

    abc178 e

--- atcoder/abc178/src/bin/e.rs
+++ atcoder/abc178/src/bin/e.rs
@@ -1,8 +1,24 @@
 #![allow(unused_imports)]
+use itertools::Itertools;
 // use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    input! {};
-    // println!("{}",);
+    input! {
+        n: isize,
+        xxyy: [(isize, isize); n]
+    };
+    use itertools::MinMaxResult;
+    let zz_minmax = xxyy.iter().map(|(x, y)| x + y).minmax();
+    let z = match zz_minmax {
+        MinMaxResult::MinMax(min, max) => max - min,
+        _ => 0,
+    };
+    let ww_minmax = xxyy.iter().map(|(x, y)| x - y).minmax();
+    let w = match ww_minmax {
+        MinMaxResult::MinMax(min, max) => max - min,
+        _ => 0,
+    };
+    let rs = z.max(w);
+    println!("{rs}");
 }
