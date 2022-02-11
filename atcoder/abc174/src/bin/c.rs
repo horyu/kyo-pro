43e1bf9f0006dc06d9900d229fc90dd1b9fd1c9e
horyu <28817604+horyu@users.noreply.github.com>
2022-02-11 11:04:40 +0900

    abc174 c

--- atcoder/abc174/src/bin/c.rs
+++ atcoder/abc174/src/bin/c.rs
@@ -5,6 +5,20 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        k: usize
+    };
+    let mut vv = vec![0; 1e6 as usize + 2];
+    vv[1] = 7 % k;
+    for i in 2..=k {
+        vv[i] = (vv[i - 1] * 10 + 7) % k;
+    }
+    #[allow(clippy::needless_range_loop)]
+    for i in 1..=k {
+        if vv[i] == 0 {
+            println!("{i}");
+            return;
+        }
+    }
+    println!("-1");
 }
