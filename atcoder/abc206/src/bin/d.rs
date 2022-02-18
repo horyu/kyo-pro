7e3b4045f2fc3b2adc39e9f232fab33f7de0f29e
horyu <28817604+horyu@users.noreply.github.com>
2022-02-18 09:02:57 +0900

    abc206 d

--- atcoder/abc206/src/bin/d.rs
+++ atcoder/abc206/src/bin/d.rs
@@ -2,10 +2,25 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use num_integer::*;
+use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    let mut rs = 0;
+    let mut uf = UnionFind::new(1e5 as usize * 2 + 2);
+    let mut l = 0;
+    let mut r = n - 1;
+    while l < r {
+        if uf.union(aa[l], aa[r]) {
+            rs += 1;
+        }
+        l += 1;
+        r -= 1;
+    }
+    println!("{rs}");
 }
