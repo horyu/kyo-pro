ca1ce8eb595ddaa49c7d48d3468591325965aa02
horyu <28817604+horyu@users.noreply.github.com>
2022-02-21 08:50:37 +0900

    abc216 d

--- atcoder/abc216/src/bin/d.rs
+++ atcoder/abc216/src/bin/d.rs
@@ -3,9 +3,48 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+    };
+    let mut aaa = vec![];
+    let mut hm: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut qq = VecDeque::new();
+    for i in 0..m {
+        input! {
+            k: usize,
+            aa: [usize; k],
+        };
+        let last = *aa.last().unwrap();
+        if let Some(e) = hm.get_mut(&last) {
+            e.push(i);
+            qq.push_back(last);
+        } else {
+            hm.insert(last, vec![i]);
+        }
+        aaa.push(aa);
+    }
+
+    let mut cnt = 0;
+    while let Some(q) = qq.pop_front() {
+        cnt += 1;
+        if let Some(ii) = hm.remove(&q) {
+            for i in ii {
+                aaa[i].pop();
+                if let Some(&last) = aaa[i].last() {
+                    if let Some(e) = hm.get_mut(&last) {
+                        e.push(i);
+                        qq.push_back(last);
+                    } else {
+                        hm.insert(last, vec![i]);
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ["No", "Yes"][(cnt == n) as usize]);
 }
