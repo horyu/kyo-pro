3e1677958645794968468ce52b87f183fe11db86
horyu <28817604+horyu@users.noreply.github.com>
2022-02-24 22:39:14 +0900

    abc219 d

--- atcoder/abc219/src/bin/d.rs
+++ atcoder/abc219/src/bin/d.rs
@@ -6,6 +6,27 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        x: usize,
+        y: usize,
+        aabb: [(usize, usize); n]
+    };
+    let mut hm: HashMap<(usize, usize), i32> = HashMap::new();
+    for (a, b) in aabb {
+        let mut new_hm = hm.clone();
+        for ((p, q), c) in hm {
+            let pp = (p + a).min(x);
+            let qq = (q + b).min(y);
+            if let Some(e) = new_hm.get_mut(&(pp, qq)) {
+                *e = (*e).min(c + 1);
+            } else {
+                new_hm.insert((pp, qq), c + 1);
+            }
+        }
+        new_hm.insert((a.min(x), b.min(y)), 1);
+        hm = new_hm;
+    }
+    let rs = hm.get(&(x, y)).unwrap_or(&-1);
+    println!("{rs}");
 }
