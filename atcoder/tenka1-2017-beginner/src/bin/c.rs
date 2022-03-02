bf84f650dddce358e46b578a5dd6eb7917afc604
horyu <28817604+horyu@users.noreply.github.com>
2022-03-02 09:21:09 +0900

    tenka1-2017-beginner c

--- atcoder/tenka1-2017-beginner/src/bin/c.rs
+++ atcoder/tenka1-2017-beginner/src/bin/c.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: isize
+    };
+    // 4xyz = N(xy + yz + zx)
+    // z(4xy - Nx - Ny)) = Nxy
+    // z = Nxy/(4xy - N(x+y))
+    for x in 1..=3500 {
+        for y in x..=3500 {
+            let denominator = 4 * x * y - n * (x + y);
+            if denominator > 0 && n * x * y % denominator == 0 {
+                let z = n * x * y / denominator;
+                println!("{x} {y} {z}");
+                return;
+            }
+        }
+    }
 }
