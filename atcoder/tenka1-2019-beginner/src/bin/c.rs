6f465444793a7ca0151d11bf8d8b65bf30737ad8
horyu <28817604+horyu@users.noreply.github.com>
2022-03-08 08:13:35 +0900

    tenka1-2019-beginner c

--- atcoder/tenka1-2019-beginner/src/bin/c.rs
+++ atcoder/tenka1-2019-beginner/src/bin/c.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        s: Chars
+    };
+    // 全部白にして右から黒を伸ばす
+    let mut rs = s.iter().filter(|&&c| c == '#').count();
+    let mut crr = rs;
+    for c in s.into_iter().rev() {
+        if c == '#' {
+            crr -= 1;
+        } else {
+            crr += 1;
+        }
+        rs = rs.min(crr);
+    }
+    println!("{rs}");
 }
