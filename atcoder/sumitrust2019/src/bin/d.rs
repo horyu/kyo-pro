37bf805b9a8855dd04fb6a9c5c0ceca8481f15a1
horyu <28817604+horyu@users.noreply.github.com>
2022-03-09 09:40:23 +0900

    sumitrust2019 d

--- atcoder/sumitrust2019/src/bin/d.rs
+++ atcoder/sumitrust2019/src/bin/d.rs
@@ -6,6 +6,24 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        s: Chars
+    };
+
+    let mut rs = 0;
+    for mut cc in (0..3).map(|_| '0'..='9').multi_cartesian_product() {
+        for &c in &s {
+            if let Some(&last) = cc.last() {
+                if c == last {
+                    cc.pop();
+                    if cc.is_empty() {
+                        rs += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    println!("{rs}");
 }
