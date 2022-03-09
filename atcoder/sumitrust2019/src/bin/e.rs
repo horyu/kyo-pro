fc5cfe68f3867300a39f0ca54ce1d459a4e8a174
horyu <28817604+horyu@users.noreply.github.com>
2022-03-10 08:26:35 +0900

    sumitrust2019 e

--- atcoder/sumitrust2019/src/bin/e.rs
+++ atcoder/sumitrust2019/src/bin/e.rs
@@ -5,7 +5,25 @@ use num_integer::*;
 use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
+const MOD: usize = 1000000007;
+
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+    if aa[0] > 0 {
+        println!("0");
+        return;
+    }
+    let mut rs = 1;
+    let mut vv = [0usize; 3];
+    for a in aa {
+        let kouho = vv.iter().positions(|&v| v == a).collect_vec();
+        rs = rs * kouho.len() % MOD;
+        if let Some(&i) = kouho.first() {
+            vv[i] += 1;
+        }
+    }
+    println!("{rs}");
 }
