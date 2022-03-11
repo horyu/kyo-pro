1ab4e1c2d5d98d0598a65d1aa127525afce33fd1
horyu <28817604+horyu@users.noreply.github.com>
2022-03-11 09:37:13 +0900

    panasonic2020 d

--- atcoder/panasonic2020/src/bin/d.rs
+++ atcoder/panasonic2020/src/bin/d.rs
@@ -6,6 +6,40 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize
+    };
+    // 1a
+
+    // 1aa
+    // 2ab
+
+    // 1aaa
+    // 2aab,aba,abb
+    // 3abc
+
+    // 1aaaa
+    // 2aaab,aaba,aabb,abaa,abab,abba,abbb
+    // 3aabc,abac,abbc,abca,abcb,abcc
+    // 4abcd
+
+    let mut vvv = vec![(1, vec![0])];
+    for _ in 1..n {
+        let mut next_vvv = vec![];
+        for (i, vv) in vvv {
+            for j in 0..i {
+                let mut tmp = vv.clone();
+                tmp.push(j);
+                next_vvv.push((i, tmp));
+            }
+            let mut tmp = vv.clone();
+            tmp.push(i);
+            next_vvv.push((i + 1, tmp));
+        }
+        vvv = next_vvv;
+    }
+    for (_i, vv) in vvv {
+        // eprintln!("{i}:{}", vv.into_iter().map(|v| (b'a' + v) as char).join(""));
+        println!("{}", vv.into_iter().map(|v| (b'a' + v) as char).join(""));
+    }
 }
