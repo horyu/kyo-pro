0ae95631f3445b2a242604855be4140d123087b9
horyu <28817604+horyu@users.noreply.github.com>
2022-03-14 08:55:26 +0900

    arc002 c

--- atcoder/arc002/src/bin/c.rs
+++ atcoder/arc002/src/bin/c.rs
@@ -5,6 +5,20 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        s: String
+    };
+    let rs = (0..4)
+        .map(|_| ['A', 'B', 'Y', 'X'])
+        .multi_cartesian_product()
+        .map(|cc| {
+            let l = cc[..2].iter().join("");
+            let r = cc[2..].iter().join("");
+            s.replace(&l, "L").replace(&r, "R").len()
+        })
+        .min()
+        .unwrap();
+
+    println!("{rs}");
 }
