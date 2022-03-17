ca0f9b2becc029dfa51a4ce1414fcdc3e370692f
horyu <28817604+horyu@users.noreply.github.com>
2022-03-18 08:56:49 +0900

    arc006 c

--- atcoder/arc006/src/bin/c.rs
+++ atcoder/arc006/src/bin/c.rs
@@ -2,9 +2,19 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ww: [usize; n]
+    };
+    let mut bts = BTreeSet::new();
+    for w in ww {
+        if let Some(&e) = bts.range(w..).next() {
+            bts.remove(&e);
+        }
+        bts.insert(w);
+    }
+    println!("{}", bts.len());
 }
