067b65ec1071324e32f4562a3a0f06c770c177c4
horyu <28817604+horyu@users.noreply.github.com>
2022-03-17 09:36:21 +0900

    arc006 b

--- atcoder/arc006/src/bin/b.rs
+++ atcoder/arc006/src/bin/b.rs
@@ -5,6 +5,25 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    let stdin = std::io::stdin();
+    let mut s = String::new();
+    stdin.read_line(&mut s).unwrap();
+    let mut iter = s.split_whitespace();
+    let n = iter.next().unwrap().parse::<usize>().unwrap();
+    let l = iter.next().unwrap().parse::<usize>().unwrap();
+    let mut ccc = vec![];
+    for _ in 0..(l + 1) {
+        s.clear();
+        stdin.read_line(&mut s).unwrap();
+        ccc.push(s.chars().into_iter().collect_vec());
+    }
+    let mut j = ccc.pop().unwrap().iter().position(|&c| c == 'o').unwrap();
+    while let Some(cc) = ccc.pop() {
+        if j != 0 && cc[j - 1] == '-' {
+            j -= 2;
+        } else if j != 2 * n - 1 && cc[j + 1] == '-' {
+            j += 2;
+        }
+    }
+    println!("{}", j / 2 + 1);
 }
