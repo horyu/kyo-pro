44469e0514887166ecd3998b82295e9beaee67f7
horyu <28817604+horyu@users.noreply.github.com>
2022-03-24 08:37:53 +0900

    arc017 b

--- atcoder/arc017/src/bin/b.rs
+++ atcoder/arc017/src/bin/b.rs
@@ -6,6 +6,27 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n]
+    };
+    if k == 1 {
+        println!("{n}");
+        return;
+    }
+
+    let mut rs = 0;
+    let mut cnt = 0;
+    for (ax, ay) in aa.into_iter().tuple_windows() {
+        if ax < ay {
+            cnt += 1;
+        } else {
+            cnt = 0;
+        }
+        if cnt >= (k - 1) {
+            rs += 1;
+        }
+    }
+    println!("{rs}");
 }
