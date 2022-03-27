e7ede2737af5cb51cc7755286eee7df0f529c307
horyu <28817604+horyu@users.noreply.github.com>
2022-03-27 16:55:33 +0900

    arc023 b

--- atcoder/arc023/src/bin/b.rs
+++ atcoder/arc023/src/bin/b.rs
@@ -6,6 +6,26 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        r: usize,
+        c: usize,
+        d: usize,
+        aaa: [[usize; c]; r]
+    };
+    let mut rs = 0;
+    for (i, aa) in aaa.into_iter().enumerate() {
+        if i > d {
+            break;
+        }
+        for (j, a) in aa.into_iter().enumerate() {
+            if i + j > d {
+                break;
+            }
+            if (i + j) % 2 == d % 2 {
+                rs = rs.max(a);
+            }
+        }
+    }
+
+    println!("{rs}");
 }
