186c7df4d524dd40074cb733fd902839512e92f8
horyu <28817604+horyu@users.noreply.github.com>
2022-03-28 08:37:04 +0900

    arc026 b

--- atcoder/arc026/src/bin/b.rs
+++ atcoder/arc026/src/bin/b.rs
@@ -6,6 +6,23 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize
+    };
+    let mut sum = if n == 1 { 0 } else { 1 };
+    for i in 2..=n.sqrt() {
+        if n % i == 0 {
+            sum += i;
+            if i != n / i {
+                sum += n / i;
+            }
+        }
+    }
+    use std::cmp::Ordering;
+    let rs = match sum.cmp(&n) {
+        Ordering::Less => "Deficient",
+        Ordering::Equal => "Perfect",
+        Ordering::Greater => "Abundant",
+    };
+    println!("{rs}");
 }
