18084f5f192a76aca9cdb499b699dddec76ef781
horyu <28817604+horyu@users.noreply.github.com>
2022-03-30 08:25:44 +0900

    arc032 b

--- atcoder/arc032/src/bin/b.rs
+++ atcoder/arc032/src/bin/b.rs
@@ -2,10 +2,19 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use num_integer::*;
+use petgraph::graph::UnGraph;
 use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(Usize1, Usize1); m]
+    };
+    let mut g = UnGraph::<(), ()>::new_undirected();
+    let nodes = (0..n).map(|_| g.add_node(())).collect_vec();
+    g.extend_with_edges(aabb.iter().map(|&(i, j)| (nodes[i], nodes[j], ())));
+    let rs = petgraph::algo::kosaraju_scc(&g).len() - 1;
+    println!("{rs}");
 }
