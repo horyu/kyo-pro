c7794061290c24e882f8b8070bbb8dc174f2807c
horyu <28817604+horyu@users.noreply.github.com>
2022-03-31 20:03:35 +0900

    arc034 b

--- atcoder/arc034/src/bin/b.rs
+++ atcoder/arc034/src/bin/b.rs
@@ -6,6 +6,28 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
+    input! {
+        n: usize
+    };
+    let mut rs = vec![];
+    for i in n.saturating_sub(200)..=n {
+        if g(i) == n {
+            rs.push(i);
+        }
+    }
+    println!("{}", rs.len());
+    for r in rs {
+        println!("{r}");
+    }
+
     // println!("{}", );
+}
+
+fn g(mut x: usize) -> usize {
+    let mut rs = x;
+    while x > 0 {
+        rs += x % 10;
+        x /= 10;
+    }
+    rs
 }
