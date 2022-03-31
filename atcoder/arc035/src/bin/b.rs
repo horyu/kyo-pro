1401357051c48d0f4e61ff7b57cd5ab1a7834a98
horyu <28817604+horyu@users.noreply.github.com>
2022-04-01 08:37:18 +0900

    arc035 b

--- atcoder/arc035/src/bin/b.rs
+++ atcoder/arc035/src/bin/b.rs
@@ -6,6 +6,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut tt: [usize; n]
+    };
+    let mut cost = 0usize;
+    let mut mul = 1usize;
+    let mut now = 0usize;
+
+    tt.sort_unstable();
+    for (k, g) in tt.into_iter().group_by(|&k| k).into_iter() {
+        let n = g.count();
+        // 初項now+k, 等差k, n個
+        cost += n * (2 * (now + k) + (n - 1) * k) / 2;
+        mul = (1..=n).fold(mul, |acc, x| acc * x % (1e9 as usize + 7));
+        now += k * n;
+    }
+    println!("{cost}\n{mul}");
 }
