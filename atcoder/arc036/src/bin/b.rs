09d47fc42bece6bfe0c9307f314a31d8dadfe7d3
horyu <28817604+horyu@users.noreply.github.com>
2022-04-03 10:26:57 +0900

    arc036 b

--- atcoder/arc036/src/bin/b.rs
+++ atcoder/arc036/src/bin/b.rs
@@ -3,9 +3,39 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        hh: [usize; n]
+    };
+    if n == 1 {
+        println!("1");
+        return;
+    }
+    let kkcc = hh
+        .into_iter()
+        .tuple_windows()
+        .map(|(hx, hy)| hx.cmp(&hy))
+        .group_by(|&k| k)
+        .into_iter()
+        .map(|(k, g)| (k, g.count()))
+        .collect_vec();
+    let mut rs = 1;
+    if kkcc[0].0.is_ne() {
+        rs = rs.max(kkcc[0].1 + 1);
+    }
+    if kkcc[kkcc.len() - 1].0.is_ne() {
+        rs = rs.max(kkcc[kkcc.len() - 1].1 + 1);
+    }
+    for ((kx, cx), (ky, cy)) in kkcc.into_iter().tuple_windows() {
+        if kx.is_le() && ky.is_gt() {
+            rs = rs.max(cx + cy + 1);
+        }
+    }
+    println!("{rs}");
 }
