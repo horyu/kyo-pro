2918abc6d4f682b70bce1202d87f0d3727f751ec
horyu <28817604+horyu@users.noreply.github.com>
2022-04-07 20:43:15 +0900

    arc042 b

--- atcoder/arc042/src/bin/b.rs
+++ atcoder/arc042/src/bin/b.rs
@@ -1,11 +1,25 @@
 #![allow(clippy::many_single_char_names)]
 #![allow(unused_imports)]
 use itertools::Itertools;
-use num_integer::*;
-use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use proconio::input;
 
+// https://qiita.com/kkdd/items/b3c5e06798e59fe2768e
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        px: f64,
+        py: f64,
+        n: usize,
+        xxyy: [(f64, f64); n]
+    };
+    let first = xxyy[0];
+    let rs = xxyy
+        .into_iter()
+        .chain(std::iter::once(first))
+        .tuple_windows()
+        .fold(1e9, |min: f64, ((ax, ay), (bx, by))| {
+            let d = ((px - ax) * (ay - by) - (py - ay) * (ax - bx)).abs()
+                / ((ax - bx).powi(2) + (ay - by).powi(2)).sqrt();
+            min.min(d)
+        });
+    println!("{rs}");
 }
