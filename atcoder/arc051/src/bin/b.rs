7ecc5121d886f134e0fb341881e7170387f59329
horyu <28817604+horyu@users.noreply.github.com>
2022-04-08 09:50:48 +0900

    arc051 b

--- atcoder/arc051/src/bin/b.rs
+++ atcoder/arc051/src/bin/b.rs
@@ -1,4 +1,4 @@
-#![allow(clippy::many_single_char_names)]
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop)]
 #![allow(unused_imports)]
 use itertools::Itertools;
 use num_integer::*;
@@ -6,6 +6,21 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        k: usize
+    };
+    let mut a = 2;
+    let mut b = 1;
+    for _ in 1..k {
+        (a, b) = (a + b, a);
+        // eprintln!("{i} {a} {b}");
+    }
+    println!("{a} {b}");
+    // let mut c = 0;
+    // while b != 0 {
+    //     eprintln!("{c} {a} {b}");
+    //     c += 1;
+    //     (a, b) = (b, a % b);
+    // }
+    // eprintln!("{c} {a} {b}")
 }
