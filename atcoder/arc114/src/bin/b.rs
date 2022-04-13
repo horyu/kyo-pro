25e7b8d3a6b345db27a3dbb075da1b986ba40f4d
horyu <28817604+horyu@users.noreply.github.com>
2022-04-13 09:14:33 +0900

    arc114 b

--- atcoder/arc114/src/bin/b.rs
+++ atcoder/arc114/src/bin/b.rs
@@ -6,6 +6,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ff: [Usize1; n]
+    };
+    let mut uf = petgraph::unionfind::UnionFind::new(n);
+    for (i, f) in ff.into_iter().enumerate() {
+        uf.union(i, f);
+    }
+    let hs = uf.into_labeling().into_iter().collect::<HashSet<_>>();
+    let mut rs = 1;
+    for _ in 0..hs.len() {
+        rs = rs * 2 % 998244353;
+    }
+    rs = (rs + 998244353 - 1) % 998244353;
+    println!("{rs}");
 }
