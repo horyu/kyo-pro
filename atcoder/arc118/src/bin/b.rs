10c7e7707f391d53c18a1a2aea5401902c24a11e
horyu <28817604+horyu@users.noreply.github.com>
2022-04-15 21:31:21 +0900

    arc118 b

--- atcoder/arc118/src/bin/b.rs
+++ atcoder/arc118/src/bin/b.rs
@@ -6,6 +6,32 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        k: usize,
+        n: usize,
+        m: usize,
+        aa: [usize; k]
+    };
+    // b/M = a/N, bN=aM
+    // b = aM/N
+    let mut iibbdd = aa
+        .iter()
+        .enumerate()
+        .map(|(i, &a)| {
+            let f = (a * m) as f64 / n as f64;
+            (i, f.floor() as usize, f - f.floor())
+        })
+        .sorted_by(|x, y| x.2.partial_cmp(&y.2).unwrap())
+        .collect_vec();
+    let sum = iibbdd.iter().map(|ibd| ibd.1).sum::<usize>();
+    iibbdd
+        .iter_mut()
+        .rev()
+        .take(m - sum)
+        .for_each(|ibd| ibd.1 += 1);
+    let mut rs = vec![0; k];
+    for (i, b, _d) in iibbdd {
+        rs[i] = b;
+    }
+    println!("{}", rs.into_iter().join(" "));
 }
