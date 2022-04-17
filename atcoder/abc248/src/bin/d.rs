b2342c48d70395d159d7e16412edcb24570f1b55
horyu <28817604+horyu@users.noreply.github.com>
2022-04-17 15:15:43 +0900

    abc248 d

--- atcoder/abc248/src/bin/d.rs
+++ atcoder/abc248/src/bin/d.rs
@@ -22,4 +22,41 @@ fn main() {
         let rs = ri - li;
         println!("{rs}");
     }
+    // Mo's algorithm
+    /*
+    let size = 200000.sqrt();
+    let mut qqq = vec![vec![]; size + 2];
+    for (i, (l, r, x)) in llrrxx.into_iter().enumerate() {
+        let l = l - 1;
+        qqq[l / size].push((r, l, x, i));
+    }
+    let mut rrss = vec![0; q];
+    for mut qq in qqq {
+        if qq.is_empty() {
+            continue;
+        }
+        qq.sort_unstable();
+        let mut l = qq[0].1;
+        let mut r = l;
+        let mut vv = vec![0; n];
+        for (qr, ql, qx, qi) in qq {
+            while qr > r {
+                vv[aa[r]] += 1;
+                r += 1;
+            }
+            while ql > l {
+                vv[aa[l]] -= 1;
+                l += 1;
+            }
+            while ql < l {
+                l -= 1;
+                vv[aa[l]] += 1;
+            }
+            rrss[qi] = vv[qx];
+        }
+    }
+    for rs in rrss {
+        println!("{rs}");
+    }
+     */
 }
