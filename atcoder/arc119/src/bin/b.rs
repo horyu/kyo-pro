002b60e1e30db8533ed101055d5c32137b6624b8
horyu <28817604+horyu@users.noreply.github.com>
2022-04-19 22:09:59 +0900

    arc119 b

--- atcoder/arc119/src/bin/b.rs
+++ atcoder/arc119/src/bin/b.rs
@@ -3,9 +3,28 @@
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        s: Chars,
+        t: Chars,
+    };
+    let ss = s
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, c)| if c == '0' { Some(i) } else { None })
+        .collect_vec();
+    let tt = t
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, c)| if c == '0' { Some(i) } else { None })
+        .collect_vec();
+    if ss.len() != tt.len() {
+        println!("-1");
+    } else {
+        let rs = std::iter::zip(ss, tt).filter(|(i, j)| i != j).count();
+        println!("{rs}");
+    }
 }
