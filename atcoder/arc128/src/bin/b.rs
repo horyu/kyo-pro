f230324ade13e56f0916ead8de518960d2ea3dbe
horyu <28817604+horyu@users.noreply.github.com>
2022-04-26 23:03:02 +0900

    arc128 b

--- atcoder/arc128/src/bin/b.rs
+++ atcoder/arc128/src/bin/b.rs
@@ -6,6 +6,27 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        t: usize,
+        mut xxyyzz: [[usize; 3]; t]
+    };
+    for xyz in xxyyzz {
+        let mut rs = std::usize::MAX;
+        for i in 0..3 {
+            let vv = xyz
+                .iter()
+                .enumerate()
+                .filter_map(|(j, &v)| if i != j { Some(v) } else { None })
+                .sorted()
+                .collect_vec();
+            if vv[0] % 3 == vv[1] % 3 {
+                rs = rs.min(vv[1]);
+            }
+        }
+        if rs == std::usize::MAX {
+            println!("-1");
+        } else {
+            println!("{rs}");
+        }
+    }
 }
