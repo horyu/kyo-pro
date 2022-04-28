2997fd09ed12f70349a58a9025ac32f6aaa8415a
horyu <28817604+horyu@users.noreply.github.com>
2022-04-28 10:35:14 +0900

    arc129 b

--- atcoder/arc129/src/bin/b.rs
+++ atcoder/arc129/src/bin/b.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop)]
 #![allow(unused_imports)]
+#![feature(int_roundings)]
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
@@ -7,6 +8,19 @@ use std::collections::*;
 
 fn main() {
     input! {
+        n: usize,
+        llrr: [(usize, usize); n]
     };
-    // println!("{}", );
+    let mut ll = 0;
+    let mut rr = std::usize::MAX;
+    for (l, r) in llrr {
+        ll = ll.max(l);
+        rr = rr.min(r);
+        let rs = if ll <= rr {
+            0
+        } else {
+            (ll - rr).div_ceil(2)
+        };
+        println!("{rs}");
+    }
 }
