b3258cd5c1ccaa856d02af6d5886073655b58dc9
horyu <28817604+horyu@users.noreply.github.com>
2022-05-06 09:07:22 +0900

    arc138 b

--- atcoder/arc138/src/bin/b.rs
+++ atcoder/arc138/src/bin/b.rs
@@ -5,6 +5,34 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [u8; n]
+    };
+    let mut aa: VecDeque<_> = aa.into_iter().collect();
+    let mut rev = false;
+    while let Some(a) = aa.pop_front() {
+        if !rev {
+            if a == 0 {
+                rev = !rev;
+            } else {
+                println!("No");
+                return;
+            }
+            while let Some(&0) = aa.back() {
+                aa.pop_back();
+            }
+        } else {
+            if a == 1 {
+                rev = !rev;
+            } else {
+                println!("No");
+                return;
+            }
+            while let Some(&1) = aa.back() {
+                aa.pop_back();
+            }
+        }
+    }
+    println!("Yes");
 }
