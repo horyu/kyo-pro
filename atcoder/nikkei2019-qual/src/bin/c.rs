38e4afeaddf840700b05bcd62aa4a33e7c2069ce
horyu <28817604+horyu@users.noreply.github.com>
2022-05-08 09:03:29 +0900

    nikkei2019-qual c

--- atcoder/nikkei2019-qual/src/bin/c.rs
+++ atcoder/nikkei2019-qual/src/bin/c.rs
@@ -6,6 +6,18 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aabb: [(isize, isize); n]
+    };
+    aabb.sort_unstable_by_key(|ab| ab.0 + ab.1);
+    let mut x = 0;
+    let mut y = 0;
+    while let Some(ab) = aabb.pop() {
+        x += ab.0;
+        if let Some(ab) = aabb.pop() {
+            y += ab.1;
+        }
+    }
+    println!("{}", x - y);
 }
