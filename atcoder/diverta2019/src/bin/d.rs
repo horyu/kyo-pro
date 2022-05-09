ee700ff12bde8fe28a5c687e9235a7fd81c0cdc3
horyu <28817604+horyu@users.noreply.github.com>
2022-05-10 08:58:51 +0900

    diverta2019 d

--- atcoder/diverta2019/src/bin/d.rs
+++ atcoder/diverta2019/src/bin/d.rs
@@ -6,6 +6,19 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+    };
+    let mut rs = 0;
+    for i in 1usize..=n.sqrt() {
+        let nn = n - i;
+        let m = nn / i;
+        if m == 0 {
+            break;
+        }
+        if n / m == n % m {
+            rs += m;
+        }
+    }
+    println!("{rs}");
 }
