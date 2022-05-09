682c4ee47d1eacff5a1c18b272cfa39655155ce5
horyu <28817604+horyu@users.noreply.github.com>
2022-05-09 09:03:05 +0900

    diverta2019 c

--- atcoder/diverta2019/src/bin/c.rs
+++ atcoder/diverta2019/src/bin/c.rs
@@ -6,6 +6,28 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ss: [String; n]
+    };
+    let (mut l, mut c, mut r, mut lr) = (0, 0, 0, 0);
+    for s in ss {
+        let b = s.starts_with('B');
+        let a = s.ends_with('A');
+        if b {
+            l += 1;
+        }
+        if a {
+            r += 1;
+        }
+        if b && a {
+            lr += 1;
+        }
+        c += s.matches("AB").count();
+    }
+    let mut rs = c + l.min(r);
+    if lr > 0 && lr == l && l == r {
+        rs -= 1;
+    }
+    println!("{rs}");
 }
