40612383a1f18263f91ecdf9e2ae4a786013cbeb
horyu <28817604+horyu@users.noreply.github.com>
2022-05-12 09:16:00 +0900

    jsc2019-qual b

--- atcoder/jsc2019-qual/src/bin/b.rs
+++ atcoder/jsc2019-qual/src/bin/b.rs
@@ -5,7 +5,33 @@ use num_integer::*;
 use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
+const MOD: u128 = 1e9 as u128 + 7;
+
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: u128,
+        aa: [u128; n],
+    };
+    let mut ll = vec![0u128; n];
+    let mut rr = vec![0u128; n];
+    for (i, ai) in aa.iter().enumerate() {
+        for aj in &aa[..i] {
+            if aj > ai {
+                ll[i] += 1;
+            }
+        }
+        for aj in &aa[(i + 1)..] {
+            if aj > ai {
+                rr[i] += 1;
+            }
+        }
+    }
+    let mut rs = 0;
+    for (l, r) in std::iter::zip(ll, rr) {
+        // l, lrl, lrlrl, ...
+        rs += ((2 * l + (r + l) * (k - 1)) * k / 2) % MOD;
+        rs %= MOD;
+    }
+    println!("{rs}");
 }
