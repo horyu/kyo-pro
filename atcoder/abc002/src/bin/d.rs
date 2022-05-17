818dc873f0b0b1bd6d2a354f0def14818961ffb9
horyu <28817604+horyu@users.noreply.github.com>
2022-05-18 08:32:58 +0900

    abc002 d

--- atcoder/abc002/src/bin/d.rs
+++ atcoder/abc002/src/bin/d.rs
@@ -1,7 +1,28 @@
 #![allow(unused_imports)]
-// use itertools::Itertools;
+#![allow(clippy::needless_range_loop)]
+use itertools::Itertools;
 use proconio::{input, marker::Usize1};
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        m: usize,
+        xxyy: [(Usize1, Usize1); m]
+    };
+    let mut vvv = vec![vec![false; n]; n];
+    for i in 0..n {
+        vvv[i][i] = true;
+    }
+    for (x, y) in xxyy {
+        vvv[x][y] = true;
+        vvv[y][x] = true;
+    }
+    for size in (1..=n).rev() {
+        for ii in (0..n).combinations(size) {
+            if ii.iter().all(|&i| ii.iter().all(|&j| vvv[i][j])) {
+                println!("{size}");
+                return;
+            }
+        }
+    }
 }
