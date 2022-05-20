08d587d9ec759cd3ba3cb18a189f176d370906c3
horyu <28817604+horyu@users.noreply.github.com>
2022-05-20 09:10:21 +0900

    typical90 typical90-002

--- atcoder/typical90/src/bin/002.rs
+++ atcoder/typical90/src/bin/002.rs
@@ -6,6 +6,31 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize
+    };
+    if n.is_odd() {
+        println!();
+        return;
+    }
+    // BFS
+    // (現在のString, 何回開けられるか、何回閉じられるか)
+    let mut rs = vec![];
+    let mut qq = VecDeque::new();
+    qq.push_back((String::new(), n / 2, 0usize));
+    while let Some((s, open, close)) = qq.pop_front() {
+        if open + close == 0 {
+            rs.push(s);
+            continue;
+        }
+        if open > 0 {
+            qq.push_back((format!("{}(", s), open - 1, close + 1));
+        }
+        if close > 0 {
+            qq.push_back((format!("{})", s), open, close - 1));
+        }
+    }
+    for r in rs {
+        println!("{r}");
+    }
 }
