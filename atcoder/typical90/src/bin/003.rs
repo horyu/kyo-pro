82b6a6a5981443d8c66f88045b41a6d40a306f2c
horyu <28817604+horyu@users.noreply.github.com>
2022-05-21 10:40:08 +0900

    typical90 typical90-003

--- atcoder/typical90/src/bin/003.rs
+++ atcoder/typical90/src/bin/003.rs
@@ -6,6 +6,34 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aabb: [(Usize1, Usize1); n - 1]
+    };
+    let mut vvv = vec![vec![]; n];
+    for (a, b) in aabb {
+        vvv[a].push(b);
+        vvv[b].push(a);
+    }
+    // 木の直径
+    let bfs = |mut pos: usize| -> (usize, usize) {
+        let mut max = 0;
+        let mut qq = VecDeque::new();
+        qq.push_back((!0, pos, 0));
+        while let Some((from, to, cost)) = qq.pop_front() {
+            if max < cost {
+                max = cost;
+                pos = to;
+            }
+            for &v in &vvv[to] {
+                if v != from {
+                    qq.push_back((to, v, cost + 1));
+                }
+            }
+        }
+        (max, pos)
+    };
+    let tmp = bfs(0);
+    let tmp = bfs(tmp.1);
+    println!("{}", tmp.0 + 1);
 }
