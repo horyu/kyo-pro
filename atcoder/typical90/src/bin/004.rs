e09da7955a0f043d80aa9e3597e16690353d0035
horyu <28817604+horyu@users.noreply.github.com>
2022-05-22 15:50:53 +0900

    typical90 004

--- atcoder/typical90/src/bin/004.rs
+++ atcoder/typical90/src/bin/004.rs
@@ -6,6 +6,23 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        h: usize,
+        w: usize,
+        aaa: [[usize; w]; h]
+    };
+    let tate = (0..w)
+        .map(|j| (0..h).map(|i| aaa[i][j]).sum::<usize>())
+        .collect_vec();
+    let yoko = (0..h)
+        .map(|i| (0..w).map(|j| aaa[i][j]).sum::<usize>())
+        .collect_vec();
+    for (i, aa) in aaa.into_iter().enumerate() {
+        let rs = aa
+            .into_iter()
+            .enumerate()
+            .map(|(j, a)| yoko[i] + tate[j] - a)
+            .join(" ");
+        println!("{rs}");
+    }
 }
