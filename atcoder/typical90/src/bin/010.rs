04688a549cac10d715cc042d6468424bec5eb76b
horyu <28817604+horyu@users.noreply.github.com>
2022-05-25 08:20:33 +0900

    typical90 010

--- atcoder/typical90/src/bin/010.rs
+++ atcoder/typical90/src/bin/010.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        ccpp: [(Usize1, usize); n],
+        q: usize,
+        llrr: [(usize, usize); q]
+    };
+    let mut vvv = vec![vec![0; n + 1];2 ];
+    for (i, (c, p)) in ccpp.into_iter().enumerate() {
+        for vv in vvv.iter_mut() {
+            vv[i + 1] = vv[i];
+        }
+        vvv[c][i + 1] += p;
+    }
+    for (l, r) in llrr {
+        println!("{} {}", vvv[0][r] - vvv[0][l - 1], vvv[1][r] - vvv[1][l - 1],);
+    }
 }
