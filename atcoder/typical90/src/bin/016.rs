38b180c739a10b69398a75c4a4b4151d16669969
horyu <28817604+horyu@users.noreply.github.com>
2022-05-28 09:58:15 +0900

    typical90 016

--- atcoder/typical90/src/bin/016.rs
+++ atcoder/typical90/src/bin/016.rs
@@ -6,6 +6,21 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: isize,
+        a: isize,
+        b: isize,
+        c: isize,
+    };
+    let mut rs = std::isize::MAX;
+    for i in 0..=((n / a).min(9999)) {
+        let nn = n - a * i;
+        for j in 0..=((nn / b).min(9999 - i)) {
+            let k = (nn - b * j) / c;
+            if i + j + k <= 9999 && b * j + c * k == nn {
+                rs = rs.min(i + j + k);
+            }
+        }
+    }
+    println!("{rs}");
 }
