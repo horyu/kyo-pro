761a2605edf697cd4381ec7f7fcc190ebf4ad3b3
horyu <28817604+horyu@users.noreply.github.com>
2022-05-28 23:09:36 +0900

    abc253 e

--- atcoder/abc253/src/bin/e.rs
+++ atcoder/abc253/src/bin/e.rs
@@ -13,11 +13,6 @@ fn main() {
         m: usize,
         k: usize,
     };
-    if k == 0 {
-        let rs = mod_pow(m, n, MOD);
-        println!("{rs}");
-        return;
-    }
     let mut dp = vec![1; m];
     for _ in 1..n {
         let mut new_dp = vec![0; m];
@@ -26,23 +21,15 @@ fn main() {
             ss.push((ss[i] + dp[i]) % MOD);
         }
         for i in 0..m {
-            new_dp[i] = (MOD + ss[m] - ss[(i + k).min(m)] + ss[(i + 1).saturating_sub(k)]) % MOD;
+            if k == 0 {
+                new_dp[i] = ss[m];
+            } else {
+                new_dp[i] =
+                    (MOD + ss[m] - ss[(i + k).min(m)] + ss[(i + 1).saturating_sub(k)]) % MOD;
+            }
         }
         dp = new_dp;
     }
     let rs = dp.into_iter().fold(0, |acc, x| (acc + x) % MOD);
     println!("{rs}");
-}
-
-// https://blog.spiralray.net/cp/modulo#i-8
-fn mod_pow(mut x: usize, mut n: usize, m: usize) -> usize {
-    let mut ans = 1;
-    while n != 0 {
-        if n.is_odd() {
-            ans = ans * x % m;
-        }
-        x = x * x % m;
-        n >>= 1;
-    }
-    ans
 }
