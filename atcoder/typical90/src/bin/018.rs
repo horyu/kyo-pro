1a03b62b7c54bb7941e1cf439f3d6e2f36a7a627
horyu <28817604+horyu@users.noreply.github.com>
2022-05-29 11:09:16 +0900

    typical90 018

--- atcoder/typical90/src/bin/018.rs
+++ atcoder/typical90/src/bin/018.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+        ee: [f64; q],
+    };
+    for e in ee {
+        let theta = e * 2.0 / t * std::f64::consts::PI;
+        let pos = (0.0, -l / 2.0 * theta.sin(), l / 2.0 * (1.0 - theta.cos()));
+        let xy = ((x - pos.0).powi(2) + (y - pos.1).powi(2)).sqrt();
+        let tan = pos.2 / xy;
+        let rs = tan.atan() * 180.0 / std::f64::consts::PI;
+        println!("{rs}");
+    }
 }
