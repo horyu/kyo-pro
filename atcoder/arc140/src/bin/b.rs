51b6c9bc493f695c05a1bac53404cbac40c301fa
horyu <28817604+horyu@users.noreply.github.com>
2022-05-30 09:10:42 +0900

    arc140 b

--- atcoder/arc140/src/bin/b.rs
+++ atcoder/arc140/src/bin/b.rs
@@ -6,6 +6,71 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        _n: usize,
+        cc: Chars,
+    };
+    // AARCC
+    // ARC
+    // R
+    // 連続した A+の数,Rの数,C+の数
+    let mut arr = [0; 3];
+    // 塊の数
+    let mut bts = BTreeSet::new();
+    for (i, c) in cc.into_iter().enumerate() {
+        match c {
+            'A' => {
+                if arr[1] == 0 && arr[2] == 0 {
+                    arr[0] += 1;
+                } else {
+                    if arr[2] > 0 {
+                        bts.insert((arr[0].min(arr[2]), i));
+                    }
+                    arr = [1, 0, 0];
+                }
+            }
+            'R' => {
+                if arr[0] > 0 && arr[1] == 0 {
+                    arr[1] = 1;
+                } else {
+                    if arr[2] > 0 {
+                        bts.insert((arr[0].min(arr[2]), i));
+                    }
+                    arr = [0; 3];
+                }
+            }
+            'C' => {
+                if arr[0] > 0 && arr[1] == 1 {
+                    arr[2] += 1;
+                } else {
+                    if arr[2] > 0 {
+                        bts.insert((arr[0].min(arr[2]), i));
+                    }
+                    arr = [0; 3];
+                }
+            }
+            _ => {
+                arr = [0; 3];
+            }
+        }
+    }
+    if arr[2] > 0 {
+        bts.insert((arr[0].min(arr[2]), 0));
+    }
+
+    let mut rs = 0;
+    while !bts.is_empty() {
+        rs += 1;
+        if rs.is_odd() {
+            if let Some(&(k, i)) = bts.iter().max() {
+                bts.remove(&(k, i));
+                if k > 1 {
+                    bts.insert((k - 1, i));
+                }
+            }
+        } else if let Some(&(k, i)) = bts.iter().min() {
+            bts.remove(&(k, i));
+        }
+    }
+    println!("{rs}");
 }
