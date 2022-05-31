3430b7d1edef686d3e4dd5d5f98dd97446927bfd
horyu <28817604+horyu@users.noreply.github.com>
2022-06-01 08:08:20 +0900

    typical90 024

--- atcoder/typical90/src/bin/024.rs
+++ atcoder/typical90/src/bin/024.rs
@@ -6,6 +6,13 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n],
+        bb: [usize; n],
+    };
+    let diff = std::iter::zip(aa, bb).fold(0, |acc, (a, b)| acc + a.abs_diff(b));
+    let tf = diff <= k && (k - diff).is_even();
+    println!("{}", ["No", "Yes"][tf as usize]);
 }
