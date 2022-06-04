ff2facccc2ea5cd97f9c6bc6335a4bd9ed6ef882
horyu <28817604+horyu@users.noreply.github.com>
2022-06-04 10:20:20 +0900

    typical90 032

--- atcoder/typical90/src/bin/032.rs
+++ atcoder/typical90/src/bin/032.rs
@@ -6,6 +6,34 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aaa: [[usize; n]; n],
+        m: usize,
+        xxyy: [(Usize1, Usize1); m],
+    };
+    let mut ng = vec![vec![false; n]; n];
+    for (x, y) in xxyy {
+        ng[x][y] = true;
+        ng[y][x] = true;
+    }
+    let rs_ott = (0..n)
+        .permutations(n)
+        .flat_map(|xx| {
+            if xx.iter().tuple_windows().any(|(&x, &y)| ng[x][y]) {
+                None
+            } else {
+                let sum = xx
+                    .into_iter()
+                    .enumerate()
+                    .fold(0, |acc, (i, x)| acc + aaa[x][i]);
+                Some(sum)
+            }
+        })
+        .min();
+    if let Some(rs) = rs_ott {
+        println!("{rs}");
+    } else {
+        println!("-1");
+    }
 }
