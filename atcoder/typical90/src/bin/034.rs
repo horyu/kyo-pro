6ead3d483fd5cdbfb30ccd10efd347880f7f392c
horyu <28817604+horyu@users.noreply.github.com>
2022-06-06 08:34:19 +0900

    typical90 034

--- atcoder/typical90/src/bin/034.rs
+++ atcoder/typical90/src/bin/034.rs
@@ -6,6 +6,28 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n]
+    };
+    let mut hm = HashMap::new();
+    let mut l = 0;
+    let mut rs = 0;
+    for r in 0..n {
+        *hm.entry(aa[r]).or_insert(0) += 1;
+        while hm.len() > k {
+            let al = aa[l];
+            if let Some(e) = hm.get_mut(&al) {
+                if *e == 1 {
+                    hm.remove(&al);
+                } else {
+                    *e -= 1;
+                }
+            }
+            l += 1;
+        }
+        rs = rs.max(r - l + 1);
+    }
+    println!("{rs}");
 }
