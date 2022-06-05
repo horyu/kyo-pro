4492c6597ba78957680044ac00d4d91a6d080397
horyu <28817604+horyu@users.noreply.github.com>
2022-06-05 12:58:02 +0900

    typical90 033

--- atcoder/typical90/src/bin/033.rs
+++ atcoder/typical90/src/bin/033.rs
@@ -1,11 +1,20 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop)]
 #![allow(unused_imports)]
+#![feature(int_roundings)]
 use itertools::Itertools;
 use num_integer::*;
 use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        h: usize,
+        w: usize,
+    };
+    let rs = if h == 1 || w == 1 {
+        h * w
+    } else {
+        h.div_ceil(2) * w.div_ceil(2)
+    };
+    println!("{rs}");
 }
