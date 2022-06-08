01ca98829f790d60a103dd755a48d24f8254ae56
horyu <28817604+horyu@users.noreply.github.com>
2022-06-08 21:07:21 +0900

    typical90 042

--- atcoder/typical90/src/bin/042.rs
+++ atcoder/typical90/src/bin/042.rs
@@ -5,7 +5,22 @@ use num_integer::*;
 use proconio::{input, marker::*};
 use std::collections::*;
 
+const MOD: usize = 10usize.pow(9u32) + 7;
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        k: usize,
+    };
+    if k % 9 != 0 {
+        println!("0");
+        return;
+    }
+    let mut dp = vec![0; k + 1];
+    dp[0] = 1;
+    for i in 1..=k {
+        for j in 1..=i.min(9) {
+            dp[i] += dp[i - j];
+            dp[i] %= MOD;
+        }
+    }
+    println!("{}", dp[k]);
 }
