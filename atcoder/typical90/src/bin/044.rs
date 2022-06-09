70fad9a79b5d5470b706a3dd3bae9ef716d4348a
horyu <28817604+horyu@users.noreply.github.com>
2022-06-10 08:26:58 +0900

    typical90 044

--- atcoder/typical90/src/bin/044.rs
+++ atcoder/typical90/src/bin/044.rs
@@ -6,6 +6,24 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        q: usize,
+        mut aa: [usize; n],
+        ttxxyy: [(u8, usize, usize); q],
+    };
+    let mut zure = 0usize;
+    for (t, x, y) in ttxxyy {
+        match t {
+            1 => {
+                aa.swap((x - 1 + n - zure) % n, (y - 1 + n - zure) % n);
+            }
+            2 => {
+                zure = (zure + 1) % n;
+            }
+            _ => {
+                println!("{}", aa[(x - 1 + n - zure) % n]);
+            }
+        }
+    }
 }
