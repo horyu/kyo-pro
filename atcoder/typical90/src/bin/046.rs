58e609899c2efa304159586aaf6728099dc08b95
horyu <28817604+horyu@users.noreply.github.com>
2022-06-11 10:38:34 +0900

    typical90 046

--- atcoder/typical90/src/bin/046.rs
+++ atcoder/typical90/src/bin/046.rs
@@ -6,6 +6,22 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        xxx: [[usize; n]; 3],
+    };
+    let mut vvv = [[0usize; 46]; 3];
+    for (i, xx) in xxx.into_iter().enumerate() {
+        for x in xx {
+            vvv[i][x % 46] += 1;
+        }
+    }
+    let mut rs = 0;
+    for i in 0..46 {
+        for j in 0..46 {
+            let k = (46 - (i + j) % 46) % 46;
+            rs += vvv[0][i] * vvv[1][j] * vvv[2][k];
+        }
+    }
+    println!("{rs}");
 }
