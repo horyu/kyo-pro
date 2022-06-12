5aa49d0f47eb59cf672f8a84e81879e25e6ab3d2
horyu <28817604+horyu@users.noreply.github.com>
2022-06-13 07:30:10 +0900

    typical90 050

--- atcoder/typical90/src/bin/050.rs
+++ atcoder/typical90/src/bin/050.rs
@@ -6,6 +6,18 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        l: usize,
+    };
+    let mut dp = vec![0usize; n + 1];
+    dp[0] = 1;
+    for i in 1..=n {
+        dp[i] += dp[i - 1];
+        if l <= i {
+            dp[i] += dp[i - l];
+        }
+        dp[i] %= 1e9 as usize + 7;
+    }
+    println!("{}", dp[n]);
 }
