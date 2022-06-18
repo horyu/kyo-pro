64785509bacba035783c7963739968b67ca9fc52
horyu <28817604+horyu@users.noreply.github.com>
2022-06-18 09:29:40 +0900

    typical90 063

--- atcoder/typical90/src/bin/063.rs
+++ atcoder/typical90/src/bin/063.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        h: usize,
+        w: usize,
+        ppp: [[usize; w]; h]
+    };
+    let mut rs = 0usize;
+    for ii in (1..=h).flat_map(|size| (0..h).combinations(size)) {
+        let mut vv = vec![0; h * w + 1];
+        for j in 0..w {
+            if ii.iter().map(|&i| ppp[i][j]).all_equal() {
+                vv[ppp[ii[0]][j]] += ii.len();
+            }
+        }
+        rs = rs.max(vv.into_iter().max().unwrap_or_default());
+    }
+    println!("{rs}");
 }
