9d70292459684875254cc8d504381b1390b46085
horyu <28817604+horyu@users.noreply.github.com>
2022-06-19 10:37:02 +0900

    typical90 064

--- atcoder/typical90/src/bin/064.rs
+++ atcoder/typical90/src/bin/064.rs
@@ -6,6 +6,33 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        q: usize,
+        aa: [isize; n],
+        llrrvv: [(Usize1, Usize1, isize); q]
+    };
+    if n == 1 {
+        print!("{}", "0\n".to_string().repeat(q));
+        return;
+    }
+    let mut dd = vec![0];
+    let mut e = 0;
+    for (ai, aj) in aa.iter().tuple_windows() {
+        dd.push(aj - ai);
+        e += (aj - ai).abs();
+    }
+    dd.push(0);
+    for (l, r, v) in llrrvv {
+        let mae = dd[l].abs() + dd[r + 1].abs();
+        if l > 0 {
+            dd[l] += v;
+        }
+        if r < n - 1 {
+            dd[r + 1] -= v;
+        }
+        let ato = dd[l].abs() + dd[r + 1].abs();
+        e += ato - mae;
+        println!("{e}");
+    }
 }
