3c483bb3c5dc913797c62d642a5bafdd3c520c10
horyu <28817604+horyu@users.noreply.github.com>
2022-06-20 09:10:30 +0900

    typical90 067

--- atcoder/typical90/src/bin/067.rs
+++ atcoder/typical90/src/bin/067.rs
@@ -6,6 +6,21 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: String,
+        k: usize,
+    };
+    let mut rs = usize::from_str_radix(&n, 8).unwrap();
+    for _ in 0..k {
+        let mut vv = vec![];
+        while rs != 0 {
+            vv.push(rs % 9);
+            rs /= 9;
+        }
+        rs = vv
+            .into_iter()
+            .rev()
+            .fold(0, |acc, v| acc * 8 + if v == 8 { 5 } else { v });
+    }
+    println!("{rs:o}");
 }
