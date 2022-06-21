266ef1e2024e6a3273d69c56f99fd54f36d209c7
horyu <28817604+horyu@users.noreply.github.com>
2022-06-22 08:26:43 +0900

    typical90 070

--- atcoder/typical90/src/bin/070.rs
+++ atcoder/typical90/src/bin/070.rs
@@ -6,6 +6,25 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut xxyy: [(isize, isize); n]
+    };
+    xxyy.sort_unstable_by_key(|xy| xy.0);
+    let cx = if n.is_odd() {
+        xxyy[n / 2].0
+    } else {
+        (xxyy[n / 2 - 1].0 + xxyy[n / 2].0) / 2
+    };
+    xxyy.sort_unstable_by_key(|xy| xy.1);
+    let cy = if n.is_odd() {
+        xxyy[n / 2].1
+    } else {
+        (xxyy[n / 2 - 1].1 + xxyy[n / 2].1) / 2
+    };
+    let mut rs = 0;
+    for (x, y) in xxyy {
+        rs += (x - cx).abs() + (y - cy).abs();
+    }
+    println!("{rs}");
 }
