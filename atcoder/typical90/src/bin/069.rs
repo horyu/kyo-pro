c6c45643ed4ae966372d0faf2545ebc50633b83a
horyu <28817604+horyu@users.noreply.github.com>
2022-06-21 09:53:44 +0900

    typical90 069

--- atcoder/typical90/src/bin/069.rs
+++ atcoder/typical90/src/bin/069.rs
@@ -6,6 +6,25 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+    };
+    const MOD: usize = 1e9 as usize + 7;
+    let mut rs = (0..(n.min(2))).fold(1, |acc, i| acc * k.saturating_sub(i) % MOD);
+    rs = rs * mod_pow(k.saturating_sub(2), n.saturating_sub(2), MOD) % MOD;
+    println!("{rs}");
+}
+
+// https://blog.spiralray.net/cp/modulo#i-8
+fn mod_pow(mut x: usize, mut n: usize, m: usize) -> usize {
+    let mut ans = 1;
+    while n != 0 {
+        if n.is_odd() {
+            ans = ans * x % m;
+        }
+        x = x * x % m;
+        n >>= 1;
+    }
+    ans
 }
