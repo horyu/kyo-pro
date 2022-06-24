7841b2b1ac258bb2275d17716e4ec0cf39cfe9d0
horyu <28817604+horyu@users.noreply.github.com>
2022-06-24 09:08:31 +0900

    typical90 075

--- atcoder/typical90/src/bin/075.rs
+++ atcoder/typical90/src/bin/075.rs
@@ -6,6 +6,38 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+    };
+    let hm = factors(n);
+    let sum = hm.values().sum::<usize>();
+    for i in 0.. {
+        if sum <= (1 << i) {
+            println!("{i}");
+            return;
+        }
+    }
+}
+
+fn factors(mut n: usize) -> HashMap<usize, usize> {
+    let mut hm = HashMap::new();
+    if n <= 1 {
+        return hm;
+    }
+    while n % 2 == 0 {
+        n /= 2;
+        *hm.entry(2).or_insert(0) += 1;
+    }
+    let mut i = 3;
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+            *hm.entry(i).or_insert(0) += 1;
+        }
+        i += 2;
+    }
+    if n != 1 {
+        hm.insert(n, 1);
+    }
+    hm
 }
