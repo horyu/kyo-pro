2961f5c6c75520c66b8be3fa9d7987f62730f6fa
horyu <28817604+horyu@users.noreply.github.com>
2022-06-25 10:07:31 +0900

    typical90 076

--- atcoder/typical90/src/bin/076.rs
+++ atcoder/typical90/src/bin/076.rs
@@ -6,6 +6,29 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aa: [usize; n],
+    };
+    let sum = aa.iter().sum::<usize>();
+    if sum % 10 != 0 {
+        println!("No");
+        return;
+    }
+    let aa = aa.repeat(2);
+    let target = sum / 10;
+    let mut crr = 0;
+    let mut l = 0;
+    for r in 0..(2 * n) {
+        crr += aa[r];
+        if crr == target {
+            println!("Yes");
+            return;
+        }
+        while l < r && target < crr {
+            crr -= aa[l];
+            l += 1;
+        }
+    }
+    println!("No");
 }
