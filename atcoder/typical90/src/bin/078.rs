3f7dabf2d3f28cf95972ab664db26920eeef9658
horyu <28817604+horyu@users.noreply.github.com>
2022-06-26 08:36:10 +0900

    typical90 078

--- atcoder/typical90/src/bin/078.rs
+++ atcoder/typical90/src/bin/078.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(Usize1, Usize1); m]
+    };
+    let mut vvv = vec![vec![]; n];
+    for (a, b) in aabb {
+        vvv[a].push(b);
+        vvv[b].push(a);
+    }
+    let rs = vvv
+        .into_iter()
+        .enumerate()
+        .filter(|(i, vv)| vv.iter().filter(|&v| v < i).count() == 1)
+        .count();
+    println!("{rs}");
 }
