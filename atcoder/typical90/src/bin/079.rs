5b33e39b74cc6c03905c30e0cdd441e793b98c95
horyu <28817604+horyu@users.noreply.github.com>
2022-06-27 08:42:55 +0900

    typical90 079

--- atcoder/typical90/src/bin/079.rs
+++ atcoder/typical90/src/bin/079.rs
@@ -6,6 +6,25 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        h: usize,
+        w: usize,
+        mut aaa: [[isize; w]; h],
+        mut bbb: [[isize; w]; h],
+    };
+    let mut rs = 0;
+    for x in 0..(h - 1) {
+        for y in 0..(w - 1) {
+            let diff = bbb[x][y] - aaa[x][y];
+            rs += diff.abs();
+            for (dx, dy) in [(0, 0), (0, 1), (1, 0), (1, 1)] {
+                aaa[x + dx][y + dy] += diff;
+            }
+        }
+    }
+    if aaa == bbb {
+        println!("Yes\n{rs}");
+    } else {
+        println!("No");
+    }
 }
