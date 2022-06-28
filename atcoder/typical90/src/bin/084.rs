7978704b875592e8764bd8781d3b204b26825d09
horyu <28817604+horyu@users.noreply.github.com>
2022-06-29 08:52:34 +0900

    typical90 084

--- atcoder/typical90/src/bin/084.rs
+++ atcoder/typical90/src/bin/084.rs
@@ -6,6 +6,25 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let mut oo = BTreeSet::new();
+    let mut xx = BTreeSet::new();
+    for (i, &c) in s.iter().enumerate() {
+        if c == 'o' {
+            oo.insert(i);
+        } else {
+            xx.insert(i);
+        }
+    }
+    let mut rs = 0usize;
+    for i in 0..n {
+        let other = if s[i] == 'o' { &xx } else {&oo};
+        if let Some(j) = other.range(i..).next() {
+            rs += n - j;
+        }
+    }
+    println!("{rs}");
 }
