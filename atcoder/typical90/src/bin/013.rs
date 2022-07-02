41f9d36068ff5c82357c747d6e0b27c946bf6c71
horyu <28817604+horyu@users.noreply.github.com>
2022-07-02 10:39:17 +0900

    typical90 013

--- atcoder/typical90/src/bin/013.rs
+++ atcoder/typical90/src/bin/013.rs
@@ -2,10 +2,26 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use num_integer::*;
+use petgraph::graph::UnGraph;
 use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        m: usize,
+        aabbcc: [(Usize1, Usize1, usize); m]
+    };
+    let mut g = UnGraph::<(), usize>::new_undirected();
+    let nodes = (0..n).map(|_| g.add_node(())).collect_vec();
+    g.extend_with_edges(aabbcc.iter().map(|&(i, j, w)| (nodes[i], nodes[j], w)));
+    
+    // 1とNからダイクストラ
+    let mut rs = vec![0; n];
+    for start in [0, n - 1] {
+        for (to, w) in petgraph::algo::dijkstra(&g, nodes[start], None, |e| *e.weight()) {
+            rs[to.index()] += w;
+        }
+    }
+    println!("{}", rs.into_iter().join("\n"));
 }
