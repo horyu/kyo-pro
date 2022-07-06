8f6ce988dedb0aadf614451635b2ccd1b9e008ca
horyu <28817604+horyu@users.noreply.github.com>
2022-07-06 21:29:32 +0900

    typical90 036

--- atcoder/typical90/src/bin/036.rs
+++ atcoder/typical90/src/bin/036.rs
@@ -6,6 +6,33 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        q: usize,
+        xxyy: [(isize, isize); n],
+        qq: [Usize1; q],
+    };
+    // 45度回転
+    // s=x-y, t=x+y
+    let mut smin = std::isize::MAX;
+    let mut smax = std::isize::MIN;
+    let mut tmin = std::isize::MAX;
+    let mut tmax = std::isize::MIN;
+    for &(x, y) in &xxyy {
+        let s = x - y;
+        let t = x + y;
+        smin = smin.min(s);
+        smax = smax.max(s);
+        tmin = tmin.min(t);
+        tmax = tmax.max(t);
+    }
+    for q in qq {
+        let (x, y) = xxyy[q];
+        let s = x - y;
+        let t = x + y;
+        let rs = [s - smin, s - smax, t - tmin, t - tmax]
+            .into_iter()
+            .fold(0, |rs, v| rs.max(v.abs()));
+        println!("{rs}")
+    }
 }
