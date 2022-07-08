b591f00c7bbace517e3fa175b0f1bd838df190f6
horyu <28817604+horyu@users.noreply.github.com>
2022-07-08 22:22:45 +0900

    typical90 039

--- atcoder/typical90/src/bin/039.rs
+++ atcoder/typical90/src/bin/039.rs
@@ -6,6 +6,30 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        aabb: [(Usize1, Usize1); n - 1],
+    };
+    let mut vvv = vec![vec![]; n];
+    for (a, b) in aabb {
+        vvv[a].push(b);
+        vvv[b].push(a);
+    }
+    let mut dp = vec![0; n];
+    dfs(&mut dp, &vvv, 0, std::usize::MAX);
+    let mut rs = 0;
+    for &d in &dp[1..] {
+        rs += d * (n - d);
+    }
+    println!("{rs}");
+}
+
+fn dfs(dp: &mut Vec<usize>, vvv: &[Vec<usize>], pos: usize, pre: usize) {
+    dp[pos] = 1;
+    for &i in &vvv[pos] {
+        if i != pre {
+            dfs(dp, vvv, i, pos);
+            dp[pos] += dp[i];
+        }
+    }
 }
