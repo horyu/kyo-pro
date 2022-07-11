47dfb69a964675d563fc19d94f718878573fa619
horyu <28817604+horyu@users.noreply.github.com>
2022-07-11 21:38:57 +0900

    typical90 060

--- atcoder/typical90/src/bin/060.rs
+++ atcoder/typical90/src/bin/060.rs
@@ -6,6 +6,28 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        mut aa: [usize; n],
+    };
+    let mut rrss = vec![0; n];
+    // https://qiita.com/python_walker/items/d1e2be789f6e7a0851e5
+    // 最長増加部分列(LIS)
+    for tf in [false, true] {
+        if tf {
+            aa.reverse();
+        }
+        let mut vv = vec![];
+        for (i, &a) in aa.iter().enumerate() {
+            if *vv.last().unwrap_or(&0) < a {
+                vv.push(a);
+            } else {
+                let j = vv.partition_point(|&v| v < a);
+                vv[j] = a;
+            }
+            rrss[if tf { n - 1 - i } else { i }] += vv.len();
+        }
+    }
+    let rs = rrss.into_iter().max().unwrap() - 1;
+    println!("{rs}");
 }
