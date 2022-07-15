c5920a3a3290e7769dbef69412ceb52a767d89c9
horyu <28817604+horyu@users.noreply.github.com>
2022-07-15 20:42:40 +0900

    typical90 081

--- atcoder/typical90/src/bin/081.rs
+++ atcoder/typical90/src/bin/081.rs
@@ -6,6 +6,37 @@ use proconio::{input, marker::*};
 use std::collections::*;
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        k: usize,
+        aabb: [(Usize1, Usize1); n]
+    };
+    const M: usize = 5000;
+    // いもす法
+    let k = k + 1;
+    let mut vvv = vec![vec![0; M + k]; M + k];
+    for (a, b) in aabb {
+        vvv[a][b] += 1;
+        vvv[a + k][b] -= 1;
+        vvv[a][b + k] -= 1;
+        vvv[a + k][b + k] += 1;
+    }
+
+    for i in 0..=M {
+        for j in 0..M {
+            vvv[i][j + 1] += vvv[i][j];
+        }
+    }
+    for i in 0..M {
+        for j in 0..=M {
+            vvv[i + 1][j] += vvv[i][j];
+        }
+    }
+    let mut rs = 0;
+    for i in 0..=M {
+        for j in 0..=M {
+            rs = rs.max(vvv[i][j]);
+        }
+    }
+    println!("{rs}");
 }
