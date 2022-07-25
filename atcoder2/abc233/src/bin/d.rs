0df309f6acba35ecf8fe6d278f9b353040e472eb
horyu <28817604+horyu@users.noreply.github.com>
2022-07-25 09:58:06 +0900

    abc233 d

--- atcoder2/abc233/src/bin/d.rs
+++ atcoder2/abc233/src/bin/d.rs
@@ -11,7 +11,22 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: isize,
+        aa: [isize; n],
     };
-    // println!("{rs}");
+    let mut hm = HashMap::new();
+    let mut sum = 0;
+    for (i, &a) in aa.iter().enumerate() {
+        sum += a;
+        *hm.entry(sum).or_insert(0) += 1;
+    }
+    let mut rs = 0usize;
+    let mut crr = 0;
+    for a in aa {
+        // eprintln!("{a:3} {:3} {:3} {:?}", k + crr, hm.get(&(k + crr)).unwrap_or(&0), &hm);
+        rs += hm.get(&(k + crr)).unwrap_or(&0);
+        crr += a;
+        *hm.get_mut(&crr).unwrap() -= 1;
+    }
+    println!("{rs}");
 }
