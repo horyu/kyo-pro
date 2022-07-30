3141015cf10d046f617301fdd561f281a4754366
horyu <28817604+horyu@users.noreply.github.com>
2022-07-30 10:37:23 +0900

    typical90 044

---
+++ atcoder2/typical90/src/bin/044.rs
@@ -0,0 +1,32 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        mut aa: [usize; n],
+        ttxxyy: [(usize, usize, usize); q]
+    };
+    let mut offset = 0;
+    for (t, x, y) in ttxxyy {
+        match t {
+            1 => {
+                aa.swap((x - 1 + n - offset) % n, (y - 1 + n - offset) % n);
+            }
+            2 => {
+                offset = (offset + 1) % n;
+            }
+            _ => {
+                println!("{}", aa[(x - 1 + n - offset) % n]);
+            }
+        }
+    }
+}
