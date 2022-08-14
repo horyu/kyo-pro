e62ad1ef963444b688644d04da4246edb840a1c2
horyu <28817604+horyu@users.noreply.github.com>
2022-08-14 10:51:00 +0900

    abc264 e

--- atcoder2/abc264/src/bin/e.rs
+++ atcoder2/abc264/src/bin/e.rs
@@ -11,7 +11,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        e: usize,
+        uuvv: [(Usize1, Usize1); e],
+        q: usize,
+        xx: [Usize1; q],
     };
-    // println!("{rs}");
+    let uuvv = uuvv
+        .into_iter()
+        .map(|(u, v)| (u.min(n), v.min(n)))
+        .collect_vec();
+
+    let mut dsu = ac_library_rs::Dsu::new(n + 1);
+    let xx_hs: HashSet<usize> = xx.clone().into_iter().collect();
+    for (i, &(u, v)) in uuvv.iter().enumerate() {
+        if xx_hs.contains(&i) {
+            continue;
+        }
+        dsu.merge(u, v);
+    }
+
+    let mut rs = vec![0; q];
+    for qq in (0..q).rev() {
+        rs[qq] = dsu.size(n) - 1;
+        let (u, v) = uuvv[xx[qq]];
+        dsu.merge(u, v);
+    }
+    let rs = rs.into_iter().join("\n");
+    println!("{rs}");
 }
