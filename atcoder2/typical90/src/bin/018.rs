5f8b27a6cb1a355886c782de4ee9e85bdb65738d
horyu <28817604+horyu@users.noreply.github.com>
2022-08-20 20:09:21 +0900

    typical90 018

--- atcoder2/typical90/src/bin/018.rs
+++ atcoder2/typical90/src/bin/018.rs
@@ -6,12 +6,32 @@ use itertools_num::ItertoolsNum as _;
 use num_integer::*;
 use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::{
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    f64::consts::PI,
+};
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+        ee: [f64; q],
     };
-    // println!("{rs}");
+    let z = 0.0;
+    let xx = 0.0;
+    for e in ee {
+        // e 0 t/2 t
+        //   0 PI  2PI
+        let theta = 2.0 * PI * e / t;
+        let yy = -(l / 2.0) * theta.sin();
+        let zz = (l / 2.0) * (1.0 - theta.cos());
+
+        let xy = ((x - xx).powi(2) + (y - yy).powi(2)).sqrt();
+        let tan = zz / xy;
+        let rs = tan.atan() * 180.0 / PI;
+        println!("{rs}");
+    }
 }
