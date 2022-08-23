289ac5a7447049b4ee3329b172a73bd0d35ba3e2
horyu <28817604+horyu@users.noreply.github.com>
2022-08-23 13:56:44 +0900

    arc114 b

---
+++ atcoder2/arc114/src/bin/b.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use ac_library_rs::ModInt998244353;
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        ff: [Usize1; n],
+    };
+    let mut dsu = ac_library_rs::Dsu::new(n);
+    for (i, f) in ff.into_iter().enumerate() {
+        dsu.merge(i, f);
+    }
+    let mut rs = ModInt998244353::new(1);
+    let size = dsu.groups().len();
+    if 1 < size {
+        rs += 1;
+        rs = rs.pow(size as u64) - 1;
+    }
+    println!("{rs}");
+}
