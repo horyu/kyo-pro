28a48f1bb3e477f0674039b45ea94fd475225edf
horyu <28817604+horyu@users.noreply.github.com>
2022-08-24 18:59:01 +0900

    abc027 c

---
+++ atcoder2/abc027/src/bin/c.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+    };
+    // https://qiita.com/hamamu/items/55433210be3c47a4dd72
+    let mut y = n + 1;
+    loop {
+        let z = y.div_ceil(2);
+        if z == 1 {
+            println!("Aoki");
+            return;
+        }
+        y = (z - 1).div_ceil(2);
+        if y == 1 {
+            println!("Takahashi");
+            return;
+        }
+    }
+}
