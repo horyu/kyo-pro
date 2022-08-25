fd4a9ebcf1e8a88ccd4d8d0468ae2eba330f4d7d
horyu <28817604+horyu@users.noreply.github.com>
2022-08-25 09:05:20 +0900

    abc084 c

---
+++ atcoder2/abc084/src/bin/c.rs
@@ -0,0 +1,27 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        ccssff: [(usize, usize, usize); n - 1],
+    };
+    for i in 0..n {
+        let mut t = 0usize;
+        for &(c, s, f) in &ccssff[i..] {
+            if t <= s {
+                t = s + c;
+            } else {
+                t = t.div_ceil(f) * f + c;
+            }
+        }
+        println!("{t}");
+    }
+}
