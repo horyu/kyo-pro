bd56a94ba244bb9b1a29ebec3ec8a958a900d43a
horyu <28817604+horyu@users.noreply.github.com>
2022-08-31 23:26:06 +0900

    abc185 f

---
+++ atcoder2/abc185/src/bin/f.rs
@@ -0,0 +1,63 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use ac_library_rs::{LazySegtree, MapMonoid, Monoid};
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+pub struct MonoidBitXor<S>(
+    std::convert::Infallible,
+    std::marker::PhantomData<fn() -> S>,
+);
+impl<S> Monoid for MonoidBitXor<S> {
+    type S = usize;
+    fn identity() -> Self::S {
+        0
+    }
+    fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
+        *a ^ *b
+    }
+}
+
+struct MapMonoidXor;
+impl MapMonoid for MapMonoidXor {
+    type M = MonoidBitXor<usize>;
+    type F = usize;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(
+        f: &Self::F,
+        x: &<Self::M as ac_library_rs::Monoid>::S,
+    ) -> <Self::M as ac_library_rs::Monoid>::S {
+        f ^ x
+    }
+
+    fn composition(f: &Self::F, g: &Self::F) -> Self::F {
+        f ^ g
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        aa: [usize; n],
+        ttxxyy: [(u8, usize, usize); q],
+    };
+    let mut ls: LazySegtree<MapMonoidXor> = aa.into();
+    for (t, x, y) in ttxxyy {
+        if t == 1 {
+            ls.apply(x - 1, y);
+        } else {
+            let rs = ls.prod(x - 1, y);
+            println!("{rs}");
+        }
+    }
+}
