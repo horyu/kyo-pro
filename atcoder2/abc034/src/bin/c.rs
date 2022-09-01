8c9aaa87cbed95cff57dc45eb7c82e9c9db94d8b
horyu <28817604+horyu@users.noreply.github.com>
2022-09-01 09:08:07 +0900

    abc034 c

---
+++ atcoder2/abc034/src/bin/c.rs
@@ -0,0 +1,31 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use ac_library_rs::ModInt1000000007;
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        w: usize,
+        h: usize,
+    };
+    // 横にw-1回 縦にh-1回移動の並び替え
+    let mut rs = ModInt1000000007::new(1);
+    let ww = w - 1;
+    let hh = h - 1;
+    for i in 1..=(hh + ww) {
+        rs *= i;
+    }
+    for i in 1..=hh {
+        rs /= i;
+    }
+    for i in 1..=ww {
+        rs /= i;
+    }
+    println!("{rs}");
+}
