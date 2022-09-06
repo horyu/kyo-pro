727bf38b03b8bdcdda97cca0ce0b1167e4dd62b9
horyu <28817604+horyu@users.noreply.github.com>
2022-09-07 00:10:10 +0900

    abc165 c

--- atcoder2/abc165/src/bin/c.rs
+++ atcoder2/abc165/src/bin/c.rs
@@ -13,47 +13,35 @@ fn main() {
         n: usize,
         m: usize,
         q: usize,
-        abcds: [(usize, usize, usize, usize); q],
+        aabbccdd: [(Usize1, Usize1, usize, usize); q],
     };
-    let mut max = 0;
+    let mut rs = 0;
     let mut nums = vec![1; n];
-    while {
-        let sum = calc(&nums, &abcds);
-        if sum > max {
-            max = sum;
-        }
-        next(&mut nums, &m)
-    } {}
-    println!("{}", max);
-}
-
-fn calc(nums: &[usize], abcds: &[(usize, usize, usize, usize)]) -> usize {
-    let mut sum = 0;
-    for &(a, b, c, d) in abcds {
-        if nums[b - 1] - nums[a - 1] == c {
-            sum += d;
-        }
-    }
-    sum
-}
-
-fn next(nums: &mut Vec<usize>, m: &usize) -> bool {
-    // 右から9のindexを探す
-    // ある → 一番左にあるmの左隣をインクリメントし右を全部それに
-    // ない → 右端を+1
-    let len = nums.len();
-    let ii = nums.iter().position(|x| x == m);
-    if let Some(i) = ii {
-        if i == 0 {
-            return false;
-        } else {
-            nums[i - 1] += 1;
-            for j in i..len {
-                nums[j] = nums[i - 1];
+    loop {
+        let mut sum = 0;
+        for &(a, b, c, d) in &aabbccdd {
+            if nums[a] + c == nums[b] {
+                sum += d;
             }
         }
-    } else {
-        nums[len - 1] += 1;
+        rs = rs.max(sum);
+
+        // 右からmのindexを探す
+        // ある → 一番左にあるmの左隣をインクリメントし右を全部それに
+        // ない → 右端を+1
+        let len = nums.len();
+        if let Some(i) = nums.iter().position(|&x| x == m) {
+            if i == 0 {
+                break;
+            } else {
+                nums[i - 1] += 1;
+                for j in i..len {
+                    nums[j] = nums[i - 1];
+                }
+            }
+        } else {
+            nums[len - 1] += 1;
+        }
     }
-    true
+    println!("{}", rs);
 }
