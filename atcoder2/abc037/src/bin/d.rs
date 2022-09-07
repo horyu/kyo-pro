e08c32c557f4a15e609207356ecd5a0c97647e68
horyu <28817604+horyu@users.noreply.github.com>
2022-09-07 14:35:32 +0900

    abc037 d

---
+++ atcoder2/abc037/src/bin/d.rs
@@ -0,0 +1,48 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use ac_library_rs::ModInt1000000007;
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        aaa: [[usize; w]; h],
+    };
+    let mut btm = BTreeMap::new();
+    for (i, aa) in aaa.iter().enumerate() {
+        for (j, a) in aa.iter().enumerate() {
+            btm.entry(*a).or_insert_with(Vec::new).push((i, j));
+        }
+    }
+    let mut vvv = vec![vec![ModInt1000000007::new(0); w]; h];
+    for (&k, vv) in btm.iter().rev() {
+        for &(i, j) in vv {
+            let mut v = ModInt1000000007::new(1);
+            if 0 < i && k < aaa[i - 1][j] {
+                v += vvv[i - 1][j];
+            }
+            if i < h - 1 && k < aaa[i + 1][j] {
+                v += vvv[i + 1][j];
+            }
+            if 0 < j && k < aaa[i][j - 1] {
+                v += vvv[i][j - 1];
+            }
+            if j < w - 1 && k < aaa[i][j + 1] {
+                v += vvv[i][j + 1];
+            }
+            vvv[i][j] = v;
+        }
+    }
+    // for vv in &vvv {
+    //     eprintln!("{}", vv.iter().join(" "));
+    // }
+    let rs = vvv.into_iter().flatten().sum::<ModInt1000000007>();
+    println!("{rs}");
+}
