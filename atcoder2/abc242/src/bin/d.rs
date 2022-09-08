f087bafac55d45aba493766ae0c3c636b6d14d3e
horyu <28817604+horyu@users.noreply.github.com>
2022-09-08 23:54:19 +0900

    abc242 d

---
+++ atcoder2/abc242/src/bin/d.rs
@@ -0,0 +1,35 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        s: Bytes,
+        q: usize,
+        ttkk: [(usize, Usize1); q],
+    };
+    // C   B   A
+    // A B C A B C
+    // BCCAABBCCAAB
+    let s = s.into_iter().map(|c| (c - b'A') as usize).collect_vec();
+    for (mut t, mut k) in ttkk {
+        let mut diff = 0;
+        while 0 < t && 0 < k {
+            if k.is_even() {
+                diff += 1;
+            } else {
+                diff += 2;
+            }
+            k /= 2;
+            t -= 1;
+        }
+        let rs = ['A', 'B', 'C'][(s[k] + t + diff) % 3];
+        println!("{rs}");
+    }
+}
