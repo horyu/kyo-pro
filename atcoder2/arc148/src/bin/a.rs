117b34a0a6f689df0478fbcbec3d77e5f6fb7d06
horyu <28817604+horyu@users.noreply.github.com>
2022-09-11 23:37:35 +0900

    arc148 ab

---
+++ atcoder2/arc148/src/bin/a.rs
@@ -0,0 +1,27 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        mut aa: [i128; n],
+    };
+    aa.sort_unstable();
+    let gcd = aa
+        .iter()
+        .tuple_windows()
+        .fold(0, |acc, (ai, aj)| (aj - ai).gcd(&acc));
+    let rs = match gcd.cmp(&1) {
+        std::cmp::Ordering::Less => 1,
+        std::cmp::Ordering::Equal => 2,
+        std::cmp::Ordering::Greater => 1,
+    };
+    println!("{rs}");
+}
---
+++ atcoder2/arc148/src/bin/b.rs
@@ -0,0 +1,37 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let string = s.iter().join("");
+    let str = string.as_str();
+    // 先頭のdを伸ばせるだけ伸ばす
+    if let Some(l) = s.iter().position(|&c| c == 'p') {
+        let mut min = string.as_str()[l..].to_owned();
+        for r in l..n {
+            min = min.min(format!(
+                "{}{}",
+                s[l..=r]
+                    .iter()
+                    .rev()
+                    .map(|&c| if c == 'd' { 'p' } else { 'd' })
+                    .join(""),
+                &str[(r + 1)..]
+            ));
+        }
+        println!("{}{}", &str[..l], min);
+    } else {
+        let rs = s.iter().join("");
+        println!("{rs}");
+    };
+}
