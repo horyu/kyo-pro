9020d44c6b59d9170b541e9875482dc5d4d43dc2
horyu <28817604+horyu@users.noreply.github.com>
2022-09-11 14:49:49 +0900

    abc044 c

---
+++ atcoder2/abc044/src/bin/c.rs
@@ -0,0 +1,32 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        a: isize,
+        xx: [isize; n],
+    };
+    let mut hm = HashMap::new();
+    for mut x in xx {
+        x -= a;
+        let mut new_hm = hm.clone();
+        *new_hm.entry((1, x)).or_insert(0) += 1usize;
+        for ((i, v), cnt) in hm {
+            *new_hm.entry((i + 1, v + x)).or_insert(0) += cnt;
+        }
+        hm = new_hm;
+    }
+    let rs = hm
+        .into_iter()
+        .filter_map(|((_i, v), cnt)| if v == 0 { Some(cnt) } else { None })
+        .sum::<usize>();
+    println!("{rs}");
+}
