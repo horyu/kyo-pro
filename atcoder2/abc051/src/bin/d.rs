9f2c3abe7d0a12477be2d34a3d351111e4e28bd3
horyu <28817604+horyu@users.noreply.github.com>
2022-09-19 09:23:23 +0900

    abc051 d

---
+++ atcoder2/abc051/src/bin/d.rs
@@ -0,0 +1,31 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        aabbcc: [(Usize1, Usize1, usize); m],
+    };
+    let mut g = vec![vec![!0usize >> 3; n]; n];
+    for &(a, b, c) in &aabbcc {
+        g[a][b] = c;
+        g[b][a] = c;
+    }
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                g[i][j] = g[i][j].min(g[i][k] + g[k][j]);
+            }
+        }
+    }
+    let rs = aabbcc.into_iter().filter(|&(a, b, c)| g[a][b] != c).count();
+    println!("{rs}");
+}
