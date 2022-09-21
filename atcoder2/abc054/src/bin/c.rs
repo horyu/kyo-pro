bd971405dcc0dfa15e6e52ca841ec6a571a9e265
horyu <28817604+horyu@users.noreply.github.com>
2022-09-21 09:52:43 +0900

    abc054 c

---
+++ atcoder2/abc054/src/bin/c.rs
@@ -0,0 +1,27 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(Usize1, Usize1); m],
+    };
+    let mut g = vec![vec![false; n]; n];
+    for (a, b) in aabb {
+        g[a][b] = true;
+        g[b][a] = true;
+    }
+    let rs = (1..n)
+        .permutations(n - 1)
+        .filter(|ii| g[0][ii[0]] && ii.iter().tuple_windows().all(|(&i, &j)| g[i][j]))
+        .count();
+    println!("{rs}");
+}
