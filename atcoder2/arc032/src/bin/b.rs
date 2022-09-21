ce2b2d98405f428df2f273b9abfdb9b36b84549a
horyu <28817604+horyu@users.noreply.github.com>
2022-09-22 08:45:56 +0900

    arc032 b

---
+++ atcoder2/arc032/src/bin/b.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        aabb: [(Usize1, Usize1); m],
+    };
+    let mut uf = UnionFind::new(n);
+    for (a, b) in aabb {
+        uf.union(a, b);
+    }
+    let rs = uf.into_labeling().into_iter().unique().count() - 1;
+    println!("{rs}");
+}
