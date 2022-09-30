770442d936857d0c21dfc935bb15bae02e2aab9a
horyu <28817604+horyu@users.noreply.github.com>
2022-09-30 14:32:45 +0900

    abc191 d

--- atcoder2/abc191/src/bin/d.rs
+++ atcoder2/abc191/src/bin/d.rs
@@ -14,31 +14,41 @@ fn main() {
         y: String,
         r: String,
     };
-    // x,y: 符号と整数部は無視する
     let read = |s: &str, is_r: bool| -> isize {
-        let vv = s.split('.').collect_vec();
-        let mut num = if is_r { vv[0].parse::<isize>().unwrap() * 10000 } else { 0 };
-        if let Some(&v) = vv.get(1) {
-            num += v.parse::<isize>().unwrap()
-                * 10isize.pow(4 - v.len() as u32);
+        let num = s.parse::<f64>().unwrap();
+        if is_r {
+            (num * 10000.0).round() as isize
+        } else {
+            (num.fract().abs() * 10000.0).round() as isize
         }
-        num
     };
     let x = read(&x, false);
     let y = read(&y, false);
     let r = read(&r, true);
-    dbg!(x, y, r);
     let rr = r.pow(2);
     let mut rs = 0;
-    for i in (-10000)..=10000 {
-        let a = i + x;
+    for i in (-100000)..=100000 {
+        // (X - x)^2 + (Y - y)^2 <= r^2
+        // -sqrt() + y <= Y <= +sqrt() + y
+        let a = i * 10000 + x;
         let aa = a.pow(2);
         if aa <= rr {
             let bb = rr - aa;
             // aa + bb = rr
             let b = bb.sqrt();
-            // b = j + y;
-
+            // b = j - y;
+            let ju = b + y;
+            let jd = -b + y;
+            if jd < 0 {
+                rs += (jd / 10000).abs() + ju / 10000 + 1;
+            } else if ju == jd {
+                if ju % 10000 == 0 {
+                    rs += 1;
+                }
+            } else {
+                rs += 1 + ju / 10000 - jd.div_ceil(10000);
+            }
+            // eprintln!("{i} {rs} {jd}..{ju}");
         }
     }
     println!("{rs}");
