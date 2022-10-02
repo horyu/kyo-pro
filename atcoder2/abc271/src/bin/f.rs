4da2e78120d1148b32bcf6544e4879f272787f5b
horyu <28817604+horyu@users.noreply.github.com>
2022-10-02 10:26:38 +0900

    abc271 f

--- atcoder2/abc271/src/bin/f.rs
+++ atcoder2/abc271/src/bin/f.rs
@@ -20,7 +20,7 @@ fn main() {
         let mut new_hhmm = vec![HashMap::new(); size];
         for (index, hm) in hhmm.into_iter().enumerate() {
             let i = index;
-            let j = size - 2 - i;
+            let j = size - 2 - index;
             let ar = aaa[i][j + 1];
             let ad = aaa[i + 1][j];
             for (k, v) in hm {
@@ -30,27 +30,38 @@ fn main() {
         }
         hhmm = new_hhmm;
     }
-    // 対角から右下まで
-    for size in (1..=(n - 1)).rev() {
+    let old_hhmm = hhmm;
+    let mut hhmm = vec![HashMap::new()];
+    hhmm[0].insert(aaa[n - 1][n - 1], 1usize);
+    // 右下から対角まで
+    for size in 2..=n {
         let mut new_hhmm = vec![HashMap::new(); size];
         for (index, hm) in hhmm.into_iter().enumerate() {
-            let i = n - 1 - size + index;
+            let i = n + 1 - size + index;
             let j = n - 1 - index;
-            if i < n - 1 {
-                let ad = aaa[i + 1][j];
-                for (&k, &v) in &hm {
-                    *new_hhmm[index].entry(k ^ ad).or_insert(0) += v;
-                }
-            }
-            if j < n - 1 {
-                let ar = aaa[i][j + 1];
-                for (&k, &v) in &hm {
-                    *new_hhmm[index - 1].entry(k ^ ar).or_insert(0) += v;
-                }
+            // eprintln!("{size} {index} {i} {j}");
+            // 対角要素を2回使わないようにする
+            let au = if size == n { 0 } else { aaa[i - 1][j] };
+            let al = if size == n { 0 } else { aaa[i][j - 1] };
+            for (k, v) in hm {
+                *new_hhmm[index].entry(k ^ au).or_insert(0) += v;
+                *new_hhmm[index + 1].entry(k ^ al).or_insert(0) += v;
             }
         }
         hhmm = new_hhmm;
     }
-    let rs = hhmm[0].get(&0).unwrap_or(&0);
+    // dbg!(&old_hhmm, &hhmm);
+    let mut rs = 0;
+    for (i, mut x, mut y) in izip!(0..n, old_hhmm, hhmm) {
+        if x.len() < y.len() {
+            std::mem::swap(&mut x, &mut y);
+        }
+        for (k, vx) in x {
+            if let Some(&vy) = y.get(&(k)) {
+                // eprintln!("{k} {vx} {vy}");
+                rs += vx * vy;
+            }
+        }
+    }
     println!("{rs}");
 }
