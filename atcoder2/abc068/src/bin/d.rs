569a2d0d2f904c84fe4c14ff93ad1f944df7a4d7
horyu <28817604+horyu@users.noreply.github.com>
2022-10-13 19:51:57 +0900

    abc068 d

---
+++ atcoder2/abc068/src/bin/d.rs
@@ -0,0 +1,34 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+/*
+0	49 49 49 .. 49 49
+1	99 48 48 .. 48 48
+2	98 98 47 .. 47 47
+3	97 97 97 .. 46 46
+
+49	51 51 51 .. 51 0
+50	50 50 50 .. 50 50
+*/
+
+fn main() {
+    input! {
+        k: usize,
+    };
+    let mut vv = [49 + k / 50; 50];
+    let amari = k % 50;
+    for i in 0..amari {
+        vv[i] += 51 - amari;
+    }
+    for i in amari..50 {
+        vv[i] -= amari;
+    }
+    println!("50\n{}", vv.into_iter().join(" "));
+}
