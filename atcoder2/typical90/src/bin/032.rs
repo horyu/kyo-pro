a4457d36b38ac07f926be9f7037bf4754ca1abdf
horyu <28817604+horyu@users.noreply.github.com>
2022-10-14 09:15:35 +0900

    typical90 032

--- atcoder2/typical90/src/bin/032.rs
+++ atcoder2/typical90/src/bin/032.rs
@@ -11,7 +11,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aaa: [[usize; n]; n],
+        m: usize,
+        xxyy: [(Usize1, Usize1); m]
     };
-    // println!("{rs}");
+    let mut ng = vec![vec![false; n]; n];
+    for (x, y) in xxyy {
+        ng[x][y] = true;
+        ng[y][x] = true;
+    }
+    let min_opt = (0..n)
+        .permutations(n)
+        .filter_map(|ii| {
+            for (&ix, &iy) in ii.iter().tuple_windows() {
+                if ng[ix][iy] {
+                    return None;
+                }
+            }
+            // eprintln!("[{}] {}", ii.iter().join(","), ii.iter().enumerate().map(|(j, &i)| aaa[j][i]).join(" "));
+            Some(
+                ii.into_iter()
+                    .enumerate()
+                    .fold(0usize, |acc, (j, i)| acc + aaa[i][j]),
+            )
+        })
+        .min();
+    if let Some(rs) = min_opt {
+        println!("{rs}");
+    } else {
+        println!("-1");
+    }
 }
