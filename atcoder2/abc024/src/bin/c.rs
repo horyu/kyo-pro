c90c224d5fee87269f984d122b78f15023b6c8d8
horyu <28817604+horyu@users.noreply.github.com>
2022-10-15 10:08:18 +0900

    abc024 c

---
+++ atcoder2/abc024/src/bin/c.rs
@@ -0,0 +1,41 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings, map_first_last)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        d: usize,
+        k: usize,
+        llrr: [(Usize1, Usize1); d],
+        sstt: [(Usize1, Usize1); k],
+    };
+    let mut ii = (0..k).collect_vec();
+    let mut cc = sstt.iter().map(|st| st.0).collect_vec();
+    let mut rrss = vec![0; k];
+    for (day, (l, r)) in llrr.into_iter().enumerate() {
+        for j in (0..ii.len()).rev() {
+            let i = ii[j];
+            let c = &mut cc[i];
+            let t = sstt[i].1;
+            if (l..=r).contains(c) {
+                if t < l {
+                    *c = l.min(*c);
+                } else if r < t {
+                    *c = r.max(*c);
+                } else {
+                    // goal
+                    rrss[i] = day + 1;
+                    ii.swap_remove(j);
+                }
+            }
+        }
+    }
+    println!("{}", rrss.iter().join("\n"));
+}
