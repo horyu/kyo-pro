821c6f43a6802ef1643cee74bf9092d1dae7e04f
horyu <28817604+horyu@users.noreply.github.com>
2022-10-26 20:18:35 +0900

    remove feature(map_first_last)

--- atcoder2/abc067/src/bin/d.rs
+++ atcoder2/abc067/src/bin/d.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
-#![feature(int_log, int_roundings, map_first_last)]
+#![feature(int_log, int_roundings)]
 #![feature(drain_filter)]
 use itertools::{iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
