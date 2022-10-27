9fe726832d91769ce2d11ee443b53931c448b0cd
horyu <28817604+horyu@users.noreply.github.com>
2022-10-27 09:07:44 +0900

    arc036 b

---
+++ atcoder2/arc036/src/bin/b.rs
@@ -0,0 +1,33 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        hh: [usize; n],
+    };
+    let mut ll = vec![0usize; n];
+    let mut rr = vec![0usize; n];
+    for i in 0..(n - 1) {
+        if hh[i] < hh[i + 1] {
+            ll[i + 1] = ll[i] + 1;
+        }
+    }
+    for i in (0..(n - 1)).rev() {
+        if hh[i] > hh[i + 1] {
+            rr[i] = rr[i + 1] + 1;
+        }
+    }
+    // for i in 0..n {
+    //     eprintln!("{i}:{} {}", ll[i], rr[i]);
+    // }
+    let rs = izip!(ll, rr).map(|(l, r)| l + r + 1).max().unwrap();
+    println!("{rs}");
+}
