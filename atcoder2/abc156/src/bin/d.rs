b03db19a18525ea88800cc099a5e66cb1d1877dc
horyu <28817604+horyu@users.noreply.github.com>
2022-10-28 21:20:01 +0900

    abc156 d

---
+++ atcoder2/abc156/src/bin/d.rs
@@ -0,0 +1,34 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings)]
+use ac_library_rs::ModInt1000000007;
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+    };
+    let mut rs = ModInt1000000007::new(2);
+    rs = rs.pow(n as u64) - 1;
+    // dbg!(&rs);
+    for x in [a, b] {
+        let mut tmp = ModInt1000000007::new(1);
+        // dbg!((n - x + 1)..=n, 1..=x);
+        for i in (n - x + 1)..=n {
+            tmp *= i;
+        }
+        for j in 1..=x {
+            tmp /= j;
+        }
+        rs -= tmp;
+        // dbg!(&tmp);
+    }
+    println!("{rs}");
+}
