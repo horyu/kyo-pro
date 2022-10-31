f3ceadfa23fdd8630deff837baea88bd4ec8c026
horyu <28817604+horyu@users.noreply.github.com>
2022-10-31 20:34:21 +0900

    abc087 d

---
+++ atcoder2/abc087/src/bin/d.rs
@@ -0,0 +1,47 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_log, int_roundings)]
+use itertools::{iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        llrrdd: [(Usize1, Usize1, isize); m],
+    };
+    let mut dsu = ac_library_rs::Dsu::new(n);
+    let mut xxdd = vec![vec![]; n];
+    for &(l, r, d) in &llrrdd {
+        dsu.merge(l, r);
+        xxdd[l].push((r, d));
+        xxdd[r].push((l, -d));
+    }
+    let mut vv = vec![0isize; n];
+    let mut pushed = vec![false; n];
+    for ii in dsu.groups() {
+        let mut qq = VecDeque::new();
+        qq.push_back(ii[0]);
+        pushed[ii[0]] = true;
+        while let Some(q) = qq.pop_front() {
+            for &(x, d) in &xxdd[q] {
+                vv[x] = vv[q] + d;
+                if !pushed[x] {
+                    pushed[x] = true;
+                    qq.push_back(x);
+                }
+            }
+        }
+    }
+    for &(l, r, d) in &llrrdd {
+        if vv[l] + d != vv[r] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
+}
