75d5140f396f158021a4e37a847d8a22814a3a21
horyu <28817604+horyu@users.noreply.github.com>
2022-11-05 16:52:50 +0900

    typical90 046

--- atcoder2/typical90/src/bin/046.rs
+++ atcoder2/typical90/src/bin/046.rs
@@ -12,6 +12,26 @@ fn main() {
     input! {
         n: usize,
         aa: [usize; n],
+        bb: [usize; n],
+        cc: [usize; n],
     };
-    // println!("{rs}");
+    const SIZE: usize = 46;
+    let to_arr = |vv: Vec<usize>| -> [usize; SIZE] {
+        let mut arr = [0; SIZE];
+        for v in vv {
+            arr[v % SIZE] += 1;
+        }
+        arr
+    };
+    let aa = to_arr(aa);
+    let bb = to_arr(bb);
+    let cc = to_arr(cc);
+    let mut rs = 0usize;
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            let k = (SIZE - (i + j) % SIZE) % SIZE;
+            rs += aa[i] * bb[j] * cc[k];
+        }
+    }
+    println!("{rs}");
 }
