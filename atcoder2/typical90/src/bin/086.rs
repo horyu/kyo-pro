fa3290a393238cf1016ef64f7a14424748face7e
horyu <28817604+horyu@users.noreply.github.com>
2022-11-09 09:46:18 +0900

    typical90 086

--- atcoder2/typical90/src/bin/086.rs
+++ atcoder2/typical90/src/bin/086.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_log, int_roundings)]
+use ac_library_rs::ModInt1000000007;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -11,7 +12,25 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        xxyyzzww: [(Usize1, Usize1, Usize1, usize); q],
     };
-    // println!("{rs}");
+    let vvv = (0..n)
+        .map(|_| [0, 1])
+        .multi_cartesian_product()
+        .collect_vec();
+    let mut rs = ModInt1000000007::new(1);
+    for j in 0..60 {
+        let mut tmp = 0;
+        for vv in &vvv {
+            if xxyyzzww
+                .iter()
+                .all(|&(x, y, z, w)| vv[x] | vv[y] | vv[z] == (w >> j) & 1)
+            {
+                tmp += 1;
+            }
+        }
+        rs *= tmp;
+    }
+    println!("{rs}");
 }
