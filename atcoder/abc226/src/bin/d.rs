6e86f1e1d98bce35ec3836356918e6e0f563634a
horyu <28817604+horyu@users.noreply.github.com>
2022-11-17 23:38:42 +0900

    abc226 d

--- atcoder/abc226/src/bin/d.rs
+++ atcoder/abc226/src/bin/d.rs
@@ -6,6 +6,20 @@ use proconio::{input, marker::*};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    input! {};
-    // println!("{}", );
+    input! {
+        n: usize,
+        xxyy: [(isize, isize); n],
+    };
+    let mut hs = HashSet::new();
+    for ((ix, iy), (jx, jy)) in xxyy.into_iter().tuple_combinations() {
+        let dx = ix - jx;
+        let dy = iy - jy;
+        let gcd = dx.abs().gcd(&dy.abs());
+        let sign = if 0 <= dx { 1 } else { -1 };
+        let x = sign * dx / gcd;
+        let y = sign * dy / gcd;
+        hs.insert((x, y));
+        hs.insert((-x, -y));
+    }
+    println!("{}", hs.len());
 }
