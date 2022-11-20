9a2f8610811e0fec9512d2772bc8a5e1a452e46b
horyu <28817604+horyu@users.noreply.github.com>
2022-11-20 16:11:37 +0900

    abc278 f

--- atcoder2/abc278/src/bin/f.rs
+++ atcoder2/abc278/src/bin/f.rs
@@ -4,14 +4,34 @@
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
-use petgraph::unionfind::UnionFind;
+use petgraph::{matrix_graph::Zero, unionfind::UnionFind};
 use proconio::{input, marker::*};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        ss: [Bytes; n],
     };
-    // println!("{rs}");
+    let mut ff = vec![];
+    let mut tt = vec![];
+    for s in ss {
+        ff.push(s[0] - b'a');
+        tt.push(*s.last().unwrap() - b'a');
+    }
+    // bit DP
+    let mut dp = vec![0usize; 1 << n];
+    for s in 1..(1 << n) {
+        for c in 0..n {
+            if 0 < 1 & (s >> c) {
+                dp[s] |= (1 & !dp[s ^ 1 << c] >> tt[c]) << ff[c];
+            }
+        }
+    }
+    let rs = if dp.last().unwrap().is_zero() {
+        "Second"
+    } else {
+        "First"
+    };
+    println!("{rs}");
 }
