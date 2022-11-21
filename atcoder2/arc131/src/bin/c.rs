6b7a3409ea68ad0b4964b5b0a738f3f340f46d54
horyu <28817604+horyu@users.noreply.github.com>
2022-11-21 09:47:36 +0900

    arc131 c

---
+++ atcoder2/arc131/src/bin/c.rs
@@ -0,0 +1,26 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        aa: [usize; n],
+    };
+    let mut tf = n.is_odd();
+    tf |= {
+        let mut v = 0;
+        for &a in &aa {
+            v ^= a;
+        }
+        aa.contains(&v)
+    };
+    let rs = ["Lose", "Win"][tf as usize];
+    println!("{rs}");
+}
