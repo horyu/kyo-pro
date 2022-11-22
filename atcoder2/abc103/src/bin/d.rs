41a971fedd10515428ec292895fab0a2e6b4e7d8
horyu <28817604+horyu@users.noreply.github.com>
2022-11-22 09:06:25 +0900

    abc103 d

---
+++ atcoder2/abc103/src/bin/d.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        mut aabb: [(isize, isize); m],
+    };
+    aabb.sort_unstable_by_key(|ab| ab.1);
+    let mut rs = 0usize;
+    let mut pre = -1;
+    for (a, b) in aabb {
+        if a < pre {
+            continue;
+        }
+        pre = b;
+        rs += 1;
+    }
+    println!("{rs}");
+}
