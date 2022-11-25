6f0e56c4c5babbb9b23f4d52c906ad9d12343ffe
horyu <28817604+horyu@users.noreply.github.com>
2022-11-25 09:56:59 +0900

    abc112 d

---
+++ atcoder2/abc112/src/bin/d.rs
@@ -0,0 +1,35 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+    };
+    if let Some(div) = divisors(m).into_iter().filter(|&k| n <= k).min() {
+        println!("{}", m / div);
+    } else {
+        println!("1");
+    }
+}
+
+fn divisors(n: usize) -> Vec<usize> {
+    let mut divisors = Vec::new();
+    for i in 1..=(f64::sqrt(n as f64) + 1e-9) as usize {
+        if n % i == 0 {
+            divisors.push(i);
+            if i != n / i {
+                divisors.push(n / i);
+            }
+        }
+    }
+    divisors.sort_unstable();
+    divisors
+}
