d4fe7703010ae185b25dec15a3d4bb4f6a394229
horyu <28817604+horyu@users.noreply.github.com>
2022-11-27 15:58:13 +0900

    abc279 e

--- atcoder2/abc279/src/bin/e.rs
+++ atcoder2/abc279/src/bin/e.rs
@@ -11,7 +11,56 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aa: [Usize1; m],
     };
-    // println!("{rs}");
+    // 0が座標aにあるとき、有効な移動候補2つ (移動のインデックス, 次の座標)
+    let mut abtm = vec![BTreeMap::new(); n];
+    for (j, a) in aa.iter().copied().enumerate() {
+        abtm[a].insert(j, vec![(j, a + 1)]);
+        abtm[a + 1].insert(j, vec![(j, a)]);
+    }
+    for (j, a) in aa.iter().copied().enumerate() {
+        if let Some((_, vv)) = abtm[a].range_mut(..j).next_back() {
+            vv.push((j, a + 1));
+        }
+        if let Some((_, vv)) = abtm[a + 1].range_mut(..j).next_back() {
+            vv.push((j, a));
+        }
+    }
+    for btm in abtm.iter_mut() {
+        for vv in btm.values_mut() {
+            vv.sort_unstable();
+        }
+    }
+    let mut start_index = 0;
+    let mut start_pos = 0;
+    let mut memo = HashMap::new();
+    for i in 0..m {
+        let mut index = start_index;
+        let mut pos = start_pos;
+        let mut history = vec![];
+        while let Some((_, jjpp)) = abtm[pos].range(index..).next() {
+            if let Some((j, p)) = jjpp.iter().copied().find(|jp| jp.0 != i) {
+                if i < index {
+                    if let Some(&rs) = memo.get(&(j, p)) {
+                        pos = rs;
+                        break;
+                    }
+                }
+                index = j + 1;
+                pos = p;
+                if j < i {
+                    start_index = index;
+                    start_pos = pos;
+                } else {
+                    history.push((j, p));
+                }
+                continue;
+            }
+            break;
+        }
+        memo.extend(history.into_iter().map(|jp| (jp, pos)));
+        println!("{}", pos + 1);
+    }
 }
