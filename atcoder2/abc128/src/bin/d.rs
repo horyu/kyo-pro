e3379d2563bb1caf5017efbd7c4c63743e9b4d0b
horyu <28817604+horyu@users.noreply.github.com>
2022-12-21 23:19:54 +0900

    abc128 d

---
+++ atcoder2/abc128/src/bin/d.rs
@@ -0,0 +1,43 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        vv: [isize; n],
+    };
+    let mut rs = 0;
+    // 左から i 個取る
+    for i in 0..=k.min(n - 1) {
+        let xx = vv[..i].iter().copied().collect_vec();
+        // eprintln!("[{}]", xx.iter().join(" "));
+        // eprintln!("[{}] 0..={}", xx.iter().join(" "), k - i);
+        // 右から j 個取る
+        for j in 0..=(k - i) {
+            let mut yy = xx.clone();
+            yy.extend(vv[i..].iter().copied().rev().take(j));
+            yy.sort_unstable();
+            yy.reverse();
+            // k-i-j 回　負の値を戻す
+            for _ in 0..(k - i - j) {
+                if let Some(&last) = yy.last() {
+                    if last.is_negative() {
+                        yy.pop();
+                        continue;
+                    }
+                }
+                break;
+            }
+            rs = rs.max(yy.into_iter().sum());
+        }
+    }
+    println!("{rs}");
+}
