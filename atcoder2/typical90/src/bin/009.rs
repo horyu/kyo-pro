f6a62a65f505f218d141391f53d26a496769a904
horyu <28817604+horyu@users.noreply.github.com>
2023-01-05 15:17:14 +0900

    typical90 009

--- atcoder2/typical90/src/bin/009.rs
+++ atcoder2/typical90/src/bin/009.rs
@@ -11,7 +11,37 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        xxyy: [(f64, f64); n],
     };
-    // println!("{rs}");
+    let mut rs = 0.0f64;
+    for i in 0..n {
+        let (ox, oy) = xxyy[i];
+        let qq = chain!(0..i, (i + 1)..n)
+            .map(|j| {
+                let (x, y) = xxyy[j];
+                let xx = x - ox;
+                let yy = y - oy;
+                // (1, 0)-(0, 0)-(x - ox, y - oy) のなす角度
+                let cos = (x - ox) / (xx.powi(2) + yy.powi(2)).sqrt();
+                let mut theta = cos.acos() * std::f64::consts::FRAC_1_PI * 180.0;
+                if yy.is_sign_negative() {
+                    theta = 360.0 - theta;
+                }
+                theta
+            })
+            .sorted_unstable_by(|a, b| a.partial_cmp(b).unwrap())
+            .collect_vec();
+        for j in 0..(n - 1) {
+            let qj = qq[j];
+            let k = qq.partition_point(|&q| q <= qj + 180.0);
+            for kk in [k.saturating_sub(1), k, k + 1] {
+                if let Some(qk) = qq.get(kk) {
+                    let abs = (qj - qk).abs();
+                    let tmp = abs.min(360.0 - abs);
+                    rs = rs.max(tmp);
+                }
+            }
+        }
+    }
+    println!("{rs}");
 }
