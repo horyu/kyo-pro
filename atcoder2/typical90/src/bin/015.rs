cd551b43d3f0100a03464610d6922304572b76d5
horyu <28817604+horyu@users.noreply.github.com>
2023-01-07 21:53:35 +0900

    typical90 015

--- atcoder2/typical90/src/bin/015.rs
+++ atcoder2/typical90/src/bin/015.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library_rs::ModInt1000000007;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -11,7 +12,59 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    const MOD: usize = 1000000007;
+    let comb = Combination::new(n, MOD);
+    for k in 1..=n {
+        let mut rs = ModInt1000000007::default();
+        for i in 1..=(n / k + 1) {
+            let s1 = n - (k - 1) * (i - 1);
+            let s2 = i;
+            if s2 <= s1 {
+                rs += comb.get(s1, s2);
+            }
+        }
+        println!("{rs}");
+    }
+}
+
+// https://github.com/kenkoooo/competitive-programming-rs/blob/master/src/math/combination.rs
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Self {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
+        }
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
+        }
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
+        }
+        Self {
+            fact,
+            inv_fact,
+            modulo,
+        }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> usize {
+        assert!(x >= y);
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
+    }
+
+    pub fn h(&self, n: usize, r: usize) -> usize {
+        self.get(n + r - 1, r)
+    }
 }
