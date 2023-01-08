af1634e91a1dfb773323fdea5fa8f99df6043cef
horyu <28817604+horyu@users.noreply.github.com>
2023-01-08 16:52:42 +0900

    typical90 019

--- atcoder2/typical90/src/bin/019.rs
+++ atcoder2/typical90/src/bin/019.rs
@@ -11,7 +11,36 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aa: [isize; 2 * n],
     };
-    // println!("{rs}");
+    let nn = 2 * n;
+    let mut dp = vec![vec![1e9 as isize; nn + 1]; nn + 1];
+    for i in 0..(nn - 1) {
+        dp[i][i + 2] = (aa[i] - aa[i + 1]).abs();
+    }
+    for range in (4..=nn).step_by(2) {
+        for l in 0..=(nn - range) {
+            let r = l + range;
+            // eprintln!("{range} {l}-{r}");
+            dp[l][r] = (aa[l] - aa[r - 1]).abs() + dp[l + 1][r - 1];
+            for m in ((l + 2)..=(r - 2)).step_by(2) {
+                dp[l][r] = dp[l][r].min(dp[l][m] + dp[m][r]);
+            }
+            // dp[l][r] = [
+            //     (aa[l] - aa[r - 1]).abs() + dp[l + 1][r - 1],
+            //     dp[l][l + 2] + dp[l + 2][r],
+            //     dp[l][r - 2] + dp[r - 2][r],
+            // ]
+            // .iter()
+            // .min()
+            // .copied()
+            // .unwrap();
+        }
+    }
+    let rs = dp[0][nn];
+    // for (i, dp) in dp.into_iter().enumerate() {
+    //     let s = dp.into_iter().map(|x| if x == (1e9 as isize) { "___".to_string() } else { format!("{x:3}")}).join(",");
+    //     eprintln!("[{i:2}] {}", s);
+    // }
+    println!("{rs}");
 }
