83caa5088b94e11074038af8ee8ceccb9c7feab3
horyu <28817604+horyu@users.noreply.github.com>
2023-01-09 14:19:08 +0900

    typical90 031

--- atcoder2/typical90/src/bin/031.rs
+++ atcoder2/typical90/src/bin/031.rs
@@ -11,7 +11,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        ww: [usize; n],
+        bb: [usize; n],
     };
-    // println!("{rs}");
+    const MAX: usize = (1 + 50) * 50 / 2 + 50;
+    let mut grundy = vec![vec![0; MAX + 50]; 50 + 1];
+    for i in 0..=50 {
+        for j in 0..=MAX {
+            let mut mex = vec![0; MAX + 50];
+            if 1 <= i {
+                mex[grundy[i - 1][j + 1]] = 1;
+            }
+            if 2 <= j {
+                for k in 1..=(j / 2) {
+                    mex[grundy[i][j - k]] = 1;
+                }
+            }
+            for k in 0..=MAX {
+                if mex[k] == 0 {
+                    grundy[i][j] = k;
+                    break;
+                }
+            }
+        }
+    }
+
+    let sum_xor = izip!(ww, bb)
+        .map(|(w, b)| grundy[w][b])
+        .fold(0, |acc, g| acc ^ g);
+    let rs = ["Second", "First"][(sum_xor != 0) as usize];
+    println!("{rs}");
 }
