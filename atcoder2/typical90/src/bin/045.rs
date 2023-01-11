f86cf94afdc341eb6a71ff43b269283c5782fa76
horyu <28817604+horyu@users.noreply.github.com>
2023-01-11 15:31:45 +0900

    typical90 045

--- atcoder2/typical90/src/bin/045.rs
+++ atcoder2/typical90/src/bin/045.rs
@@ -11,7 +11,103 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
+        xxyy: [(isize, isize); n],
     };
-    // println!("{rs}");
+    // bitDP
+    let mut d = vec![vec![0; n]; n];
+    for (i, (ax, ay)) in xxyy.iter().copied().enumerate() {
+        for (j, (bx, by)) in xxyy.iter().copied().enumerate() {
+            d[i][j] = (ax - bx).pow(2) + (ay - by).pow(2);
+        }
+    }
+    let nn = 1 << n;
+    let mut cost = vec![0; nn];
+    for i in 1..nn {
+        for j in 0..n {
+            for kk in 0..j {
+                if (i >> j) & 1 == 1 && (i >> kk) & 1 == 1 {
+                    cost[i] = cost[i].max(d[j][kk]);
+                }
+            }
+        }
+    }
+    let mut dp = vec![vec![1 << 60; nn]; k + 1];
+    dp[0][0] = 0;
+    for i in 1..=k {
+        for j in 1..nn {
+            let mut kk = j;
+            while kk != 0 {
+                dp[i][j] = dp[i][j].min(dp[i - 1][j - kk].max(cost[kk]));
+                kk = (kk - 1) & j;
+            }
+        }
+    }
+    let rs = dp[k][nn - 1];
+    println!("{rs}");
+}
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        n: usize,
+        k: usize,
+        xxyy: [(isize, isize); n],
+    };
+    let mut used = vec![false; n];
+    used[0] = true;
+    let rs = dfs(&mut used, &mut vec![0], k - 1, 0, &xxyy);
+    println!("{rs}");
+}
+
+fn dfs(
+    used: &mut Vec<bool>,
+    gg: &mut Vec<usize>,
+    extra_group: usize,
+    max: usize,
+    xxyy: &[(isize, isize)],
+) -> usize {
+    let cal = |ii: &[usize]| -> usize {
+        // eprintln!("{}", ii.iter().join("|"));
+        ii.iter()
+            .copied()
+            .map(|i| xxyy[i])
+            .tuple_combinations()
+            .map(|(a, b)| a.0.abs_diff(b.0).pow(2u32) + a.1.abs_diff(b.1).pow(2u32))
+            .max()
+            .unwrap_or_default()
+    };
+
+    // 未使用点のインデックス
+    let ii = used.iter().copied().positions(|tf| !tf).collect_vec();
+
+    if ii.len() <= extra_group {
+        return max.max(cal(gg));
+    }
+    if extra_group == 0 {
+        let mut ii = ii;
+        ii.extend_from_slice(gg);
+        return max.max(cal(&ii));
+    }
+
+    // グループを切り直す
+    let i = ii[0];
+    used[i] = true;
+    let mut new_max = max.max(cal(gg));
+    new_max = new_max.max(dfs(used, &mut vec![i], extra_group - 1, new_max, xxyy));
+    used[i] = false;
+    // 現在のグループに追加してDFS
+    for i in ii {
+        if i < gg.last().copied().unwrap() {
+            continue;
+        }
+        used[i] = true;
+        gg.push(i);
+        new_max = new_max.min(dfs(used, gg, extra_group, max, xxyy));
+        gg.pop();
+        used[i] = false;
+    }
+
+    new_max
+    // dbg!(new_max)
 }
