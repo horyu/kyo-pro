69dffc06f44d9e4c2247e61d47a6745a2bad2b74
horyu <28817604+horyu@users.noreply.github.com>
2023-01-13 14:25:03 +0900

    typical90 054

--- atcoder2/typical90/src/bin/054.rs
+++ atcoder2/typical90/src/bin/054.rs
@@ -11,7 +11,45 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
     };
-    // println!("{rs}");
+    let i2rr = (0..m)
+        .map(|_| {
+            input! {
+                k: usize,
+                rr: [Usize1; k],
+            };
+            rr
+        })
+        .collect_vec();
+    let mut r2ii = vec![vec![]; n];
+    for (i, rr) in i2rr.iter().enumerate() {
+        for &r in rr {
+            r2ii[r].push(i);
+        }
+    }
+
+    let mut qq = VecDeque::new();
+    let mut checked_i = vec![false; m];
+
+    let mut rs = vec![-1; n];
+    rs[0] = 0;
+    qq.push_back(0);
+
+    while let Some(qr) = qq.pop_front() {
+        for &i in &r2ii[qr] {
+            if !checked_i[i] {
+                checked_i[i] = true;
+                for &r in &i2rr[i] {
+                    if rs[r] == -1 {
+                        rs[r] = rs[qr] + 1;
+                        qq.push_back(r);
+                    }
+                }
+            }
+        }
+    }
+
+    let rs = rs.iter().join("\n");
+    println!("{rs}");
 }
