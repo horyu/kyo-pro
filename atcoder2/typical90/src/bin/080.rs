595746ec607a89db82c4c4fb933436655a88fd54
horyu <28817604+horyu@users.noreply.github.com>
2023-01-14 19:00:00 +0900

    typical90 080

--- atcoder2/typical90/src/bin/080.rs
+++ atcoder2/typical90/src/bin/080.rs
@@ -11,7 +11,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        d: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut rs = 0isize;
+    for i in 0..(1 << n) {
+        let mut bit = 0;
+        let mut conditions = 0;
+        for j in 0..n {
+            if (i >> j) & 1 == 1 {
+                bit |= aa[j];
+                conditions += 1;
+            }
+        }
+        let mut free_digits = 0;
+        for j in 0..d {
+            if (bit >> j) & 1 == 0 {
+                free_digits += 1;
+            }
+        }
+        if conditions.is_even() {
+            rs += 1 << free_digits;
+        } else {
+            rs -= 1 << free_digits;
+        }
+    }
+    println!("{rs}");
 }
