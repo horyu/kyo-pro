a7490ebbc092d3d18e105a81b8cce7750ead7dcc
horyu <28817604+horyu@users.noreply.github.com>
2023-01-15 14:45:01 +0900

    typical90 074

--- atcoder2/typical90/src/bin/074.rs
+++ atcoder2/typical90/src/bin/074.rs
@@ -11,7 +11,41 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Chars,
     };
-    // println!("{rs}");
+    let ii = s
+        .iter()
+        .copied()
+        .map(|c| match c {
+            'a' => 0,
+            'b' => 1,
+            _ => 2,
+        })
+        .collect_vec();
+
+    let mut hm = HashMap::new();
+    for i in 0..3 {
+        hm.insert(vec![i], i);
+    }
+
+    let rs = dfs(&ii, &mut hm);
+    println!("{rs}");
+}
+
+fn dfs(ii: &[usize], hm: &mut HashMap<Vec<usize>, usize>) -> usize {
+    if let Some(cnt) = hm.get(&ii.to_vec()).copied() {
+        return cnt;
+    }
+
+    let len = ii.len();
+    let init_cnt = dfs(&ii[..(len - 1)], hm);
+
+    let rs = match ii[len - 1] {
+        0 => init_cnt,
+        1 => init_cnt + dfs(&vec![1; len - 1], hm) + 1,
+        _ => init_cnt + dfs(&vec![1; len - 1], hm) * 2 + 2,
+    };
+    hm.insert(ii.to_vec(), rs);
+
+    rs
 }
