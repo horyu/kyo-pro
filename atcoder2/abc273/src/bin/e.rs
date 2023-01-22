4436438cd85469bf92d558514dd7faa2480058ff
horyu <28817604+horyu@users.noreply.github.com>
2023-01-22 12:11:28 +0900

    abc273 e

--- atcoder2/abc273/src/bin/e.rs
+++ atcoder2/abc273/src/bin/e.rs
@@ -12,22 +12,37 @@ fn main() {
     input! {
         q: usize,
     };
-    let mut rs = vec![-1; q];
+    let mut rs = vec![];
+    let mut aa = vec![];
+    let mut cur = None;
+    let mut hm = HashMap::new();
     for i in 0..q {
-        input! {
-            s: String,
-        };
+        input! { s: String };
         match s.as_str() {
             "ADD" => {
                 input! {x: isize};
+                aa.push((cur, x));
+                cur = Some(aa.len() - 1);
             }
-            "DELETE" => {}
+            "DELETE" => {
+                if let Some(i) = cur {
+                    cur = aa[i].0;
+                }
+            }
             "SAVE" => {
                 input! {y: isize};
+                hm.insert(y, cur);
             }
+            // LOAD
             _ => {
                 input! {z: isize};
+                cur = hm.get(&z).copied().unwrap_or_default();
             }
+        }
+        if let Some(i) = cur {
+            rs.push(aa[i].1);
+        } else {
+            rs.push(-1)
         }
     }
     println!("{}", rs.into_iter().join(" "));
