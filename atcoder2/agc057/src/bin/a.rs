4171ddc6722aad341dbbe40a3178651539674d93
horyu <28817604+horyu@users.noreply.github.com>
2023-01-29 18:18:16 +0900

    agc057 a

---
+++ atcoder2/agc057/src/bin/a.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        t: usize,
+        llrr: [(usize, usize); t],
+    };
+    for (l, r) in llrr {
+        if l.ilog10() == r.ilog10() {
+            println!("{}", r - l + 1);
+            continue;
+        }
+        let base = 10usize.pow(r.ilog10());
+        let mut rs = r + 1 - base;
+        if r / base == 1 {
+            rs += base - l.max(r % base + 1).max(r / 10 + 1);
+        }
+        println!("{rs}");
+    }
+}
