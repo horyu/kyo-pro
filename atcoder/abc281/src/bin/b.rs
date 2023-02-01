72852308d62311492ac890df5145afe4b0fbddf0
horyu <28817604+horyu@users.noreply.github.com>
2023-02-02 08:03:59 +0900

    abc281 b

--- atcoder/abc281/src/bin/b.rs
+++ atcoder/abc281/src/bin/b.rs
@@ -1,3 +1,18 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop)]
+#![allow(clippy::uninlined_format_args)]
+#![allow(unused_imports)]
+use itertools::Itertools;
+use num_integer::*;
+use proconio::{input, marker::*};
+use std::collections::*;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    };
+    let tf = regex::Regex::new("^[A-Z][1-9]\\d{5}[A-Z]$")
+        .unwrap()
+        .is_match(s.as_str());
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{}", rs)
 }
