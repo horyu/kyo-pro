a8c238af810c16bfdd734b0bc2241c6265cb2328
horyu <28817604+horyu@users.noreply.github.com>
2023-02-06 08:18:31 +0900

    abc112 c

--- atcoder2/abc112/src/bin/c.rs
+++ atcoder2/abc112/src/bin/c.rs
@@ -11,7 +11,25 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        xxyyhh: [(usize, usize, usize); n],
     };
-    // println!("{rs}");
+    let (h_min, h_max) = match xxyyhh.iter().map(|xyh| xyh.2).minmax() {
+        itertools::MinMaxResult::NoElements => unreachable!(),
+        itertools::MinMaxResult::OneElement(h) => (h, h),
+        itertools::MinMaxResult::MinMax(min, max) => (min, max),
+    };
+    for i in 0usize..=100 {
+        for j in 0usize..=100 {
+            for k in h_max..=(h_min + 200) {
+                if xxyyhh
+                    .iter()
+                    .copied()
+                    .all(|(x, y, h)| k.saturating_sub(i.abs_diff(x) + j.abs_diff(y)) == h)
+                {
+                    println!("{i} {j} {k}");
+                    return;
+                }
+            }
+        }
+    }
 }
