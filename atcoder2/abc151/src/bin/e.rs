98eb1762d050e4b4d918549c6fa2961ade4e490c
horyu <28817604+horyu@users.noreply.github.com>
2023-02-25 22:24:01 +0900

    abc151 e

---
+++ atcoder2/abc151/src/bin/e.rs
@@ -0,0 +1,78 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use ac_library_rs::ModInt1000000007;
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        mut aa: [isize; n],
+    };
+    aa.sort_unstable();
+    if k == 1 {
+        println!("0");
+        return;
+    }
+
+    let comb = Combination::new(n, 1000000007);
+    let mut rs = ModInt1000000007::default();
+    for (i, a) in aa.into_iter().enumerate() {
+        if k - 1 <= i {
+            rs += a * comb.get(i, k - 1) as isize;
+            // eprintln!("{i} {a}: +{} ", comb.get(i, k - 1));
+        }
+        if k - 1 <= n.saturating_sub(i + 1) {
+            rs -= a * comb.get(n.saturating_sub(i + 1), k - 1) as isize;
+            // eprintln!("{i} {a}: -{} ", comb.get(n.saturating_sub(i + 1), k - 1));
+        }
+    }
+    println!("{rs}");
+}
+
+// https://github.com/kenkoooo/competitive-programming-rs/blob/master/src/math/combination.rs
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Self {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
+        }
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
+        }
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
+        }
+        Self {
+            fact,
+            inv_fact,
+            modulo,
+        }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> usize {
+        assert!(x >= y);
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
+    }
+
+    pub fn h(&self, n: usize, r: usize) -> usize {
+        self.get(n + r - 1, r)
+    }
+}
