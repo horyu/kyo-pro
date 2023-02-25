39e5d402138cbe553ae747cb67071ff85b182589
horyu <28817604+horyu@users.noreply.github.com>
2023-02-26 08:42:31 +0900

    abc242 g

--- atcoder2/abc242/src/bin/g.rs
+++ atcoder2/abc242/src/bin/g.rs
@@ -11,7 +11,53 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aa: [Usize1; n],
+        q: usize,
+        llrr: [(usize, usize); q],
     };
-    // println!("{rs}");
+    // Mo's algorithm
+    let size = n.sqrt();
+    let mut qqq = vec![vec![]; size + 2];
+    for (i, (l, r)) in llrr.into_iter().enumerate() {
+        let l = l - 1;
+        qqq[l / size].push((r, l, i));
+    }
+    let mut rrss = vec![0; q];
+    for mut qq in qqq {
+        if qq.is_empty() {
+            continue;
+        }
+        qq.sort_unstable();
+        let mut l = qq[0].1;
+        let mut r = l;
+        let mut vv = vec![0; n];
+        let mut cnt = 0;
+        for (qr, ql, qi) in qq {
+            while r < qr {
+                vv[aa[r]] += 1;
+                if vv[aa[r]].is_even() {
+                    cnt += 1;
+                }
+                r += 1;
+            }
+            while l < ql {
+                vv[aa[l]] -= 1;
+                if vv[aa[l]].is_odd() {
+                    cnt -= 1;
+                }
+                l += 1;
+            }
+            while ql < l {
+                l -= 1;
+                vv[aa[l]] += 1;
+                if vv[aa[l]].is_even() {
+                    cnt += 1;
+                }
+            }
+            rrss[qi] = cnt;
+        }
+    }
+    for rs in rrss {
+        println!("{rs}");
+    }
 }
