002bcfaf277c0a13013e3cc0b33758a7a380785e
horyu <28817604+horyu@users.noreply.github.com>
2023-02-28 09:30:25 +0900

    abc198 d

--- atcoder2/abc198/src/bin/d.rs
+++ atcoder2/abc198/src/bin/d.rs
@@ -14,33 +14,27 @@ fn main() {
         s2: Chars,
         s3: Chars,
     };
-    let mut hs = HashSet::new();
+    let mut cc = vec![];
     for &c in chain!(&s1, &s2, &s3) {
-        hs.insert(c);
+        cc.push(c);
     }
-    let len = hs.len();
+    cc.sort_unstable();
+    cc.dedup();
+    let len = cc.len();
     if 10 < len {
         println!("UNSOLVABLE");
         return;
     }
-    let cc = hs.into_iter().collect_vec();
-    let s_to_num = |s: &[char], ii: &[usize]| -> usize {
-        s.iter().fold(0, |acc, &c| {
-            acc * 10 + ii[cc.iter().position(|&cc_c| cc_c == c).unwrap()]
-        })
-    };
-    // eprintln!("{}", cc.iter().join(""));
     for ii in (0usize..=9).permutations(len) {
-        let n1 = s_to_num(&s1, &ii);
-        let n2 = s_to_num(&s2, &ii);
-        let n3 = s_to_num(&s3, &ii);
-        // eprintln!("{n1} {n2} {n3} : {}", ii.iter().join(","));
-        if [(n1, &s1), (n2, &s2), (n3, &s3)]
-            .into_iter()
-            .any(|(n, s)| n == 0 || n.ilog10() + 1 != s.len() as u32)
-        {
+        let c_to_num = |c: char| -> usize { ii[cc.iter().position(|&cc| cc == c).unwrap()] };
+        if [s1[0], s2[0], s3[0]].iter().any(|&c| c_to_num(c) == 0) {
             continue;
         }
+        let s_to_num =
+            |s: &[char]| -> usize { s.iter().copied().fold(0, |acc, c| acc * 10 + c_to_num(c)) };
+        let n1 = s_to_num(&s1);
+        let n2 = s_to_num(&s2);
+        let n3 = s_to_num(&s3);
         if n1 + n2 == n3 {
             println!("{n1}\n{n2}\n{n3}");
             return;
