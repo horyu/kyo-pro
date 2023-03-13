8f9088a42203458853c85528b5ab03a0f262576c
horyu <28817604+horyu@users.noreply.github.com>
2023-03-13 19:14:35 +0900

    abc157 e

--- atcoder2/abc157/src/bin/e.rs
+++ atcoder2/abc157/src/bin/e.rs
@@ -11,7 +11,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Bytes,
+        q: usize,
     };
-    // println!("{rs}");
+    let mut s = s.into_iter().map(|b| (b - b'a') as usize).collect_vec();
+    let mut bbttss = vec![BTreeSet::<usize>::new(); 26];
+    for (i, &c) in s.iter().enumerate() {
+        bbttss[c].insert(i);
+    }
+    for _ in 0..q {
+        input! {x: usize};
+        if x == 1 {
+            input! {i: Usize1, c: char};
+            let c = (c as u8 - b'a') as usize;
+            let ori_c = s[i];
+            if c != ori_c {
+                bbttss[ori_c].remove(&i);
+                bbttss[c].insert(i);
+                s[i] = c;
+            }
+        } else {
+            input! {l: Usize1, r: Usize1};
+            let mut rs = 0;
+            for bts in &bbttss {
+                if bts.range(l..=r).into_iter().next().is_some() {
+                    rs += 1;
+                }
+            }
+            println!("{rs}");
+        }
+    }
 }
