147751ac7b15c564bf77483d958f8ac129df4a59
horyu <28817604+horyu@users.noreply.github.com>
2023-03-23 20:43:51 +0900

    abc169 e

---
+++ atcoder2/abc169/src/bin/e.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        aabb: [(usize, usize); n],
+    };
+    // https://blog.hamayanhamayan.com/entry/2020/06/01/210806
+    let aa = aabb.iter().map(|ab| ab.0).sorted_unstable().collect_vec();
+    let bb = aabb.iter().map(|ab| ab.1).sorted_unstable().collect_vec();
+    if n.is_even() {
+        let l = aa[n / 2] + aa[n / 2 - 1];
+        let r = bb[n / 2] + bb[n / 2 - 1];
+        println!("{}", r - l + 1);
+    } else {
+        let l = aa[n / 2];
+        let r = bb[n / 2];
+        println!("{}", r - l + 1);
+    }
+}
