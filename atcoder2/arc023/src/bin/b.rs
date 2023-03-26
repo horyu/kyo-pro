70a0f01c469286f4277df3b91187b3eb68b60a68
horyu <28817604+horyu@users.noreply.github.com>
2023-03-26 09:12:50 +0900

    arc023 b

---
+++ atcoder2/arc023/src/bin/b.rs
@@ -0,0 +1,33 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        r: usize,
+        c: usize,
+        d: usize,
+        aaa: [[usize; c]; r],
+    };
+    let mut rs = 0;
+    for i in 0..r {
+        if d < i {
+            break;
+        }
+        for j in 0..c {
+            if d < i + j {
+                break;
+            }
+            if (i + j) % 2 == d % 2 {
+                rs = rs.max(aaa[i][j]);
+            }
+        }
+    }
+    println!("{rs}");
+}
