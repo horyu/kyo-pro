fddeaa9217c1f6b617ece1a380a23d0fa3605c27
horyu <28817604+horyu@users.noreply.github.com>
2023-03-27 09:51:13 +0900

    abc174 e

---
+++ atcoder2/abc174/src/bin/e.rs
@@ -0,0 +1,38 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n],
+    };
+    if k == 0 {
+        let rs = aa.into_iter().max().unwrap();
+        println!("{rs}");
+        return;
+    }
+    let mut ng = 0usize;
+    let mut ok = 1e9 as usize;
+    while 1 < ok - ng {
+        let m = (ok + ng) / 2;
+        let mut tmp = 0;
+        for &a in &aa {
+            tmp += a.div_ceil(m) - 1;
+        }
+        if tmp <= k {
+            ok = m;
+        } else {
+            ng = m;
+        }
+    }
+    let rs = ok;
+    println!("{rs}");
+}
