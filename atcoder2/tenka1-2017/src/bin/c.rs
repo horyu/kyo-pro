be2d2010af708f35ac7a7d3e5d52f8c00a06a15e
horyu <28817604+horyu@users.noreply.github.com>
2023-04-03 08:15:57 +0900

    tenka1-2017 c

---
+++ atcoder2/tenka1-2017/src/bin/c.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: isize,
+    };
+    // 4/n = 1/x + 1/y + 1/z
+    // 4xyz = n(xy + yz + zx)
+    // z(4xy - nx - ny) = nxy
+    // z = nxy/(4xy - nx - ny)
+    for x in 1..=3500 {
+        for y in 1..=3500 {
+            if let Some(z) = (n * x * y).checked_div(4 * x * y - n * x - n * y) {
+                if (1..=3500).contains(&z) && 4 * x * y * z == n * (x * y + y * z + z * x) {
+                    println!("{x} {y} {z}");
+                    return;
+                }
+            }
+        }
+    }
+}
