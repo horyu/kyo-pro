967cca4552bf38cf61ed920aa8a46d41a658c394
horyu <28817604+horyu@users.noreply.github.com>
2023-04-06 19:23:01 +0900

    abc183 e

---
+++ atcoder2/abc183/src/bin/e.rs
@@ -0,0 +1,48 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use ac_library_rs::ModInt1000000007;
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        s: [Chars; h],
+    };
+    let mut d2iijj = vec![vec![]; h + w];
+    for i in 0..h {
+        for j in 0..w {
+            d2iijj[i + j].push((i, j));
+        }
+    }
+    // 貰うDP、壁だったらそこをリセット
+    let mut dp = vec![vec![ModInt1000000007::new(0); w]; h];
+    let mut yoko = vec![ModInt1000000007::new(0); h];
+    let mut tate = vec![ModInt1000000007::new(0); w];
+    let mut naname = vec![ModInt1000000007::new(0); h + w];
+    dp[0][0] += 1;
+    for (i, j) in d2iijj.into_iter().flatten() {
+        // 斜め用index
+        let k = (h - 1 - i) + j;
+        if s[i][j] == '#' {
+            yoko[i] *= 0;
+            tate[j] *= 0;
+            naname[k] *= 0;
+            continue;
+        }
+        dp[i][j] += yoko[i] + tate[j] + naname[k];
+        let v = dp[i][j];
+        yoko[i] += v;
+        tate[j] += v;
+        naname[k] += v;
+        // eprintln!("{i} {j} {}", dp[i][j]);
+    }
+    let rs = dp[h - 1][w - 1];
+    println!("{rs}");
+}
