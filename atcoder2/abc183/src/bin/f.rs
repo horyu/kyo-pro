8652d0f8834dde7b8f162c16224322d13cc6248d
horyu <28817604+horyu@users.noreply.github.com>
2023-04-10 18:54:17 +0900

    abc183 f

--- atcoder2/abc183/src/bin/f.rs
+++ atcoder2/abc183/src/bin/f.rs
@@ -11,7 +11,39 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        cc: [Usize1; n],
+        qq: [(usize, Usize1, Usize1); q],
     };
-    // println!("{rs}");
+    let mut uf = UnionFind::new(n);
+    let mut hhmm = vec![HashMap::new(); n];
+    for (i, c) in cc.iter().copied().enumerate() {
+        hhmm[i].insert(c, 1usize);
+    }
+    for (q_type, a, b) in qq {
+        if q_type == 1 {
+            let ia = uf.find(a);
+            let ib = uf.find(b);
+            if ia == ib {
+                continue;
+            }
+            let (sml_i, big_i) = if hhmm[ia].len() < hhmm[ib].len() {
+                (ia, ib)
+            } else {
+                (ib, ia)
+            };
+            let sml_hm = std::mem::take(&mut hhmm[sml_i]);
+            for (k, v) in sml_hm {
+                *hhmm[big_i].entry(k).or_insert(0) += v;
+            }
+
+            uf.union(a, b);
+            if sml_i == uf.find(a) {
+                hhmm.swap(sml_i, big_i);
+            }
+        } else {
+            let rs = hhmm[uf.find(a)].get(&b).copied().unwrap_or_default();
+            println!("{rs}");
+        }
+    }
 }
