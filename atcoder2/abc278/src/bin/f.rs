3f239e334cedf6c3c25a1de95fafdb0dcafd894d
horyu <28817604+horyu@users.noreply.github.com>
2023-04-27 21:36:34 +0900

    abc278 f

--- atcoder2/abc278/src/bin/f.rs
+++ atcoder2/abc278/src/bin/f.rs
@@ -4,7 +4,7 @@
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
-use petgraph::{matrix_graph::Zero, unionfind::UnionFind};
+use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
@@ -13,25 +13,20 @@ fn main() {
         n: usize,
         ss: [Bytes; n],
     };
-    let mut ff = vec![];
-    let mut tt = vec![];
-    for s in ss {
-        ff.push(s[0] - b'a');
-        tt.push(*s.last().unwrap() - b'a');
-    }
+    let ffee = ss
+        .into_iter()
+        .map(|s| (s[0] - b'a', s.last().copied().unwrap() - b'a'))
+        .collect_vec();
     // bit DP
-    let mut dp = vec![0usize; 1 << n];
-    for s in 1..(1 << n) {
+    let mut dp = vec![0; 1 << n];
+    for s in 0..(1usize << n) {
         for c in 0..n {
-            if 0 < 1 & (s >> c) {
-                dp[s] |= (1 & !dp[s ^ 1 << c] >> tt[c]) << ff[c];
+            if 0 != 1 & (s >> c) {
+                let (f, e) = ffee[c];
+                dp[s] |= (1 & !dp[s ^ (1 << c)] >> e) << f;
             }
         }
     }
-    let rs = if dp.last().unwrap().is_zero() {
-        "Second"
-    } else {
-        "First"
-    };
+    let rs = ["Second", "First"][(dp.last().copied().unwrap() != 0) as usize];
     println!("{rs}");
 }
