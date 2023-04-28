8756805e302f589a9af0e274cd4411de9b4cf897
horyu <28817604+horyu@users.noreply.github.com>
2023-04-28 12:55:35 +0900

    abc191 e

--- atcoder2/abc191/src/bin/e.rs
+++ atcoder2/abc191/src/bin/e.rs
@@ -11,7 +11,43 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aabbcc: [(Usize1, Usize1, usize); m],
     };
-    // println!("{rs}");
+    let mut rs = vec![!0usize; n];
+    let mut g = vec![HashMap::new(); n];
+    for (a, b, c) in aabbcc.iter().copied() {
+        if a == b {
+            rs[a] = rs[a].min(c);
+        } else {
+            let e = g[a].entry(b).or_insert(!0);
+            *e = c.min(*e);
+        }
+    }
+    for i in 0..n {
+        use std::cmp::Reverse;
+        let mut bh = BinaryHeap::from_iter(g[i].iter().map(|(&k, &v)| (Reverse(v), k)));
+        let mut confirmed = vec![false; n];
+        while let Some((Reverse(d), j)) = bh.pop() {
+            if j == i {
+                rs[i] = rs[i].min(d);
+                break;
+            }
+            if !confirmed[j] {
+                confirmed[j] = true;
+                for (&k, &c) in g[j].iter() {
+                    if !confirmed[k] {
+                        bh.push((Reverse(d + c), k));
+                    }
+                }
+            }
+        }
+    }
+    for rs in rs {
+        if rs == !0 {
+            println!("-1");
+        } else {
+            println!("{rs}");
+        }
+    }
 }
