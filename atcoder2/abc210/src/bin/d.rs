30638e8345eb99fba550b59d617e0b2cf5bcfbc2
horyu <28817604+horyu@users.noreply.github.com>
2023-05-04 12:39:57 +0900

    abc210 d

--- atcoder2/abc210/src/bin/d.rs
+++ atcoder2/abc210/src/bin/d.rs
@@ -12,9 +12,36 @@ fn main() {
     input! {
         h: usize,
         w: usize,
-        c: usize,
-        aaa: [[usize; w]; h],
+        c: isize,
+        aaa: [[isize; w]; h],
     };
-
-    // println!("{rs}");
+    // https://kanpurin.hatenablog.com/entry/2021/07/17/232213
+    let solve = |aaa: &[Vec<isize>]| -> isize {
+        let mut dp = vec![vec![isize::MAX; w + 1]; h + 1];
+        let mut ans = 1 << 60;
+        for i in 0..h {
+            for j in 0..w {
+                dp[i + 1][j + 1] = dp[i + 1][j].min(dp[i][j + 1]);
+                if dp[i + 1][j + 1] != isize::MAX {
+                    ans = ans.min(dp[i + 1][j + 1] + aaa[i][j] + c * (i + j) as isize);
+                }
+                dp[i + 1][j + 1] = dp[i + 1][j + 1].min(aaa[i][j] - c * (i + j) as isize);
+            }
+        }
+        ans
+    };
+    let mut rs = isize::MAX;
+    for i in 0..4 {
+        let mut bbb = aaa.clone();
+        if 0 < i & 1 {
+            bbb.reverse();
+        }
+        if 0 < i & 2 {
+            for bb in bbb.iter_mut() {
+                bb.reverse();
+            }
+        }
+        rs = rs.min(solve(&bbb));
+    }
+    println!("{rs}");
 }
