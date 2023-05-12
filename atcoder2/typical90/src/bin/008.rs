462bc18cf3c98a32253d8007eb910e839a0cf40d
horyu <28817604+horyu@users.noreply.github.com>
2023-05-13 08:20:22 +0900

    typical90 008

--- atcoder2/typical90/src/bin/008.rs
+++ atcoder2/typical90/src/bin/008.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library_rs::ModInt1000000007;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -11,7 +12,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Chars,
     };
-    // println!("{rs}");
+    let vv = s
+        .iter()
+        .copied()
+        .filter_map(|c| match c {
+            'a' => Some(0),
+            't' => Some(1),
+            'c' => Some(2),
+            'o' => Some(3),
+            'd' => Some(4),
+            'e' => Some(5),
+            'r' => Some(6),
+            _ => None,
+        })
+        .collect_vec();
+    let mut dp = vec![ModInt1000000007::default(); 9];
+    for v in vv {
+        if v == 0 {
+            dp[v] += 1;
+        } else {
+            dp[v] = dp[v] + dp[v - 1];
+        }
+    }
+    let rs = dp[6];
+    println!("{rs}");
 }
