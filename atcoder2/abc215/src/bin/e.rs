6b2f1cbe74450f7710b8eba5d946de20c154bce9
horyu <28817604+horyu@users.noreply.github.com>
2023-05-12 22:04:36 +0900

    abc215 e

---
+++ atcoder2/abc215/src/bin/e.rs
@@ -0,0 +1,47 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use ac_library::ModInt998244353;
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        s: Bytes,
+    };
+    // https://atcoder.jp/contests/abc215/editorial/2483
+    let s = s.into_iter().map(|b| (b - b'A') as usize).collect_vec();
+    let mut dp = vec![vec![[ModInt998244353::default(); 10]; 1024]; n + 1];
+    for (i, b) in s.iter().copied().enumerate() {
+        for u in 0..1024 {
+            for j in 0..10 {
+                let tmp = dp[i][u][j];
+                dp[i + 1][u][j] = tmp;
+                if j == b {
+                    dp[i + 1][u][j] += tmp;
+                }
+            }
+        }
+        for v in 0..1024 {
+            if 0 == (v & (1 << b)) {
+                for j in 0..10 {
+                    let tmp = dp[i][v][j];
+                    dp[i + 1][v | (1 << b)][b] += tmp;
+                }
+            }
+        }
+        dp[i + 1][1 << b][b] += 1;
+    }
+    let mut rs = ModInt998244353::default();
+    for u in 0..1024 {
+        for j in 0..10 {
+            rs += dp[n][u][j];
+        }
+    }
+    println!("{rs}");
+}
