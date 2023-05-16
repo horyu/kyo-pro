83766fa41dc5b02187ffdec6aa73a52e39c97775
horyu <28817604+horyu@users.noreply.github.com>
2023-05-16 21:55:10 +0900

    abc216 f

--- atcoder2/abc216/src/bin/f.rs
+++ atcoder2/abc216/src/bin/f.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library_rs::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,6 +13,33 @@ fn main() {
     input! {
         n: usize,
         aa: [usize; n],
+        bb: [usize; n],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc216/editorial/2560
+    let ii = (0..n)
+        .sorted_unstable_by(|&i, &j| {
+            let (ai, aj) = (aa[i], aa[j]);
+            let (bi, bj) = (bb[i], bb[j]);
+            ai.cmp(&aj).then(bi.cmp(&bj))
+        })
+        .collect_vec();
+    let cc = ii.iter().copied().map(|i| aa[i]).collect_vec();
+    let dd = ii.iter().copied().map(|i| bb[i]).collect_vec();
+
+    let mut rs = ModInt998244353::default();
+    let mut arr = [ModInt998244353::default(); 5001];
+    arr[0] += 1;
+    for (c, d) in izip!(cc, dd) {
+        let mut new_arr = arr;
+        for i in 0..=5000 {
+            if d <= i {
+                new_arr[i] += arr[i - d];
+            }
+            if i + d <= c {
+                rs += arr[i];
+            }
+        }
+        arr = new_arr;
+    }
+    println!("{rs}");
 }
