0f6214f8c244b3cb6754b28fa2c048230a7f0249
horyu <28817604+horyu@users.noreply.github.com>
2023-05-17 09:51:42 +0900

    abc280 d

--- atcoder2/abc280/src/bin/d.rs
+++ atcoder2/abc280/src/bin/d.rs
@@ -10,8 +10,47 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        k: usize,
     };
-    // println!("{rs}");
+    let mut rs = 0;
+    for (k, v) in factors(k) {
+        let mut tmp = 1;
+        let mut cnt = 0;
+        for mut i in 1.. {
+            tmp = k * i;
+            while i % k == 0 {
+                i /= k;
+                cnt += 1;
+            }
+            cnt += 1;
+            if v <= cnt {
+                break;
+            }
+        }
+        rs = rs.max(tmp);
+    }
+    println!("{rs}");
+}
+
+fn factors(mut n: usize) -> HashMap<usize, usize> {
+    let mut hm = HashMap::new();
+    if n <= 1 {
+        return hm;
+    }
+    while n % 2 == 0 {
+        n /= 2;
+        *hm.entry(2).or_insert(0) += 1;
+    }
+    let mut i = 3;
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+            *hm.entry(i).or_insert(0) += 1;
+        }
+        i += 2;
+    }
+    if n != 1 {
+        hm.insert(n, 1);
+    }
+    hm
 }
