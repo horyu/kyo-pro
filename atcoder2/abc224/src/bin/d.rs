59b8ad048e305baee89cac7a16d538e8eded9931
horyu <28817604+horyu@users.noreply.github.com>
2023-05-30 08:54:45 +0900

    abc224 d

--- atcoder2/abc224/src/bin/d.rs
+++ atcoder2/abc224/src/bin/d.rs
@@ -10,8 +10,41 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvv: [(Usize1, Usize1); m],
+        pp: [Usize1; 8],
     };
-    // println!("{rs}");
+    let mut arr: [usize; 9] = [8; 9];
+    for (i, p) in pp.iter().copied().enumerate() {
+        arr[p] = i;
+    }
+    // eprintln!("{arr:?}");
+    let mut g = vec![vec![]; 9];
+    for (u, v) in uuvv.iter().copied() {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    // for (i, vv) in g.iter().enumerate() {
+    //     eprintln!("{i} {vv:?}");
+    // }
+    let mut qq = VecDeque::new();
+    qq.push_back((arr, 0));
+    let mut pushed = HashSet::new();
+    pushed.insert(arr);
+    while let Some((qarr, qc)) = qq.pop_front() {
+        if qarr.iter().enumerate().all(|(i, &p)| i == p) {
+            println!("{qc}");
+            return;
+        }
+        let i = qarr.iter().position(|&p| p == 8).unwrap();
+        for j in g[i].iter().copied() {
+            let mut arr = qarr;
+            arr.swap(i, j);
+            if pushed.insert(arr) {
+                // eprintln!("{qc}({i}:{j}) {qarr:?}->{arr:?}");
+                qq.push_back((arr, qc + 1));
+            }
+        }
+    }
+    println!("-1");
 }
