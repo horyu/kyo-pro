b88bc773bd89e6e1a78bc02ef1db96a417adfa6c
horyu <28817604+horyu@users.noreply.github.com>
2023-06-05 21:23:11 +0900

    abc281 d

--- atcoder2/abc281/src/bin/d.rs
+++ atcoder2/abc281/src/bin/d.rs
@@ -11,7 +11,28 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
+        d: isize,
+        aa: [isize; n],
     };
-    // println!("{rs}");
+    let mut rs = -1;
+    let mut hm: HashMap<(isize, usize), isize> = HashMap::new();
+    hm.insert((0, 0), 0);
+    for a in aa {
+        let mut new_hm = hm.clone();
+        for ((km, kc), s) in hm {
+            let kkm = (km + a) % d;
+            let kkc = kc + 1;
+            if kkc == k {
+                if kkm == 0 {
+                    rs = rs.max(s + a);
+                }
+            } else {
+                let e = new_hm.entry((kkm, kkc)).or_default();
+                *e = (*e).max(s + a);
+            }
+        }
+        hm = new_hm;
+    }
+    println!("{rs}");
 }
