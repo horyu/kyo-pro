d715f97c75bec3c8051da9eb86243abf5bbb651a
horyu <28817604+horyu@users.noreply.github.com>
2023-06-20 09:16:42 +0900

    abc242 e

--- atcoder2/abc242/src/bin/e.rs
+++ atcoder2/abc242/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -10,8 +11,31 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
+        nnss: [(usize, Bytes); t],
     };
-    // println!("{rs}");
+    for (n, s) in nnss {
+        let mut rs = ModInt998244353::default();
+        let m = n.div_ceil(2);
+        for i in 0..m {
+            let kouho = s[i] - b'A';
+            rs += ModInt998244353::new(26).pow((m - 1 - i) as u64) * kouho;
+        }
+        // abc  n3 i0
+        // abcd n4 i01
+        let mut tf = true;
+        for i in 0..m {
+            // eprintln!("{n} {i} {} {}", s[m - 1 - i] as char, s[n / 2 + i] as char);
+            match s[m - 1 - i].cmp(&s[n / 2 + i]) {
+                std::cmp::Ordering::Less => break,
+                std::cmp::Ordering::Equal => continue,
+                std::cmp::Ordering::Greater => {
+                    tf = false;
+                    break;
+                }
+            }
+        }
+        rs += tf as u32;
+        println!("{rs}");
+    }
 }
