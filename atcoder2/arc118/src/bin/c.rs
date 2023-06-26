b29e656be1ae41bb44de271a1b13ec106cc1ffae
horyu <28817604+horyu@users.noreply.github.com>
2023-06-26 21:34:09 +0900

    arc118 c

--- atcoder2/arc118/src/bin/c.rs
+++ atcoder2/arc118/src/bin/c.rs
@@ -11,7 +11,23 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut bts = BTreeSet::new();
+    for i in 1.. {
+        for x in [2 * 3, 2 * 5, 3 * 5] {
+            let y = i * x;
+            if y <= 10000 && bts.insert(i * x) && bts.len() == n {
+                println!("{}", bts.iter().join(" "));
+                // dbg!(bts.iter().copied().tuple_combinations().all(|(a, b)| a.gcd(&b) > 1));
+                // dbg!(bts.iter().copied().fold(105, |a, b| a.gcd(&b)));
+                return;
+            }
+        }
+    }
 }
+/*
+2,3,5
+2.3 2.5 3.5 2.3.5 = 2(3+5+3*5)
+2,3,5,7
+2.3 2.5 2.7 2.3.5 2.3.7 3.5.7 (2+3+4+5+6+7+8)*(2.3 2.5 2.7 2.3.5 2.3.7)
+ */
