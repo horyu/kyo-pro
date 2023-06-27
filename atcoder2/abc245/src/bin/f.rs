f2dda856fe7801f125e80d762e02d8b730e8176e
horyu <28817604+horyu@users.noreply.github.com>
2023-06-27 22:46:38 +0900

    abc245 f

--- atcoder2/abc245/src/bin/f.rs
+++ atcoder2/abc245/src/bin/f.rs
@@ -11,7 +11,39 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvv: [(Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc245/editorial/3664
+    let mut g = vec![vec![]; n];
+    for (u, v) in uuvv.iter().copied() {
+        g[u].push(v);
+    }
+    let vvv = pathfinding::directed::strongly_connected_components::strongly_connected_components(
+        &(0..n).collect_vec(),
+        |&i| g[i].clone(),
+    );
+    let k = vvv.len();
+    let mut jj = vec![0; n];
+    for (i, vv) in vvv.iter().enumerate() {
+        for &v in vv {
+            jj[v] = i;
+        }
+    }
+
+    let mut rs = 0;
+    let mut dp = vec![false; k];
+    for (i, vv) in vvv.iter().enumerate() {
+        if vv.len() == 1 {
+            for v in g[vv[0]].iter().copied() {
+                dp[i] |= dp[jj[v]];
+            }
+        } else {
+            dp[i] = true;
+        }
+        if dp[i] {
+            rs += vv.len();
+        }
+    }
+    println!("{rs}");
 }
