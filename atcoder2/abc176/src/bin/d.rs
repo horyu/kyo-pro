a55bb80df5be014f55ace4889c41f8a7ed4ddaba
horyu <28817604+horyu@users.noreply.github.com>
2023-07-05 08:59:31 +0900

    abc176 d

--- atcoder2/abc176/src/bin/d.rs
+++ atcoder2/abc176/src/bin/d.rs
@@ -10,8 +10,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        ch: Usize1,
+        cw: Usize1,
+        dh: Usize1,
+        dw: Usize1,
+        ss: [Chars; h],
     };
-    // println!("{rs}");
+    use std::cmp::Reverse;
+    let mut bh = BinaryHeap::new();
+    let mut dd = vec![vec![!0usize; w]; h];
+    bh.push((Reverse(0), ch, cw));
+    dd[ch][cw] = 0;
+    while let Some((Reverse(qm), qi, qj)) = bh.pop() {
+        for i in qi.saturating_sub(2)..h.min(qi + 3) {
+            for j in qj.saturating_sub(2)..w.min(qj + 3) {
+                let m = qm + (1 < qi.abs_diff(i) + qj.abs_diff(j)) as usize;
+                if ss[i][j] == '.' && m < dd[i][j] {
+                    // eprintln!("{} {qm}->{m} ({qi},{qj})->({i},{j})", dd[i][j]);
+                    dd[i][j] = m;
+                    bh.push((Reverse(m), i, j));
+                }
+            }
+        }
+    }
+    if dd[dh][dw] == !0 {
+        println!("-1");
+    } else {
+        println!("{}", dd[dh][dw]);
+    }
 }
