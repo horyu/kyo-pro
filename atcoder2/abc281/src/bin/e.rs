a5add84f2b39fbaba81ed554f9c3350a4b5f234d
horyu <28817604+horyu@users.noreply.github.com>
2023-07-20 08:52:42 +0900

    abc281 e

--- atcoder2/abc281/src/bin/e.rs
+++ atcoder2/abc281/src/bin/e.rs
@@ -11,7 +11,46 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        k: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut yobi = BTreeSet::new();
+    for (i, a) in aa[0..(m - 1)].iter().copied().enumerate() {
+        yobi.insert((a, i));
+    }
+    let mut main = BTreeSet::new();
+    let mut sum = 0;
+    let mut rs = vec![];
+    for (r, ar) in aa.iter().copied().enumerate().skip(m - 1) {
+        yobi.insert((ar, r));
+        while main.len() < k {
+            if let Some((ai, i)) = yobi.pop_first() {
+                sum += ai;
+                main.insert((ai, i));
+            }
+        }
+
+        if let Some((main_max, _)) = main.last().copied() {
+            if let Some((yobi_min, _)) = yobi.first().copied() {
+                if yobi_min < main_max {
+                    sum = sum + yobi_min - main_max;
+                    let max = main.pop_last().unwrap();
+                    let min = yobi.pop_first().unwrap();
+                    main.insert(min);
+                    yobi.insert(max);
+                }
+            }
+        }
+        rs.push(sum);
+        // eprintln!("main:{}", main.iter().map(|kv| format!("{kv:?}")).join(" "));
+        // eprintln!("yobi:{}", yobi.iter().map(|kv| format!("{kv:?}")).join(" "));
+
+        let rm_value = (aa[r + 1 - m], r + 1 - m);
+        if main.remove(&rm_value) {
+            sum -= rm_value.0;
+        };
+        yobi.remove(&rm_value);
+    }
+    println!("{}", rs.iter().join(" "));
 }
