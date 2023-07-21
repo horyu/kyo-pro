8a66f5b311188a82c8c2f98da4aa3cf5bb2be669
horyu <28817604+horyu@users.noreply.github.com>
2023-07-21 22:22:05 +0900

    abc276 f

--- atcoder2/abc276/src/bin/f.rs
+++ atcoder2/abc276/src/bin/f.rs
@@ -1,8 +1,10 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
+use nalgebra::coordinates::X;
 use num_integer::*;
 use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
@@ -13,5 +15,20 @@ fn main() {
         n: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    const MAX: usize = 2e5 as usize;
+
+    let mut ft1 = ac_library::FenwickTree::new(MAX + 1, 0usize);
+    let mut ft2 = ac_library::FenwickTree::new(MAX + 1, 0usize);
+
+    let mut sum = ModInt998244353::default();
+    for (i, a) in aa.iter().copied().enumerate() {
+        let ma = ModInt998244353::new(a);
+        let d = ma * ft1.sum(0..=a) + ft2.sum((a + 1)..);
+        sum += d * 2 + ma;
+        let rs = sum / (i + 1).pow(2u32);
+        println!("{rs}");
+
+        ft1.add(a, 1);
+        ft2.add(a, a);
+    }
 }
