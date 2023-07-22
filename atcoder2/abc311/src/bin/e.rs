fa55cc63743b33b31188ed92391a7af998bde96f
horyu <28817604+horyu@users.noreply.github.com>
2023-07-23 08:33:50 +0900

    abc311 e

--- atcoder2/abc311/src/bin/e.rs
+++ atcoder2/abc311/src/bin/e.rs
@@ -10,8 +10,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
         n: usize,
-        aa: [usize; n],
+        aabb: [(usize, usize); n],
     };
-    // println!("{rs}");
+    const MAX: usize = 3003;
+    let mut ng = vec![vec![false; MAX]; MAX];
+    for (a, b) in aabb {
+        ng[a][b] = true;
+    }
+    let mut dp = vec![vec![0usize; MAX]; MAX];
+    let mut rs = 0usize;
+    for i in 1..=h {
+        for j in 1..=w {
+            if ng[i][j] {
+                continue;
+            }
+            dp[i][j] = dp[i - 1][j - 1].min(dp[i][j - 1]).min(dp[i - 1][j]) + 1;
+            rs += dp[i][j];
+        }
+    }
+    // for i in 1..=h {
+    //     eprintln!("{}", (1..=w).map(|j| format!("{:2}", dp[i][j])).join(" "));
+    // }
+    println!("{rs}");
 }
