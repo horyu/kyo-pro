5dd3c7c8f34468da526a63c9f97e72baba6f2fef
horyu <28817604+horyu@users.noreply.github.com>
2023-08-01 22:55:02 +0900

    abc290 e

--- atcoder2/abc290/src/bin/e.rs
+++ atcoder2/abc290/src/bin/e.rs
@@ -11,7 +11,28 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aa: [Usize1; n],
     };
-    // println!("{rs}");
+    let mut pp = vec![vec![]; n];
+    for (i, a) in aa.iter().copied().enumerate() {
+        pp[a].push(i);
+    }
+    let mut rs = 0usize;
+    for i in 1..=n {
+        rs += (n + 1 - i) * (i / 2);
+    }
+    for pp in pp {
+        let mut l = 0;
+        let mut r = pp.len().saturating_sub(1);
+        while l < r {
+            if pp[l] + 1 < n - pp[r] {
+                rs -= (r - l) * (pp[l] + 1);
+                l += 1;
+            } else {
+                rs -= (r - l) * (n - pp[r]);
+                r -= 1;
+            }
+        }
+    }
+    println!("{rs}");
 }
