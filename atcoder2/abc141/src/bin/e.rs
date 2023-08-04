a485421da231c5da9cbe14e8acac7dbedf1e31f3
horyu <28817604+horyu@users.noreply.github.com>
2023-08-04 19:21:39 +0900

    abc141 e

--- atcoder2/abc141/src/bin/e.rs
+++ atcoder2/abc141/src/bin/e.rs
@@ -13,6 +13,25 @@ fn main() {
         n: usize,
         s: Chars,
     };
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+    let mut rs = 0;
+    for i in (0..n).rev() {
+        for j in ((i + 1)..n).rev() {
+            if s[i] == s[j] {
+                dp[i][j] = dp[i][j].max(dp[i + 1][j + 1] + 1);
+            }
+            rs = rs.max(dp[i][j].min(j - i));
+        }
+    }
+    println!("{rs}");
+}
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        n: usize,
+        s: Chars,
+    };
     // https://drken1215.hatenablog.com/entry/2019/09/16/014600
     let mut rs = 0;
     for i in 0..n {
