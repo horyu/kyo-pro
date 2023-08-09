3ec2fa7f171316acc5f0e54d0435bce9a838295c
horyu <28817604+horyu@users.noreply.github.com>
2023-08-09 21:15:05 +0900

    abc298 f

--- atcoder2/abc298/src/bin/f.rs
+++ atcoder2/abc298/src/bin/f.rs
@@ -11,7 +11,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        rrccxx: [(isize, isize, usize); n],
     };
-    // println!("{rs}");
+    let mut hm = HashMap::new();
+    let mut tate = counter::Counter::<isize>::new();
+    let mut yoko = counter::Counter::<isize>::new();
+    for (r, c, x) in rrccxx.iter().copied() {
+        hm.insert((r, c), x);
+        tate[&r] += x;
+        yoko[&c] += x;
+    }
+    let yoko = yoko
+        .into_iter()
+        .sorted_unstable_by_key(|cx| cx.1)
+        .rev()
+        .collect_vec();
+    let mut rs = 0;
+    for (r, rsum) in tate {
+        for (c, csum) in yoko.iter().copied() {
+            if let Some(x) = hm.get(&(r, c)) {
+                rs = rs.max(rsum + csum - x);
+            } else {
+                rs = rs.max(rsum + csum);
+                break;
+            }
+        }
+    }
+
+    println!("{rs}");
 }
