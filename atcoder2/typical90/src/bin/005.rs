ef18d72927b54f2f878b6a81e3eee0425095fc3e
horyu <28817604+horyu@users.noreply.github.com>
2023-08-11 22:36:18 +0900

    typical90 005

--- atcoder2/typical90/src/bin/005.rs
+++ atcoder2/typical90/src/bin/005.rs
@@ -11,7 +11,66 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        b: usize,
+        k: usize,
+        cc: [usize; k],
     };
-    // println!("{rs}");
+    const MOD: usize = 1e9 as usize + 7;
+    const LOG: usize = 61;
+    // https://github.com/E869120/kyopro_educational_90/blob/main/sol/005-03.cpp
+
+    let mut ten = [0; LOG];
+    for i in 0..LOG {
+        ten[i] = mod_pow(10, 1 << i, b);
+    }
+
+    let mut dp = vec![vec![0; 1009]; LOG + 1];
+    let mut ans = dp.clone();
+
+    for c in cc.iter().copied() {
+        dp[0][c % b] += 1;
+    }
+
+    for i in 0..LOG {
+        for j in 0..b {
+            for k in 0..b {
+                let next = (j * ten[i] + k) % b;
+                dp[i + 1][next] += dp[i][j] * dp[i][k];
+                dp[i + 1][next] %= MOD;
+            }
+        }
+    }
+
+    ans[0][0] = 1;
+    for i in 0..LOG {
+        if 0 != n & (1 << i) {
+            for j in 0..b {
+                for k in 0..b {
+                    let next = (j * ten[i] + k) % b;
+                    ans[i + 1][next] += ans[i][j] * dp[i][k];
+                    ans[i + 1][next] %= MOD;
+                }
+            }
+        } else {
+            for j in 0..b {
+                ans[i + 1][j] = ans[i][j];
+            }
+        }
+    }
+
+    let rs = ans[LOG][0];
+    println!("{rs}");
+}
+
+fn mod_pow(mut x: usize, mut n: usize, m: usize) -> usize {
+    x %= m;
+    let mut ans = 1;
+    while n != 0 {
+        if n.is_odd() {
+            ans = ans * x % m;
+        }
+        x = x * x % m;
+        n >>= 1;
+    }
+    ans
 }
