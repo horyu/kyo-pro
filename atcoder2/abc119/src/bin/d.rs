0bf0590f2e9bc7d3c8bba4bfed14c662a37a89c1
horyu <28817604+horyu@users.noreply.github.com>
2023-08-13 08:32:06 +0900

    abc119 d

--- atcoder2/abc119/src/bin/d.rs
+++ atcoder2/abc119/src/bin/d.rs
@@ -13,23 +13,19 @@ fn main() {
         a: usize,
         b: usize,
         q: usize,
-        ss: [isize; a],
-        tt: [isize; b],
-        xx: [isize; q],
+        ss: [usize; a],
+        tt: [usize; b],
+        xx: [usize; q],
     };
-    let ss = BTreeSet::from_iter(chain!([std::isize::MIN >> 2], ss, [std::isize::MAX >> 2]));
-    let tt = BTreeSet::from_iter(chain!([std::isize::MIN >> 2], tt, [std::isize::MAX >> 2]));
+    let sbts = BTreeSet::from_iter(ss);
+    let tbts = BTreeSet::from_iter(tt);
     for x in xx {
-        let ss = [
-            ss.range(..=x).next_back().copied().unwrap(),
-            ss.range(x..).next().copied().unwrap(),
-        ];
-        let tt = [
-            tt.range(..=x).next_back().copied().unwrap(),
-            tt.range(x..).next().copied().unwrap(),
-        ];
-        let mut rs = std::usize::MAX;
-        for (s, t) in ss.into_iter().cartesian_product(tt) {
+        let sl = sbts.range(..x).next_back().copied().unwrap_or(1 << 60);
+        let sr = sbts.range(x..).next().copied().unwrap_or(1 << 60);
+        let tl = tbts.range(..x).next_back().copied().unwrap_or(1 << 60);
+        let tr = tbts.range(x..).next().copied().unwrap_or(1 << 60);
+        let mut rs = usize::MAX;
+        for (s, t) in iproduct!([sl, sr], [tl, tr]) {
             rs = rs.min(x.abs_diff(s) + s.abs_diff(t));
             rs = rs.min(x.abs_diff(t) + t.abs_diff(s));
         }
