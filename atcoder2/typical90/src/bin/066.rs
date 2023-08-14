6b516d1495e6caf92987bdc1c6142d64c5341fe1
horyu <28817604+horyu@users.noreply.github.com>
2023-08-15 08:07:24 +0900

    typical90 066

--- atcoder2/typical90/src/bin/066.rs
+++ atcoder2/typical90/src/bin/066.rs
@@ -11,21 +11,20 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        llrr: [(Usize1, Usize1); n],
+        llrr: [(usize, usize); n],
     };
     let mut rs = 0.0;
-    let mut arr = [0.0; 100];
-    for (l, r) in llrr {
-        let mut new_arr = arr;
-        let size = (r - l + 1) as f64;
-        for i in l..=r {
-            let bigs = arr[(i + 1)..].iter().sum::<f64>();
-            if 0.0 < bigs {
-                rs += bigs / size;
+    for ((li, ri), (lj, rj)) in llrr.iter().copied().tuple_combinations() {
+        let mut cnt = 0;
+        for i in li..=ri {
+            for j in lj..=rj {
+                if j < i {
+                    cnt += 1;
+                }
             }
-            new_arr[i] += 1.0 / size;
         }
-        arr = new_arr;
+        rs += cnt as f64 / ((ri - li + 1) * (rj - lj + 1)) as f64;
     }
+
     println!("{rs}");
 }
