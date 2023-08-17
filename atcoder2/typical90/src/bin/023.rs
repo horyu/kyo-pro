90aba1ac46885abb01b604f73da3759bf029a193
horyu <28817604+horyu@users.noreply.github.com>
2023-08-17 21:37:36 +0900

    typical90 023

--- atcoder2/typical90/src/bin/023.rs
+++ atcoder2/typical90/src/bin/023.rs
@@ -10,8 +10,36 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        ccc: [Chars; h],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/typical90/submissions/21987422
+    const MOD: usize = 1e9 as usize + 7;
+    let mut dp = HashMap::new();
+    let mut next = HashMap::new();
+    dp.insert(0usize, 1usize);
+    for cc in ccc {
+        for (j, c) in cc.iter().copied().enumerate() {
+            for (x, way) in dp.drain() {
+                *next.entry(x >> 1).or_default() += way;
+                if c == '.' && x & 1 == 0 {
+                    let mut xx = x | 1 | (1 << w);
+                    if 0 < j {
+                        xx |= 1 << (w - 1);
+                    }
+                    if j + 1 < w {
+                        xx |= (1 << 1) | (1 << (w + 1));
+                    }
+                    *next.entry(xx >> 1).or_default() += way;
+                }
+            }
+            std::mem::swap(&mut dp, &mut next);
+        }
+        for w in dp.values_mut() {
+            *w %= MOD;
+        }
+    }
+    let rs = dp.values().sum::<usize>() % MOD;
+    println!("{rs}");
 }
