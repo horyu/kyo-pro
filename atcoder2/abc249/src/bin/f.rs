b2cb2f8c0f1e132a8642c9442d90dfe2438ed602
horyu <28817604+horyu@users.noreply.github.com>
2023-08-18 20:16:39 +0900

    abc249 f

--- atcoder2/abc249/src/bin/f.rs
+++ atcoder2/abc249/src/bin/f.rs
@@ -11,7 +11,37 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
+        ttyy: [(usize, isize); n],
     };
-    // println!("{rs}");
+    // a
+    // saa
+    // sa
+    // saaa
+    let mut vv = vec![(0isize, vec![])];
+    for (t, y) in ttyy {
+        if t == 1 {
+            vv.push((y, vec![]));
+        } else {
+            vv.last_mut().unwrap().1.push(y);
+        }
+    }
+    let mut rs = isize::MIN;
+    let mut bh = BinaryHeap::new();
+    let mut sum = 0;
+    for (i, (s, aa)) in vv.into_iter().rev().take(k + 1).enumerate() {
+        for a in aa {
+            if a < 0 {
+                bh.push(a);
+            } else {
+                sum += a;
+            }
+        }
+        while k < i + bh.len() {
+            let a = bh.pop().unwrap();
+            sum += a;
+        }
+        rs = rs.max(s + sum);
+    }
+    println!("{rs}");
 }
