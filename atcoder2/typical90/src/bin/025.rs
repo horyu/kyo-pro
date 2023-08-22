1d1b5ac4c2b41592b23f3993b9cfb451f222fa30
horyu <28817604+horyu@users.noreply.github.com>
2023-08-22 22:48:54 +0900

    typical90 025

--- atcoder2/typical90/src/bin/025.rs
+++ atcoder2/typical90/src/bin/025.rs
@@ -11,7 +11,53 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        b: usize,
     };
-    // println!("{rs}");
+    let mut rs = 0;
+    for i in 1..=11 {
+        rs += dfs(i, 9, &Vec::new(), n, b);
+    }
+    if i_to_vec(b).contains(&0) && b <= n {
+        rs += 1;
+    }
+
+    println!("{rs}");
+}
+
+fn i_to_vec(i: usize) -> Vec<usize> {
+    let mut v = vec![];
+    let mut tmp = i;
+    while tmp != 0 {
+        v.push(tmp % 10);
+        tmp /= 10;
+    }
+    v
+}
+
+fn dfs(pos: usize, last: usize, vv: &Vec<usize>, n: usize, b: usize) -> usize {
+    let mut cnt = 0;
+    if pos == 0 {
+        let mut rem = 1;
+        for &v in vv {
+            rem *= v;
+        }
+        let goal = rem + b;
+        if goal <= n {
+            let mut ww = i_to_vec(goal);
+            ww.sort_unstable();
+            ww.reverse();
+            if ww == *vv {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
+    for i in 1..=last {
+        let mut ww = vv.clone();
+        ww.push(i);
+        cnt += dfs(pos - 1, i, &ww, n, b);
+    }
+
+    cnt
 }
