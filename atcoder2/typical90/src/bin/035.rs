8498561e621ee82b8f0f5b3c6816585623509162
horyu <28817604+horyu@users.noreply.github.com>
2023-08-24 22:12:01 +0900

    typical90 035

--- atcoder2/typical90/src/bin/035.rs
+++ atcoder2/typical90/src/bin/035.rs
@@ -11,7 +11,83 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aabb: [(Usize1, Usize1); n - 1],
+        q: usize,
     };
-    // println!("{rs}");
+    // https://github.com/E869120/kyopro_educational_90/blob/main/sol/035-04.cpp
+    let mut g = vec![vec![]; n];
+    for (a, b) in aabb.iter().copied() {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut bits = 0;
+    while (1 << bits) < n {
+        bits += 1;
+    }
+    let mut par = vec![vec![0; n]; bits];
+    let mut depth = vec![0; n];
+    let mut id = vec![0; n];
+    let mut vert_id = 0;
+    build_tree(0, 0, &mut par, &mut id, &mut depth, &mut vert_id, &g);
+    for i in 1..bits {
+        for j in 0..n {
+            par[i][j] = par[i - 1][par[i - 1][j]];
+        }
+    }
+    let lca = |va: usize, vb: usize| -> usize {
+        let (mut va, mut vb) = if depth[va] < depth[vb] {
+            (vb, va)
+        } else {
+            (va, vb)
+        };
+        for i in (0..bits).rev() {
+            if (1 << i) <= depth[va] - depth[vb] {
+                va = par[i][va];
+            }
+        }
+        if va == vb {
+            return va;
+        }
+        for i in (0..bits).rev() {
+            if par[i][va] != par[i][vb] {
+                va = par[i][va];
+                vb = par[i][vb];
+            }
+        }
+        par[0][va]
+    };
+    for _ in 0..q {
+        input! {
+            k: usize,
+            mut vv: [Usize1; k],
+        };
+        vv.sort_unstable_by_key(|&v| id[v]);
+        let mut rs = 0;
+        for i in 0..k {
+            rs += depth[vv[i]];
+            rs -= depth[lca(vv[i], vv[(i + 1) % k])];
+        }
+        println!("{rs}");
+    }
+}
+
+fn build_tree(
+    pos: usize,
+    pre: usize,
+    par: &mut Vec<Vec<usize>>,
+    id: &mut Vec<usize>,
+    depth: &mut Vec<isize>,
+    vert_id: &mut usize,
+    g: &Vec<Vec<usize>>,
+) {
+    par[0][pos] = pre;
+    id[pos] = *vert_id;
+    *vert_id += 1;
+    for &i in g[pos].iter() {
+        if i == pre {
+            continue;
+        }
+        depth[i] = depth[pos] + 1;
+        build_tree(i, pos, par, id, depth, vert_id, g);
+    }
 }
