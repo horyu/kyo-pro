d221afba61295390abbee1a72e13b95daa03ee03
horyu <28817604+horyu@users.noreply.github.com>
2023-08-26 23:32:42 +0900

    abc282 d

--- atcoder2/abc282/src/bin/d.rs
+++ atcoder2/abc282/src/bin/d.rs
@@ -11,7 +11,52 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvv: [(Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    for (u, v) in uuvv.iter().copied() {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let mut rs = 0;
+    let mut sum = 0usize;
+    let mut cc = vec![!0; n];
+    for i in 0..n {
+        if cc[i] != !0 {
+            continue;
+        }
+        let mut qq = VecDeque::new();
+        qq.push_back(i);
+        let mut ii = vec![i];
+        cc[i] = 0;
+        // c0 -> c1 への接続だけカウントする
+        let mut c1 = 0;
+        while let Some(qi) = qq.pop_front() {
+            for &qj in &g[qi] {
+                if cc[qj] != !0 {
+                    if cc[qi] == cc[qj] {
+                        println!("0");
+                        return;
+                    }
+                    continue;
+                }
+                cc[qj] = 1 - cc[qi];
+                if cc[qj] == 1 {
+                    c1 += 1;
+                }
+                qq.push_back(qj);
+                ii.push(qj);
+            }
+        }
+
+        rs += sum * ii.len();
+        for &i in &ii {
+            if cc[i] == 0 {
+                rs += c1 - g[i].len();
+            }
+        }
+        sum += ii.len();
+    }
+    println!("{rs}");
 }
