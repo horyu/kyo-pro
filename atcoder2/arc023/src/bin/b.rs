55c80e508e0aeba50526f5eab6f547ad5a4e6116
horyu <28817604+horyu@users.noreply.github.com>
2023-08-30 09:23:32 +0900

    arc023 b

--- atcoder2/arc023/src/bin/b.rs
+++ atcoder2/arc023/src/bin/b.rs
@@ -31,3 +31,53 @@ fn main() {
     }
     println!("{rs}");
 }
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        r: usize,
+        c: usize,
+        d: usize,
+        aaa: [[usize; c]; r],
+    };
+    let udlr = |i: usize, j: usize| -> Vec<(usize, usize)> {
+        let mut vv = vec![];
+        if 0 < i {
+            vv.push((i - 1, j));
+        }
+        if i < r - 1 {
+            vv.push((i + 1, j));
+        }
+        if 0 < j {
+            vv.push((i, j - 1));
+        }
+        if j < c - 1 {
+            vv.push((i, j + 1));
+        }
+        vv
+    };
+    let mut hhss = [HashSet::new(), HashSet::new()];
+    hhss[0].insert((0, 0));
+    let mut tmp = HashSet::from([(0, 0)]);
+    for dd in 0..d {
+        let mut new_tmp = HashSet::new();
+        for (i, j) in tmp {
+            for (ni, nj) in udlr(i, j) {
+                if hhss[(dd + 1) % 2].insert((ni, nj)) {
+                    new_tmp.insert((ni, nj));
+                }
+            }
+        }
+        if new_tmp.is_empty() {
+            break;
+        }
+        tmp = new_tmp;
+    }
+    let rs = hhss[d % 2]
+        .iter()
+        .copied()
+        .map(|(i, j)| aaa[i][j])
+        .max()
+        .unwrap_or_default();
+    println!("{rs}");
+}
