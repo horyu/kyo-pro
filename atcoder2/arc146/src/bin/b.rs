ea0a7ba1b3c27faf711601dfb1ccb80d197f1ff7
horyu <28817604+horyu@users.noreply.github.com>
2023-09-01 22:09:52 +0900

    arc146 b

--- atcoder2/arc146/src/bin/b.rs
+++ atcoder2/arc146/src/bin/b.rs
@@ -15,26 +15,26 @@ fn main() {
         k: usize,
         aa: [usize; n],
     };
-    let mut ccdd = aa.into_iter().map(|a| (a, 0usize)).collect_vec();
+    let mut aacc = aa.into_iter().map(|a| (a, 0usize)).collect_vec();
     let mut rs = 0usize;
     for i in (0..=31).rev() {
         let target = 1usize << i;
-        let mut new_ccdd = vec![];
+        let mut new_aacc = vec![];
         let mut sums = vec![];
-        for &(c, d) in &ccdd {
-            if 0 < c & target {
-                sums.push(d);
-                new_ccdd.push((c, d));
+        for (a, c) in aacc.iter().copied() {
+            if 0 < a & target {
+                sums.push(c);
+                new_aacc.push((a, c));
             } else {
-                let new_d = d + target - (c & (target - 1));
-                sums.push(new_d);
-                new_ccdd.push((target, new_d));
+                let new_c = c + target - (a & (target - 1));
+                sums.push(new_c);
+                new_aacc.push((target, new_c));
             }
         }
         let sum = sums.into_iter().sorted_unstable().take(k).sum::<usize>();
         if sum <= m {
             rs |= target;
-            ccdd = new_ccdd;
+            aacc = new_aacc;
         }
     }
     println!("{rs}");
