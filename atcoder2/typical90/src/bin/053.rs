c567fdd453ff265d91b942ae05a1c6efb4b7b993
horyu <28817604+horyu@users.noreply.github.com>
2023-09-07 22:33:39 +0900

    typical90 053

--- atcoder2/typical90/src/bin/053.rs
+++ atcoder2/typical90/src/bin/053.rs
@@ -9,9 +9,51 @@ use proconio::{input, marker::*};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
 fn main() {
+    let mut stdin =
+        proconio::source::line::LineSource::new(std::io::BufReader::new(std::io::stdin()));
+    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
     };
-    // println!("{rs}");
+    for _ in 0..t {
+        input! {n: usize};
+        if n <= 5 {
+            let mut rs = 0;
+            for i in 0..n {
+                println!("? {}", i + 1);
+                input! {a: isize};
+                rs = rs.max(a);
+            }
+            println!("! {rs}");
+            continue;
+        }
+        let m = 1597;
+        // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+        let mut aa = vec![None; m];
+        aa[0] = Some(-1);
+        for i in (n + 1)..m {
+            aa[i] = Some(-(i as isize))
+        }
+        let mut q = |i: usize| {
+            if aa[i].is_none() {
+                println!("? {i}");
+                input! {a: isize};
+                aa[i] = Some(a);
+            }
+            aa[i].unwrap()
+        };
+        let (mut l, mut r) = (0, m);
+        let (mut x, mut y) = (610, 987);
+        for _ in 0..14 {
+            let ax = q(x);
+            let ay = q(y);
+            if ax < ay {
+                (l, r, x, y) = (x, r, y, r - (y - x));
+            } else {
+                (l, r, x, y) = (l, y, l + (y - x), x);
+            }
+        }
+        let rs = aa.into_iter().flatten().max().unwrap();
+        println!("! {rs}");
+    }
 }
