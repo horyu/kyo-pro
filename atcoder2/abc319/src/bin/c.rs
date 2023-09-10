bed7a3e8551e88a3733a905d18089770a7fcfab1
horyu <28817604+horyu@users.noreply.github.com>
2023-09-10 15:58:18 +0900

    abc319 c

--- atcoder2/abc319/src/bin/c.rs
+++ atcoder2/abc319/src/bin/c.rs
@@ -10,9 +10,9 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        ccc: [[isize; 3]; 3],
+        cc: [isize; 9],
     };
-    let bunbo = (1..=9).fold(1.0, |acc, i| acc * i as f64);
+    let bunbo = (1..=9).product::<usize>() as f64;
     let mut bunshi = 0.0;
     // dbg!((0..9).permutations(9).count());
     for ii in (0..9).permutations(9) {
@@ -27,22 +27,17 @@ fn main() {
             [0, 4, 8],
             [2, 4, 6],
         ] {
-            let arr = [
-                ccc[xx[0] / 3][xx[0] % 3],
-                ccc[xx[1] / 3][xx[1] % 3],
-                ccc[xx[2] / 3][xx[2] % 3],
-            ];
-            if arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[0] {
-                // 順番をたしかめる
-                let yy = xx
+            if !xx.iter().copied().map(|x| cc[x]).all_unique()
+                && xx
                     .iter()
                     .copied()
                     .sorted_unstable_by_key(|&x| ii.iter().copied().position(|i| i == x).unwrap())
-                    .collect_vec();
-                if ccc[yy[0] / 3][yy[0] % 3] == ccc[yy[1] / 3][yy[1] % 3] {
-                    ok = false;
-                    break;
-                }
+                    .take(2)
+                    .map(|x| cc[x])
+                    .all_equal()
+            {
+                ok = false;
+                break;
             }
         }
         if ok {
