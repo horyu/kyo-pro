ca8ccf1f68270d10d90e8630f076c432623f117c
horyu <28817604+horyu@users.noreply.github.com>
2023-09-18 10:28:50 +0900

    diverta2019 d

---
+++ atcoder2/diverta2019/src/bin/d.rs
@@ -0,0 +1,27 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+    };
+    if n == 1 {
+        println!("0");
+        return;
+    }
+    let mut rs = 0usize;
+    for amari in 1..=n.sqrt() {
+        let m = (n - amari) / amari;
+        if n / m == n % m {
+            rs += m;
+        }
+    }
+    println!("{rs}");
+}
