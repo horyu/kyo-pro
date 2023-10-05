26735c245659ebc15fa44de6910358aafda8f20e
horyu <28817604+horyu@users.noreply.github.com>
2023-10-05 21:52:36 +0900

    typical90 090

--- atcoder2/typical90/src/bin/090.rs
+++ atcoder2/typical90/src/bin/090.rs
@@ -11,7 +11,22 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
     };
-    // println!("{rs}");
+    const MOD: usize = 998244353;
+    // https://github.com/E869120/kyopro_educational_90/blob/main/sol/090-02.cpp
+    let mut rs = ndarray::arr2(&[[1usize, 0], [0, 1]]);
+    let mut mat = ndarray::arr2(&[[1usize, 1], [1, 0]]);
+    let mut b = n;
+    while 0 < b {
+        if b & 1 == 1 {
+            rs = rs.dot(&mat);
+            rs %= MOD;
+        }
+        mat = mat.dot(&mat);
+        mat %= MOD;
+        b >>= 1;
+    }
+    let rs = (rs[[0, 0]] + rs[[1, 0]]) % MOD;
+    println!("{rs}");
 }
