4ea29e06ad0d474a3c774c89396cf38fba66388b
horyu <28817604+horyu@users.noreply.github.com>
2023-10-15 10:01:25 +0900

    abc324 d

--- atcoder2/abc324/src/bin/d.rs
+++ atcoder2/abc324/src/bin/d.rs
@@ -10,26 +10,19 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
+        n: u32,
         s: Bytes,
     };
     let mut scc = [0; 10];
     for &c in &s {
         scc[(c - b'0') as usize] += 1;
     }
-    if scc[0] == n {
-        println!("1");
-        return;
-    }
     let mut rs = 0usize;
-    for i in 1isize.. {
+    for i in 0..=10usize.pow(n).sqrt() {
         let mut ii = i.pow(2);
-        if n < ii.to_string().len() {
-            break;
-        }
         let mut icc = [0; 10];
         while 0 < ii {
-            icc[(ii % 10) as usize] += 1;
+            icc[ii % 10] += 1;
             ii /= 10;
         }
         if (1..=9).all(|i| scc[i] == icc[i]) {
