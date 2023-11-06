dcb182a6e764b179ac81e40e5019adb7d7d7914b
horyu <28817604+horyu@users.noreply.github.com>
2023-11-06 19:55:01 +0900

    arc024 b

---
+++ atcoder2/arc024/src/bin/b.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        mut cc: [usize; n],
+    };
+    if cc.iter().all_equal() {
+        println!("-1");
+        return;
+    }
+    let zure = cc.iter().position(|&c| c != cc[0]).unwrap();
+    cc.rotate_left(zure);
+    let mut rs = 0;
+    for (_, g) in cc.into_iter().group_by(|&c| c).into_iter() {
+        rs = rs.max((g.into_iter().count() - 1) / 2 + 1);
+    }
+    println!("{rs}");
+}
