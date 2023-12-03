9ef407c263753c5f8990a819fe1e52860354bf3c
horyu <28817604+horyu@users.noreply.github.com>
2023-12-03 10:10:32 +0900

    abc271 f

--- atcoder2/abc271/src/bin/f.rs
+++ atcoder2/abc271/src/bin/f.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use counter::Counter;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,44 +13,42 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aaa: [[usize; n]; n],
+        mut aaa: [[usize; n]; n],
     };
-    // https://seelx3.hatenablog.jp/entry/2022/10/02/004652
-    let mut lu = vec![HashMap::new(); n];
-    let mut rd = lu.clone();
-
-    for i in 0..(1 << (n - 1)) {
-        let (mut x, mut y) = (0, 0);
-        let mut p = 0;
-        for j in 0..n {
-            p ^= aaa[x][y];
-            if 0 != i & (1 << j) {
-                x += 1;
-            } else {
-                y += 1;
+    let f = |aaa: &Vec<Vec<usize>>| {
+        let mut bbb: Vec<Vec<Counter<usize, usize>>> = vec![vec![Counter::default(); n]; n];
+        bbb[0][0][&aaa[0][0]] += 1usize;
+        for i in 0..(n - 1) {
+            // eprintln!("{i} {:?}", 0..(n - 1 - i));
+            for j in 0..(n - 1 - i) {
+                for (k, v) in std::mem::take(&mut bbb[i][j]) {
+                    for (ii, jj) in [(i + 1, j), (i, j + 1)] {
+                        bbb[ii][jj][&(k ^ aaa[ii][jj])] += v;
+                    }
+                }
             }
         }
-        *lu[x].entry(p).or_insert(0usize) += 1;
-    }
-    for i in 0..(1 << (n - 1)) {
-        let (mut x, mut y) = (n - 1, n - 1);
-        let mut q = 0;
-        for j in 0..n {
-            q ^= aaa[x][y];
-            if 0 != i & (1 << j) {
-                x = x.saturating_sub(1);
-            } else {
-                y = y.saturating_sub(1);
-            }
+        (0..n)
+            .map(|i| std::mem::take(&mut bbb[i][n - 1 - i]))
+            .collect_vec()
+    };
+    let v1 = f(&aaa);
+    // 右上から左下への対角線で反転
+    for i in 0..(n - 1) {
+        for j in 0..(n - 1 - i) {
+            aaa[i][j] ^= aaa[n - 1 - j][n - 1 - i];
+            aaa[n - 1 - j][n - 1 - i] ^= aaa[i][j];
+            aaa[i][j] ^= aaa[n - 1 - j][n - 1 - i];
         }
-        *rd[x].entry(q).or_insert(0) += 1;
     }
-
-    let mut rs = 0;
-    for (i, (lu, rd)) in izip!(lu, rd).enumerate() {
-        for (k, v) in lu {
-            if let Some(vv) = rd.get(&(k ^ aaa[i][n - 1 - i])) {
-                rs += v * vv;
+    let v2 = f(&aaa);
+    // dbg!(&v1, &v2);
+    let mut rs = 0usize;
+    for i in 0..n {
+        for (&k1, &v1) in &v1[i] {
+            if let Some(v2) = v2[i].get(&(k1 ^ aaa[i][n - 1 - i])) {
+                rs += v1 * v2;
+                // eprintln!("{i}{}  {k1} {v1} {} {v2}", aaa[i][n - 1 - i], k1 ^ aaa[i][n - 1 - i]);
             }
         }
     }
