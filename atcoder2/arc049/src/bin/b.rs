aded753de9ce969b8cd3f8932ee7b50495db21bb
horyu <28817604+horyu@users.noreply.github.com>
2023-12-10 23:07:38 +0900

    arc049 b

--- atcoder2/arc049/src/bin/b.rs
+++ atcoder2/arc049/src/bin/b.rs
@@ -12,75 +12,28 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        xxyycc: [(isize, isize, isize); n],
+        xxyycc: [(f64, f64, f64); n],
     };
-    const MAX: isize = 1e5 as isize;
-    let mut p = (-MAX..=MAX)
-        .min_by_key(|&tmp| {
-            xxyycc
-                .iter()
-                .map(|&(x, _, c)| (x - tmp).abs() * c)
-                .max()
-                .unwrap()
-        })
-        .unwrap() as f64;
-    let mut q = (-MAX..=MAX)
-        .min_by_key(|&tmp| {
-            xxyycc
-                .iter()
-                .map(|&(_, y, c)| (y - tmp).abs() * c)
-                .max()
-                .unwrap()
-        })
-        .unwrap() as f64;
-    dbg!(p, q);
-    // 三部探索
-    let mut l = p - 1.0;
-    let mut r = p + 1.0;
+    let mut l = 0.0;
+    let mut r = 1e9;
     for _ in 0..64 {
-        let m1 = (l * 2.0 + r) / 3.0;
-        let m2 = (l + r * 2.0) / 3.0;
-        let f1 = xxyycc
-            .iter()
-            .map(|&(x, _, c)| (x as f64 - m1).abs() * c as f64)
-            .fold(0.0f64, |acc, v| acc.max(v));
-        let f2 = xxyycc
-            .iter()
-            .map(|&(x, _, c)| (x as f64 - m2).abs() * c as f64)
-            .fold(0.0f64, |acc, v| acc.max(v));
-        if f1 < f2 {
-            r = m2;
+        let m = (l + r) / 2.0;
+        let mut xd = f64::MIN;
+        let mut xu = f64::MAX;
+        let mut yl = f64::MIN;
+        let mut yr = f64::MAX;
+        for (x, y, c) in xxyycc.iter().copied() {
+            xd = xd.max(x - m / c);
+            xu = xu.min(x + m / c);
+            yl = yl.max(y - m / c);
+            yr = yr.min(y + m / c);
+        }
+        if xd <= xu && yl <= yr {
+            r = m;
         } else {
-            l = m1;
+            l = m;
         }
     }
-    p = l;
-    let mut l = q - 1.0;
-    let mut r = q + 1.0;
-    for _ in 0..64 {
-        let m1 = (l * 2.0 + r) / 3.0;
-        let m2 = (l + r * 2.0) / 3.0;
-        let f1 = xxyycc
-            .iter()
-            .map(|&(_, y, c)| (y as f64 - m1).abs() * c as f64)
-            .fold(0.0f64, |acc, v| acc.max(v));
-        let f2 = xxyycc
-            .iter()
-            .map(|&(_, y, c)| (y as f64 - m2).abs() * c as f64)
-            .fold(0.0f64, |acc, v| acc.max(v));
-        if f1 < f2 {
-            r = m2;
-        } else {
-            l = m1;
-        }
-    }
-    q = l;
-    dbg!(p, q);
-    let rs = xxyycc
-        .iter()
-        .map(|&(xx, yy, cc)| {
-            ((xx as f64 - p).abs() * cc as f64).max((yy as f64 - q).abs() * cc as f64)
-        })
-        .fold(0.0f64, |acc, v| acc.max(v));
-    println!("{rs}");
+    let rs = l;
+    println!("{rs:.15}");
 }
