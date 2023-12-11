ade7b98f63bcf3fdf5b5daeee1f52af8b1b9c6d7
horyu <28817604+horyu@users.noreply.github.com>
2023-12-11 09:33:52 +0900

    abc253 e

--- atcoder2/abc253/src/bin/e.rs
+++ atcoder2/abc253/src/bin/e.rs
@@ -16,6 +16,34 @@ fn main() {
         m: usize,
         k: usize,
     };
+    let mut dp = vec![vec![ModInt998244353::default(); m]; n];
+    for j in 0..m {
+        dp[0][j] = 1.into();
+    }
+    for i in 0..(n - 1) {
+        let mut ft = ac_library::FenwickTree::new(m, ModInt998244353::default());
+        for j in 0..m {
+            ft.add(j, dp[i][j]);
+        }
+        for j in 0..m {
+            dp[i + 1][j] =
+                ft.sum(..) - ft.sum(j.saturating_sub(k.saturating_sub(1))..(m.min(j + k)));
+        }
+    }
+    let mut rs = ModInt998244353::default();
+    for j in 0..m {
+        rs += dp[n - 1][j];
+    }
+    println!("{rs}");
+}
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        n: usize,
+        m: usize,
+        k: usize,
+    };
     if k == 0 {
         // 2 3 0
         // 1-[1,2,3], 2-[1,2,3], 3-[1,2,3]
