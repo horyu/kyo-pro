e8128151bc9158f1c3b6dc1fc78b6f7816cf7d2a
horyu <28817604+horyu@users.noreply.github.com>
2023-12-20 23:16:51 +0900

    abc291 d

--- atcoder2/abc291/src/bin/d.rs
+++ atcoder2/abc291/src/bin/d.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,23 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aabb: [[usize; 2]; n],
     };
-    // println!("{rs}");
+    let mut arr = [ModInt998244353::new(1); 2];
+    let mut pre = [!0; 2];
+    for ab in aabb {
+        let ab = [ab[0], ab[1]];
+        let mut arr2 = [ModInt998244353::new(0); 2];
+        for (i, ab) in ab.iter().copied().enumerate() {
+            for (j, cd) in pre.iter().copied().enumerate() {
+                if ab != cd {
+                    arr2[i] += arr[j];
+                }
+            }
+        }
+        arr = arr2;
+        pre = ab;
+    }
+    let rs = (arr[0] + arr[1]) / 2;
+    println!("{rs}");
 }
