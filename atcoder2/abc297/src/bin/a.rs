707f1a11779531bdf615686b92959cafb274afea
horyu <28817604+horyu@users.noreply.github.com>
2024-01-02 23:36:53 +0900

    abc297 ab

---
+++ atcoder2/abc297/src/bin/a.rs
@@ -0,0 +1,25 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        d: usize,
+        tt: [usize; n],
+    };
+    for (tl, tr) in tt.iter().copied().tuple_windows() {
+        if tr - tl <= d {
+            println!("{tr}");
+            return;
+        }
+    }
+    println!("-1");
+}
---
+++ atcoder2/abc297/src/bin/b.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        s: Chars,
+    };
+    let tf = s.iter().position(|&c| c == 'B').unwrap() % 2
+        != s.iter().rposition(|&c| c == 'B').unwrap() % 2
+        && (s.iter().position(|&c| c == 'R').unwrap()
+            ..(s.iter().rposition(|&c| c == 'R').unwrap()))
+            .any(|i| s[i] == 'K');
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{rs}");
+}
