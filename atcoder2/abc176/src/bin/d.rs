0852190f126dd9323f3158bbc2d5ebfb00b2fa63
horyu <28817604+horyu@users.noreply.github.com>
2024-01-07 23:45:32 +0900

    abc176 d

--- atcoder2/abc176/src/bin/d.rs
+++ atcoder2/abc176/src/bin/d.rs
@@ -19,6 +19,44 @@ fn main() {
         dw: Usize1,
         ss: [Chars; h],
     };
+    // 01-BFS
+    let mut qq = VecDeque::new();
+    let mut counter = counter::Counter::<(usize, usize)>::new();
+    counter[&(ch, cw)] = 0;
+    qq.push_back((ch, cw, 0));
+    while let Some((qi, qj, qm)) = qq.pop_front() {
+        if qi == dh && qj == dw {
+            println!("{qm}");
+            return;
+        }
+        for i in qi.saturating_sub(2)..h.min(qi + 3) {
+            for j in qj.saturating_sub(2)..w.min(qj + 3) {
+                let m = qm + (1 < qi.abs_diff(i) + qj.abs_diff(j)) as usize;
+                if ss[i][j] == '.' && m < counter.get(&(i, j)).copied().unwrap_or(usize::MAX) {
+                    counter[&(i, j)] = m;
+                    if m == qm {
+                        qq.push_front((i, j, m));
+                    } else {
+                        qq.push_back((i, j, m));
+                    }
+                }
+            }
+        }
+    }
+    println!("-1");
+}
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        h: usize,
+        w: usize,
+        ch: Usize1,
+        cw: Usize1,
+        dh: Usize1,
+        dw: Usize1,
+        ss: [Chars; h],
+    };
     use std::cmp::Reverse;
     let mut bh = BinaryHeap::new();
     let mut dd = vec![vec![!0usize; w]; h];
