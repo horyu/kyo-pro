2ae2174921f539b01453cab84273ecb965de1e31
horyu <28817604+horyu@users.noreply.github.com>
2024-01-08 07:49:03 +0900

    abc297 e

--- atcoder2/abc297/src/bin/e.rs
+++ atcoder2/abc297/src/bin/e.rs
@@ -12,7 +12,26 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        k: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    // 優先度尽きキューを使ってk番目の値を取り出す
+    let mut bh = BinaryHeap::new();
+    let mut hs = HashSet::new();
+    bh.push(R(0));
+    hs.insert(0);
+    let mut cnt = 0;
+    while let Some(R(v)) = bh.pop() {
+        if cnt == k {
+            println!("{v}");
+            return;
+        }
+        cnt += 1;
+        for i in 0..n {
+            let new_v = v + aa[i];
+            if hs.insert(new_v) {
+                bh.push(R(new_v));
+            }
+        }
+    }
 }
