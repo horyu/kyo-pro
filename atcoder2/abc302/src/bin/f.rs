f52d19ada3fcae6fb62cadc7d58c97d51f75b462
horyu <28817604+horyu@users.noreply.github.com>
2024-01-19 20:27:59 +0900

    abc302 f

--- atcoder2/abc302/src/bin/f.rs
+++ atcoder2/abc302/src/bin/f.rs
@@ -14,6 +14,41 @@ fn main() {
         n: usize,
         m: usize,
     };
+    let mut g = vec![vec![]; n + m];
+    let mut qq = VecDeque::new();
+    let mut pushed = vec![false; n + m];
+    for i in 0..n {
+        input! {a: usize, ss: [Usize1; a]};
+        for s in ss.iter().copied() {
+            g[i].push(n + s);
+            g[n + s].push(i);
+            if s == 0 {
+                qq.push_back((i, 0));
+                pushed[i] = true;
+            }
+        }
+    }
+    while let Some((qi, qd)) = qq.pop_front() {
+        if qi == n + m - 1 {
+            println!("{qd}");
+            return;
+        }
+        for next in g[qi].iter().copied() {
+            if !pushed[next] {
+                pushed[next] = true;
+                qq.push_back((next, qd + usize::from(next < n)));
+            }
+        }
+    }
+    println!("-1");
+}
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        n: usize,
+        m: usize,
+    };
     let mut aa = vec![];
     let mut ss = vec![];
     let mut num_to_i = vec![vec![]; m];
