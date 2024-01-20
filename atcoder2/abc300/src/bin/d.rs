8efc5d223789a31d03b9e1c80a9b7777e65b6876
horyu <28817604+horyu@users.noreply.github.com>
2024-01-20 23:40:57 +0900

    abc300 d

--- atcoder2/abc300/src/bin/d.rs
+++ atcoder2/abc300/src/bin/d.rs
@@ -11,8 +11,52 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        n: u128,
     };
-    // println!("{rs}");
+    const SIZE: usize = 1e6 as usize;
+    let mut vv = vec![true; SIZE + 1];
+    vv[0] = false;
+    vv[1] = false;
+    let mut primes = vec![];
+    for i in 2..=SIZE {
+        if vv[i] {
+            primes.push(i as u128);
+            let mut j = i * 2;
+            while j <= SIZE {
+                vv[j] = false;
+                j += i;
+            }
+        }
+    }
+    let mut rs = 0usize;
+    let len = primes.len();
+    for i in 0..(len - 2) {
+        let aa = primes[i] * primes[i];
+        if n <= aa {
+            break;
+        }
+        for j in (i + 1)..(len - 1) {
+            let aab = aa * primes[j];
+            if n <= aab {
+                break;
+            }
+            // 二分探索
+            let mut l = j + 1;
+            if n < aab * primes[l] {
+                break;
+            }
+            let mut r = len;
+            while l < r {
+                let m = (l + r) / 2;
+                if aab * primes[m] * primes[m] <= n {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+            rs += l - j - 1;
+        }
+    }
+
+    println!("{rs}");
 }
