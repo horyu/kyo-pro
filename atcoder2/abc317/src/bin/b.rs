cb53b975f00a0fb9c4eeb3500cfd5ea5a92635a8
horyu <28817604+horyu@users.noreply.github.com>
2024-01-23 20:57:03 +0900

    abc317 ab

---
+++ atcoder2/abc317/src/bin/b.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        aa: [usize; n],
+    };
+    let mut bts = BTreeSet::new();
+    for a in aa.iter().copied() {
+        bts.insert(a);
+    }
+    let min = bts.first().copied().unwrap();
+    let max = bts.last().copied().unwrap();
+    for i in min..max {
+        if !bts.contains(&i) {
+            println!("{i}");
+            return;
+        }
+    }
+}
