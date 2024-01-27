67777fce708a001ae3f266ffc326e00a5fbb4bbf
horyu <28817604+horyu@users.noreply.github.com>
2024-01-28 07:21:05 +0900

    abc198 e

--- atcoder2/abc198/src/bin/e.rs
+++ atcoder2/abc198/src/bin/e.rs
@@ -12,7 +12,38 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        cc: [Usize1; n],
+        aabb: [(Usize1, Usize1); n - 1],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    for (a, b) in aabb.iter().copied() {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut rs = vec![];
+    dfs(&g, &cc, !0, 0, &mut vec![0; 1e5 as usize], &mut rs);
+    rs.sort_unstable();
+    let rs = rs.iter().join("\n");
+    println!("{rs}");
+}
+
+fn dfs(
+    g: &[Vec<usize>],
+    cc: &[usize],
+    from: usize,
+    to: usize,
+    cc_cnt: &mut [usize],
+    rs: &mut Vec<usize>,
+) {
+    cc_cnt[cc[to]] += 1;
+    if cc_cnt[cc[to]] == 1 {
+        rs.push(to + 1);
+    }
+    for next in g[to].iter().copied() {
+        if next == from {
+            continue;
+        }
+        dfs(g, cc, to, next, cc_cnt, rs);
+    }
+    cc_cnt[cc[to]] -= 1;
 }
