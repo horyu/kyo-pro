1516fa5ff571c5940bcf7b45f79e412ff79ce455
horyu <28817604+horyu@users.noreply.github.com>
2024-01-27 22:48:04 +0900

    abc317 c

--- atcoder2/abc317/src/bin/c.rs
+++ atcoder2/abc317/src/bin/c.rs
@@ -12,7 +12,25 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aabbcc: [(Usize1, Usize1, usize); m],
     };
-    // println!("{rs}");
+    let mut ddd = vec![vec![!0; n]; n];
+    for (a, b, c) in aabbcc {
+        ddd[a][b] = c;
+        ddd[b][a] = c;
+    }
+    let mut rs = 0;
+    for ii in (0..n).permutations(n) {
+        let mut tmp = 0;
+        for (il, ir) in ii.iter().copied().tuple_windows() {
+            if ddd[il][ir] != !0 {
+                tmp += ddd[il][ir];
+            } else {
+                break;
+            }
+        }
+        rs = rs.max(tmp);
+    }
+    println!("{rs}");
 }
