0ab26acb445429e2d5fa6820cac22801309d3803
horyu <28817604+horyu@users.noreply.github.com>
2024-02-06 09:10:20 +0900

    abc325 e

--- atcoder2/abc325/src/bin/e.rs
+++ atcoder2/abc325/src/bin/e.rs
@@ -17,6 +17,38 @@ fn main() {
         c: usize,
         ddd: [[usize; n]; n],
     };
+    let mut g = vec![vec![]; n * 2];
+    for i in 0..n {
+        g[i].push((n + i, 0));
+    }
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let d = ddd[i][j];
+            g[i].push((j, d * a));
+            g[j].push((i, d * a));
+            g[n + i].push((n + j, d * b + c));
+            g[n + j].push((n + i, d * b + c));
+        }
+    }
+    if let Some((_, rs)) = pathfinding::directed::dijkstra::dijkstra(
+        &0,
+        |&i| g[i].iter().copied(),
+        |&i| i == 2 * n - 1,
+    ) {
+        println!("{rs}");
+    } else {
+        println!("-1");
+    }
+}
+
+fn main2() {
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        ddd: [[usize; n]; n],
+    };
     use std::cmp::Reverse as R;
     let mut bh = BinaryHeap::new();
     let mut checked = vec![vec![false; 2]; n];
