7871086e1cc9f7e6632b46abfba9af58c1a4f6c3
horyu <28817604+horyu@users.noreply.github.com>
2024-02-09 23:19:19 +0900

    abc321 c

--- atcoder2/abc321/src/bin/c.rs
+++ atcoder2/abc321/src/bin/c.rs
@@ -11,8 +11,18 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        k: usize,
     };
-    // println!("{rs}");
+    let mut qq = VecDeque::from_iter(1usize..=9);
+    let mut cnt = 0;
+    while let Some(q) = qq.pop_front() {
+        cnt += 1;
+        if cnt == k {
+            println!("{q}");
+            return;
+        }
+        for i in 0..(q % 10) {
+            qq.push_back(q * 10 + i);
+        }
+    }
 }
