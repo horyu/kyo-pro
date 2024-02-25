79b34e094693d6c4a093247bdc1fc7e80a2df5dc
horyu <28817604+horyu@users.noreply.github.com>
2024-02-25 22:31:27 +0900

    arc149 c

--- atcoder2/arc149/src/bin/c.rs
+++ atcoder2/arc149/src/bin/c.rs
@@ -12,7 +12,43 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/arc149/editorial/4911
+    let mut rs = vec![vec![0; n]; n];
+    match n {
+        3 => {
+            rs[0] = vec![5, 9, 1];
+            rs[1] = vec![3, 7, 8];
+            rs[2] = vec![6, 2, 4];
+        }
+        4 => {
+            rs[0] = vec![9, 11, 13, 15];
+            rs[1] = vec![1, 3, 5, 7];
+            rs[2] = vec![8, 6, 4, 2];
+            rs[3] = vec![10, 12, 14, 16];
+        }
+        5 => {
+            rs[0] = vec![5, 7, 11, 13, 17];
+            rs[1] = vec![19, 23, 25, 21, 1];
+            rs[2] = vec![3, 9, 15, 24, 8];
+            rs[3] = vec![6, 12, 18, 2, 4];
+            rs[4] = vec![10, 14, 16, 20, 22];
+        }
+        _ => {
+            let nn = n.pow(2u32);
+            let mut bb = vec![];
+            bb.extend((1..=nn).filter(|&x| x % 2 == 1 && x % 3 != 0));
+            bb.extend((1..=nn).filter(|&x| x % 2 == 1 && x % 3 == 0));
+            bb.extend((1..=nn).filter(|&x| x % 2 == 0 && x % 3 == 0));
+            bb.extend((1..=nn).filter(|&x| x % 2 == 0 && x % 3 != 0));
+            for i in 0..n {
+                for j in 0..n {
+                    rs[i][j] = bb[i * n + j];
+                }
+            }
+        }
+    }
+    for r in rs {
+        println!("{}", r.iter().join(" "));
+    }
 }
