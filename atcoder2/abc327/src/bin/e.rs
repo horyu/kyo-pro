1e2f77bef98c552bcdf372bd31da6c29f064a850
horyu <28817604+horyu@users.noreply.github.com>
2024-02-27 21:33:53 +0900

    abc327 e

--- atcoder2/abc327/src/bin/e.rs
+++ atcoder2/abc327/src/bin/e.rs
@@ -4,6 +4,7 @@
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
+use num_traits::Pow;
 use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
@@ -12,7 +13,20 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        pp: [f64; n],
     };
-    // println!("{rs}");
+    // 分子だけを考えて、最後に計算する
+    let mut dp = vec![0.0f64; n + 1];
+    for (i, p) in pp.iter().copied().enumerate() {
+        for j in (0..=i).rev() {
+            dp[j + 1] = dp[j + 1].max(dp[j] * 0.9 + p);
+        }
+    }
+    let mut rs = f64::MIN;
+    for (k, bunsi) in dp.iter().copied().enumerate().skip(1) {
+        let bunbo = (1.0 - 0.9f64.powi(k as i32)) / (1.0 - 0.9);
+        rs = rs.max(bunsi / bunbo - 1200.0 / (k as f64).sqrt());
+    }
+
+    println!("{rs}");
 }
