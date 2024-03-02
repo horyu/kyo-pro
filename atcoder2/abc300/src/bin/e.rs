98486733d803d9ce584dd4e135d6022aea188c52
horyu <28817604+horyu@users.noreply.github.com>
2024-03-02 09:42:24 +0900

    abc300 e

--- atcoder2/abc300/src/bin/e.rs
+++ atcoder2/abc300/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,22 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut btm = BTreeMap::new();
+    btm.insert(1, ModInt998244353::new(1));
+    let r = ModInt998244353::new(1) / 5;
+    while let Some((k, v)) = btm.pop_first() {
+        if k == n {
+            println!("{v}");
+            return;
+        }
+        for i in 2..=6 {
+            let kk = k * i;
+            let vv = v * r;
+            if kk <= n {
+                *btm.entry(kk).or_default() += vv;
+            }
+        }
+    }
+    println!("0");
 }
