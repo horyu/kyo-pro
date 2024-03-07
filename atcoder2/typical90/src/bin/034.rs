0711aae515367c2539bc82a924eb83280b90c6e5
horyu <28817604+horyu@users.noreply.github.com>
2024-03-07 19:30:36 +0900

    typical90 034

--- atcoder2/typical90/src/bin/034.rs
+++ atcoder2/typical90/src/bin/034.rs
@@ -12,7 +12,22 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        k: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut counter = counter::Counter::<_>::new();
+    let mut l = 0;
+    let mut rs = 0;
+    for r in 0..n {
+        counter[&aa[r]] += 1;
+        while k < counter.len() {
+            counter[&aa[l]] -= 1;
+            if counter[&aa[l]] == 0 {
+                counter.remove(&aa[l]);
+            }
+            l += 1;
+        }
+        rs = rs.max(r - l + 1);
+    }
+    println!("{rs}");
 }
