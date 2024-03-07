749dc31df3e0a515e53d7496c0d7147bf1fffb8a
horyu <28817604+horyu@users.noreply.github.com>
2024-03-08 06:43:58 +0900

    abc329 abc

---
+++ atcoder2/abc329/src/bin/c.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let mut counter = counter::Counter::<_>::new();
+    for (k, g) in s.iter().copied().group_by(|&c| c).into_iter() {
+        let count = g.count();
+        counter[&k] = count.max(counter[&k]);
+    }
+    let mut rs = 0;
+    for v in counter.values() {
+        rs += v;
+    }
+
+    println!("{rs}");
+}
