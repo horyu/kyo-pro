b1b5dc60d05655dc7071b7272492475a5c78cd38
horyu <28817604+horyu@users.noreply.github.com>
2024-03-09 22:43:45 +0900

    abc329 d

--- atcoder2/abc329/src/bin/d.rs
+++ atcoder2/abc329/src/bin/d.rs
@@ -12,7 +12,25 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aa: [usize; m],
     };
-    // println!("{rs}");
+    let mut btm: BTreeMap<i32, BTreeSet<usize>> = BTreeMap::new();
+    let mut cc = vec![0; n + 1];
+    for a in aa {
+        if 0 < cc[a] {
+            if let Some(set) = btm.get_mut(&cc[a]) {
+                set.remove(&a);
+                if set.is_empty() {
+                    btm.remove(&cc[a]);
+                }
+            }
+        }
+        cc[a] += 1;
+        btm.entry(cc[a]).or_default().insert(a);
+        if let Some(e) = btm.last_entry() {
+            let rs = e.get().first().unwrap();
+            println!("{rs}");
+        }
+    }
 }
