bce7bdff44baeb71cf3448f306beece9bda8f4d7
horyu <28817604+horyu@users.noreply.github.com>
2024-03-10 23:12:15 +0900

    abc330 ab

---
+++ atcoder2/abc330/src/bin/a.rs
@@ -0,0 +1,20 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        l: usize,
+        aa: [usize; n],
+    };
+    let rs = aa.iter().copied().filter(|&a| l <= a).count();
+    println!("{rs}");
+}
---
+++ atcoder2/abc330/src/bin/b.rs
@@ -0,0 +1,31 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        l: usize,
+        r: usize,
+        aa: [usize; n],
+    };
+    let mut rrss = vec![];
+    for a in aa {
+        if (l..=r).contains(&a) {
+            rrss.push(a);
+        } else if a < l {
+            rrss.push(l);
+        } else {
+            rrss.push(r);
+        }
+    }
+    let rs = rrss.iter().join(" ");
+    println!("{rs}");
+}
