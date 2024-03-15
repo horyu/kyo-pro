5b7b284041c8712f186fa2ad76132e8b030360c4
horyu <28817604+horyu@users.noreply.github.com>
2024-03-15 23:35:28 +0900

    abc329 e

--- atcoder2/abc329/src/bin/e.rs
+++ atcoder2/abc329/src/bin/e.rs
@@ -12,7 +12,48 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        mut s: Chars,
+        t: Chars,
     };
-    // println!("{rs}");
+    let is_ok =
+        |l: usize, s: &[char]| -> bool { izip!(&t, &s[l..]).all(|(&t, &s)| t == s || s == '?') };
+
+    let mut qq = VecDeque::new();
+    let mut pushed = vec![false; n];
+    let mut cnt = 0;
+    for l in 0..=(n - m) {
+        if is_ok(l, &s) {
+            qq.push_back(l);
+            pushed[l] = true;
+        }
+    }
+    while let Some(ql) = qq.pop_front() {
+        for i in 0..m {
+            if ql + i < n && s[ql + i] != '?' {
+                s[ql + i] = '?';
+                cnt += 1;
+            }
+        }
+        // 左側
+        for l in (ql.saturating_sub(m - 1))..ql {
+            if !pushed[l] && is_ok(l, &s) {
+                qq.push_front(l);
+                for j in l..ql {
+                    pushed[j] = true;
+                }
+            }
+        }
+        // 右側
+        for r in ((ql + 1)..(ql + m).min(n - m + 1)).rev() {
+            if !pushed[r] && is_ok(r, &s) {
+                qq.push_back(r);
+                for j in (ql + 1)..=r {
+                    pushed[j] = true;
+                }
+            }
+        }
+    }
+    let rs = ["No", "Yes"][(n == cnt) as usize];
+    println!("{rs}");
 }
