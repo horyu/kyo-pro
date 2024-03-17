43d9556444bc5f79c9328433aa58b8fb140cf65e
horyu <28817604+horyu@users.noreply.github.com>
2024-03-17 09:18:59 +0900

    abc329 f

--- atcoder2/abc329/src/bin/f.rs
+++ atcoder2/abc329/src/bin/f.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+#![feature(get_many_mut)]
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,26 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        cc: [Usize1; n],
+        aabb: [(Usize1, Usize1); q],
     };
-    // println!("{rs}");
+    let mut hhss = cc
+        .into_iter()
+        .map(|c| HashSet::<usize>::from_iter([c]))
+        .collect_vec();
+    for (a, b) in aabb {
+        if let Ok([ahs, bhs]) = hhss.get_many_mut([a, b]) {
+            if ahs.len() < bhs.len() {
+                bhs.extend(ahs.drain());
+            } else {
+                ahs.extend(bhs.drain());
+            }
+            if bhs.is_empty() {
+                std::mem::swap(ahs, bhs);
+            }
+            let rs = bhs.len();
+            println!("{rs}");
+        }
+    }
 }
