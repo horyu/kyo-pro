78a42a2a09ceb9e9edc8ade065dbd3e6be5aae85
horyu <28817604+horyu@users.noreply.github.com>
2024-03-21 20:31:18 +0900

    abc164 d

--- atcoder2/abc164/src/bin/d.rs
+++ atcoder2/abc164/src/bin/d.rs
@@ -36,3 +36,28 @@ fn main() {
     }
     println!("{rs}");
 }
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        s: Bytes,
+    };
+    const N: usize = 2019;
+    let mut vv = [0usize; N];
+    let mut rs = 0;
+    for b in s {
+        let b = (b - b'0') as usize;
+        let mut new_vv = [0; N];
+        for (i, v) in vv.iter().copied().enumerate() {
+            new_vv[(i * 10 + b) % N] += v;
+        }
+        new_vv[b] += 1;
+        vv = new_vv;
+        // let tmp = vv.iter().copied().enumerate().filter(|iv| 0 < iv.1)
+        //     .map(|ic| format!("{ic:?}"))
+        //     .join(" ");
+        // eprintln!("{tmp}");
+        rs += vv[0];
+    }
+    println!("{rs}");
+}
