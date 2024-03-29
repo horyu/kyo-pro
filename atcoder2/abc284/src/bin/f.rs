28977e7d098ff7221b6bde35e6002a6572a63b5c
horyu <28817604+horyu@users.noreply.github.com>
2024-03-29 21:34:13 +0900

    abc284 f

--- atcoder2/abc284/src/bin/f.rs
+++ atcoder2/abc284/src/bin/f.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        t: Bytes,
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc284/editorial/5469
+    let a = t[..n].iter().copied().collect_vec();
+    let b = t[n..].iter().copied().rev().collect_vec();
+
+    let x = chain!(&a, &b).copied().collect_vec();
+    let mut za_x = ac_library::z_algorithm_arbitrary(&x);
+    za_x.push(0);
+
+    let y = chain!(&b, &a).copied().collect_vec();
+    let mut za_y = ac_library::z_algorithm_arbitrary(&y);
+    za_y.push(0);
+    for i in 0..=n {
+        if za_x[2 * n - i] < i {
+            continue;
+        }
+        if za_y[n + i] < n - i {
+            continue;
+        }
+        let rs = chain!(&t[..i], &t[(n + i)..])
+            .map(|&b| b as char)
+            .collect::<String>();
+        println!("{rs}");
+        println!("{i}");
+        return;
+    }
+    println!("-1");
 }
