99c056689277cd56af20788f2f3546817674f01f
horyu <28817604+horyu@users.noreply.github.com>
2024-03-31 22:44:02 +0900

    abc285 e

--- atcoder2/abc285/src/bin/e.rs
+++ atcoder2/abc285/src/bin/e.rs
@@ -14,5 +14,27 @@ fn main() {
         n: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    if n == 1 {
+        println!("0");
+        return;
+    }
+    // cc[i] = i日間割当可能日があるときの最大の生産量（0,i+1日目を休みとする）
+    let mut cc = vec![0; n + 1];
+    cc[1] = aa[0];
+    cc[2] = aa[0] * 2;
+    for i in 3..n {
+        if i % 2 == 1 {
+            cc[i] = cc[i - 1] + aa[i / 2];
+        } else {
+            cc[i] = cc[i - 1] + aa[(i - 1) / 2];
+        }
+    }
+    for i in 3..n {
+        for j in 1..=(i / 2) {
+            cc[i] = cc[i].max(cc[j] + cc[i - 1 - j]);
+        }
+    }
+    // eprintln!("{}", cc.iter().join(" "));
+    let rs = cc[n - 1];
+    println!("{rs}");
 }
