d590b6007d0d544efe6ec33e4021018b15b90d5a
horyu <28817604+horyu@users.noreply.github.com>
2024-04-02 23:06:18 +0900

    abc332 ab

---
+++ atcoder2/abc332/src/bin/b.rs
@@ -0,0 +1,32 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        k: usize,
+        g: usize,
+        m: usize,
+    };
+    let mut grass = 0;
+    let mut mugcup = 0;
+    for _ in 0..k {
+        if grass == g {
+            grass = 0;
+        } else if mugcup == 0 {
+            mugcup = m;
+        } else {
+            let x = mugcup.min(g - grass);
+            grass += x;
+            mugcup -= x;
+        }
+    }
+    println!("{grass} {mugcup}");
+}
