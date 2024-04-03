482f74102408b94a1e086d07f0b1083ba7662b51
horyu <28817604+horyu@users.noreply.github.com>
2024-04-03 20:22:26 +0900

    abc272 f

--- atcoder2/abc272/src/bin/f.rs
+++ atcoder2/abc272/src/bin/f.rs
@@ -12,7 +12,26 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Chars,
+        t: Chars,
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc272/submissions/35720986
+    let x = chain!(&s, &s, &['!'], &t, &t, &['~'],)
+        .copied()
+        .collect_vec();
+    let sa = ac_library::suffix_array_arbitrary(&x);
+
+    let mut tmp = 0usize;
+    let mut rs = 0usize;
+    for i in sa {
+        if i < n {
+            tmp += 1;
+        }
+        // 1つ目のtの範囲内であれば、それ以前に出現したsのsuffixのインデックスの数が対象
+        if ((2 * n + 1)..=(3 * n)).contains(&i) {
+            rs += tmp;
+        }
+    }
+
+    println!("{rs}");
 }
