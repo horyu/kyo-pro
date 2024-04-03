d6aa4a7d3864b3def2af79e5cb210e593707ad44
horyu <28817604+horyu@users.noreply.github.com>
2024-04-04 07:01:13 +0900

    snv102 c

--- atcoder2/abc102/src/bin/c.rs
+++ atcoder2/abc102/src/bin/c.rs
@@ -18,14 +18,15 @@ fn main() {
         .iter()
         .copied()
         .enumerate()
-        .map(|(i, a)| a - (i + 1) as isize)
+        .map(|(i, a)| a - i as isize)
         .sorted_unstable()
         .collect_vec();
-    let b = if n.is_odd() {
-        cc[n / 2]
+    let len = cc.len();
+    let mid = if len % 2 == 0 {
+        (cc[len / 2 - 1] + cc[len / 2]) / 2
     } else {
-        (cc[n / 2 - 1] + cc[n / 2]) / 2
+        cc[len / 2]
     };
-    let rs = cc.iter().copied().map(|c| (c - b).abs()).sum::<isize>();
+    let rs = cc.iter().copied().map(|c| (c - mid).abs()).sum::<isize>();
     println!("{rs}");
 }
