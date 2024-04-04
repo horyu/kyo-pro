79b787715e10f557a780c5e8232a57dac6dc4f7e
horyu <28817604+horyu@users.noreply.github.com>
2024-04-05 08:08:01 +0900

    abc333 e

--- atcoder2/abc333/src/bin/e.rs
+++ atcoder2/abc333/src/bin/e.rs
@@ -12,7 +12,45 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        ttxx: [(usize, Usize1); n],
     };
-    // println!("{rs}");
+    // aaa[i] = ポーションiの位置配列
+    // bab[i] = ポーションiの位置配列
+    let mut aaa = vec![vec![]; n];
+    let mut bbb = vec![vec![]; n];
+    for (i, (t, x)) in ttxx.iter().copied().enumerate() {
+        if t == 1 {
+            aaa[x].push(i);
+        } else {
+            bbb[x].push(i);
+        }
+    }
+    // vv[i] = 1:位置iでポーションを拾う -1:位置iでポーションを使う
+    let mut vv = vec![0; n];
+    // bの位置にできるだけ近く前に出現するaを拾う
+    for (mut aa, mut bb) in izip!(aaa, bbb) {
+        while let Some(b) = bb.pop() {
+            vv[b] = -1;
+            let mut ok = false;
+            while let Some(a) = aa.pop() {
+                if b < a {
+                    continue;
+                }
+                vv[a] = 1;
+                ok = true;
+                break;
+            }
+            if !ok {
+                println!("-1");
+                return;
+            }
+        }
+    }
+
+    let k = vv.iter().copied().cumsum::<i32>().max().unwrap();
+    let ww = izip!(vv, ttxx)
+        .filter_map(|(v, (t, x))| (t == 1).then_some(v))
+        .join(" ");
+    println!("{k}");
+    println!("{ww}");
 }
