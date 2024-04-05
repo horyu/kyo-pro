2ce4db3191a4dc8f2116702e0aae6ee34226ee2c
horyu <28817604+horyu@users.noreply.github.com>
2024-04-06 08:32:26 +0900

    abc332 c

--- atcoder2/abc332/src/bin/c.rs
+++ atcoder2/abc332/src/bin/c.rs
@@ -12,7 +12,27 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        s: Chars,
     };
-    // println!("{rs}");
+    let mut c1 = 0usize;
+    let mut c2 = 0;
+    let mut rs = 0;
+    for c in chain!(s, ['0']) {
+        match c {
+            '0' => {
+                rs = rs.max(c1.saturating_sub(m) + c2);
+                c1 = 0;
+                c2 = 0;
+            }
+            '1' => {
+                c1 += 1;
+            }
+            '2' => {
+                c2 += 1;
+            }
+            _ => unreachable!(),
+        }
+    }
+    println!("{rs}");
 }
