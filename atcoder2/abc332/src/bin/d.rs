13b637018edeb4d63d0627eff8ca0fddef735e17
horyu <28817604+horyu@users.noreply.github.com>
2024-04-07 21:42:09 +0900

    abc332 d

--- atcoder2/abc332/src/bin/d.rs
+++ atcoder2/abc332/src/bin/d.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+#![feature(map_try_insert)]
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -11,8 +12,42 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        aaa: [[usize; w]; h],
+        bbb: [[usize; w]; h],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc332/editorial/7922
+    let mut hm = HashMap::new();
+    hm.insert(aaa.clone(), 0);
+    let mut qq = VecDeque::new();
+    qq.push_back(aaa);
+    while let Some(aaa) = qq.pop_front() {
+        let cnt = hm.get(&aaa).copied().unwrap();
+        if aaa == bbb {
+            println!("{cnt}");
+            return;
+        }
+
+        for i in 0..(h - 1) {
+            let mut ccc = aaa.clone();
+            ccc.swap(i, i + 1);
+            if !hm.contains_key(&ccc) {
+                hm.insert(ccc.clone(), cnt + 1);
+                qq.push_back(ccc);
+            }
+        }
+        for j in 0..(w - 1) {
+            let mut ccc = aaa.clone();
+            for i in 0..h {
+                ccc[i].swap(j, j + 1);
+            }
+            if !hm.contains_key(&ccc) {
+                hm.insert(ccc.clone(), cnt + 1);
+                qq.push_back(ccc);
+            }
+        }
+    }
+
+    println!("-1");
 }
