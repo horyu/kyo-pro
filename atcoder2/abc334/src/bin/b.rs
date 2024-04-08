a5dac8ac70303956fd645a5c09f5323d0e5acf5a
horyu <28817604+horyu@users.noreply.github.com>
2024-04-09 07:54:17 +0900

    abc334 ab

---
+++ atcoder2/abc334/src/bin/b.rs
@@ -0,0 +1,24 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        a: isize,
+        m: isize,
+        l: isize,
+        r: isize,
+    };
+    let l = l - a;
+    let r = r - a;
+    // l..=r の間の km の個数
+    let rs = r.div_euclid(m) - (l - 1).div_euclid(m);
+    println!("{rs}");
+}
