fdb6d5342b33651d743beb498bcafb00bf70636f
horyu <28817604+horyu@users.noreply.github.com>
2024-04-18 08:06:05 +0900

    abc335 c

--- atcoder2/abc335/src/bin/c.rs
+++ atcoder2/abc335/src/bin/c.rs
@@ -12,7 +12,28 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
     };
-    // println!("{rs}");
+    let mut qq = VecDeque::from_iter((1..=n).map(|i| (i as isize, 0isize)));
+    for _ in 0..q {
+        input! { t: usize};
+        if t == 1 {
+            input! { c: char };
+            qq.pop_back();
+            let (dx, dy) = match c {
+                'R' => (1, 0),
+                'L' => (-1, 0),
+                'U' => (0, 1),
+                'D' => (0, -1),
+                _ => unreachable!(),
+            };
+            let (x, y) = qq[0];
+            let (nx, ny) = (x + dx, y + dy);
+            qq.push_front((nx, ny));
+        } else {
+            input! { p: Usize1 };
+            let (x, y) = qq[p];
+            println!("{x} {y}");
+        }
+    }
 }
