d8d4d919bc716251766b928866e6c53ac9482526
horyu <28817604+horyu@users.noreply.github.com>
2024-04-25 08:27:45 +0900

    abc223 d

--- atcoder2/abc223/src/bin/d.rs
+++ atcoder2/abc223/src/bin/d.rs
@@ -12,7 +12,37 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aabb: [(Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    let mut pp = vec![0; n];
+    let mut dsu = ac_library::Dsu::new(n);
+    for (a, b) in aabb.iter().copied() {
+        g[a].push(b);
+        pp[b] += 1;
+        dsu.merge(a, b);
+    }
+    let mut bh = BinaryHeap::new();
+    for (i, p) in pp.iter().copied().enumerate() {
+        if p == 0 {
+            bh.push(R(i));
+        }
+    }
+    let mut rrss = vec![];
+    while let Some(R(i)) = bh.pop() {
+        rrss.push(i + 1);
+        for &j in &g[i] {
+            pp[j] -= 1;
+            if pp[j] == 0 {
+                bh.push(R(j));
+            }
+        }
+    }
+    if rrss.len() < n {
+        println!("-1");
+        return;
+    }
+    let rs = rrss.iter().join(" ");
+    println!("{rs}");
 }
