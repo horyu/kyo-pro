354f185a63e96078e9fb2d54f97aa640563f541c
horyu <28817604+horyu@users.noreply.github.com>
2024-05-02 09:38:05 +0900

    abc337 ab

---
+++ atcoder2/abc337/src/bin/a.rs
@@ -0,0 +1,25 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        xxyy: [(usize, usize); n],
+    };
+    let x_sum = xxyy.iter().map(|(x, _)| x).sum::<usize>();
+    let y_sum = xxyy.iter().map(|(_, y)| y).sum::<usize>();
+    let rs = match x_sum.cmp(&y_sum) {
+        Ordering::Less => "Aoki",
+        Ordering::Equal => "Draw",
+        Ordering::Greater => "Takahashi",
+    };
+    println!("{rs}");
+}
