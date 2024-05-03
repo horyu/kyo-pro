11cd3509588bccd8f41ef5c3a2f2b6942a700fc0
horyu <28817604+horyu@users.noreply.github.com>
2024-05-03 21:20:07 +0900

    abc336 e

--- atcoder2/abc336/src/bin/e.rs
+++ atcoder2/abc336/src/bin/e.rs
@@ -11,8 +11,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        nn: Bytes,
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc336/editorial/9055
+    let nn = nn.into_iter().map(|n| (n - b'0') as usize).collect_vec();
+    let len = nn.len();
+    let mut rs = 0usize;
+    for s in 1..=(9 * 14) {
+        let mut dp = vec![vec![vec![[0usize; 2]; s]; s + 1]; len + 1];
+        dp[0][0][0][1] = 1;
+        for d in 0..len {
+            for i in 0..=s {
+                for j in 0..s {
+                    for f in 0..2 {
+                        for t in 0..10 {
+                            if s < i + t {
+                                break;
+                            }
+                            if (f == 1) && (nn[d] < t) {
+                                break;
+                            }
+                            dp[d + 1][i + t][(j * 10 + t) % s][f & usize::from(nn[d] == t)] +=
+                                dp[d][i][j][f];
+                        }
+                    }
+                }
+            }
+        }
+        rs += dp[len][s][0][0] + dp[len][s][0][1];
+    }
+    println!("{rs}");
 }
