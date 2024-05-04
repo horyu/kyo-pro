b7bd41e7b5be7c294bda8c2f420ccd5a24a4b36c
horyu <28817604+horyu@users.noreply.github.com>
2024-05-04 09:11:55 +0900

    abc170 e

--- atcoder2/abc170/src/bin/e.rs
+++ atcoder2/abc170/src/bin/e.rs
@@ -7,51 +7,66 @@ use num_integer::*;
 use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
-use std::{
-    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
-    ops::Not,
-};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+use ac_library::{LazySegtree, MapMonoid, Min};
+struct MinMonoid;
+impl MapMonoid for MinMonoid {
+    type M = Min<usize>;
+    type F = usize;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(&f: &usize, &x: &usize) -> usize {
+        f + x
+    }
+
+    fn composition(&f: &usize, &g: &usize) -> usize {
+        f + g
+    }
+}
 
 fn main() {
     input! {
         n: usize,
         q: usize,
-        aabb: [(usize, Usize1); n],
+        mut aabb: [(usize, Usize1); n],
         ccdd: [(Usize1, Usize1); q],
     };
-    // 園i 園児j レートa
-    let mut i_to_aajj = vec![BTreeSet::new(); 2 * 1e5 as usize];
-    let mut j_to_ia = vec![(0, 0); n];
-    for (j, (a, b)) in aabb.iter().copied().enumerate() {
-        i_to_aajj[b].insert((a, j));
-        j_to_ia[j] = (b, a);
+    let mut ens = vec![BTreeSet::<(usize, usize)>::new(); 2e5 as usize];
+    for (i, (rate, j)) in aabb.iter().copied().enumerate() {
+        ens[j].insert((rate, i));
     }
-    // (a, j)
-    let mut bts = i_to_aajj
-        .iter()
-        .enumerate()
-        .filter_map(|(i, aajj)| aajj.iter().max().copied())
-        .collect::<BTreeSet<_>>();
+    let mut ls = LazySegtree::<MinMonoid>::from(vec![1e10 as usize; 2e5 as usize]);
+    for (j, en) in ens.iter().enumerate() {
+        if let Some(&(rate, _)) = en.last() {
+            ls.set(j, rate);
+        }
+    }
     for (c, d) in ccdd {
-        let (i, a) = j_to_ia[c];
-
-        i_to_aajj[i].remove(&(a, c));
-        if bts.remove(&(a, c)) {
-            if let Some(aj) = i_to_aajj[i].iter().max().copied() {
-                bts.insert(aj);
+        // 削除
+        {
+            let (rate, j) = aabb[c];
+            ens[j].remove(&(rate, c));
+            if let Some(&(r, _)) = ens[j].last() {
+                ls.set(j, r);
+            } else {
+                ls.set(j, 1e10 as usize);
+            }
+        }
+        // 追加
+        {
+            aabb[c].1 = d;
+            let (rate, j) = aabb[c];
+            ens[j].insert((rate, c));
+            if let Some(&(r, _)) = ens[j].last() {
+                ls.set(j, r);
             }
         }
 
-        // btsから移動先のトップを削除して挿入し直す
-        if let Some(aj) = i_to_aajj[d].iter().max().copied() {
-            bts.remove(&aj);
-        }
-        i_to_aajj[d].insert((a, c));
-        bts.insert(i_to_aajj[d].iter().max().copied().unwrap());
-
-        j_to_ia[c].0 = d;
-
-        let rs = bts.iter().min().copied().unwrap().0;
+        let rs = ls.all_prod();
         println!("{rs}");
     }
 }
