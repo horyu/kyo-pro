b32743ab3474966dee49c1b0301ea62f73731820
horyu <28817604+horyu@users.noreply.github.com>
2024-05-07 09:07:36 +0900

    abc337 d

--- atcoder2/abc337/src/bin/d.rs
+++ atcoder2/abc337/src/bin/d.rs
@@ -11,8 +11,67 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        k: usize,
+        ss: [Chars; h],
     };
-    // println!("{rs}");
+    // o.x => 012
+    let ss = ss
+        .into_iter()
+        .map(|s| {
+            s.into_iter()
+                .map(|c| match c {
+                    'o' => 0,
+                    '.' => 1,
+                    'x' => 2,
+                    _ => unreachable!(),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let mut rs = !0usize;
+    // 横方向
+    for i in 0..h {
+        if w < k {
+            break;
+        }
+        let mut arr = [0; 3];
+        let mut r = 0;
+        for l in 0..(w - k + 1) {
+            while r < l + k {
+                arr[ss[i][r]] += 1;
+                r += 1;
+            }
+            if arr[2] == 0 {
+                rs = rs.min(arr[1]);
+            }
+            arr[ss[i][l]] -= 1;
+        }
+    }
+    // // 縦方向
+    for j in 0..w {
+        if h < k {
+            break;
+        }
+        let mut arr = [0; 3];
+        let mut r = 0;
+        for l in 0..(h - k + 1) {
+            while r < l + k {
+                arr[ss[r][j]] += 1;
+                r += 1;
+            }
+            if arr[2] == 0 {
+                rs = rs.min(arr[1]);
+            }
+            arr[ss[l][j]] -= 1;
+        }
+    }
+
+    if rs == !0 {
+        println!("-1");
+    } else {
+        println!("{rs}");
+    }
 }
