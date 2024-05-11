d4fb5d6a035f0f7bdefc6991d6442d0151524b64
horyu <28817604+horyu@users.noreply.github.com>
2024-05-11 23:11:18 +0900

    abc353 abcde

---
+++ atcoder2/abc353/src/bin/b.rs
@@ -0,0 +1,32 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n],
+    };
+    let mut crr = 0;
+    let mut rs = 0;
+    for a in aa {
+        if crr + a <= k {
+            crr += a;
+        } else {
+            rs += 1;
+            crr = a;
+        }
+    }
+    if 0 < crr {
+        rs += 1;
+    }
+    println!("{rs}");
+}
---
+++ atcoder2/abc353/src/bin/d.rs
@@ -0,0 +1,30 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use ac_library::ModInt998244353;
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        aa: [usize; n],
+    };
+    let mut keta_cntss = [0; 12];
+    let mut keta_sums = [ModInt998244353::default(); 12];
+    let mut rs = ModInt998244353::default();
+    for (i, a) in aa.iter().copied().enumerate() {
+        let keta = 1 + a.ilog10();
+        for (&kc, &ks) in izip!(&keta_cntss, &keta_sums) {
+            rs += ks * 10usize.pow(keta) + kc * a;
+        }
+        keta_cntss[keta as usize] += 1;
+        keta_sums[keta as usize] += a;
+    }
+    println!("{rs}");
+}
