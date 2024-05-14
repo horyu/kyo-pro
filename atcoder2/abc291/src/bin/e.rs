77cc94362cd94e933baa560ff5b1608db3c39c5b
horyu <28817604+horyu@users.noreply.github.com>
2024-05-14 13:11:07 +0900

    abc291 e

--- atcoder2/abc291/src/bin/e.rs
+++ atcoder2/abc291/src/bin/e.rs
@@ -12,7 +12,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        xxyy: [(Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    let mut roots = vec![true; n];
+    for (x, y) in xxyy.iter().copied() {
+        g[x].push(y);
+        roots[y] = false;
+    }
+    let roots = roots.into_iter().positions(|tf| tf).collect_vec();
+    // eprintln!("{roots:?}");
+    use pathfinding::directed::topological_sort::topological_sort;
+    if let Ok(vv) = topological_sort(&roots, |&i| g[i].iter().copied()) {
+      if let Ok(ww) = topological_sort(&roots, |&i| g[i].iter().copied().rev()) {
+        if roots.len() == 1 && vv.len() == n && vv == ww {
+            println!("Yes");
+            let mut rrss = vec![0; n];
+            for (i, v) in vv.iter().copied().enumerate() {
+                rrss[v] = i + 1;
+            }
+            println!("{}", rrss.iter().join(" "));
+            return;
+        }
+      }
+    }
+    println!("No");
 }
