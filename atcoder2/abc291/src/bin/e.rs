850e49e551608e2420349d2669685dc9d41272b2
horyu <28817604+horyu@users.noreply.github.com>
2024-05-16 20:27:47 +0900

    abc291 f

--- atcoder2/abc291/src/bin/e.rs
+++ atcoder2/abc291/src/bin/e.rs
@@ -25,17 +25,17 @@ fn main() {
     // eprintln!("{roots:?}");
     use pathfinding::directed::topological_sort::topological_sort;
     if let Ok(vv) = topological_sort(&roots, |&i| g[i].iter().copied()) {
-      if let Ok(ww) = topological_sort(&roots, |&i| g[i].iter().copied().rev()) {
-        if roots.len() == 1 && vv.len() == n && vv == ww {
-            println!("Yes");
-            let mut rrss = vec![0; n];
-            for (i, v) in vv.iter().copied().enumerate() {
-                rrss[v] = i + 1;
+        if let Ok(ww) = topological_sort(&roots, |&i| g[i].iter().copied().rev()) {
+            if roots.len() == 1 && vv.len() == n && vv == ww {
+                println!("Yes");
+                let mut rrss = vec![0; n];
+                for (i, v) in vv.iter().copied().enumerate() {
+                    rrss[v] = i + 1;
+                }
+                println!("{}", rrss.iter().join(" "));
+                return;
             }
-            println!("{}", rrss.iter().join(" "));
-            return;
         }
-      }
     }
     println!("No");
 }
--- atcoder2/abc291/src/bin/f.rs
+++ atcoder2/abc291/src/bin/f.rs
@@ -12,7 +12,58 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        ss: [Chars; n],
     };
-    // println!("{rs}");
+    let mut rg = vec![vec![]; n];
+    let mut lg = vec![vec![]; n];
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.iter().copied().enumerate() {
+            if c == '1' {
+                rg[i].push(i + 1 + j);
+                lg[i + 1 + j].push(i);
+            }
+        }
+    }
+    // rd[i] = 都市0から都市iまでの最小移動回数
+    let mut rd = vec![!0; n];
+    rd[0] = 0;
+    for (i, jj) in rg.iter().enumerate() {
+        for j in jj.iter().copied() {
+            if rd[i] != !0 {
+                rd[j] = rd[j].min(rd[i] + 1);
+            }
+        }
+    }
+    // ld[i] = 都市n-1から都市iまでの最小移動回数
+    let mut ld = vec![!0; n];
+    ld[n - 1] = 0;
+    for (i, jj) in lg.iter().enumerate().rev() {
+        for j in jj.iter().copied() {
+            if ld[i] != !0 {
+                ld[j] = ld[j].min(ld[i] + 1);
+            }
+        }
+    }
+    // 都市kを経由しない場合の都市0から都市n-1までの最小移動回数
+    let mut rrss = vec![-1; n - 2];
+    for k in 1..(n - 1) {
+        let mut rs = !0usize;
+        for i in (k.saturating_sub(m - 1))..k {
+            if rd[i] == !0 {
+                continue;
+            }
+            for &j in rg[i].iter() {
+                if k < j && ld[j] != !0 {
+                    rs = rs.min(rd[i] + ld[j] + 1);
+                }
+            }
+        }
+
+        if rs != !0 {
+            rrss[k - 1] = rs as i32;
+        }
+    }
+    let rs = rrss.iter().join(" ");
+    println!("{rs}");
 }
