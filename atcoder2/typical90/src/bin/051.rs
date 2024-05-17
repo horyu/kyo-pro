1cfffde83c13dc6d483c456581e772ac0c7652a1
horyu <28817604+horyu@users.noreply.github.com>
2024-05-17 09:29:21 +0900

    typical90 051

--- atcoder2/typical90/src/bin/051.rs
+++ atcoder2/typical90/src/bin/051.rs
@@ -12,7 +12,57 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        p: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let h = n / 2;
+
+    let mut xxx = vec![vec![]; k + 1];
+    for bit in 0u32..(1 << h) {
+        let idx = bit.count_ones() as usize;
+        if k < idx {
+            continue;
+        }
+        let mut sum = 0;
+        for i in 0..h {
+            if (bit >> i) & 1 != 0 {
+                sum += aa[i];
+            }
+        }
+        if sum <= p {
+            xxx[idx].push(sum);
+        }
+    }
+
+    let mut yyy = vec![vec![]; k + 1];
+    for bit in 0u32..(1 << (n - h)) {
+        let idx = bit.count_ones() as usize;
+        if k < idx {
+            continue;
+        }
+        let mut sum = 0;
+        for i in 0..(n - h) {
+            if (bit >> i) & 1 != 0 {
+                sum += aa[h + i];
+            }
+        }
+        if sum <= p {
+            yyy[idx].push(sum);
+        }
+    }
+    yyy.iter_mut().for_each(|yy| yy.sort_unstable());
+    // for (ky, yy) in yyy.iter().enumerate() {
+    //     eprintln!("{ky}: {yy:?}");
+    // }
+
+    let mut rs = 0usize;
+    for (kx, xx) in xxx.into_iter().enumerate() {
+        for x in xx {
+            let ky = k - kx;
+            let pos = yyy[ky].partition_point(|&y| x + y <= p);
+            rs += pos;
+        }
+    }
+    println!("{rs}");
 }
