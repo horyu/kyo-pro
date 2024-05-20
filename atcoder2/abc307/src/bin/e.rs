1ae2741badb28f44deed08dadf8e7934f8a1f809
horyu <28817604+horyu@users.noreply.github.com>
2024-05-21 08:59:38 +0900

    abc307 e

--- atcoder2/abc307/src/bin/e.rs
+++ atcoder2/abc307/src/bin/e.rs
@@ -15,6 +15,28 @@ fn main() {
         n: usize,
         m: usize,
     };
+    // https://atcoder.jp/contests/abc307/editorial/6643
+    // 包除原理
+    const MOD: usize = 998244353;
+    let comb = Combination::new(n, MOD);
+    let mut rs = ModInt998244353::default();
+    for k in 0..=n {
+        let r = mod_pow(m, n.saturating_sub(k + 1) + 1, MOD) * comb.get(n, k);
+        if k.is_odd() {
+            rs -= r;
+        } else {
+            rs += r;
+        }
+    }
+    println!("{rs}");
+}
+
+#[allow(dead_code)]
+fn main2() {
+    input! {
+        n: usize,
+        m: usize,
+    };
     // dp = i人目が0人目と [同じ組み合わせ数, 異なる組み合わせ数]
     let mut dp = [ModInt998244353::new(0); 2];
     dp[0] += m;
@@ -23,4 +45,59 @@ fn main() {
     }
     let rs = dp[1];
     println!("{rs}");
+}
+
+// https://blog.spiralray.net/cp/modulo#i-8
+fn mod_pow(mut x: usize, mut n: usize, m: usize) -> usize {
+    x %= m;
+    let mut ans = 1;
+    while n != 0 {
+        if n.is_odd() {
+            ans = ans * x % m;
+        }
+        x = x * x % m;
+        n >>= 1;
+    }
+    ans
+}
+
+// https://github.com/kenkoooo/competitive-programming-rs/blob/master/src/math/combination.rs
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Self {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
+        }
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
+        }
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
+        }
+        Self {
+            fact,
+            inv_fact,
+            modulo,
+        }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> usize {
+        assert!(x >= y);
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
+    }
+
+    pub fn h(&self, n: usize, r: usize) -> usize {
+        self.get(n + r - 1, r)
+    }
 }
