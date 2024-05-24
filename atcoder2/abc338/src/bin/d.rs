35ee5331749567a22aaa0762e628540e5ead56c6
horyu <28817604+horyu@users.noreply.github.com>
2024-05-24 09:02:06 +0900

    abc338 d

--- atcoder2/abc338/src/bin/d.rs
+++ atcoder2/abc338/src/bin/d.rs
@@ -12,7 +12,47 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        xx: [Usize1; m],
     };
-    // println!("{rs}");
+    let mut imos = vec![0; n];
+    for (xi, xj) in xx.iter().copied().tuple_windows() {
+        let xl = xi.min(xj);
+        let xr = xi.max(xj);
+        // l→0→(n-1)→r
+        let dl = xl + 1 + (n - 1 - xr);
+        // l→r
+        let dr = xr - xl;
+
+        let d = dl as isize - dr as isize;
+        match d.cmp(&0) {
+            // l→rの区間は|d|だけ長い
+            Ordering::Less => {
+                imos[xl] += d.abs();
+                imos[xr] -= d.abs();
+            },
+            // どちら側の橋を落としても距離が変わらない
+            Ordering::Equal => (),
+            Ordering::Greater => {
+                imos[0] += d;
+                imos[xl] -= d;
+                imos[xr] += d;
+            },
+        }
+    }
+    for i in 1..n {
+        imos[i] += imos[i - 1];
+    }
+    let remove_pos = imos.iter().copied().position_max().unwrap_or_default();
+    let mut rs = 0;
+    for (xi, xj) in xx.iter().copied().tuple_windows() {
+        let xl = xi.min(xj);
+        let xr = xi.max(xj);
+        if (xl..xr).contains(&remove_pos) {
+            rs += xl + 1 + (n - 1 - xr);
+        } else {
+            rs += xr - xl;
+        }
+    }
+    println!("{rs}");
 }
