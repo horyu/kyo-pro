521c79f9a4db51dbd041e6b91b09c782a0de70ee
horyu <28817604+horyu@users.noreply.github.com>
2024-05-29 09:55:19 +0900

    arc034 b

--- atcoder2/arc034/src/bin/b.rs
+++ atcoder2/arc034/src/bin/b.rs
@@ -12,7 +12,33 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    // f(a) = aの各桁の和
+    // n = a + f(a), n <= 1e18
+
+    // ある数xについてf(x)の最大値は 9*(xの桁数)
+    let mut rrss = vec![];
+    let ilog10 = n.ilog10();
+    for d in 1.max(ilog10)..=(18.min(ilog10 + 1)) {
+        // eprintln!("d={d}");
+        for f in 1..=(9 * d as usize) {
+            if let Some(a) = n.checked_sub(f) {
+                let mut x = a;
+                let mut sum = 0;
+                while x > 0 {
+                    sum += x % 10;
+                    x /= 10;
+                }
+                if sum == f {
+                    rrss.push(a);
+                }
+            }
+        }
+    }
+    rrss.sort_unstable();
+    rrss.dedup();
+    println!("{}", rrss.len());
+    for rs in rrss {
+        println!("{}", rs);
+    }
 }
