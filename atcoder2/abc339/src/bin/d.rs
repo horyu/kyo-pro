4fae131f81b08e0290f54a0f7f00ec87a2def95e
horyu <28817604+horyu@users.noreply.github.com>
2024-06-05 20:37:27 +0900

    abc339 d

--- atcoder2/abc339/src/bin/d.rs
+++ atcoder2/abc339/src/bin/d.rs
@@ -12,7 +12,57 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        ss: [Chars; n],
     };
-    // println!("{rs}");
+    let mut block = vec![vec![false; n]; n];
+    let mut starts = vec![];
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.iter().copied().enumerate() {
+            match c {
+                '#' => block[i][j] = true,
+                'P' => {
+                    starts.push(i as i32);
+                    starts.push(j as i32);
+                }
+                _ => (),
+            }
+        }
+    }
+    let mut qq = VecDeque::new();
+    let mut pushed = HashSet::new();
+    let arr: [i32; 4] = starts.try_into().unwrap();
+    qq.push_back((arr, 0));
+    pushed.insert(arr);
+    let dxdy = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+    let n = n as i32;
+    while let Some((arr, qd)) = qq.pop_front() {
+        for (dx, dy) in dxdy {
+            let mut brr = arr;
+            for (x, y) in brr.iter_mut().tuples() {
+                *x += dx;
+                *y += dy;
+            }
+            for v in brr.iter_mut() {
+                if *v < 0 {
+                    *v = 0;
+                } else if n <= *v {
+                    *v = n - 1;
+                }
+            }
+            for (x, y) in brr.iter_mut().tuples() {
+                if block[*x as usize][*y as usize] {
+                    *x -= dx;
+                    *y -= dy;
+                }
+            }
+            if brr[0..2] == brr[2..] {
+                println!("{}", qd + 1);
+                return;
+            }
+            if pushed.insert(brr) {
+                qq.push_back((brr, qd + 1));
+            }
+        }
+    }
+    println!("-1");
 }
