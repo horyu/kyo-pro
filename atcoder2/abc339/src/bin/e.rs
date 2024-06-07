f095fc611819c81e64ce9336846e8bfb2518bee3
horyu <28817604+horyu@users.noreply.github.com>
2024-06-07 09:00:11 +0900

    abc339 e

--- atcoder2/abc339/src/bin/e.rs
+++ atcoder2/abc339/src/bin/e.rs
@@ -9,10 +9,38 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+use ac_library::{LazySegtree, MapMonoid, Max, Monoid};
+struct MaxMonoid;
+impl MapMonoid for MaxMonoid {
+    type M = Max<usize>;
+    type F = usize;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(&f: &Self::F, &x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
+        f + x
+    }
+
+    fn composition(&f: &Self::F, &g: &Self::F) -> Self::F {
+        f + g
+    }
+}
+
 fn main() {
     input! {
         n: usize,
+        d: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut ls = ac_library::LazySegtree::<MaxMonoid>::new(5e5 as usize * 2 + 1);
+
+    for a in aa {
+        let x = ls.prod(a.saturating_sub(d)..=(a + d));
+        ls.set(a, x + 1);
+    }
+
+    let rs = ls.all_prod();
+    println!("{rs}");
 }
