3a85a735c2fa077bb0432bf09529bed49b79444a
horyu <28817604+horyu@users.noreply.github.com>
2024-06-21 22:00:24 +0900

    abc341 d

--- atcoder2/abc341/src/bin/d.rs
+++ atcoder2/abc341/src/bin/d.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        k: usize,
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc341/editorial/9334
+    let kk = k - 1;
+    let min = n.min(m);
+    let max = n.max(m);
+    let lcm = n.lcm(&m);
+    // 1..=lcm までの間にmin,maxの片方せしか割り切れない数
+    let cnt = lcm / min - 1 + lcm / max - 1;
+    // let cnt = if max == lcm {
+    //     lcm / min - 1
+    // } else {
+    //     lcm / min - 1 + lcm / max - 1
+    // };
+    dbg!(lcm, cnt);
+    dbg!(kk / cnt);
+    dbg!(k - kk / cnt * cnt);
+    let mut arr = [min, max];
+    for _ in 0..(kk % cnt) {
+        if arr[0] < arr[1] {
+            arr[0] += min;
+        } else {
+            arr[1] += max;
+        }
+    }
+    let rs = kk / cnt * lcm + arr.iter().copied().min().unwrap();
+    println!("{rs}");
 }
