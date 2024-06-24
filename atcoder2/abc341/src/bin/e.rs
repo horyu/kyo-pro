f000fe2ef1fc7aa0115afb686f69e6922de2556c
horyu <28817604+horyu@users.noreply.github.com>
2024-06-24 21:47:01 +0900

    abc341 e

--- atcoder2/abc341/src/bin/e.rs
+++ atcoder2/abc341/src/bin/e.rs
@@ -12,7 +12,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        s: Chars,
+        ttllrr: [(usize, Usize1, Usize1); q],
     };
-    // println!("{rs}");
+    let ttff = s.into_iter().map(|c| c == '0').collect_vec();
+    let mut bts = BTreeSet::new();
+    for (i, (a, b)) in ttff.iter().copied().tuple_windows().enumerate() {
+        if a == b {
+            bts.insert(i);
+        }
+    }
+    for (t, l, r) in ttllrr {
+        if t == 1 {
+            if 0 < l && !bts.remove(&(l - 1)) {
+                bts.insert(l - 1);
+            }
+            if !bts.remove(&r) {
+                bts.insert(r);
+            }
+            // eprintln!("{:?}", bts);
+        } else {
+            let tf = bts.range(l..r).next().is_none();
+            let rs = ["No", "Yes"][tf as usize];
+            println!("{rs}");
+        }
+    }
 }
