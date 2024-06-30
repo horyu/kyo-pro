6b3d84e31b241ca23502a51794022847ab2c947a
horyu <28817604+horyu@users.noreply.github.com>
2024-06-30 19:38:25 +0900

    abc342 d

--- atcoder2/abc342/src/bin/d.rs
+++ atcoder2/abc342/src/bin/d.rs
@@ -14,5 +14,53 @@ fn main() {
         n: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let pf = PrimeFact::new(2e5 as usize);
+    let mut zeros = 0;
+    let mut counter = counter::Counter::<_>::new();
+    let mut rs = 0;
+    for (i, a) in aa.iter().copied().enumerate() {
+        if a == 0 {
+            rs += i;
+            zeros += 1;
+            continue;
+        }
+        let mut bts = BTreeSet::new();
+        for (k, v) in pf.get(a) {
+            if v % 2 == 1 {
+                bts.insert(k);
+            }
+        }
+        rs += counter[&bts] + zeros;
+        counter[&bts] += 1;
+    }
+    println!("{rs}");
+}
+
+// https://algo-logic.info/prime-fact/
+struct PrimeFact {
+    spf: Vec<usize>,
+}
+impl PrimeFact {
+    fn new(n: usize) -> Self {
+        let mut spf = (0..=n).collect_vec();
+        for i in (2..=n).take_while(|x| x * x <= n) {
+            if spf[i] == i {
+                for j in (i..=n).step_by(i) {
+                    if spf[j] == j {
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+        Self { spf }
+    }
+
+    fn get(&self, mut n: usize) -> HashMap<usize, usize> {
+        let mut hm = HashMap::new();
+        while 1 < n {
+            *hm.entry(self.spf[n]).or_insert(0) += 1usize;
+            n /= self.spf[n];
+        }
+        hm
+    }
 }
