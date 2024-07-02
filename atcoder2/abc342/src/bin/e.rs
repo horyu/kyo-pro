9bf06fd562065b64c9760c2e431d0c058d5696e0
horyu <28817604+horyu@users.noreply.github.com>
2024-07-02 22:17:25 +0900

    abc342 e

--- atcoder2/abc342/src/bin/e.rs
+++ atcoder2/abc342/src/bin/e.rs
@@ -12,7 +12,39 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        llddkkccaabb: [(usize, usize, usize, usize, Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    let mut ff = vec![vec![]; n];
+    for (l, d, k, c, a, b) in llddkkccaabb {
+        ff[b].push((a, l, d, k, c));
+    }
+    let mut poped = vec![0; n];
+
+    let mut bh = BinaryHeap::new();
+    bh.push((1 << 60, n - 1));
+    while let Some((qt, qi)) = bh.pop() {
+        if poped[qi] != 0 {
+            continue;
+        }
+        poped[qi] = qt;
+        for (ni, l, d, k, c) in ff[qi].iter().copied() {
+            if poped[ni] != 0 || qt < l + c {
+                continue;
+            }
+            // l + k_max * d + c <= qt
+            // k_max <= (qt - l - c) / d
+            let kk = (k - 1).min((qt - l - c) / d);
+            let nt = l + kk * d;
+            // eprintln!("{start} {qi}->{ni} {qt}:{nt}");
+            bh.push((nt, ni));
+        }
+    }
+    for p in poped[..(n - 1)].iter().copied() {
+        if p == 0 {
+            println!("Unreachable");
+            continue;
+        }
+        println!("{p}");
+    }
 }
