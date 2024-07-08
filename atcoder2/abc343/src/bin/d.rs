27c804ce4fb9607913533d9ad92692e1be064c47
horyu <28817604+horyu@users.noreply.github.com>
2024-07-09 07:55:59 +0900

    abc343 d

--- atcoder2/abc343/src/bin/d.rs
+++ atcoder2/abc343/src/bin/d.rs
@@ -12,7 +12,23 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        t: usize,
+        aabb: [(Usize1, usize); t],
     };
-    // println!("{rs}");
+    let mut cc = vec![0; n];
+    let mut counter = counter::Counter::<_>::new();
+    counter[&0] = n;
+    for (a, b) in aabb {
+        let pre_c = cc[a];
+        counter[&pre_c] -= 1;
+        if counter[&pre_c] == 0 {
+            counter.remove(&pre_c);
+        }
+
+        cc[a] += b;
+        counter[&cc[a]] += 1;
+
+        let rs = counter.len();
+        println!("{rs}");
+    }
 }
