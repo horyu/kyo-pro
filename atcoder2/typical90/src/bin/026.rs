f0c9b968be6b7ce6479c734020be3ee417d853fa
horyu <28817604+horyu@users.noreply.github.com>
2024-07-08 09:57:42 +0900

    typical90 026

--- atcoder2/typical90/src/bin/026.rs
+++ atcoder2/typical90/src/bin/026.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aabb: [(Usize1, Usize1); n -1],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    for (u, v) in aabb.iter().copied() {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let mut xxx = vec![vec![]; 2];
+    let mut qq = VecDeque::new();
+    qq.push_back((0, 0, !0));
+    while let Some((qi, qd, qf)) = qq.pop_front() {
+        xxx[qd % 2].push(qi);
+        if xxx[qd % 2].len() == n / 2 {
+            let rs = std::mem::take(&mut xxx[qd % 2])
+                .into_iter()
+                .map(|x| x + 1)
+                .sorted_unstable()
+                .join(" ");
+            println!("{rs}");
+            return;
+        }
+        for i in g[qi].iter().copied() {
+            if i == qf {
+                continue;
+            }
+            qq.push_back((i, qd + 1, qi));
+        }
+    }
 }
