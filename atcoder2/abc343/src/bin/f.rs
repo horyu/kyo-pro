78065d35489f7e9b1f16c05cd62154fc50475f2b
horyu <28817604+horyu@users.noreply.github.com>
2024-07-11 23:02:22 +0900

    abc343 f

--- atcoder2/abc343/src/bin/f.rs
+++ atcoder2/abc343/src/bin/f.rs
@@ -12,7 +12,51 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        mut aa: [usize; n],
+        ttxxyy: [(usize, usize, usize); q],
     };
-    // println!("{rs}");
+    let sqrt_n = n.sqrt();
+    let mut bbttmm = vec![BTreeMap::<usize, usize>::default(); sqrt_n + 2];
+    for (i, a) in aa.iter().copied().enumerate() {
+        *bbttmm[i / sqrt_n].entry(a).or_default() += 1usize;
+    }
+
+    for (t, x, y) in ttxxyy {
+        if t == 1 {
+            let p = x - 1;
+            // 減らす
+            let old_a = aa[p];
+            let e = bbttmm[p / sqrt_n].get_mut(&old_a).unwrap();
+            *e -= 1;
+            if *e == 0 {
+                bbttmm[p / sqrt_n].remove(&old_a);
+            }
+            // 増やす
+            let new_a = y;
+            aa[p] = new_a;
+            *bbttmm[p / sqrt_n].entry(new_a).or_default() += 1;
+            continue;
+        }
+
+        let l = x - 1;
+        let r = y - 1;
+        // aa[l..=r] における2番目に大きい値の個数を求める
+        let mut btm = BTreeMap::<usize, usize>::new();
+        let mut i = l;
+        while i <= r {
+            if i % sqrt_n == 0 && i + sqrt_n - 1 <= r {
+                // 区間の左端が sqrt_n で割り切れる場合
+                for (&k, &cnt) in bbttmm[i / sqrt_n].iter().rev().take(2) {
+                    *btm.entry(k).or_default() += cnt;
+                }
+                i += sqrt_n;
+            } else {
+                *btm.entry(aa[i]).or_default() += 1;
+                i += 1;
+            }
+        }
+        let rs = btm.into_iter().rev().nth(1).unwrap_or_default().1;
+        println!("{rs}");
+    }
 }
