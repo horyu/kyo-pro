38407fcfafd595a1f2f56fd9b3f0af242d9d68bd
horyu <28817604+horyu@users.noreply.github.com>
2024-07-16 09:34:47 +0900

    arc152 a

---
+++ atcoder2/arc152/src/bin/a.rs
@@ -0,0 +1,38 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        l: usize,
+        aa: [usize; n],
+    };
+    let mut bh = BinaryHeap::new();
+    bh.push(l);
+    for a in aa {
+        let x = bh.pop().unwrap();
+        if a == 2 && x < 2 {
+            println!("No");
+            return;
+        }
+        match x.cmp(&(1 + a)) {
+            Ordering::Less => (),
+            Ordering::Equal => {
+                bh.push(1);
+            },
+            Ordering::Greater => {
+                bh.push(1);
+                bh.push(x - (1 + a));
+            },
+        }
+    }
+    println!("Yes");
+}
