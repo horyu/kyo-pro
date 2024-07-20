1e96420bc6b8fc5dcde1cd6cff9d20cff7c76509
horyu <28817604+horyu@users.noreply.github.com>
2024-07-20 20:33:40 +0900

    arc154 b

--- atcoder2/arc154/src/bin/b.rs
+++ atcoder2/arc154/src/bin/b.rs
@@ -12,7 +12,35 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Bytes,
+        t: Bytes,
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/arc154/editorial/5533
+    if s.iter().counts() != t.iter().counts() {
+        println!("-1");
+        return;
+    }
+    // 二分探索
+    let mut l = 0;
+    let mut r = n - 1;
+    while 1 < r - l {
+        let m = (r + l) / 2;
+        // s[m..] が tの部分文字列になっているか
+        let mut j = 0;
+        let tf = s[m..].iter().copied().all(|sc| {
+            if let Some(jj) = t[j..].iter().copied().position(|tc| tc == sc) {
+                j += jj + 1;
+                true
+            } else {
+                false
+            }
+        });
+        if tf {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    let rs = r;
+    println!("{rs}");
 }
