fd2c4b984c596cea3c051242f24ba5ab06275ec2
horyu <28817604+horyu@users.noreply.github.com>
2024-08-01 08:39:10 +0900

    abc346 ab

---
+++ atcoder2/abc346/src/bin/b.rs
@@ -0,0 +1,37 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        w: usize,
+        b: usize,
+    };
+    const BASE: &str = "wbwbwwbwbwbw";
+    let s = BASE.repeat(100).chars().collect::<Vec<char>>();
+    for i in 0..BASE.len() {
+        let mut ww = 0;
+        let mut bb = 0;
+        let mut j = i;
+        while ww <= w && bb <= b {
+            if s[j] == 'w' {
+                ww += 1;
+            } else {
+                bb += 1;
+            }
+            if ww == w && bb == b {
+                println!("Yes");
+                return;
+            }
+            j += 1;
+        }
+    }
+    println!("No");
+}
