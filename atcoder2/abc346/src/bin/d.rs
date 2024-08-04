658be486687c902dacec732fd497d9cafb320250
horyu <28817604+horyu@users.noreply.github.com>
2024-08-04 11:36:39 +0900

    abc346 d

--- atcoder2/abc346/src/bin/d.rs
+++ atcoder2/abc346/src/bin/d.rs
@@ -12,7 +12,50 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        s: Chars,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    // 左から (0101.., 1010..) のコストを累積和で求める
+    let mut ll = vec![[0; 2]; n + 1];
+    for (i, (a, c)) in izip!(aa.iter().copied(), s.iter().copied()).enumerate() {
+        ll[i + 1] = ll[i];
+        if (i % 2 == 0) ^ (c == '0') {
+            ll[i + 1][0] += a;
+        } else {
+            ll[i + 1][1] += a;
+        }
+    }
+    // 右から (0101.., 1010..) のコストを累積和で求める
+    let mut rr = vec![[0; 2]; n + 1];
+    for (i, (a, c)) in izip!(aa.iter().copied(), s.iter().copied())
+        .enumerate()
+        .rev()
+    {
+        rr[i] = rr[i + 1];
+        if ((n - 1 - i) % 2 == 0) ^ (c == '0') {
+            rr[i][0] += a;
+        } else {
+            rr[i][1] += a;
+        }
+    }
+    // for (i, l) in ll.iter().enumerate() {
+    //     eprintln!("l[{i}] = {:?}", l);
+    // }
+    // for (i, r) in rr.iter().enumerate() {
+    //     eprintln!("r[{i}] = {:?}", r);
+    // }
+    let mut rs = usize::MAX;
+    for i in 0..(n - 1) {
+        // i, i+1 文字目が 0のとき
+        // eprintln!("[{i}] ll[{i}] rr[{}]", 2 + i);
+        for zo in [0, 1] {
+            // 例えば n=6 i=2 zo=0 のとき, 010010
+            let mut cost = aa[i] * usize::from((s[i] as u8 - b'0') != zo as u8)
+                + aa[i + 1] * usize::from((s[i + 1] as u8 - b'0') != zo as u8);
+            cost += ll[i][(i + zo) % 2];
+            cost += rr[2 + i][(n - i + zo) % 2];
+            rs = rs.min(cost);
+        }
+    }
+    println!("{rs}");
 }
