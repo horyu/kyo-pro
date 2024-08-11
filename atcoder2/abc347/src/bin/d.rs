292b5b964036324177d6e25febeda57bbac0239d
horyu <28817604+horyu@users.noreply.github.com>
2024-08-11 10:01:51 +0900

    abc347 d

--- atcoder2/abc347/src/bin/d.rs
+++ atcoder2/abc347/src/bin/d.rs
@@ -11,8 +11,50 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        a: u32,
+        b: u32,
+        c: usize,
     };
-    // println!("{rs}");
+    eprintln!("{c:b}");
+    let mut aa = a;
+    let mut bb = b;
+    let mut x = 0usize;
+    let mut y = 0usize;
+    for i in 0..60 {
+        if 0 == c & 1 << i {
+            continue;
+        }
+        // aa か bb の大きい方から引く
+        if aa < bb {
+            bb -= 1;
+            y |= 1 << i;
+        } else if 0 < aa {
+            aa -= 1;
+            x |= 1 << i;
+        }
+    }
+    let mut amari = aa + bb;
+    dbg!(amari);
+    if amari % 2 == 1 {
+        println!("-1");
+        return;
+    }
+    for i in 0..60 {
+        if amari == 0 {
+            break;
+        }
+        if 0 == (c & 1 << i) {
+            amari -= 2;
+            x |= 1 << i;
+            y |= 1 << i;
+        }
+    }
+    eprintln!("{x:064b} {}", x.count_ones());
+    eprintln!("{y:064b} {}", y.count_ones());
+    eprintln!("{c:064b}");
+    if x.count_ones() == a && y.count_ones() == b && x ^ y == c {
+        println!("{x} {y}");
+    } else {
+        println!("-1");
+    }
 }
