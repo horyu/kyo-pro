e94d85167a45d3edf57845bb4ea975f637f1fdec
horyu <28817604+horyu@users.noreply.github.com>
2024-08-12 09:47:13 +0900

    arc135 c

--- atcoder2/arc135/src/bin/c.rs
+++ atcoder2/arc135/src/bin/c.rs
@@ -15,18 +15,18 @@ fn main() {
         mut aa: [isize; n],
     };
     // https://atcoder.jp/contests/arc135/editorial/3356
-    const SIZE: usize = 30;
 
     aa.push(0);
     let mut ss = vec![0; n + 1];
 
-    for k in 0..SIZE {
+    for k in 0..30 {
         let ones = aa.iter().copied().map(|a| (a >> k) & 1).collect_vec();
         let c1 = ones.iter().sum::<isize>();
         let c0 = n as isize - c1;
 
+        let add = [c1 << k, (c1 - (c1 - c0)) << k];
         for i in 0..=n {
-            ss[i] += (c1 - (c1 - c0) * ones[i]) << k;
+            ss[i] += add[ones[i] as usize];
         }
     }
     let rs = ss.iter().max().copied().unwrap();
