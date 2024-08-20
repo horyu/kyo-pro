bf0b94fa3ddd1f9609c1a54c77aa5c2b75b9fb2a
horyu <28817604+horyu@users.noreply.github.com>
2024-08-20 09:09:20 +0900

    abc350 c

--- atcoder2/abc350/src/bin/c.rs
+++ atcoder2/abc350/src/bin/c.rs
@@ -12,7 +12,28 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        mut i2a: [Usize1; n],
     };
-    // println!("{rs}");
+    let mut a2i = vec![0; n];
+    for (i, a) in i2a.iter().copied().enumerate() {
+        a2i[a] = i;
+    }
+    let mut rrss = vec![];
+    for a in 0..n {
+        if a2i[a] == a {
+            continue;
+        }
+        let i = a2i[a];
+        rrss.push((a + 1, i + 1));
+        let b = i2a[a];
+        i2a.swap(a, i);
+        a2i.swap(a, b);
+        // eprintln!("a:{a} i:{i} b:{b} i2a:{i2a:?} a2i:{a2i:?}");
+    }
+    // eprintln!("{i2a:?}");
+    // eprintln!("{a2i:?}");
+    println!("{}", rrss.len());
+    for (i, j) in rrss {
+        println!("{i} {j}");
+    }
 }
