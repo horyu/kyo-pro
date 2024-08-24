6454d0f0d412f6528901ab7b1bdc416d43e662e3
horyu <28817604+horyu@users.noreply.github.com>
2024-08-24 21:11:36 +0900

    abc350 e

--- atcoder2/abc350/src/bin/e.rs
+++ atcoder2/abc350/src/bin/e.rs
@@ -12,7 +12,23 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        a: usize,
+        x: f64,
+        y: f64,
     };
-    // println!("{rs}");
+    let mut btm = BTreeMap::new();
+    btm.insert(0, 0.0);
+    let rs = f(&mut btm, a, x, y, n);
+    println!("{rs}");
+}
+
+fn f(memo: &mut BTreeMap<usize, f64>, a: usize, x: f64, y: f64, v: usize) -> f64 {
+    if let Some(&rs) = memo.get(&v) {
+        return rs;
+    }
+    let p = x + f(memo, a, x, y, v / a);
+    let q = 6.0 / 5.0 * y + 1.0 / 5.0 * (2..=6).fold(0.0, |acc, i| acc + f(memo, a, x, y, v / i));
+    let rs = p.min(q);
+    memo.insert(v, rs);
+    rs
 }
