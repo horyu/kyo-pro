c8baa3bc9b4dfe1a48a0d7f350a0694c9983eb39
horyu <28817604+horyu@users.noreply.github.com>
2024-08-31 20:30:48 +0900

    abc352 e

--- atcoder2/abc352/src/bin/e.rs
+++ atcoder2/abc352/src/bin/e.rs
@@ -12,7 +12,55 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
     };
-    // println!("{rs}");
+    let mut cc = vec![];
+    let mut aaa = vec![];
+    for _ in 0..m {
+        input! {
+            k: usize,
+            c: usize,
+            aa: [Usize1; k],
+        };
+        cc.push(c);
+        aaa.push(aa);
+    }
+    let mut dsu = ac_library::Dsu::new(n);
+    let mut rs = 0;
+    for i in (0..m).sorted_unstable_by_key(|&i| cc[i]) {
+        let c = cc[i];
+        let mut news = vec![];
+        let mut olds = HashSet::new();
+        for a in aaa[i].iter().copied() {
+            if dsu.size(a) == 1 {
+                news.push(a);
+            } else {
+                olds.insert(dsu.leader(a));
+            }
+        }
+
+        // 新規頂点軍を結合
+        for (nl, nr) in news.iter().copied().tuple_windows() {
+            dsu.merge(nl, nr);
+            rs += c;
+        }
+        // 既存頂点を結合
+        for (ol, or) in olds.iter().copied().tuple_windows() {
+            dsu.merge(ol, or);
+            rs += c;
+        }
+        // 新規と既存を結合
+        if let Some(n) = news.first().copied() {
+            if let Some(o) = olds.iter().copied().next() {
+                dsu.merge(n, o);
+                rs += c;
+            }
+        }
+        // eprintln!("[{i}] {rs}: {c} {news:?} {olds:?} {:?}", aaa[i]);
+    }
+    if dsu.size(0) != n {
+        println!("-1");
+        return;
+    }
+    println!("{rs}");
 }
