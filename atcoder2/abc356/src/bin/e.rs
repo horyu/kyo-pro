4ab46aae19b54bfc47c4e149128885b072893d7c
horyu <28817604+horyu@users.noreply.github.com>
2024-09-04 20:42:53 +0900

    abc356 e

--- atcoder2/abc356/src/bin/e.rs
+++ atcoder2/abc356/src/bin/e.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        mut aa: [usize; n],
     };
-    // println!("{rs}");
+    aa.sort_unstable();
+    let mut ft = ac_library::FenwickTree::new(1e6 as usize + 1, 0isize);
+    let mut bts = BTreeSet::new();
+    for a in aa {
+        bts.insert(a);
+        ft.add(a, 1);
+    }
+
+    let mut rs = 0;
+    while let Some(a) = bts.pop_first() {
+        let a_cnt = ft.sum(a..=a);
+        ft.add(a, -a_cnt);
+        // eprintln!("{a} {a_cnt}");
+
+        rs += a_cnt.saturating_sub(1) * a_cnt / 2;
+        for k in 1..=(1e6 as usize / a) {
+            let l = a * k;
+            let r = (1e6 as usize + 1).min(a * (k + 1));
+            let cnt = ft.sum(l..r);
+            rs += (k as isize) * a_cnt * cnt;
+            // if 0 < cnt {
+            //     eprintln!("{k}({l}..{r}) {cnt}: {}", (k as isize) * a_cnt * cnt);
+            // }
+        }
+    }
+    println!("{rs}");
 }
