22ed785f8e701e96df9077b8d028f21bf076aae1
horyu <28817604+horyu@users.noreply.github.com>
2024-09-11 22:04:17 +0900

    abc357 e

--- atcoder2/abc357/src/bin/e.rs
+++ atcoder2/abc357/src/bin/e.rs
@@ -12,7 +12,39 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        i2i: [Usize1; n],
     };
-    // println!("{rs}");
+    // 末端がループで、それ以外は最終的にループに繋がる有向グラフ
+    let mut dsu = ac_library::Dsu::new(n);
+    for (i, j) in i2i.iter().copied().enumerate() {
+        dsu.merge(i, j);
+    }
+    let mut rs = 0;
+    let mut cc = vec![0; n];
+    for gg in dsu.groups() {
+        // eprintln!("!{gg:?}");
+        let iii =
+            pathfinding::directed::strongly_connected_components::strongly_connected_components(
+                &gg,
+                |&i| std::iter::once(i2i[i]),
+            );
+        for ii in iii {
+            // eprintln!("{ii:?}");
+            let i = ii[0];
+            let j = i2i[i];
+            if cc[j] == 0 {
+                // 末端
+                let len = ii.len();
+                rs += len.pow(2);
+                for i in ii {
+                    cc[i] = len;
+                }
+                continue;
+            }
+            cc[i] += 1 + cc[j];
+            rs += cc[i];
+        }
+    }
+    // eprintln!("{cc:?}");
+    println!("{rs}");
 }
