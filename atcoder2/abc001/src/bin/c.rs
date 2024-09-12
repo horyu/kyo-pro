a08d2e50df61dea5381ef4cbd46dda4118a4a39a
horyu <28817604+horyu@users.noreply.github.com>
2024-09-12 22:44:52 +0900

    abc001 d

--- atcoder2/abc001/src/bin/c.rs
+++ atcoder2/abc001/src/bin/c.rs
@@ -1,7 +1,6 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
-#![feature(exclusive_range_pattern)]
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
--- atcoder2/abc001/src/bin/d.rs
+++ atcoder2/abc001/src/bin/d.rs
@@ -12,7 +12,35 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        ss: [String; n],
     };
-    // println!("{rs}");
+    // 1日を00:00から24:00まで5分刻みで区切る
+    let mut ttff = vec![false; 60 / 5 * 24 + 1];
+    for s in ss {
+        //     012345678
+        // s: "hhmm-hhmm"
+        let lh = s[0..2].parse::<usize>().unwrap();
+        let lm = s[2..4].parse::<usize>().unwrap() / 5;
+
+        let mut rh = s[5..7].parse::<usize>().unwrap();
+        let mut rm = s[7..9].parse::<usize>().unwrap().div_ceil(5);
+        if rm == 60 {
+            rh += 1;
+            rm = 0;
+        }
+        for i in (lh * 12 + lm)..(rh * 12 + rm) {
+            ttff[i] = true;
+        }
+    }
+    // eprintln!("{}", ttff.iter().map(|tf| if *tf { 'T' } else { 'F' }).collect::<String>());
+    let mut pre = false;
+    ttff.push(false);
+    for (i, tf) in ttff.into_iter().enumerate() {
+        if !pre && tf {
+            print!("{:02}{:02}-", i / 12, i % 12 * 5);
+        } else if pre && !tf {
+            println!("{:02}{:02}", i / 12, i % 12 * 5);
+        }
+        pre = tf;
+    }
 }
