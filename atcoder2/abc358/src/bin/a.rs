088eba4f21843adb9aacd3510c387ab909362ec2
horyu <28817604+horyu@users.noreply.github.com>
2024-09-13 19:06:14 +0900

    abc358 ab

---
+++ atcoder2/abc358/src/bin/a.rs
@@ -0,0 +1,19 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::io::Read;
+
+fn main() {
+    let mut s = String::with_capacity(32);
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let tf = s == "AtCoder Land\n";
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{rs}");
+}
---
+++ atcoder2/abc358/src/bin/b.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        a: usize,
+        tt: [usize; n],
+    };
+    let mut now = 0;
+    for t in tt {
+        now = now.max(t) + a;
+        println!("{now}");
+    }
+}
