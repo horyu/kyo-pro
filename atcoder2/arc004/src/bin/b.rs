4d010a361250c4dfa4c405e1c4f5281c2f0b4ec3
horyu <28817604+horyu@users.noreply.github.com>
2024-09-14 18:29:10 +0900

    arc004 b

---
+++ atcoder2/arc004/src/bin/b.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        dd: [usize; n],
+    };
+    let sum = dd.iter().sum::<usize>();
+    println!("{sum}");
+
+    let max = dd.iter().max().copied().unwrap();
+    let diff = max.saturating_sub(sum - max);
+    println!("{diff}");
+}
