6e1256400cf6257d9a0c7a0f84c29b43c51decec
horyu <28817604+horyu@users.noreply.github.com>
2024-09-22 21:07:55 +0900

    abc359 e

--- atcoder2/abc359/src/bin/e.rs
+++ atcoder2/abc359/src/bin/e.rs
@@ -12,7 +12,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        hh: [usize; n],
     };
-    // println!("{rs}");
+    // 現在の(敷居の高さ, 敷居が有効な幅)の配列
+    let mut xxyy = vec![(usize::MAX, 0)];
+    let mut vv = vec![];
+    let mut turn = 1;
+    for h in hh {
+        xxyy.push((h, 1));
+        turn += h;
+        for pos in (0..(xxyy.len() - 1)).rev() {
+            let (x, y) = xxyy[pos];
+            match x.cmp(&h) {
+                Ordering::Less | Ordering::Equal => {
+                    turn += (h - x) * y;
+                    xxyy.swap_remove(pos);
+                    xxyy[pos].1 += y;
+                }
+                Ordering::Greater => {
+                    break;
+                }
+            }
+        }
+        vv.push(turn);
+    }
+    let rs = vv.iter().join(" ");
+    println!("{rs}");
 }
