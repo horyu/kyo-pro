17ce50f9d7a6001de71512cffa0d0debc8f232c5
horyu <28817604+horyu@users.noreply.github.com>
2024-09-29 20:24:18 +0900

    abc240 e

--- atcoder2/abc240/src/bin/e.rs
+++ atcoder2/abc240/src/bin/e.rs
@@ -12,7 +12,60 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        uuvv: [(Usize1, Usize1); n - 1],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc240/editorial/3426
+    let mut g = vec![vec![]; n];
+    for (u, v) in uuvv.iter().copied() {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    // vv[葉] = 何番目の葉か
+    let mut vv = vec![!0; n];
+    dfs1(&g, &mut vv, 0, !0, &mut 0);
+
+    let mut ll = vec![!0; n];
+    let mut rr = vec![0; n];
+    dfs2(&g, &vv, &mut ll, &mut rr, 0, !0);
+
+    for (l, r) in izip!(ll, rr) {
+        println!("{} {}", l + 1, r + 1);
+    }
+}
+
+fn dfs1(g: &[Vec<usize>], leaves: &mut Vec<usize>, i: usize, p: usize, num: &mut usize) {
+    if i != 0 && g[i].len() == 1 {
+        leaves[i] = *num;
+        *num += 1;
+        return;
+    }
+    for &j in &g[i] {
+        if j == p {
+            continue;
+        }
+        dfs1(g, leaves, j, i, num);
+    }
+}
+
+fn dfs2(
+    g: &[Vec<usize>],
+    vv: &[usize],
+    ll: &mut Vec<usize>,
+    rr: &mut Vec<usize>,
+    i: usize,
+    p: usize,
+) {
+    if vv[i] != !0 {
+        ll[i] = vv[i];
+        rr[i] = vv[i];
+        return;
+    }
+    for j in g[i].iter().copied() {
+        if j == p {
+            continue;
+        }
+        dfs2(g, vv, ll, rr, j, i);
+        ll[i] = ll[i].min(ll[j]);
+        rr[i] = rr[i].max(rr[j]);
+    }
 }
