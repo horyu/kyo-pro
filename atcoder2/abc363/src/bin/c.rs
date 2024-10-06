69b4b379e8d2fe7be64f2879912944461be3815b
horyu <28817604+horyu@users.noreply.github.com>
2024-10-06 19:47:32 +0900

    abc363 c

--- atcoder2/abc363/src/bin/c.rs
+++ atcoder2/abc363/src/bin/c.rs
@@ -4,6 +4,7 @@
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
+use permutohedron::LexicalPermutation;
 use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
@@ -12,7 +13,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
+        mut s: Chars,
     };
-    // println!("{rs}");
+    s.sort_unstable();
+    let mut rs = 0;
+    loop {
+        if (0..=(n - k)).all(|mut l| {
+            let mut r = l + k - 1;
+            while l < r {
+                if s[l] != s[r] {
+                    return true;
+                }
+                l += 1;
+                r -= 1;
+            }
+            false
+        }) {
+            rs += 1;
+        }
+        if !s.next_permutation() {
+            break;
+        }
+    }
+
+    println!("{rs}");
 }
