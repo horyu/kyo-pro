23121dceb8e3769b3554395b13d0acff11c1c1d8
horyu <28817604+horyu@users.noreply.github.com>
2024-10-12 20:35:48 +0900

    abc364 d

--- atcoder2/abc364/src/bin/d.rs
+++ atcoder2/abc364/src/bin/d.rs
@@ -12,7 +12,40 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        mut aa: [isize; n],
+        bbkk: [(isize, usize); q],
     };
-    // println!("{rs}");
+    aa.sort_unstable();
+    // eprintln!("{aa:?}");
+    for (b, mut k) in bbkk {
+        // let orik = k;
+        let mut l = aa.partition_point(|&a| a < b);
+        let mut r = aa.partition_point(|&a| a <= b);
+        k = k.saturating_sub(r - l);
+        while 0 < k {
+            if l == 0 {
+                r += k;
+                k = 0;
+            } else if r == n {
+                l -= k;
+                k = 0;
+            } else {
+                let m = k.div_ceil(2);
+                let ll = l.saturating_sub(m);
+                let rr = (r + m).min(n);
+                if b - aa[ll] <= aa[rr - 1] - b {
+                    k -= l - ll;
+                    l = ll;
+                } else {
+                    k -= rr - r;
+                    r = rr;
+                }
+            }
+        }
+        // dbg!(l..r);
+        // assert!(orik <=  r - l);
+        let rs = (b - aa[l]).max(aa[r - 1] - b);
+        println!("{rs}");
+    }
 }
