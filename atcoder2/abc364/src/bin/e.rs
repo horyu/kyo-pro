564d2ee6008aa93ce21fccea0b8efbfb32a63fd4
horyu <28817604+horyu@users.noreply.github.com>
2024-10-14 10:49:05 +0900

    abc364 e

--- atcoder2/abc364/src/bin/e.rs
+++ atcoder2/abc364/src/bin/e.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        x: usize,
+        y: usize,
+        aabb: [(usize, usize); n],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc364/editorial/10550
+    // dp[i][j][k] = 料理1..=iからj個選んで甘さがkになる最小のしょっぱさ
+    let mut dp = vec![vec![vec![1usize << 60; x + 1]; n + 1]; n + 1];
+    dp[0][0][0] = 0;
+    for i in 0..n {
+        let (a, b) = aabb[i];
+        for j in 0..=i {
+            for k in 0..=x {
+                dp[i + 1][j][k] = dp[i + 1][j][k].min(dp[i][j][k]);
+                if k + a <= x {
+                    dp[i + 1][j + 1][k + a] = dp[i + 1][j + 1][k + a].min(dp[i][j][k] + b);
+                }
+            }
+        }
+    }
+    for p in (0..=n).rev() {
+        for q in 0..=x {
+            if dp[n][p][q] <= y {
+                let rs = (p + 1).min(n);
+                println!("{rs}");
+                return;
+            }
+        }
+    }
 }
