596ec0b2246da6a3c2215adb69ded6aed33e8126
horyu <28817604+horyu@users.noreply.github.com>
2024-10-16 08:41:06 +0900

    arc027 b

--- atcoder2/arc027/src/bin/b.rs
+++ atcoder2/arc027/src/bin/b.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
-use itertools::{chain, iproduct, izip, Itertools};
+use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
 use petgraph::unionfind::UnionFind;
@@ -12,39 +12,25 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        s1: Chars,
-        s2: Chars,
+        mut s1: Chars,
+        mut s2: Chars,
     };
-    let c2i = chain!(&s1, &s2)
+    let i2c = chain!(&s1, &s2)
         .copied()
-        .sorted_unstable()
-        .dedup()
+        .unique()
         .enumerate()
-        .map(|(i, c)| (c, i))
         .collect::<HashMap<_, _>>();
-    let i2c = c2i.iter().map(|(c, i)| (*i, *c)).collect::<HashMap<_, _>>();
+    let c2i = i2c.iter().map(|(&i, &c)| (c, i)).collect::<HashMap<_, _>>();
     let mut dsu = ac_library::Dsu::new(c2i.len());
-    for (ci, c2) in izip!(&s1, &s2) {
-        let i1 = c2i[ci];
-        let i2 = c2i[c2];
-        dsu.merge(i1, i2);
-    }
-    if !('0'..='9')
-        .filter_map(|c| c2i.get(&c).copied())
-        .map(|i| dsu.leader(i))
-        .all_unique()
-    {
-        println!("0");
-        return;
+    for (c1, c2) in izip!(&s1, &s2) {
+        dsu.merge(c2i[c1], c2i[c2]);
     }
     let mut rs = 1usize;
-    for gg in dsu.groups() {
-        let cc = gg.iter().map(|&i| i2c[&i]).collect_vec();
-        // eprintln!("{cc:?}");
-        if cc.iter().copied().any(|c| c.is_ascii_digit()) {
+    for vv in dsu.groups() {
+        if vv.iter().any(|v| i2c[v].is_ascii_digit()) {
             continue;
         }
-        if cc.contains(&s1[0]) || cc.contains(&s2[0]) {
+        if dsu.same(vv[0], c2i[&s1[0]]) || dsu.same(vv[0], c2i[&s2[0]]) {
             rs *= 9;
         } else {
             rs *= 10;
