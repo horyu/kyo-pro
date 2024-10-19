69c9815fbfce4960cfd0581046229cc0380b0b86
horyu <28817604+horyu@users.noreply.github.com>
2024-10-20 08:49:18 +0900

    abc365 d

--- atcoder2/abc365/src/bin/d.rs
+++ atcoder2/abc365/src/bin/d.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Chars,
     };
-    // println!("{rs}");
+    let mut hm = HashMap::new();
+    hm.insert('@', 0);
+    for c in s {
+        let mut new_hm = HashMap::new();
+        for (k, v) in hm {
+            // あいこ
+            if k != c {
+                let e = new_hm.entry(c).or_default();
+                *e = v.max(*e);
+            }
+            // 勝ち
+            let nk = match c {
+                'R' => 'P',
+                'S' => 'R',
+                'P' => 'S',
+                _ => unreachable!(),
+            };
+            if nk != k {
+                let e = new_hm.entry(nk).or_default();
+                *e = (v + 1).max(*e);
+            }
+        }
+        hm = new_hm;
+    }
+    let rs = hm.values().max().unwrap();
+    println!("{rs}");
 }
