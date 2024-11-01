c889e2a75b53dc9b8efe7f8e3c9dabbdf256f4b8
horyu <28817604+horyu@users.noreply.github.com>
2024-11-01 20:17:04 +0900

    arc156 b

--- atcoder2/arc156/src/bin/b.rs
+++ atcoder2/arc156/src/bin/b.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,67 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        k: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/arc156/editorial/5718
+    let comb = Combination::new(n + k + 1, ModInt998244353::modulus() as usize);
+
+    let mut arr = vec![false; 4e5 as usize + 1];
+    for a in aa.iter().copied() {
+        arr[a] = true;
+    }
+    let mut need = 1;
+    let mut rs = ModInt998244353::default();
+    for i in 0..(n + k) {
+        let Some(rest) = k.checked_sub(need) else {
+            break;
+        };
+        rs += comb.get(rest + i, i);
+        if !arr[i] {
+            need += 1;
+        }
+    }
+    println!("{rs}");
+}
+
+// https://github.com/kenkoooo/competitive-programming-rs/blob/master/src/math/combination.rs
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Self {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
+        }
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
+        }
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
+        }
+        Self {
+            fact,
+            inv_fact,
+            modulo,
+        }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> usize {
+        assert!(x >= y);
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
+    }
+
+    pub fn h(&self, n: usize, r: usize) -> usize {
+        self.get(n + r - 1, r)
+    }
 }
