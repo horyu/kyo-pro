1d96645521706a1feac26af55d5e9eb5fd74ba86
horyu <28817604+horyu@users.noreply.github.com>
2024-11-02 09:10:56 +0900

    typical90 062

--- atcoder2/typical90/src/bin/062.rs
+++ atcoder2/typical90/src/bin/062.rs
@@ -14,33 +14,32 @@ fn main() {
         n: usize,
         aabb: [(Usize1, Usize1); n],
     };
-    let mut ppp = vec![HashSet::new(); n];
+    // 依存元への辺を張る
+    let mut ff = vec![vec![]; n];
     let mut qq = VecDeque::new();
     let mut pushed = vec![false; n];
     for (i, (a, b)) in aabb.iter().copied().enumerate() {
-        if i == a || i == b {
+        ff[a].push(i);
+        ff[b].push(i);
+        if a == i || b == i {
             pushed[i] = true;
             qq.push_back(i);
         }
-        for j in [a, b] {
-            if i != j {
-                ppp[j].insert(i);
-            }
-        }
     }
-    let mut rs = vec![];
+    let mut rrss = vec![];
     while let Some(qi) = qq.pop_front() {
-        rs.push(qi + 1);
-        for qj in ppp[qi].iter().copied() {
-            if pushed[qj] {
+        rrss.push(qi + 1);
+        for i in ff[qi].iter().copied() {
+            if pushed[i] {
                 continue;
             }
-            pushed[qj] = true;
-            qq.push_back(qj);
+            pushed[i] = true;
+            qq.push_back(i);
         }
     }
-    if rs.len() == n {
-        println!("{}", rs.into_iter().rev().join("\n"));
+    if rrss.len() == n {
+        let rs = rrss.into_iter().rev().join("\n");
+        println!("{rs}");
     } else {
         println!("-1");
     }
