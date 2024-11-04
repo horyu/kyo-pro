8364da3e0d70b422d3e90722acf6389950b707c2
horyu <28817604+horyu@users.noreply.github.com>
2024-11-04 09:23:26 +0900

    arc157 a

---
+++ atcoder2/arc157/src/bin/a.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+    };
+    // (a, b, c, d) = (XX, XY, YX, YY)の個数
+    let tf = if 0 < b + c {
+        b.abs_diff(c) <= 1
+    } else {
+        (a == 0) || (d == 0)
+    };
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{rs}");
+}
