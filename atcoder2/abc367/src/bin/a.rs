0ae943da4b359463207a348578f9d634275e4fa8
horyu <28817604+horyu@users.noreply.github.com>
2024-11-11 08:50:35 +0900

    abc367 ab

---
+++ atcoder2/abc367/src/bin/a.rs
@@ -0,0 +1,25 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+    let tf = if b < c {
+        !(b..c).contains(&a)
+    } else {
+        (c..b).contains(&a)
+    };
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{rs}");
+}
