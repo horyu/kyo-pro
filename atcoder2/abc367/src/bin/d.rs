fc6817c9c1ef7fffb05e5146cdfc770a31a9ffa7
horyu <28817604+horyu@users.noreply.github.com>
2024-11-16 23:48:26 +0900

    abc367 d

--- atcoder2/abc367/src/bin/d.rs
+++ atcoder2/abc367/src/bin/d.rs
@@ -12,7 +12,22 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        m: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut mm = btreemultimap::BTreeMultiMap::new();
+    let mut crr = 0;
+    for (i, a) in aa.iter().cycle().enumerate().take(n * 2) {
+        mm.insert(crr, i);
+        crr = (crr + a) % m;
+    }
+    let mut rs = 0;
+    let mut tmp = 0usize;
+    for (i, a) in aa.iter().copied().enumerate() {
+        if let Some(vv) = mm.get_vec(&tmp) {
+            rs += vv.partition_point(|&v| v < i + n) - vv.partition_point(|&v| v <= i);
+        }
+        tmp = (tmp + a) % m;
+    }
+    println!("{rs}");
 }
