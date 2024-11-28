cfc246d270c7d5d2c80cfe1fd8e4ac5d7d263c70
horyu <28817604+horyu@users.noreply.github.com>
2024-11-28 19:39:08 +0900

    abc368 f

--- atcoder2/abc368/src/bin/f.rs
+++ atcoder2/abc368/src/bin/f.rs
@@ -14,5 +14,34 @@ fn main() {
         n: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    let mut tmp = 0;
+    for a in aa {
+        tmp ^= factors(a).into_values().sum::<usize>();
+    }
+    let tf = tmp != 0;
+    let rs = if tf { "Anna" } else { "Bruno" };
+    println!("{rs}");
+}
+
+fn factors(mut n: usize) -> HashMap<usize, usize> {
+    let mut hm = HashMap::new();
+    if n <= 1 {
+        return hm;
+    }
+    while n % 2 == 0 {
+        n /= 2;
+        *hm.entry(2).or_insert(0) += 1;
+    }
+    let mut i = 3;
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+            *hm.entry(i).or_insert(0) += 1;
+        }
+        i += 2;
+    }
+    if n != 1 {
+        hm.insert(n, 1);
+    }
+    hm
 }
