56267827a12ef2a578aba55f0cce0ebf0bc7547c
horyu <28817604+horyu@users.noreply.github.com>
2024-11-29 08:15:33 +0900

    abc100 d

--- atcoder2/abc100/src/bin/d.rs
+++ atcoder2/abc100/src/bin/d.rs
@@ -13,18 +13,18 @@ fn main() {
     input! {
         n: usize,
         m: usize,
-        mut xxyyzz: [[isize; 3]; n],
+        xxyyzz: [[isize; 3]; n],
     };
-    let mut rs = 0isize;
-    for vv in (0..3).map(|i| [1, -1]).multi_cartesian_product() {
-        xxyyzz.sort_unstable_by_key(|xyz| izip!(&vv, xyz).fold(0, |acc, (v, xyz)| acc - v * xyz));
-        let mut ww = [0; 3];
-        for xyz in &xxyyzz[..m] {
-            for (i, xyz) in xyz.iter().enumerate() {
-                ww[i] += xyz;
-            }
-        }
-        rs = rs.max(ww.into_iter().map(|w| w.abs()).sum::<isize>());
+    let mut rs = 0;
+    for ss in (0..3).map(|_| [1, -1]).multi_cartesian_product() {
+        let sum = xxyyzz
+            .iter()
+            .map(|xyz| izip!(xyz, &ss).fold(0, |acc, (v, s)| acc + v * s))
+            .sorted_unstable()
+            .rev()
+            .take(m)
+            .sum::<isize>();
+        rs = rs.max(sum);
     }
     println!("{rs}");
 }
