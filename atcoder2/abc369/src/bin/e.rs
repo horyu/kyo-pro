34a12e52b06d6b59190d8b2e2072c73d95b74066
horyu <28817604+horyu@users.noreply.github.com>
2024-12-06 09:20:24 +0900

    abc369 e

--- atcoder2/abc369/src/bin/e.rs
+++ atcoder2/abc369/src/bin/e.rs
@@ -12,7 +12,49 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvvtt: [(Usize1, Usize1, usize); m],
+        q: usize,
     };
-    // println!("{rs}");
+    // ワーシャルフロイド法
+    let mut dist = vec![vec![1usize << 60; n]; n];
+    for i in 0..n {
+        dist[i][i] = 0;
+    }
+    for (u, v, t) in uuvvtt.iter().copied() {
+        let d = dist[u][v].min(t);
+        dist[u][v] = d;
+        dist[v][u] = d;
+    }
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                dist[i][j] = dist[i][j].min(dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    for _ in 0..q {
+        input! { k: usize, bb: [Usize1; k] };
+        let mut rs = !0;
+        // 橋の利用順で全探索
+        for bb in bb.iter().copied().permutations(k) {
+            // 橋の向きで全探索
+            for bits in 0..(1 << k) {
+                let mut tmp = 0;
+                let mut last = 0;
+                for i in 0..k {
+                    let (mut u, mut v, t) = uuvvtt[bb[i]];
+                    if bits & (1 << i) != 0 {
+                        (u, v) = (v, u);
+                    }
+                    tmp += dist[last][u] + t;
+                    last = v;
+                }
+                tmp += dist[last][n - 1];
+                rs = rs.min(tmp);
+            }
+        }
+        println!("{rs}");
+    }
 }
