a99dcb28b6b9402d15299fce89c1fc4188cdd868
horyu <28817604+horyu@users.noreply.github.com>
2024-12-10 08:52:01 +0900

    abc370 d

--- atcoder2/abc370/src/bin/d.rs
+++ atcoder2/abc370/src/bin/d.rs
@@ -11,8 +11,40 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        q: usize,
+        rrcc: [(Usize1, Usize1); q],
     };
-    // println!("{rs}");
+    let mut x2yy = vec![BTreeSet::from_iter(0..w); h];
+    let mut y2xx = vec![BTreeSet::from_iter(0..h); w];
+
+    for (r, c) in rrcc {
+        if x2yy[r].remove(&c) {
+            y2xx[c].remove(&r);
+            continue;
+        }
+
+        // LR削除
+        if let Some(y) = x2yy[r].range(..c).next_back().copied() {
+            y2xx[y].remove(&r);
+            x2yy[r].remove(&y);
+        }
+        if let Some(y) = x2yy[r].range(c..).next().copied() {
+            y2xx[y].remove(&r);
+            x2yy[r].remove(&y);
+        }
+        // UD削除
+        if let Some(x) = y2xx[c].range(..r).next_back().copied() {
+            x2yy[x].remove(&c);
+            y2xx[c].remove(&x);
+        }
+        if let Some(x) = y2xx[c].range(r..).next().copied() {
+            x2yy[x].remove(&c);
+            y2xx[c].remove(&x);
+        }
+    }
+
+    let rs = x2yy.iter().map(|yy| yy.len()).sum::<usize>();
+    println!("{rs}");
 }
