d104e4a838deac2d58fb84f0f98aa6acdcabeaf1
horyu <28817604+horyu@users.noreply.github.com>
2024-12-11 20:49:12 +0900

    abc370 e

--- atcoder2/abc370/src/bin/e.rs
+++ atcoder2/abc370/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,22 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: isize,
+        aa: [isize; n],
     };
-    // println!("{rs}");
+    let mut hm = HashMap::new();
+    hm.insert(0isize, ModInt998244353::new(1));
+    let mut all = ModInt998244353::new(1);
+    let mut acc = 0;
+    for (i, a) in aa.into_iter().enumerate() {
+        acc += a;
+        let ban = acc - k;
+        let cur = all - hm.get(&ban).copied().unwrap_or_default();
+        *hm.entry(acc).or_insert(ModInt998244353::default()) += cur;
+        all += cur;
+        if i + 1 == n {
+            let rs = cur.val();
+            println!("{rs}");
+        }
+    }
 }
