c4e46adfca89e19898fb3e2027409bf13a2c6ac3
horyu <28817604+horyu@users.noreply.github.com>
2024-12-14 10:41:31 +0900

    abc371 c

--- atcoder2/abc371/src/bin/c.rs
+++ atcoder2/abc371/src/bin/c.rs
@@ -12,7 +12,70 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        mg: usize,
+        uuvv: [(Usize1, Usize1); mg],
+        mh: usize,
+        aabb: [(Usize1, Usize1); mh],
     };
-    // println!("{rs}");
+    let mut ddd = vec![vec![0; n]; n];
+    for i in 0..(n - 1) {
+        input! { aa: [usize; n - 1 - i] };
+        for (j, a) in aa.into_iter().enumerate() {
+            ddd[i][i + 1 + j] = a;
+            ddd[i + 1 + j][i] = a;
+        }
+    }
+    // for dd in &ddd {
+    //     eprintln!("{dd:?}");
+    // }
+
+    let mut gg = vec![HashSet::new(); n];
+    for (u, v) in uuvv {
+        gg[u].insert(v);
+        gg[v].insert(u);
+    }
+    let mut gh = vec![HashSet::new(); n];
+    for (a, b) in aabb {
+        gh[a].insert(b);
+        gh[b].insert(a);
+    }
+
+    let mut rs = !0;
+    for pp in (0..n).permutations(n) {
+        // グラフHの頂点iをグラフGの頂点pp[i]に対応させる
+        let mut cost = 0;
+        let mut added = vec![vec![false; n]; n];
+        for (i, hsh) in gh.iter().enumerate() {
+            let p = pp[i];
+            // HにあってGにない辺のコストを加算
+            for &j in hsh {
+                let q = pp[j];
+                if !gg[p].contains(&q) {
+                    if added[p][q] || added[q][p] {
+                        continue;
+                    }
+                    added[p][q] = true;
+                    added[q][p] = true;
+                    cost += ddd[i][j];
+                }
+            }
+            // HになくGにある辺のコストを加算
+            for j in 0..n {
+                if hsh.contains(&j) {
+                    continue;
+                }
+                let q = pp[j];
+                if gg[p].contains(&q) {
+                    if added[p][q] || added[q][p] {
+                        continue;
+                    }
+                    added[p][q] = true;
+                    added[q][p] = true;
+                    cost += ddd[i][j];
+                }
+            }
+        }
+        rs = rs.min(cost);
+    }
+    println!("{rs}");
 }
