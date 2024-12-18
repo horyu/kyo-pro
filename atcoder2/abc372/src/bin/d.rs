cc63bb08a8f12849007879ab13e562a01a185c8a
horyu <28817604+horyu@users.noreply.github.com>
2024-12-19 08:56:59 +0900

    abc372 d

--- atcoder2/abc372/src/bin/d.rs
+++ atcoder2/abc372/src/bin/d.rs
@@ -12,7 +12,31 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        hh: [Usize1; n],
     };
-    // println!("{rs}");
+    let mut h2i = vec![!0; n];
+    for (i, h) in hh.iter().copied().enumerate() {
+        h2i[h] = i;
+    }
+    // 後ろから見ると単調現象（前から見ると単調増加）になる部分列の大きさ
+    let mut cc = vec![1; n];
+    let mut rrss = vec![0; n];
+    let mut bts = BTreeSet::new();
+    for i in (0..n).rev() {
+        // eprintln!("{i} {rrss:?} {bts:?}");
+        let hi = hh[i];
+        if let Some(hj) = bts.range(hi..).next().copied() {
+            let cj = cc[h2i[hj]];
+            rrss[i] += cj;
+            cc[i] += cj;
+        }
+        while let Some(h) = bts.range(..hi).next().copied() {
+            bts.remove(&h);
+            rrss[i] += 1;
+        }
+        bts.insert(hi);
+    }
+    // eprintln!("  {rrss:?} {bts:?}");
+    let rs = rrss.iter().join(" ");
+    println!("{rs}");
 }
