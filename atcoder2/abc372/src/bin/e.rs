a12c0c6210915d7f81b3ce6b497c5eebb32f7b80
horyu <28817604+horyu@users.noreply.github.com>
2024-12-20 08:16:26 +0900

    abc372 e

--- atcoder2/abc372/src/bin/e.rs
+++ atcoder2/abc372/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+#![feature(get_many_mut)]
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
+        ttaabb: [(usize, Usize1, Usize1); q],
     };
-    // println!("{rs}");
+    let mut dsu = ac_library::Dsu::new(n);
+    let mut iii = (0..n).map(|i| vec![i]).collect_vec();
+    for (t, a, b) in ttaabb {
+        if t == 1 {
+            if !dsu.same(a, b) {
+                let (la, lb) = (dsu.leader(a), dsu.leader(b));
+                let (from, to) = if la == dsu.merge(a, b) {
+                    (lb, la)
+                } else {
+                    (la, lb)
+                };
+                if let Ok([ff, tt]) = iii.get_many_mut([from, to]) {
+                    tt.append(ff);
+                    tt.sort_unstable_by_key(|&i| R(i));
+                    tt.truncate(10);
+                }
+            }
+            continue;
+        }
+        let la = dsu.leader(a);
+        if let Some(v) = iii[la].get(b) {
+            let rs = v + 1;
+            println!("{rs}");
+        } else {
+            println!("-1");
+        }
+    }
 }
