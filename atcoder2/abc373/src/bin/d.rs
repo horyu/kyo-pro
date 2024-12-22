f7ef7cc4620c5dc06d8a711720ad2190d046a944
horyu <28817604+horyu@users.noreply.github.com>
2024-12-23 06:51:26 +0900

    abc373 d

--- atcoder2/abc373/src/bin/d.rs
+++ atcoder2/abc373/src/bin/d.rs
@@ -12,7 +12,36 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvvww: [(Usize1, Usize1, isize); m],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    let mut dsu = ac_library::Dsu::new(n);
+    for &(u, v, w) in &uuvvww {
+        g[u].push((v, w));
+        g[v].push((u, -w));
+        dsu.merge(u, v);
+    }
+    let mut rrss = vec![0isize; n];
+    let mut pushed = vec![false; n];
+    for group in dsu.groups() {
+        let mut qq = VecDeque::new();
+        qq.push_back(group[0]);
+        pushed[group[0]] = true;
+        while let Some(u) = qq.pop_front() {
+            for &(v, w) in &g[u] {
+                if pushed[v] {
+                    continue;
+                }
+                pushed[v] = true;
+                qq.push_back(v);
+                rrss[v] = rrss[u] + w;
+            }
+        }
+    }
+    // for (u, v, w) in uuvvww {
+    //     assert!(rrss[v] - rrss[u] == w);
+    // }
+    let rs = rrss.iter().join(" ");
+    println!("{rs}");
 }
