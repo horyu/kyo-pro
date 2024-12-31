952a95a5ba109f5ca4d696bb86ee8c6657482515
horyu <28817604+horyu@users.noreply.github.com>
2024-12-31 09:17:20 +0900

    abc375 ab

---
+++ atcoder2/abc375/src/bin/a.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let rs = s
+        .into_iter()
+        .tuple_windows()
+        .filter(|tup| matches!(tup, ('#', '.', '#')))
+        .count();
+    println!("{rs}");
+}
---
+++ atcoder2/abc375/src/bin/b.rs
@@ -0,0 +1,24 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        mut xxyy: [(f64, f64); n],
+    };
+    xxyy.push((0.0, 0.0));
+    let mut rs = 0.0;
+    for ((px, py), (qx, qy)) in xxyy.into_iter().circular_tuple_windows() {
+        // rs += ((px - qx).powi(2) + (py - qy).powi(2)).sqrt();
+        rs += (px - qx).hypot(py - qy);
+    }
+    println!("{rs}");
+}
