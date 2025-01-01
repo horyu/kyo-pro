dda62c924284bd768ccdc58585613e2a33103c60
horyu <28817604+horyu@users.noreply.github.com>
2025-01-01 10:47:18 +0900

    abc375 c

--- atcoder2/abc375/src/bin/c.rs
+++ atcoder2/abc375/src/bin/c.rs
@@ -12,7 +12,36 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aaa: [Chars; n],
     };
-    // println!("{rs}");
+    let mut bbb = aaa.clone();
+    for d in 0..(n / 2) {
+        // 外側から距離iの部分を -45*(d + 1)度回転させる
+        // 上辺・下辺
+        for i in [d, n - 1 - d] {
+            // eprintln!("{d} {i} {:?}", d..=(n - 1 - d));
+            for j in d..=(n - 1 - d) {
+                let (mut x, mut y) = (i, j);
+                for _ in 0..=(d % 4) {
+                    (x, y) = (y, n - 1 - x);
+                }
+                bbb[x][y] = aaa[i][j];
+            }
+        }
+        // 左辺・右辺
+        for j in [d, n - 1 - d] {
+            // eprintln!("{d} {:?} {j}", d..=(n - 1 - d));
+            for i in d..=(n - 1 - d) {
+                let (mut x, mut y) = (i, j);
+                for _ in 0..=(d % 4) {
+                    (x, y) = (y, n - 1 - x);
+                }
+                bbb[x][y] = aaa[i][j];
+            }
+        }
+    }
+    for bb in bbb {
+        let rs = bb.iter().join("");
+        println!("{rs}");
+    }
 }
