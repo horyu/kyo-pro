b1f803032142f01040367933e564dd5ae5f83fe5
horyu <28817604+horyu@users.noreply.github.com>
2025-01-10 09:05:56 +0900

    abc377 ab

---
+++ atcoder2/abc377/src/bin/b.rs
@@ -0,0 +1,37 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        ss: [Chars; 8],
+    };
+    let mut ii = [true; 8];
+    let mut jj = [true; 8];
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.iter().copied().enumerate() {
+            if c == '#' {
+                ii[i] = false;
+                jj[j] = false;
+            }
+        }
+    }
+    let mut rs = 0;
+    for i in 0..8 {
+        if ii[i] {
+            for j in 0..8 {
+                if jj[j] {
+                    rs += 1;
+                }
+            }
+        }
+    }
+    println!("{rs}");
+}
