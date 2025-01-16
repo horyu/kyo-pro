a3d987ae5541cf5cc838e7f2dc5c90215accf0db
horyu <28817604+horyu@users.noreply.github.com>
2025-01-16 22:24:10 +0900

    abc378 e

--- atcoder2/abc378/src/bin/e.rs
+++ atcoder2/abc378/src/bin/e.rs
@@ -9,10 +9,42 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+/*
+M(x,y) = (Ax + .. + Ay) % M
+S(i) = M(1) + .. + M(i)
+
+S(i) = S(i-1) + i * Ai - M * cnt(m - ai以上)
+
+*/
+
 fn main() {
     input! {
         n: usize,
+        m: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+
+    let mut rs = 0usize;
+    let mut ft = ac_library::FenwickTree::new(m, 0usize);
+    let mut pre = 0;
+    let mut a_sum = 0;
+    for (i, a) in aa.into_iter().enumerate() {
+        let a = a % m;
+        // dbg!(a);
+        // a_sum を足して解釈したとき ft内の値が m - a 以上のものの個数
+        // [0, m) -> [a_sum, m + a_sum)
+        // [m - a, m) -> [m - a + a_sum, m + a_sum)
+        let cnt = if a_sum <= a {
+            // dbg!(m - a + a_sum, a_sum);
+            ft.sum((m - a + a_sum)..) + ft.sum(..a_sum)
+        } else {
+            // dbg!((m - a + a_sum) % m , a_sum);
+            ft.sum(((m - a + a_sum) % m )..a_sum)
+        };
+        pre = pre + (i + 1) * a - m * cnt;
+        rs += pre;
+        ft.add(m - a_sum, 1);
+        a_sum = (a_sum + a) % m;
+    }
+    println!("{rs}");
 }
