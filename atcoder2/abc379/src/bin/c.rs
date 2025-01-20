1863d434335be4f0262af20b04cd23d99f39be2c
horyu <28817604+horyu@users.noreply.github.com>
2025-01-20 09:18:00 +0900

    abc379 c

--- atcoder2/abc379/src/bin/c.rs
+++ atcoder2/abc379/src/bin/c.rs
@@ -12,7 +12,38 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        xx: [usize; m],
+        aa: [usize; m],
     };
-    // println!("{rs}");
+    if aa.iter().sum::<usize>() != n {
+        println!("-1");
+        return;
+    }
+    let mut btm = BTreeMap::new();
+    for (x, a) in izip!(xx, aa) {
+        btm.insert(x, a);
+    }
+    let mut pos = 1;
+    let mut rs = 0usize;
+    while let Some((k, v)) = btm.pop_first() {
+        // eprintln!("{pos} {k} {:?}", pos.cmp(&k));
+        match pos.cmp(&k) {
+            Ordering::Less => {
+                println!("-1");
+                return;
+            }
+            Ordering::Equal => {
+                // v-1個の石をpos以上の位置に移動する => 1..=(v-1).sum
+                rs += v * (v - 1) / 2;
+                pos += v;
+            }
+            Ordering::Greater => {
+                // v個の石をkからpos以上の位置に移動する => (pos-k)..=(pos-k+v-1).sum
+                rs += (2 * pos - 2 * k + v - 1) * v / 2;
+                pos += v;
+            }
+        }
+    }
+    println!("{rs}");
 }
