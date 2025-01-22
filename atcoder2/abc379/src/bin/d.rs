3031e1db5f8acf45f77dd702174f07a42e3ac08c
horyu <28817604+horyu@users.noreply.github.com>
2025-01-22 21:28:19 +0900

    abc379 d

--- atcoder2/abc379/src/bin/d.rs
+++ atcoder2/abc379/src/bin/d.rs
@@ -11,8 +11,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        q: usize,
     };
-    // println!("{rs}");
+    let mut qq = VecDeque::new();
+    let mut day = 0usize;
+    for _ in 0..q {
+        input! {x: u32};
+        match x {
+            1 => {
+                qq.push_back(day);
+            }
+            2 => {
+                input! {t: usize};
+                day += t;
+            }
+            _ => {
+                input! {h: usize};
+                let mut rs = 0usize;
+                while qq.front().is_some_and(|&d| h <= day - d) {
+                    qq.pop_front();
+                    rs += 1;
+                }
+                println!("{rs}");
+            }
+        }
+    }
 }
