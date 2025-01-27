3b126f3ddfd1c22db995146874f1d20a76586904
horyu <28817604+horyu@users.noreply.github.com>
2025-01-27 09:40:23 +0900

    abc380 e

--- atcoder2/abc380/src/bin/e.rs
+++ atcoder2/abc380/src/bin/e.rs
@@ -12,7 +12,41 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
     };
-    // println!("{rs}");
+    // left => (color, right)
+    let mut btm = BTreeMap::from_iter((1..=n).map(|i| (i, (i, i))));
+    let mut cnts = vec![1; n + 1];
+    for _ in 0..q {
+        input! { t: u32 };
+        if t == 1 {
+            input! { x: usize, nc: usize };
+            let (&cl, &(cc, cr)) = btm.range(..=x).next_back().unwrap();
+            if cc == nc {
+                continue;
+            }
+            let size = cr - cl + 1;
+            cnts[cc] -= size;
+            cnts[nc] += size;
+            btm.entry(cl).and_modify(|v| v.0 = nc);
+            if let Some((&rl, &(rc, rr))) = btm.range((x + 1)..).next() {
+                if nc == rc {
+                    btm.remove(&rl);
+                    btm.entry(cl).and_modify(|v| v.1 = rr);
+                }
+            }
+            if let Some((&ll, &(lc, lr))) = btm.range(..cl).next_back() {
+                if lc == nc {
+                    let r = btm.remove(&cl).unwrap().1;
+                    btm.entry(ll).and_modify(|v| v.1 = r);
+                }
+            }
+            // eprintln!("{cnts:?}");
+            // eprintln!("{btm:?}");
+        } else {
+            input! { c: usize };
+            let rs = cnts[c];
+            println!("{rs}");
+        }
+    }
 }
