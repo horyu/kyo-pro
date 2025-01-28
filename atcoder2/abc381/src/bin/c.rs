d39a4c99161867fb7efc9277117381ab0b5a7406
horyu <28817604+horyu@users.noreply.github.com>
2025-01-29 07:12:08 +0900

    abc381 c

--- atcoder2/abc381/src/bin/c.rs
+++ atcoder2/abc381/src/bin/c.rs
@@ -12,7 +12,15 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Chars,
     };
-    // println!("{rs}");
+    let mut rs = 1;
+    for pos in s.iter().copied().positions(|c| c == '/') {
+        let mut next = 1;
+        while let (Some('1'), Some('2')) = (s.get(pos.overflowing_sub(next).0), s.get(pos + next)) {
+            next += 1;
+        }
+        rs = rs.max(1 + (next - 1) * 2);
+    }
+    println!("{rs}");
 }
