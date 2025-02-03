68a6d8bdad93789993ac3d605440e2a7b860db75
horyu <28817604+horyu@users.noreply.github.com>
2025-02-03 20:48:56 +0900

    abc382 d

--- atcoder2/abc382/src/bin/d.rs
+++ atcoder2/abc382/src/bin/d.rs
@@ -12,7 +12,43 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
     };
-    // println!("{rs}");
+    // let n = 12; let m = 120;
+    let mut rs = vec![];
+    let mut vv = vec![];
+    dfs(&mut rs, &mut vv, n, m);
+    println!("{}", rs.len());
+    let rs = rs.iter().join("\n");
+    println!("{rs}");
+}
+
+fn dfs(rs: &mut Vec<String>, vv: &mut Vec<usize>, n: usize, m: usize) -> bool {
+    if vv.len() == n {
+        // if vv.iter().any(|&v| m < v) || vv.iter().tuple_windows().any(|(&a, &b)| !(a + 10 <= b)) {
+        //     panic!("{vv:?}");
+        // }
+        rs.push(vv.iter().join(" "));
+        return false;
+    }
+    if let Some(last) = vv.last().copied() {
+        if m < last + 10 {
+            return true;
+        }
+        for i in (last + 10)..=(last + 20).min(m) {
+            vv.push(i);
+            let tf = dfs(rs, vv, n, m);
+            vv.pop();
+            if tf {
+                break;
+            }
+        }
+    } else {
+        for i in 1..=10 {
+            vv.push(i);
+            dfs(rs, vv, n, m);
+            vv.pop();
+        }
+    }
+    false
 }
