824b9b1e6da5594c1eb09908ed8ad19dfe6598d6
horyu <28817604+horyu@users.noreply.github.com>
2025-02-06 08:38:41 +0900

    abc383 c

--- atcoder2/abc383/src/bin/c.rs
+++ atcoder2/abc383/src/bin/c.rs
@@ -11,8 +11,47 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        d: usize,
+        ss: [Chars; h],
     };
-    // println!("{rs}");
+    let mut hs = HashSet::new();
+    let mut qq = VecDeque::new();
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.iter().copied().enumerate() {
+            if c == 'H' {
+                hs.insert((i, j));
+                qq.push_back((i, j, 0));
+            }
+        }
+    }
+    let udlr = |i: usize, j: usize| -> Vec<(usize, usize)> {
+        let mut vv = vec![];
+        if 0 < i {
+            vv.push((i - 1, j));
+        }
+        if i < h - 1 {
+            vv.push((i + 1, j));
+        }
+        if 0 < j {
+            vv.push((i, j - 1));
+        }
+        if j < w - 1 {
+            vv.push((i, j + 1));
+        }
+        vv.retain(|&(vi, vj)| ss[vi][vj] == '.');
+        vv
+    };
+    while let Some((qi, qj, qd)) = qq.pop_front() {
+        if qd < d {
+            for (i, j) in udlr(qi, qj) {
+                if hs.insert((i, j)) {
+                    qq.push_back((i, j, qd + 1));
+                }
+            }
+        }
+    }
+    let rs = hs.len();
+    println!("{rs}");
 }
