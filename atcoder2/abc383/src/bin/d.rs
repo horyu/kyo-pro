384c4a4049dde69d28c5cd435479dd960f1adb36
horyu <28817604+horyu@users.noreply.github.com>
2025-02-07 23:08:32 +0900

    abc383 d

--- atcoder2/abc383/src/bin/d.rs
+++ atcoder2/abc383/src/bin/d.rs
@@ -12,7 +12,44 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    let vv = sieve_of_eratosthenes(1e6 as usize);
+    // 正の約数が9個
+    // a^8 または a^2 * b^2
+    // a^8
+    // dbg!(n.nth_root(8));
+    // dbg!(n.nth_root(8).pow(8));
+    // let mut rs = n.nth_root(8) - 1;
+    let mut rs = (2..=n.nth_root(8)).filter(|&i| vv[i]).count();
+    // a^2 * b^2
+    let primes = vv
+        .into_iter()
+        .positions(|tf| tf)
+        .collect_vec();
+    for (i, a) in primes.iter().copied().enumerate() {
+        let aa = a * a;
+        if n <= aa * aa {
+            break;
+        }
+        rs += primes[(i + 1)..].partition_point(|&b| aa.saturating_mul(b * b) <= n);
+    }
+
+    println!("{rs}");
+}
+
+// エラトステネスの篩
+fn sieve_of_eratosthenes(n: usize) -> Vec<bool> {
+    let mut vv = vec![true; n + 1];
+    vv[0] = false;
+    vv[1] = false;
+    for i in 2..=n {
+        if vv[i] {
+            let mut j = i * 2;
+            while j <= n {
+                vv[j] = false;
+                j += i;
+            }
+        }
+    }
+    vv
 }
