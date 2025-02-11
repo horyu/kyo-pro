9f367f5274d538f6e9e884b49ec2d4ab8b5e79e3
horyu <28817604+horyu@users.noreply.github.com>
2025-02-11 09:24:53 +0900

    abc384 e

--- atcoder2/abc384/src/bin/e.rs
+++ atcoder2/abc384/src/bin/e.rs
@@ -11,8 +11,46 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        x: u128,
+        p: Usize1,
+        q: Usize1,
+        sss: [[u128; w]; h],
     };
-    // println!("{rs}");
+    let mut bh = BinaryHeap::new();
+    let mut hs = HashSet::new();
+    let udlr = |i: usize, j: usize| -> Vec<(usize, usize)> {
+        let mut vv = vec![];
+        if 0 < i {
+            vv.push((i - 1, j));
+        }
+        if i < h - 1 {
+            vv.push((i + 1, j));
+        }
+        if 0 < j {
+            vv.push((i, j - 1));
+        }
+        if j < w - 1 {
+            vv.push((i, j + 1));
+        }
+        vv
+    };
+    for (i, j) in udlr(p, q) {
+        bh.push((R(sss[i][j]), i, j));
+        hs.insert((i, j));
+    }
+    hs.insert((p, q));
+    let mut rs = sss[p][q];
+    while let Some((R(qp), qi, qj)) = bh.pop() {
+        if qp * x < rs {
+            rs += qp;
+            for (i, j) in udlr(qi, qj) {
+                if hs.insert((i, j)) {
+                    bh.push((R(sss[i][j]), i, j));
+                }
+            }
+        }
+    }
+    println!("{rs}");
 }
