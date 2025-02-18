7adf13fc364c1238cf5501a7df5c4e876200985a
horyu <28817604+horyu@users.noreply.github.com>
2025-02-18 09:35:44 +0900

    abc386 c

--- atcoder2/abc386/src/bin/c.rs
+++ atcoder2/abc386/src/bin/c.rs
@@ -11,8 +11,36 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        k: usize,
+        s: Chars,
+        t: Chars,
     };
-    // println!("{rs}");
+    if 1 < s.len().abs_diff(t.len()) {
+        println!("No");
+        return;
+    }
+    let (s, t) = if s.len() < t.len() { (s, t) } else { (t, s) };
+    let tf = if s.len() == t.len() {
+        izip!(s.iter(), t.iter())
+            .filter(|(&sc, &tc)| sc != tc)
+            .count()
+            <= 1
+    } else {
+        // sに任意の1文字を追加 = tの任意の1文字を削除
+        let mut i = 0;
+        let mut j = 0;
+        let mut diff = 0;
+        while i < s.len() && j < t.len() {
+            if s[i] == t[j] {
+                i += 1;
+                j += 1;
+            } else {
+                j += 1;
+                diff += 1;
+            }
+        }
+        diff + s.len() - i <= 1
+    };
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{rs}");
 }
