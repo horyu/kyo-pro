b0466024d1018bae889f421e8e0988562bb8c37d
horyu <28817604+horyu@users.noreply.github.com>
2025-02-26 08:57:27 +0900

    abc387 d

--- atcoder2/abc387/src/bin/d.rs
+++ atcoder2/abc387/src/bin/d.rs
@@ -11,8 +11,62 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        h: usize,
+        w: usize,
+        ss: [Chars; h],
     };
-    // println!("{rs}");
+    let (mut si, mut sj) = (0, 0);
+    let (mut gi, mut gj) = (0, 0);
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.iter().copied().enumerate() {
+            match c {
+                'S' => {
+                    (si, sj) = (i, j);
+                }
+                'G' => {
+                    (gi, gj) = (i, j);
+                }
+                _ => (),
+            }
+        }
+    }
+    let udlr = |i: usize, j: usize| -> Vec<(usize, usize)> {
+        let mut vv = vec![];
+        if 0 < i {
+            vv.push((i - 1, j));
+        }
+        if i < h - 1 {
+            vv.push((i + 1, j));
+        }
+        if 0 < j {
+            vv.push((i, j - 1));
+        }
+        if j < w - 1 {
+            vv.push((i, j + 1));
+        }
+        vv.retain(|&(vi, vj)| ss[vi][vj] != '#');
+        vv
+    };
+    let mut qq = VecDeque::new();
+    let mut pushed = vec![vec![[false; 2]; w]; h];
+    for dir in 0..2 {
+        qq.push_back((si, sj, 0, dir));
+        pushed[si][sj][dir] = true;
+    }
+    while let Some((qi, qj, qd, qdir)) = qq.pop_front() {
+        if (qi, qj) == (gi, gj) {
+            println!("{qd}");
+            return;
+        }
+        let qa = ss[qi][qj];
+        for (i, j) in udlr(qi, qj) {
+            let dir = usize::from(qi == i);
+            if pushed[i][j][dir] || qdir == dir {
+                continue;
+            }
+            pushed[i][j][dir] = true;
+            qq.push_back((i, j, qd + 1, dir));
+        }
+    }
+    println!("-1");
 }
