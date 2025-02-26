ff9f2fa1e3e999b0fd77cbb689c8500f8a80dd60
horyu <28817604+horyu@users.noreply.github.com>
2025-02-27 06:44:43 +0900

    abc388 ab

---
+++ atcoder2/abc388/src/bin/a.rs
@@ -0,0 +1,17 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    let mut buf = [0];
+    use std::io;
+    let _ = io::Read::read_exact(&mut io::stdin(), &mut buf);
+    println!("{}UPC", buf[0] as char);
+}
