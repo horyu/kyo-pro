0ce21361483fd561ef76e1b40ce4d3970aac4c6c
horyu <28817604+horyu@users.noreply.github.com>
2025-03-04 09:33:01 +0900

    abc389 c

--- atcoder2/abc389/src/bin/c.rs
+++ atcoder2/abc389/src/bin/c.rs
@@ -11,8 +11,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        q: usize,
     };
-    // println!("{rs}");
+    let mut ft = ac_library::FenwickTree::new(3e5 as usize, 0usize);
+    let mut l = 0;
+    let mut r = 0;
+    for _ in 0..q {
+        input! {t: u32};
+        match t {
+            1 => {
+                input! {x: usize};
+                ft.add(r, x);
+                r += 1;
+            }
+            2 => {
+                l += 1;
+            }
+            3 => {
+                input! {k: Usize1};
+                let rs = ft.sum(l..(l + k));
+                println!("{rs}");
+            }
+            _ => unreachable!(),
+        }
+        // eprintln!("[{l}-{r}]{t}: {}", (0..r).map(|i| ft.sum(i..=i)).join(" "));
+    }
 }
