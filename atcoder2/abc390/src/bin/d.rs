5f9de92f4f7b4053a7ba13470f34286242b844c3
horyu <28817604+horyu@users.noreply.github.com>
2025-03-09 20:41:56 +0900

    abc390 d

--- atcoder2/abc390/src/bin/d.rs
+++ atcoder2/abc390/src/bin/d.rs
@@ -14,7 +14,35 @@ fn main() {
         n: usize,
         aa: [usize; n],
     };
-    let mut rs = 0;
-    // 全くわからない
+    // https://atcoder.jp/contests/abc390/editorial/12045
+    let mut ss = [0usize; 12];
+    let mut hs = HashSet::new();
+    dfs(&aa, &mut ss, &mut hs, 0, 0, 0);
+    let rs = hs.len();
     println!("{rs}");
+}
+
+fn dfs(
+    aa: &[usize],
+    ss: &mut [usize; 12],
+    hs: &mut HashSet<usize>,
+    idx: usize,
+    size: usize,
+    mut val: usize,
+) {
+    for i in 0..=size {
+        val ^= ss[i];
+        ss[i] += aa[idx];
+        val ^= ss[i];
+        if idx == aa.len() - 1 {
+            hs.insert(val);
+        } else if i < size {
+            dfs(aa, ss, hs, idx + 1, size, val);
+        } else {
+            dfs(aa, ss, hs, idx + 1, size + 1, val);
+        }
+        val ^= ss[i];
+        ss[i] -= aa[idx];
+        val ^= ss[i];
+    }
 }
