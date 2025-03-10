a8e39a855f010fa9f24291cd6a7469715a2ffd58
horyu <28817604+horyu@users.noreply.github.com>
2025-03-10 23:06:08 +0900

    abc390 e

--- atcoder2/abc390/src/bin/e.rs
+++ atcoder2/abc390/src/bin/e.rs
@@ -12,7 +12,51 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        x: usize,
+        vvaacc: [(Usize1, usize, usize); n],
     };
-    // println!("{rs}");
+    let mut ss = [0; 3];
+    let mut ddd = vec![vec![0; x + 1]; 3];
+    for (v, a, c) in vvaacc.into_iter().sorted_unstable_by_key(|vac| vac.2) {
+        ss[v] += a;
+        for i in ((c + 1)..=x).rev() {
+            if 0 < ddd[v][i - c] {
+                ddd[v][i] = ddd[v][i].max(ddd[v][i - c] + a);
+            }
+        }
+        ddd[v][c] = ddd[v][c].max(a);
+    }
+    let mut bbttmm = vec![BTreeMap::new(); 3];
+    for (v, dd) in ddd.into_iter().enumerate() {
+        for (c, d) in dd.into_iter().enumerate() {
+            if 0 < d {
+                bbttmm[v].insert(d, c);
+            }
+        }
+    }
+    // for btm in &bbttmm {
+    //     eprintln!("{btm:?}");
+    // }
+    let mut ok = 0;
+    let mut ng = ss.into_iter().min().unwrap() + 1;
+    while 1 < ng - ok {
+        // 目標ビタミン
+        let mid = ok.midpoint(ng);
+        let mut sum = 0;
+        for btm in bbttmm.iter() {
+            if let Some((_d, c)) = btm.range(mid..).next() {
+                sum += c;
+            } else {
+                sum = x + 1;
+                break;
+            }
+        }
+        if sum <= x {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    let rs = ok;
+    println!("{rs}");
 }
