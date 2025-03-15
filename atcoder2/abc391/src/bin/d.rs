499e04f0e75945bb3015ec7cd30d55f8a33487df
horyu <28817604+horyu@users.noreply.github.com>
2025-03-15 18:55:05 +0900

    abc391 d

--- atcoder2/abc391/src/bin/d.rs
+++ atcoder2/abc391/src/bin/d.rs
@@ -12,7 +12,36 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        w: usize,
+        xxyy: [(Usize1, Usize1); n],
+        q: usize,
+        ttaa: [(usize, Usize1); q],
     };
-    // println!("{rs}");
+    // dd[i] = 下からi番目の層の最大値
+    let mut dd = vec![0];
+    // cc[i] = i列目のブロックの個数
+    let mut cc = vec![0; w];
+    // i2d[i] = ブロックiの層
+    let mut i2d = vec![0; n];
+    for (i, (x, y)) in xxyy
+        .iter()
+        .copied()
+        .enumerate()
+        .sorted_unstable_by_key(|ixy| ixy.1 .1)
+    {
+        if let Some(d) = dd.get_mut(cc[x]) {
+            *d = y.max(*d);
+        } else {
+            dd.push(y);
+        }
+        i2d[i] = cc[x];
+        cc[x] += 1;
+    }
+    let c_min = cc.iter().copied().min().unwrap();
+    for (t, a) in ttaa {
+        let d = i2d[a];
+        let tf = c_min <= d || t <= dd[d];
+        let rs = ["No", "Yes"][tf as usize];
+        println!("{rs}");
+    }
 }
