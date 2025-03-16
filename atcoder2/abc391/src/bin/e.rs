f7c3868d42611ac71d15305918de4692bf6f21d7
horyu <28817604+horyu@users.noreply.github.com>
2025-03-16 17:13:09 +0900

    abc391 e

--- atcoder2/abc391/src/bin/e.rs
+++ atcoder2/abc391/src/bin/e.rs
@@ -12,7 +12,41 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aa: Bytes,
     };
-    // println!("{rs}");
+    let aa = aa.into_iter().map(|x| x - b'0').collect_vec();
+    let mut aaa = vec![aa.clone()];
+    let mut bb = aa.clone();
+    for _ in 0..n {
+        bb = bb
+            .into_iter()
+            .tuples()
+            .map(|(x, y, z)| u8::from(2 <= x + y + z))
+            .collect_vec();
+        aaa.push(bb.clone());
+        // eprintln!("{}", bb.iter().join(""));
+    }
+    if bb[0] == 0 {
+        aaa = aaa
+            .into_iter()
+            .map(|tt| tt.into_iter().map(|x| 1 ^ x).collect_vec())
+            .collect_vec();
+    }
+    let rs = dfs(&aaa, n, 0);
+    println!("{rs}");
+}
+
+// 0にするコスト
+fn dfs(aaa: &[Vec<u8>], i: usize, j: usize) -> usize {
+    if aaa[i][j] == 0 {
+        return 0;
+    }
+    if i == 0 {
+        return 1;
+    }
+    let cc = (3 * j..3 * (j + 1))
+        .map(|jj| dfs(aaa, i - 1, jj))
+        .sorted_unstable()
+        .collect_vec();
+    cc[0] + cc[1]
 }
