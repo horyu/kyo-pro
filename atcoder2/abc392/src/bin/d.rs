d0ec914fe60d2adb6e89e510233008b2426d267c
horyu <28817604+horyu@users.noreply.github.com>
2025-03-20 08:43:00 +0900

    abc392 d

--- atcoder2/abc392/src/bin/d.rs
+++ atcoder2/abc392/src/bin/d.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
-use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools::{chain, iproduct, izip, Itertools};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
 use petgraph::unionfind::UnionFind;
@@ -12,7 +12,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
-    // println!("{rs}");
+    // let mut aaa = vec![];
+    let mut kk = vec![];
+    let mut hhmm = vec![];
+    for _ in 0..n {
+        input! {k: usize, aa: [usize; k]};
+        kk.push(k);
+        hhmm.push(aa.into_iter().counts());
+    }
+    let mut rs = 0.0f64;
+    for i in 0..n {
+        let ik = kk[i];
+        let ihm = &hhmm[i];
+        for j in (i + 1)..n {
+            let jk = kk[j];
+            let jhm = &hhmm[j];
+            let bunbo = ik * jk;
+            let mut bunsi = 0;
+            let (sml, big) = if ik < jk { (ihm, jhm) } else { (jhm, ihm) };
+            for (k, v) in sml {
+                bunsi += v * big.get(k).unwrap_or(&0);
+            }
+            rs = rs.max(bunsi as f64 / bunbo as f64);
+        }
+    }
+    println!("{rs:0.15}");
 }
