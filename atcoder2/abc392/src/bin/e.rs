41fae52148576150129350bee68aff79693de9c0
horyu <28817604+horyu@users.noreply.github.com>
2025-03-21 09:40:02 +0900

    abc392 e

--- atcoder2/abc392/src/bin/e.rs
+++ atcoder2/abc392/src/bin/e.rs
@@ -12,7 +12,39 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aabb: [(Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    let mut dsu = ac_library::Dsu::new(n);
+    let mut dups = vec![];
+    for (i, (a, b)) in aabb.iter().copied().enumerate() {
+        if dsu.same(a, b) {
+            dups.push(i);
+        } else {
+            dsu.merge(a, b);
+        }
+    }
+    let mut hs = HashSet::new();
+    for i in 0..n {
+        hs.insert(dsu.leader(i));
+    }
+    let mut rrss = vec![];
+    for i in dups {
+        if hs.len() == 1 {
+            break;
+        }
+        let (a, b) = aabb[i];
+        let la = dsu.leader(a);
+        // b側を hs の適当なノードに繋ぎ変える
+        let lx = hs.iter().copied().find(|&x| x != la).unwrap();
+        rrss.push((i + 1, a + 1, lx + 1));
+        let l = dsu.merge(la, lx);
+        hs.remove(&la);
+        hs.remove(&lx);
+        hs.insert(l);
+    }
+    println!("{}", rrss.len());
+    for (i, p, q) in rrss {
+        println!("{i} {p} {q}");
+    }
 }
