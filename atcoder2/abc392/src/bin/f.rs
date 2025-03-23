d5bff8bd5b4c166514b369286deb09738bc2ccc0
horyu <28817604+horyu@users.noreply.github.com>
2025-03-24 07:58:43 +0900

    abc392 f

--- atcoder2/abc392/src/bin/f.rs
+++ atcoder2/abc392/src/bin/f.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::{Additive, Segtree};
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -14,13 +15,14 @@ fn main() {
         n: usize,
         pp: [Usize1; n],
     };
-    let mut ft = ac_library::FenwickTree::new(n, 0usize);
+    // https://atcoder.jp/contests/abc392/editorial/12145
+    // https://atcoder.jp/contests/abc392/submissions/63877896
     let mut vv = vec![0; n];
+    let mut st = Segtree::<Additive<usize>>::from(vec![1; n]);
     for (i, p) in pp.into_iter().enumerate().rev() {
-        let pos = ft.sum(..=p) + p;
+        let pos = st.max_right(0, |&x| x <= p);
         vv[pos] = i + 1;
-        ft.add(p, 1);
-        eprintln!("{vv:?} {}", (0..n).map(|i| ft.sum(i..=i)).join(" "));
+        st.set(pos, 0);
     }
     let rs = vv.iter().join(" ");
     println!("{rs}");
