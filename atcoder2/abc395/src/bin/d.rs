e92bb476404ec53c3f5235e5e68877662dc07bec
horyu <28817604+horyu@users.noreply.github.com>
2025-04-09 11:55:35 +0900

    abc395 d

--- atcoder2/abc395/src/bin/d.rs
+++ atcoder2/abc395/src/bin/d.rs
@@ -12,7 +12,31 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        q: usize,
     };
-    // println!("{rs}");
+    // h2s[i] = j; 鳩i が 巣j にいる
+    let mut h2s = (0..n).collect_vec();
+    // s2p[i] = j; 巣i が 場所j にある
+    let mut s2p = (0..n).collect_vec();
+    let mut p2s = (0..n).collect_vec();
+    for i in 0..q {
+        input! {op: i32, a: Usize1};
+        if op == 3 {
+            // 鳩a のいる場所を出力
+            let rs = s2p[h2s[a]] + 1;
+            println!("{rs}");
+            continue;
+        }
+        input! {b: Usize1};
+        if op == 1 {
+            // 鳩a を 場所b の巣に移動
+            h2s[a] = p2s[b];
+        } else {
+            // 場所a と 場所b にある巣を交換
+            let sa = p2s[a];
+            let sb = p2s[b];
+            s2p.swap(sa, sb);
+            p2s.swap(a, b);
+        }
+    }
 }
