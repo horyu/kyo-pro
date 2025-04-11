bd16da66ba4cfaf38e4545b721266bfc9f4ebd54
horyu <28817604+horyu@users.noreply.github.com>
2025-04-11 22:54:02 +0900

    abc395 f

--- atcoder2/abc395/src/bin/f.rs
+++ atcoder2/abc395/src/bin/f.rs
@@ -12,7 +12,27 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        x: usize,
+        uudd: [(usize, usize); n],
     };
+    let (u_min, u_max, ud_min, ud_max) =
+        uudd.iter()
+            .copied()
+            .fold((!0, 0, !0, 0), |(min_u, max_u, min_ud, max_ud), (u, d)| {
+                (
+                    min_u.min(u),
+                    max_u.max(u),
+                    min_ud.min(u + d),
+                    max_ud.max(u + d),
+                )
+            });
+    // let mut ok = 0;
+    // let mut ng = ud_min + 1;
+    // while 1 < ng - ok {
+    //     let mid = ok.midpoint(ng);
+    //     let mut sml = mid.saturating_sub(uudd[0].1);
+    //     let mut big = mid.min(uudd[0].0);
+    // }
+
     // println!("{rs}");
 }
