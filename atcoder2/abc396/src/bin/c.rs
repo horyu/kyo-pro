653d55541b05f57241ee82927e169bf9e0b31894
horyu <28817604+horyu@users.noreply.github.com>
2025-04-14 23:44:42 +0900

    abc396 c

--- atcoder2/abc396/src/bin/c.rs
+++ atcoder2/abc396/src/bin/c.rs
@@ -12,7 +12,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        mut bb: [isize; n],
+        mut ww: [isize; m],
     };
-    // println!("{rs}");
+    bb.sort_unstable();
+    ww.sort_unstable();
+    let mut tmp = 0;
+    let mut b_cnt = 0;
+    while let Some(b) = bb.pop_if(|b| 0 <= *b) {
+        tmp += b;
+        b_cnt += 1;
+    }
+    let mut rs = tmp;
+    let mut w_cnt = 0;
+    while let Some(w) = ww.pop_if(|_| w_cnt < b_cnt) {
+        w_cnt += 1;
+        tmp += w;
+        rs = rs.max(tmp);
+    }
+    while let (Some(b), Some(w)) = (bb.pop(), ww.pop()) {
+        tmp += b;
+        tmp += w;
+        rs = rs.max(tmp);
+    }
+    println!("{rs}");
 }
