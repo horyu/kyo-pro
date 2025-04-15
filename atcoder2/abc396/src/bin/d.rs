b870a84a5f18b6776764a70d654e5fdf0c3a766a
horyu <28817604+horyu@users.noreply.github.com>
2025-04-15 09:14:05 +0900

    abc396 d

--- atcoder2/abc396/src/bin/d.rs
+++ atcoder2/abc396/src/bin/d.rs
@@ -12,7 +12,31 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvvww: [(Usize1, Usize1, usize); m],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![!0usize; n]; n];
+    for (u, v, w) in uuvvww.iter().copied() {
+        g[u][v] = w;
+        g[v][u] = w;
+    }
+    let mut rs = !0;
+    // 頂点0 -> (1~n-1)の並び替え -> 頂点n-1
+    for size in 0..(n - 1) {
+        for vv in (1..(n - 1)).permutations(size) {
+            let mut tmp = 0;
+            let mut ok = true;
+            for (i, j) in chain!([0], vv, [n - 1]).tuple_windows() {
+                if g[i][j] == !0 {
+                    ok = false;
+                    break;
+                }
+                tmp ^= g[i][j];
+            }
+            if ok {
+                rs = rs.min(tmp);
+            }
+        }
+    }
+    println!("{rs}");
 }
