d3c43d8f18130e5d751bf99975ba5cf8a7aefbde
horyu <28817604+horyu@users.noreply.github.com>
2025-04-16 23:07:12 +0900

    abc396 e

--- atcoder2/abc396/src/bin/e.rs
+++ atcoder2/abc396/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+#![feature(map_try_insert)]
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -12,7 +13,56 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        xxyyzz: [(Usize1, Usize1, usize); m],
     };
-    // println!("{rs}");
+
+    let mut g = vec![vec![]; n];
+    let mut dsu = ac_library::Dsu::new(n);
+    for (x, y, z) in xxyyzz.iter().copied() {
+        g[x].push((y, z));
+        g[y].push((x, z));
+        dsu.merge(x, y);
+    }
+    let mut aa = vec![0; n];
+    for ii in dsu.groups() {
+        // bit 単位で xor が成立するか確かめる
+        for d in 0..32 {
+            let mut pre_sum = !0;
+            for b in [0, 1] {
+                let mut qq = VecDeque::new();
+                let mut hm = HashMap::new();
+                qq.push_back((ii[0], b));
+                hm.insert(ii[0], b);
+                let mut sum = b;
+                while let Some((i, b)) = qq.pop_front() {
+                    for (j, z) in g[i].iter().copied() {
+                        if hm.contains_key(&j) {
+                            continue;
+                        }
+                        let zz = (z >> d) & 1;
+                        let bb = b ^ zz;
+                        if hm.try_insert(j, bb).is_ok() {
+                            qq.push_back((j, bb));
+                            sum += bb;
+                        }
+                    }
+                }
+                if sum < pre_sum {
+                    pre_sum = sum;
+                    for (i, b) in hm {
+                        // aa[i] の d ビット目を b にする
+                        aa[i] &= !(1 << d);
+                        aa[i] |= b << d;
+                    }
+                }
+            }
+        }
+    }
+    if xxyyzz.iter().copied().all(|(x, y, z)| aa[x] ^ aa[y] == z) {
+        let rs = aa.iter().join(" ");
+        println!("{rs}");
+    } else {
+        println!("-1");
+    }
 }
