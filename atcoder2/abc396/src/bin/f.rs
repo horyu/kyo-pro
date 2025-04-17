3fb705b1dba7c06884f589f28b8d6f2d84d20d22
horyu <28817604+horyu@users.noreply.github.com>
2025-04-17 21:48:10 +0900

    abc396 f

--- atcoder2/abc396/src/bin/f.rs
+++ atcoder2/abc396/src/bin/f.rs
@@ -12,7 +12,25 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        m: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    const MAX: usize = 2e5 as usize;
+    let a2ii: multimap::MultiMap<usize, usize> =
+        multimap::MultiMap::from_iter(aa.iter().copied().enumerate().map(|(i, a)| (a, i)));
+
+    // 転倒数を求める
+    let mut rs = 0;
+    let mut ft = ac_library::FenwickTree::new(MAX, 0usize);
+    for (i, a) in aa.iter().copied().enumerate() {
+        rs += i - ft.sum(..=a);
+        ft.add(a, 1);
+    }
+    println!("{rs}");
+    for k in 1..m {
+        if let Some(ii) = a2ii.get_vec(&(m - k)) {
+            // ここでいい感じに計算する
+        }
+        println!("{rs}");
+    }
 }
