21bd806ee15ac2e8bc0c0a56e9f5a73c217f9227
horyu <28817604+horyu@users.noreply.github.com>
2025-04-20 17:22:23 +0900

    abc397 ab

---
+++ atcoder2/abc397/src/bin/b.rs
@@ -0,0 +1,33 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        s: Chars,
+    };
+    let n = s.len();
+    let mut i = 0;
+    for c in s {
+        if i % 2 == 0 {
+            if c == 'o' {
+                i += 1;
+            }
+        } else {
+            if c == 'i' {
+                i += 1;
+            }
+        }
+        i += 1;
+    }
+    i = i.div_ceil(&2) * 2;
+    let rs = i - n;
+    println!("{rs}");
+}
