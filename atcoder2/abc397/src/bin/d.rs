fce4eff48b8c125d9a583ade1c550e276b895236
horyu <28817604+horyu@users.noreply.github.com>
2025-04-22 22:57:44 +0900

    abc397 d

--- atcoder2/abc397/src/bin/d.rs
+++ atcoder2/abc397/src/bin/d.rs
@@ -11,8 +11,24 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        n: u128,
     };
-    // println!("{rs}");
+    // xxx - yyy = n
+    // xxx - n = yyy
+    for x in (n.nth_root(3) + 1)..(4e7 as u128) {
+        let yyy = x.pow(3u32) - n;
+        let y = yyy.nth_root(3);
+        if y.pow(3u32) == yyy {
+            println!("{x} {y}");
+            return;
+        }
+    }
+
+    // (i + 1)^3 - i^3 <= 1e18 となる最大のi
+    // let i = 577_350_268u128; // 5.7e8
+    // dbg!(i, i.ilog10());
+    // let diff = (i + 1).pow(3u32) - i.pow(3u32);
+    // dbg!(diff, diff.ilog10());
+
+    println!("-1");
 }
