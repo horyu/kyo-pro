af6d2f71c0f7798b01159a8a07b39fc0c13823e3
horyu <28817604+horyu@users.noreply.github.com>
2025-04-24 23:11:30 +0900

    abc397 e

--- atcoder2/abc397/src/bin/e.rs
+++ atcoder2/abc397/src/bin/e.rs
@@ -9,10 +9,49 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+/*
+木をn本の頂点数kのパスに分解できるか
+切り方は 2通り
+- 直線の端を切る
+- Y字の真ん中を含めて切る
+
+方法
+- 全ての葉ノードを集める
+- 各葉ノードに対して、長さkのパスを作って間に分岐がなければ切る。切断したノードから距離k以内の葉ノードを集める
+- 各葉ノードに対して共通祖先への距離を祖先側に登録し、パスの長さがkにできたら切断する。切断したノードから距離k以内の葉ノードを集める
+*/
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
+        uuvv: [(Usize1, Usize1); n * k - 1],
     };
-    // println!("{rs}");
+    let mut g = vec![HashSet::new(); n * k];
+    for (u, v) in uuvv.iter().copied() {
+        g[u].insert(v);
+        g[v].insert(u);
+    }
+    let mut hs = HashSet::<_>::from_iter(0..n);
+    let mut ll = HashSet::new();
+    while !hs.is_empty() {
+        let len = hs.len();
+        let new_ll = hs.iter().copied()
+            .filter(|&x| g[x].len() == 1)
+            .collect_vec();
+        if new_ll.is_empty() {
+            println!("No");
+            return;
+        }
+        for new_l in new_ll {
+            hs.remove(&new_l);
+            ll.insert(new_l);
+        }
+
+
+        if len == hs.len() {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
