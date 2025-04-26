ca6ccbd9d88a4eca123a209b39fd9ad408071a32
horyu <28817604+horyu@users.noreply.github.com>
2025-04-26 13:32:44 +0900

    abc397 e

--- atcoder2/abc397/src/bin/e.rs
+++ atcoder2/abc397/src/bin/e.rs
@@ -9,49 +9,51 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
-/*
-木をn本の頂点数kのパスに分解できるか
-切り方は 2通り
-- 直線の端を切る
-- Y字の真ん中を含めて切る
-
-方法
-- 全ての葉ノードを集める
-- 各葉ノードに対して、長さkのパスを作って間に分岐がなければ切る & 切断したノードから距離k以内の葉ノードを再処理対象に加える
-// ここで葉ノードは全て流さk未満となる
-- 各葉ノードに対して分岐を持つ内部ノードへの距離を登録し、パスの長さの和をkにできたら切断する。切断したノードから距離k以内の葉ノードを再処理対象に加える
-*/
 fn main() {
     input! {
         n: usize,
         k: usize,
         uuvv: [(Usize1, Usize1); n * k - 1],
     };
-    let mut g = vec![HashSet::new(); n * k];
+    let mut g = vec![vec![]; n * k];
     for (u, v) in uuvv.iter().copied() {
-        g[u].insert(v);
-        g[v].insert(u);
+        g[u].push(v);
+        g[v].push(u);
     }
-    let mut hs = HashSet::<_>::from_iter(0..n);
-    let mut ll = HashSet::new();
-    while !hs.is_empty() {
-        let len = hs.len();
-        let new_ll = hs.iter().copied()
-            .filter(|&x| g[x].len() == 1)
-            .collect_vec();
-        if new_ll.is_empty() {
+    // https://atcoder.jp/contests/abc397/submissions/63550824
+    let mut vv = vec![(0, !0, 0)];
+    // 部分木の大きさ
+    let mut sizes = vec![1; n * k];
+    while let Some((cur, from, t)) = vv.pop() {
+        if t == 0 {
+            vv.push((cur, from, 1));
+            for next in g[cur].iter().copied() {
+                if next != from {
+                    vv.push((next, cur, 0));
+                }
+            }
+            continue;
+        }
+        // 子ノードの数
+        let mut cnt = 0;
+        for next in g[cur].iter().copied() {
+            if next != from {
+                sizes[cur] += sizes[next];
+                if 0 < sizes[next] {
+                    cnt += 1;
+                }
+            }
+        }
+        if k < sizes[cur] || 3 <= cnt {
             println!("No");
             return;
         }
-        for new_l in new_ll {
-            hs.remove(&new_l);
-            ll.insert(new_l);
-        }
-
-
-        if len == hs.len() {
+        if sizes[cur] < k && 2 <= cnt {
             println!("No");
             return;
+        }
+        if sizes[cur] == k {
+            sizes[cur] = 0;
         }
     }
     println!("Yes");
