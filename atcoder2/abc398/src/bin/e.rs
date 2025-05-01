018ccb3d64c3b907a388bafa9856d06bec826dce
horyu <28817604+horyu@users.noreply.github.com>
2025-05-01 22:26:25 +0900

    abc398 e

--- atcoder2/abc398/src/bin/e.rs
+++ atcoder2/abc398/src/bin/e.rs
@@ -10,9 +10,60 @@ use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
 fn main() {
+    let mut stdin =
+        proconio::source::line::LineSource::new(std::io::BufReader::new(std::io::stdin()));
+    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
     input! {
         n: usize,
-        aa: [usize; n],
+        uuvv: [(Usize1, Usize1); n - 1],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    for (u, v) in uuvv.iter().copied() {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    // https://atcoder.jp/contests/abc398/editorial/12483
+    // 木を二部グラフに分ける
+    let mut cc = vec![!0; n];
+    let mut stack = vec![(0, 0)];
+    while let Some((u, c)) = stack.pop() {
+        cc[u] = c;
+        for &v in g[u].iter() {
+            if cc[v] == !0 {
+                stack.push((v, 1 ^ c));
+            }
+        }
+    }
+    // eprintln!("{cc:?}");
+    let (aa, bb) = (0..n).partition::<Vec<_>, _>(|&i| cc[i] == 0);
+    // eprintln!("{aa:?}\n{bb:?}");
+    // 奇閉路候補
+    let mut bts = BTreeSet::new();
+    for (a, b) in iproduct!(aa, bb) {
+        bts.insert((a.min(b), b.max(a)));
+    }
+    for (u, v) in uuvv.iter().copied() {
+        bts.remove(&(u, v));
+    }
+    if bts.len() % 2 == 1 {
+        println!("First");
+        let (a, b) = bts.pop_first().unwrap();
+        println!("{} {}", a + 1, b + 1);
+    } else {
+        println!("Second");
+    }
+    loop {
+        input! {
+            i: isize,
+            j: isize,
+        };
+        if i == -1 && j == -1 {
+            return;
+        }
+        let i = i as usize - 1;
+        let j = j as usize - 1;
+        bts.remove(&(i, j));
+        let (a, b) = bts.pop_first().unwrap();
+        println!("{} {}", a + 1, b + 1);
+    }
 }
