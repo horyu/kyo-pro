5a3aedee4dd3f4407d77f355ed49612a0fe53907
horyu <28817604+horyu@users.noreply.github.com>
2025-05-02 22:50:03 +0900

    abc398 f

--- atcoder2/abc398/src/bin/f.rs
+++ atcoder2/abc398/src/bin/f.rs
@@ -11,8 +11,39 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        s: Chars,
     };
-    // println!("{rs}");
+    // https://snuke.hatenablog.com/entry/2014/12/02/235837
+    // 回文 Manacher
+    let s = itertools::Itertools::intersperse(s.into_iter(), '#').collect_vec();
+    let n = s.len();
+    let mut rr = vec![0; n];
+    let mut i = 0;
+    let mut j = 0;
+    while i < n {
+        while j <= i && i + j < n && s[i - j] == s[i + j] {
+            j += 1;
+        }
+        rr[i] = j;
+        let mut k = 1;
+        while k <= i && k + rr[i - k] < j {
+            rr[i + k] = rr[i - k];
+            k += 1;
+        }
+        i += k;
+        j -= k;
+    }
+    // eprintln!("{rr:?}");
+    for c in (n / 2)..n {
+        let r = rr[c];
+        if c + r < n {
+            continue;
+        }
+        // println!("{c} {r}");
+        let rs = chain!(s.iter(), s[..(c + 1 - r)].iter().rev())
+            .filter(|&&c| c != '#')
+            .join("");
+        println!("{rs}");
+        return;
+    }
 }
