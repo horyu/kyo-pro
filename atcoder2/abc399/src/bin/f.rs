a97be8ecc68f26c012d8078e794ebeb1c46cf876
horyu <28817604+horyu@users.noreply.github.com>
2025-05-09 23:04:19 +0900

    abc399 f

--- atcoder2/abc399/src/bin/f.rs
+++ atcoder2/abc399/src/bin/f.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -9,10 +10,27 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+/*
+愚直
+let mut rs = ModInt998244353::default();
+for l in 0..n {
+    for r in l..n {
+        let mut tmp = ModInt998244353::default();
+        for i in l..=r {
+            tmp += aa[i];
+        }
+        rs += tmp.pow(k);
+    }
+}
+println!("{rs}");
+*/
+
 fn main() {
     input! {
         n: usize,
+        k: usize,
         aa: [usize; n],
     };
+    // let mut rs = ModInt998244353::default();
     // println!("{rs}");
 }
