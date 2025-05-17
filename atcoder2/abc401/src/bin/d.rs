2cd3e54eeeeaf97d26b4e08dd562d3414ea1fdae
horyu <28817604+horyu@users.noreply.github.com>
2025-05-17 20:22:40 +0900

    abc401 d

--- atcoder2/abc401/src/bin/d.rs
+++ atcoder2/abc401/src/bin/d.rs
@@ -12,7 +12,44 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: usize,
+        mut s: Chars,
     };
-    // println!("{rs}");
+    let mut kk = 0;
+    for i in 0..n {
+        if s[i] == 'o' {
+            kk += 1;
+            if let Some(c) = s.get_mut(i.wrapping_sub(1)) {
+                *c = '.';
+            }
+            if let Some(c) = s.get_mut(i + 1) {
+                *c = '.';
+            }
+        }
+    }
+    let iii = (0..n)
+        .group_by(|i| s[*i] == '?')
+        .into_iter()
+        .filter_map(|(tf, g)| tf.then(|| g.collect_vec()))
+        .collect_vec();
+    let max = iii.iter().fold(0, |acc, ii| acc + ii.len().div_ceil(2));
+    if k == kk {
+        for ii in iii {
+            for i in ii {
+                s[i] = '.';
+            }
+        }
+    } else if max == k - kk {
+        for ii in iii {
+            if ii.len() % 2 == 0 {
+                continue;
+            }
+            let i0 = ii[0];
+            for i in ii {
+                s[i] = ['o', '.'][(i - i0) % 2];
+            }
+        }
+    };
+    let rs = s.iter().join("");
+    println!("{rs}");
 }
