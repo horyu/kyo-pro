088ca3dfc94082e78f1c8c3c8c3888ff27a756f9
horyu <28817604+horyu@users.noreply.github.com>
2025-05-18 23:41:43 +0900

    abc401 e

--- atcoder2/abc401/src/bin/e.rs
+++ atcoder2/abc401/src/bin/e.rs
@@ -12,7 +12,26 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvv: [(Usize1, Usize1); m],
     };
-    // println!("{rs}");
+    let mut g = vec![BTreeSet::new(); n];
+    let mut cc = vec![0; n];
+    for (u, v) in uuvv.iter().copied() {
+        // g[u].insert(v);
+        g[v].insert(u);
+        cc[u] += 1;
+        cc[v] += 1;
+    }
+    // 0:                      g[0][1..]
+    // 1: g[1][..1]が存在する ? g[0][2..] & g[1][2..] : -1
+    // 2: g[2][..2]が存在する ? g[..=2][3..] : -1
+    let mut joined = BTreeSet::new();
+    for k in 0..n {
+        if g[k].range(..k).next().is_none() {
+
+        }
+
+        // println!("{rs}");
+    }
 }
