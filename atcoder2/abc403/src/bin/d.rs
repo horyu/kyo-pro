f2f90a7c7de49df1875083d1aa5b5371deab8364
horyu <28817604+horyu@users.noreply.github.com>
2025-05-29 21:49:30 +0900

    abc403 d

--- atcoder2/abc403/src/bin/d.rs
+++ atcoder2/abc403/src/bin/d.rs
@@ -12,7 +12,41 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
+        d: usize,
         aa: [usize; n],
     };
-    // println!("{rs}");
+    if d == 0 {
+        let rs = aa
+            .into_iter()
+            .counts()
+            .into_values()
+            .map(|c| c - 1)
+            .sum::<usize>();
+        println!("{rs}");
+        return;
+    }
+    let mut dsu = ac_library::Dsu::new(n);
+    let mut hm = HashMap::new();
+    for (i, a) in aa.iter().copied().enumerate() {
+        for k in [a.wrapping_sub(d), a, a + d] {
+            if let Some(&j) = hm.get(&k) {
+                dsu.merge(i, j);
+            }
+        }
+        hm.insert(a, i);
+    }
+    let mut rs = 0;
+    for ii in dsu.groups() {
+        // (d=1) 1 1 2 3 4 4 のときWA
+        let bb = ii.iter().copied().map(|i| aa[i]).collect_vec();
+        let min = bb.iter().copied().min().unwrap();
+        // [min + 2n*d, min + (2n+1)*d]
+        let mut cc = [0; 2];
+        for b in bb {
+            let k = (b - min) / d;
+            cc[k % 2] += 1;
+        }
+        rs += cc[0].min(cc[1]);
+    }
+    println!("{rs}");
 }
