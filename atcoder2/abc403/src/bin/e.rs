da7cb52e57f84e221c431be1f959deb18bcdc4f2
horyu <28817604+horyu@users.noreply.github.com>
2025-05-31 22:59:48 +0900

    abc403 e

--- atcoder2/abc403/src/bin/e.rs
+++ atcoder2/abc403/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -11,8 +12,107 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        q: usize,
+        ttss: [(usize, Chars); q],
     };
-    // println!("{rs}");
+    let mut xx = Trie::<26, 'a'>::new();
+    let mut yy: Vec<Vec<char>> = vec![];
+    for (t, s) in ttss {
+        if t == 1 {
+            xx.insert_autoid(&s);
+            yy.retain(|y| !xx.start_with(&y));
+        } else {
+            if !xx.start_with(&s) {
+                yy.push(s);
+            }
+        }
+        let rs = yy.len();
+        println!("{rs}");
+        if q < 5 {
+            eprintln!("{yy:?}");
+        }
+    }
+}
+
+// https://algo-logic.info/trie-tree/
+#[allow(unused)]
+#[derive(Clone, Debug)]
+struct Node<const CHAR_SIZE: usize> {
+    next: Vec<Option<usize>>,
+    accept: Vec<usize>,
+    c: usize,
+    common: usize,
+}
+
+impl<const CHAR_SIZE: usize> Node<CHAR_SIZE> {
+    fn new(c: usize) -> Self {
+        Self {
+            next: vec![None; CHAR_SIZE],
+            accept: vec![],
+            c,
+            common: 0,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Trie<const CHAR_SIZE: usize, const BASE: char> {
+    nodes: Vec<Node<CHAR_SIZE>>,
+}
+
+#[allow(unused)]
+impl<const CHAR_SIZE: usize, const BASE: char> Trie<CHAR_SIZE, BASE> {
+    fn new() -> Self {
+        let nodes = vec![Node::new(0)];
+        Self { nodes }
+    }
+
+    fn insert(&mut self, word: &[char], word_id: usize) {
+        let mut node_id = 0;
+        for &ch in word {
+            let c = (ch as u8 - BASE as u8) as usize;
+            if self.nodes[node_id].next[c].is_none() {
+                let new_id = self.nodes.len();
+                self.nodes[node_id].next[c] = Some(new_id);
+                self.nodes.push(Node::new(c));
+            }
+            self.nodes[node_id].common += 1;
+            node_id = self.nodes[node_id].next[c].unwrap();
+        }
+        self.nodes[node_id].common += 1;
+        self.nodes[node_id].accept.push(word_id);
+    }
+
+    fn insert_autoid(&mut self, word: &[char]) {
+        let word_id = self.nodes[0].common;
+        self.insert(word, word_id);
+    }
+
+    fn search(&self, word: &[char], prefix: bool) -> bool {
+        let mut node_id = 0;
+        for &ch in word {
+            let c = (ch as u8 - BASE as u8) as usize;
+            match self.nodes[node_id].next[c] {
+                Some(next_id) => node_id = next_id,
+                None => return false,
+            }
+        }
+        if prefix {
+            true
+        } else {
+            !self.nodes[node_id].accept.is_empty()
+        }
+    }
+
+    fn start_with(&self, prefix: &[char]) -> bool {
+        self.search(prefix, true)
+    }
+
+    fn count(&self) -> usize {
+        self.nodes[0].common
+    }
+
+    fn size(&self) -> usize {
+        self.nodes.len()
+    }
 }
