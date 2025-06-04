ef55c1ae90f5c6a33885ce86c36280a6d25254c5
horyu <28817604+horyu@users.noreply.github.com>
2025-06-05 08:39:58 +0900

    abc404 d

--- atcoder2/abc404/src/bin/d.rs
+++ atcoder2/abc404/src/bin/d.rs
@@ -12,7 +12,47 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        cc: [usize; n],
     };
-    // println!("{rs}");
+    let mut i2jj = vec![vec![]; n];
+    for j in 0..m {
+        input! {k: usize, ii: [Usize1; k]};
+        for i in ii {
+            i2jj[i].push(j);
+        }
+    }
+    let mut rs = !0;
+    for bits1 in 0..(1usize << n) {
+        let mut xx = vec![0; m];
+        let mut c1 = 0;
+        for i in 0..n {
+            if bits1 & (1 << i) != 0 {
+                c1 += cc[i];
+                for j in i2jj[i].iter().copied() {
+                    xx[j] += 1;
+                }
+            }
+        }
+        if xx.iter().all(|&x| 2 <= x) {
+            rs = rs.min(c1);
+            continue;
+        }
+        for bits2 in bits1..(1usize << n) {
+            let mut c2 = c1;
+            let mut yy = xx.clone();
+            for i in 0..n {
+                if bits2 & (1 << i) != 0 {
+                    c2 += cc[i];
+                    for j in i2jj[i].iter().copied() {
+                        yy[j] += 1;
+                    }
+                }
+            }
+            if yy.iter().all(|&y| 2 <= y) {
+                rs = rs.min(c2);
+            }
+        }
+    }
+    println!("{rs}");
 }
