0578983205a9b71788e08e34b5c0d626fd33f6d6
horyu <28817604+horyu@users.noreply.github.com>
2025-06-10 21:44:50 +0900

    abc405 e

--- atcoder2/abc405/src/bin/e.rs
+++ atcoder2/abc405/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -11,8 +12,18 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
     };
+    // A < C
+    // A < D
+    // B < D
+
+    // A < D を事前に配置する
+    // Dの前後 D+1 個のスポットにCを C 個配置する
+    // Dの前のスポット（Dの前に置かれたCの数だけスポットが増える）にBを配置する
+    // let mut rs = ModInt998244353::default();
     // println!("{rs}");
 }
