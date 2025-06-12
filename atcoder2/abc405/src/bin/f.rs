75511b6565e9a65d31098847eb7eb42c3b9d80c8
horyu <28817604+horyu@users.noreply.github.com>
2025-06-12 23:14:49 +0900

    abc405 f

--- atcoder2/abc405/src/bin/f.rs
+++ atcoder2/abc405/src/bin/f.rs
@@ -12,7 +12,33 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aabb: [(Usize1, Usize1); m],
+        q: usize,
+        ccdd: [(Usize1, Usize1); q],
     };
-    // println!("{rs}");
+    let n4 = n * 4;
+    let mut ft1 = ac_library::FenwickTree::new(n4, 0isize);
+    let mut ft2 = ac_library::FenwickTree::new(n4, 0isize);
+    for (a, b) in aabb.iter().copied() {
+        ft1.add(a * 2, 1);
+        ft1.add(b * 2, -1);
+        let aa = (a * 2 + n) % n4;
+        let bb = (b * 2 + n) % n4;
+        let min = aa.min(bb);
+        let max = aa.max(bb);
+        ft2.add(min, 1);
+        ft2.add(max, -1);
+    }
+    for (c, d) in ccdd.iter().copied() {
+        let c = c * 2;
+        let d = d * 2;
+        let mut rs = ft1.sum(c..d).abs();
+        let cc = (c + n) % n4;
+        let dd = (d + n) % n4;
+        let min = cc.min(dd);
+        let max = cc.max(dd);
+        rs = rs.max(ft2.sum(min..max).abs());
+        println!("{rs}");
+    }
 }
