95ea1c9498af6c7081000f86c21b05f6cd270640
horyu <28817604+horyu@users.noreply.github.com>
2025-06-15 08:30:44 +0900

    abc406 ab

---
+++ atcoder2/abc406/src/bin/b.rs
@@ -0,0 +1,26 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        k: u32,
+        aa: [usize; n],
+    };
+    let mut rs = 1usize;
+    for a in aa {
+        rs = rs.saturating_mul(a);
+        if k <= rs.ilog10() {
+            rs = 1;
+        }
+    }
+    println!("{rs}");
+}
