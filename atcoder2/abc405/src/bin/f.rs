33f1004f8f40978de1dce312573a87479fd03d2e
horyu <28817604+horyu@users.noreply.github.com>
2025-06-14 09:04:42 +0900

    abc405 f

--- atcoder2/abc405/src/bin/f.rs
+++ atcoder2/abc405/src/bin/f.rs
@@ -17,23 +17,35 @@ fn main() {
         q: usize,
         ccdd: [(Usize1, Usize1); q],
     };
+    // https://atcoder.jp/contests/abc405/editorial/13011
     let n2 = n * 2;
     let mut ft = ac_library::FenwickTree::new(n2 * 2, 0isize);
-    for (a, b) in aabb.iter().copied() {
-        if b - a < n {
-            ft.add(a, 1);
-            ft.add(b, -1);
-        } else {
-            ft.add(b, 1);
-            ft.add(a + n2, -1);
+
+    let mut pp = vec![!0; n2];
+    for (i, (a, b)) in aabb.iter().copied().enumerate() {
+        pp[a] = i;
+        pp[b] = i;
+    }
+    let mut gg = vec![vec![]; n2];
+    for (j, (c, d)) in ccdd.iter().copied().enumerate() {
+        gg[d].push(j);
+    }
+    let mut rrss = vec![0; q];
+    for i in 0..n2 {
+        if pp[i] != !0 {
+            ft.add(i, 1);
+            if let Some((a, b)) = aabb.get(pp[i]).copied() {
+                if b == i {
+                    let tmp = ft.sum(a..=a);
+                    ft.add(a, -(tmp + 1));
+                }
+            }
+        }
+        for j in gg[i].iter().copied() {
+            rrss[j] = ft.sum(ccdd[j].0..i);
         }
     }
-    for (c, d) in ccdd.iter().copied() {
-        let rs = if d - c < n {
-            ft.sum(c..d).abs()
-        } else {
-            ft.sum(d..(c + n2)).abs()
-        };
+    for rs in rrss {
         println!("{rs}");
     }
 }
