7600d87af1b405c7650100e6a0f411f469ca4265
horyu <28817604+horyu@users.noreply.github.com>
2025-06-16 23:10:10 +0900

    abc406 c

--- atcoder2/abc406/src/bin/c.rs
+++ atcoder2/abc406/src/bin/c.rs
@@ -12,7 +12,43 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        pp: [usize; n],
     };
-    // println!("{rs}");
+    // p[i-i] < p[i] > p[i+1] を満たすi
+    let mut xx = BTreeSet::new();
+    // p[i-i] > p[i] < p[i+1] を満たすi
+    let mut yy = BTreeSet::new();
+    for (i, (p1, p2, p3)) in pp.iter().copied().tuple_windows().enumerate() {
+        if p1 < p2 && p2 > p3 {
+            xx.insert(i);
+        } else if p1 > p2 && p2 < p3 {
+            yy.insert(i);
+        }
+    }
+    eprintln!("xx:{xx:?}\nyy:{yy:?}");
+    let mut rs = 0;
+    for (i, (p1, p2)) in pp.iter().copied().tuple_windows().enumerate() {
+        if p2 <= p1 {
+            continue;
+        }
+        let xxjj = xx.range(i..).copied().take(2).collect_vec();
+        let yyjj = yy.range(i..).copied().take(2).collect_vec();
+        let jj = chain!(&xxjj, &yyjj)
+            .copied()
+            .chain(std::iter::once(n - 3))
+            .sorted_unstable()
+            .collect_vec();
+        if !xxjj.is_empty()
+            && !yyjj.is_empty()
+            && (jj[0] == xxjj[0] || jj[0] == yyjj[0])
+            && (jj[1] == xxjj[0] || jj[1] == yyjj[0])
+        {
+            rs += jj[2] - jj[1] + 1;
+            eprintln!("{i} {jj:?} {xxjj:?} {yyjj:?} +{}", jj[2] - jj[1] + 1);
+            for k in jj[1]..=jj[2] {
+                eprintln!("{i}-{k}: {}", pp[i..=k].iter().join(","));
+            }
+        }
+    }
+    println!("{rs}");
 }
