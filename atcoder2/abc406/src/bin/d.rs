72bd717d322fe04aa0eb2befe1f77ec4bce1c290
horyu <28817604+horyu@users.noreply.github.com>
2025-06-18 08:35:40 +0900

    abc406 d

--- atcoder2/abc406/src/bin/d.rs
+++ atcoder2/abc406/src/bin/d.rs
@@ -11,8 +11,48 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
         n: usize,
-        aa: [usize; n],
+        xxyy: [(usize, usize); n],
+        q: usize,
+        ttvv: [(usize, usize); q],
     };
-    // println!("{rs}");
+    let mut i2jj: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut j2ii: HashMap<usize, HashSet<usize>> = HashMap::new();
+    for (x, y) in xxyy {
+        i2jj.entry(x).or_insert(HashSet::default()).insert(y);
+        j2ii.entry(y).or_insert(HashSet::default()).insert(x);
+    }
+    for (t, v) in ttvv {
+        let mut rs = 0;
+        if t == 1 {
+            // 行削除
+            if let Some(jj) = i2jj.remove(&v) {
+                rs = jj.len();
+                for j in jj {
+                    if let Some(ii) = j2ii.get_mut(&j) {
+                        ii.remove(&v);
+                        if ii.is_empty() {
+                            j2ii.remove(&j);
+                        }
+                    }
+                }
+            }
+        } else {
+            // 列削除
+            if let Some(ii) = j2ii.remove(&v) {
+                rs = ii.len();
+                for i in ii {
+                    if let Some(jj) = i2jj.get_mut(&i) {
+                        jj.remove(&v);
+                        if jj.is_empty() {
+                            i2jj.remove(&i);
+                        }
+                    }
+                }
+            }
+        }
+        println!("{rs}");
+    }
 }
