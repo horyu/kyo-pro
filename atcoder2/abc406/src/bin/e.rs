7bb1218a322da73e8dee7a6eb3032fc48a0f10c4
horyu <28817604+horyu@users.noreply.github.com>
2025-06-19 23:11:40 +0900

    abc406 e

--- atcoder2/abc406/src/bin/e.rs
+++ atcoder2/abc406/src/bin/e.rs
@@ -11,8 +11,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
+        nnkk: [(usize, usize); t],
     };
+    for (n, k) in nnkk {
+        let size = n.ilog2() as usize + 1;
+        // dp[i][b][j] = i桁目までオンビットの数がj個となる数
+        let mut dp = vec![vec![vec![0; k + 1]; 2]; size];
+        dp[0][0][0] = 1; // 0 bits,
+        for i in 0..size - 1 {
+            for b in 0..2 {
+                for j in 0..=k {
+                    if dp[i][b][j] == 0 {
+                        continue;
+                    }
+                    // i桁目を0にする
+                    dp[i + 1][b][j] += dp[i][b][j];
+                    // i桁目を1にする
+                    if b == 0 && j + 1 <= k {
+                        dp[i + 1][1][j + 1] += dp[i][b][j];
+                    }
+                }
+            }
+        }
+    }
     // println!("{rs}");
 }
