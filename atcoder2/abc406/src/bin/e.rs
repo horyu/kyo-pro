7adea42cbbb226e2f56a12dec08fcf654973c398
horyu <28817604+horyu@users.noreply.github.com>
2025-06-20 23:23:52 +0900

    abc406 e

--- atcoder2/abc406/src/bin/e.rs
+++ atcoder2/abc406/src/bin/e.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
 #![allow(unused_imports, unused_variables)]
 #![feature(int_roundings)]
+use ac_library::ModInt998244353;
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
@@ -14,26 +15,29 @@ fn main() {
         t: usize,
         nnkk: [(usize, usize); t],
     };
+    // https://atcoder.jp/contests/abc406/editorial/13071
     for (n, k) in nnkk {
-        let size = n.ilog2() as usize + 1;
-        // dp[i][b][j] = i桁目までオンビットの数がj個となる数
-        let mut dp = vec![vec![vec![0; k + 1]; 2]; size];
-        dp[0][0][0] = 1; // 0 bits,
-        for i in 0..size - 1 {
-            for b in 0..2 {
-                for j in 0..=k {
-                    if dp[i][b][j] == 0 {
-                        continue;
-                    }
-                    // i桁目を0にする
-                    dp[i + 1][b][j] += dp[i][b][j];
-                    // i桁目を1にする
-                    if b == 0 && j + 1 <= k {
-                        dp[i + 1][1][j + 1] += dp[i][b][j];
-                    }
+        let l = n.ilog2() as usize + 1; // n のビット長
+        // i ビット目までを埋めた時点で N より小さいことが確定している非負整数であって，
+        // 立っているビットが上から i ビット目までであり，
+        // dp1​(i,j)：立っているビット数が j であるものの個数
+        // dp2​(i,j)：立っているビット数が j であるものの総和
+        let mut dp1 = vec![vec![ModInt998244353::default(); k + 1]; l];
+        let mut dp2 = dp1.clone();
+        dp1[0][0] += 1;
+
+        // N未満が確定している場合の遷移
+        for i in 0..l {
+            for j in 0..=k {
+                dp1[i + 1][j] = dp1[i + 1][j] + dp1[i][j];
+                dp2[i + 1][j] = dp2[i + 1][j] + dp2[i][j];
+                if j + 1 <= k {
+                    dp1[i + 1][j + 1] = dp1[i + 1][j + 1] + dp1[i][j];
+                    dp2[i + 1][j + 1] = dp2[i + 1][j + 1] + dp1[i][j] * (2usize << (k - 1 - i));
                 }
             }
         }
+        // TODO
     }
     // println!("{rs}");
 }
