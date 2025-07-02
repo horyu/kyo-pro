01688a8988229bf138d4436d59d4f8eef6b4c311
horyu <28817604+horyu@users.noreply.github.com>
2025-07-02 20:55:37 +0900

    abc408 d

--- atcoder2/abc408/src/bin/d.rs
+++ atcoder2/abc408/src/bin/d.rs
@@ -11,8 +11,67 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
     };
-    // println!("{rs}");
+    for _ in 0..t {
+        input! {
+            n: usize,
+            s: Chars,
+        };
+        // vv := 1の個数 → 0の個数 → 1の個数 → ...
+        let mut vv = vec![];
+        for (c, g) in s.iter().copied().group_by(|&c| c).into_iter() {
+            if vv.is_empty() && c == '0' {
+                continue;
+            }
+            vv.push(g.count());
+        }
+        if s[n - 1] == '0' {
+            vv.pop();
+        }
+        let m = vv.len();
+        if m <= 1 {
+            println!("0");
+            continue;
+        }
+        // ll[i][j] := vv[..i] において右端が j (0 or 1) の時の最小操作数
+        let mut ll = vec![[0, 1 << 60]; m + 1];
+        for (i, v) in vv.iter().copied().enumerate() {
+            if i % 2 == 0 {
+                // 1
+                ll[i + 1][0] = ll[i][0] + v;
+                ll[i + 1][1] = ll[i][0].min(ll[i][1]);
+            } else {
+                // 0
+                ll[i + 1][0] = ll[i][0];
+                ll[i + 1][1] = (ll[i][0] + v).min(ll[i][1] + v);
+            }
+        }
+
+        // rr[i][j] := vv[i..] において左端が j (0 or 1) の時の最小操作数
+        let mut rr = vec![[0; 2]; m + 1];
+        rr[m][1] = 1 << 60;
+        for (i, v) in vv.iter().copied().enumerate().rev() {
+            if i % 2 == 0 {
+                // 1
+                rr[i][0] = rr[i + 1][0] + v;
+                rr[i][1] = rr[i + 1][0].min(rr[i + 1][1]);
+            } else {
+                // 0
+                rr[i][0] = rr[i + 1][0];
+                rr[i][1] = (rr[i + 1][0] + v).min(rr[i + 1][1] + v);
+            }
+        }
+        let mut rs = usize::MAX;
+        for i in 0..=m {
+            // vv[..i] = 0, vv[i..] = 1
+            rs = rs.min(ll[i][0] + rr[i][1]);
+            // vv[..i] = 1, vv[i..] = 0
+            rs = rs.min(ll[i][1] + rr[i][0]);
+        }
+        // println!("{vv:?}");
+        // println!("{ll:?}");
+        // println!("{rr:?}");
+        println!("{rs}");
+    }
 }
