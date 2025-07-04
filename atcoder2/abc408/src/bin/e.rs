92ffd56c497c872b63f52deb2430a855cef0259d
horyu <28817604+horyu@users.noreply.github.com>
2025-07-04 21:11:12 +0900

    abc408 e

--- atcoder2/abc408/src/bin/e.rs
+++ atcoder2/abc408/src/bin/e.rs
@@ -4,6 +4,7 @@
 use itertools::{chain, iproduct, izip, Itertools as _};
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
+use pathfinding::prelude::dijkstra;
 use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
@@ -12,7 +13,33 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        uuvvww: [(Usize1, Usize1, usize); m],
     };
-    // println!("{rs}");
+    let mut g = vec![btreemultimap::BTreeMultiMap::new(); n];
+    let mut rs = 0;
+    for (u, v, w) in uuvvww {
+        g[u].insert(w, v);
+        g[v].insert(w, u);
+        rs |= w;
+    }
+    for d in (0..30).rev() {
+        if rs & (1 << d) == 0 {
+            continue;
+        }
+        let tmp = rs ^ (1 << d);
+        if pathfinding::prelude::dfs(
+            0,
+            |&i| {
+                g[i].range(..=tmp)
+                    .filter_map(|(&k, &v)| (tmp & k == k).then_some(v))
+            },
+            |&i| i == n - 1,
+        )
+        .is_some()
+        {
+            rs = tmp;
+        }
+    }
+    println!("{rs}");
 }
