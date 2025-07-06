104bbe2e0d4c90fa2fc1c2617af2e5de50a54ff5
horyu <28817604+horyu@users.noreply.github.com>
2025-07-06 22:02:46 +0900

    abc408 f

--- atcoder2/abc408/src/bin/f.rs
+++ atcoder2/abc408/src/bin/f.rs
@@ -9,17 +9,53 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+use ac_library::{LazySegtree, MapMonoid, Max};
+struct MaxMonoid;
+impl MapMonoid for MaxMonoid {
+    type M = Max<usize>;
+    type F = usize;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(&f: &usize, &x: &usize) -> usize {
+        f + x
+    }
+
+    fn composition(&f: &usize, &g: &usize) -> usize {
+        f + g
+    }
+}
+
 fn main() {
     input! {
         n: usize,
         d: usize,
         r: usize,
-        hh: [usize; n],
+        i2h: [usize; n],
     };
-    let mut mm = btreemultimap::BTreeMultiMap::new();
-    for (i, h) in hh.iter().copied().enumerate() {
-        mm.insert(h, i);
+    let mut h2i = vec![0; n + 1];
+    for (i, h) in i2h.iter().copied().enumerate() {
+        h2i[h] = i;
     }
-    // 小さいhから遅延セグ木を更新する
-    // println!("{rs}");
+    let mut ls = LazySegtree::<MaxMonoid>::new(n);
+    let mut rs = 0;
+    let mut h2v = vec![0; n + 1];
+    for (h, i) in h2i.iter().copied().enumerate().skip(1) {
+        // eprintln!("[{h:2}]{i:2}: {}", (0..n).map(|i| ls.get(i)).join(" "));
+        if d < h {
+            let j = h2i[h - d];
+            let v = h2v[h - d];
+            ls.set(j, v);
+        }
+        let max = ls.prod(i.saturating_sub(r)..=(n - 1).min(i + r));
+        // eprintln!("{:?} {max}", i.saturating_sub(r)..=(n - 1).min(i + r));
+        let v = max + 1;
+        rs = rs.max(v);
+        h2v[h] = v;
+    }
+    // eprintln!("  : {}", (0..n).map(|i| ls.get(i)).join(" "));
+    rs -= 1;
+    println!("{rs}");
 }
