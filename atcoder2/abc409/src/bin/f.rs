ed23f00c73ba68627df3f2ff26752ce421709f4c
horyu <28817604+horyu@users.noreply.github.com>
2025-07-13 20:04:51 +0900

    abc409 f

--- atcoder2/abc409/src/bin/f.rs
+++ atcoder2/abc409/src/bin/f.rs
@@ -17,39 +17,43 @@ fn main() {
     };
     let mut dsu = ac_library::Dsu::new(n + q);
     let mut xxyy = xxyy.into_iter().map(|(x, y)| (x + y, x - y)).collect_vec();
-    let mut mm = btreemultimap::BTreeMultiMap::new();
+    let mut bh = BinaryHeap::new();
+    let mut mm = multimap::MultiMap::new();
     let d = |a: (isize, isize), b: (isize, isize)| (a.0.abs_diff(b.0)).max(a.1.abs_diff(b.1));
     for (i, ixy) in xxyy.iter().copied().enumerate() {
         for (j, jxy) in xxyy.iter().copied().enumerate().skip(i + 1) {
-            mm.insert(d(ixy, jxy), (i, j));
+            let d = d(ixy, jxy);
+            if !mm.contains_key(&d) {
+                bh.push(R(d));
+            }
+            mm.insert(d, (i, j));
         }
     }
+    let mut rs = String::new();
     for _ in 0..q {
         input! {t: usize};
         match t {
             1 => {
                 input! {a: isize, b: isize};
                 let xy = (a + b, a - b);
-                let mut tmp = multimap::MultiMap::new();
+                let mut hm = HashMap::<usize, usize>::new();
                 for (i, ixy) in xxyy.iter().copied().enumerate() {
                     let l = dsu.leader(i);
-                    tmp.insert(l, d(xy, ixy));
+                    let d = d(xy, ixy);
+                    hm.entry(l).and_modify(|e| *e = (*e).min(d)).or_insert(d);
                 }
-                for (l, dd) in tmp {
-                    let min = dd.into_iter().min().unwrap();
-                    mm.insert(min, (l, n));
+                for (l, d) in hm {
+                    if !mm.contains_key(&d) {
+                        bh.push(R(d));
+                    }
+                    mm.insert(d, (l, n));
                 }
                 xxyy.push(xy);
                 n += 1;
             }
             2 => {
                 let mut merged = false;
-                while let Some((k, iijj)) = mm
-                    .keys()
-                    .next()
-                    .copied()
-                    .map(|k| (k, mm.remove(&k).unwrap()))
-                {
+                while let Some((k, iijj)) = bh.pop().map(|R(k)| (k, mm.remove(&k).unwrap())) {
                     for (i, j) in iijj {
                         if dsu.same(i, j) {
                             continue;
@@ -58,20 +62,22 @@ fn main() {
                         merged = true;
                     }
                     if merged {
-                        println!("{k}");
+                        rs.push_str(k.to_string().as_str());
+                        rs.push('\n');
                         break;
                     }
                 }
                 if !merged {
-                    println!("-1");
+                    rs.push_str("-1\n");
                 }
             }
             _ => {
                 input! {u: Usize1, v: Usize1};
                 let tf = dsu.same(u, v);
-                let rs = ["No", "Yes"][tf as usize];
-                println!("{rs}");
+                let noyes = ["No\n", "Yes\n"][tf as usize];
+                rs.push_str(noyes);
             }
         }
     }
+    print!("{rs}");
 }
