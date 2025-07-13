fcd818527d7b604bc9d5e22fb59190bd568201c5
horyu <28817604+horyu@users.noreply.github.com>
2025-07-14 08:43:34 +0900

    abc410 ab

---
+++ atcoder2/abc410/src/bin/b.rs
@@ -0,0 +1,32 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables)]
+#![feature(int_roundings)]
+use itertools::{chain, iproduct, izip, Itertools as _};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        xx: [usize; q],
+    };
+    let mut bb = vec![0; n];
+    let mut rs = vec![];
+    for x in xx {
+        if x == 0 {
+            let pos = bb.iter().copied().position_min().unwrap();
+            bb[pos] += 1;
+            rs.push(pos + 1);
+        } else {
+            bb[x - 1] += 1;
+            rs.push(x);
+        }
+    }
+    let rs = rs.into_iter().join(" ");
+    println!("{rs}");
+}
