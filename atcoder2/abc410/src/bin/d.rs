b0266e879334b8d5d443843971952542cc3068a6
horyu <28817604+horyu@users.noreply.github.com>
2025-07-16 08:59:08 +0900

    abc410 d

--- atcoder2/abc410/src/bin/d.rs
+++ atcoder2/abc410/src/bin/d.rs
@@ -12,7 +12,29 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        aabbww: [(Usize1, Usize1, usize); m],
     };
-    // println!("{rs}");
+    let mut g = vec![vec![]; n];
+    for (a, b, w) in aabbww.iter().copied() {
+        g[a].push((b, w));
+    }
+    let mut pushed = vec![vec![false; 2usize << 10]; n];
+    let mut qq = VecDeque::new();
+    qq.push_back((0, 0));
+    pushed[0][0] = true;
+    while let Some((qi, qw)) = qq.pop_front() {
+        for (i, w) in g[qi].iter().copied() {
+            let ww = qw ^ w;
+            if !pushed[i][ww] {
+                pushed[i][ww] = true;
+                qq.push_back((i, ww));
+            }
+        }
+    }
+    if let Some(rs) = pushed[n - 1].iter().position(|&tf| tf) {
+        println!("{rs}");
+    } else {
+        println!("-1");
+    }
 }
