790b674f0264a052d2e3d75fad99368f17dd57ac
horyu <28817604+horyu@users.noreply.github.com>
2025-07-24 22:52:16 +0900

    abc411 e

--- atcoder2/abc411/src/bin/e.rs
+++ atcoder2/abc411/src/bin/e.rs
@@ -9,10 +9,35 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+/*
+3面ダイス3つで考える
+a:1  45
+b: 2 4  7
+c:  3  67
+P(X) = ダイスの最大値がXとなる組み合わせ
+P(1) = 0; P(2) = 0; P(3) = 1;
+P(4) = 1+2 = 3; P(5) = 2;
+P(6) = 3*2 = 6; P(7) = 3*2+3*3 = 15;
+*/
+
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        aaa: [[usize; 6]; n],
     };
+    let a2b = aaa
+        .iter()
+        .flatten()
+        .copied()
+        .sorted_unstable()
+        .dedup()
+        .enumerate()
+        .map(|(a, b)| (b, a))
+        .collect::<HashMap<_, _>>();
+    let bbb = aaa
+        .iter()
+        .map(|a| a.iter().map(|&b| a2b[&b]).collect_vec())
+        .collect_vec();
+
     // println!("{rs}");
 }
