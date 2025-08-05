7a67f7d172782a51c59ce9b4c95d6e38650b9bdc
horyu <28817604+horyu@users.noreply.github.com>
2025-08-05 09:29:43 +0900

    abc413 e

--- atcoder2/abc413/src/bin/e.rs
+++ atcoder2/abc413/src/bin/e.rs
@@ -16,8 +16,37 @@ fn main() {
     for _ in 0..t {
         input! {
             n: usize,
-            pp: [usize; n],
+            pp: [usize; 1 << n],
         };
-        // TODO
+        let len = pp.len();
+        use ac_library::{Min, Segtree};
+        // 再帰的に中心で分割→要素で見て辞書順早い方を含む方を左に配置
+        let st: Segtree<Min<usize>> = Segtree::<Min<usize>>::from_iter(pp.iter().copied());
+        let mut rrss = Vec::with_capacity(len);
+        fn f(
+            l: usize,
+            r: usize,
+            st: &Segtree<Min<usize>>,
+            pp: &[usize],
+            rrss: &mut Vec<usize>,
+        ) {
+            if l == r {
+                rrss.push(pp[l]);
+                return;
+            }
+            let m = (l + r).div_ceil(2);
+            let left = st.prod(l..m);
+            let right = st.prod(m..=r);
+            if left < right {
+                f(l, m - 1, st, pp, rrss);
+                f(m, r, st, pp, rrss);
+            } else {
+                f(m, r, st, pp, rrss);
+                f(l, m - 1, st, pp, rrss);
+            }
+        }
+        f( 0, len - 1, &st, &pp, &mut rrss);
+        let rs = rrss.into_iter().join(" ");
+        println!("{rs}");
     }
 }
