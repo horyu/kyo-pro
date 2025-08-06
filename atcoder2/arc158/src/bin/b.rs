66878181ba6cadd1a0c0373e50e7bd6e80e734bf
horyu <28817604+horyu@users.noreply.github.com>
2025-08-07 08:59:30 +0900

    arc158 b

--- atcoder2/arc158/src/bin/b.rs
+++ atcoder2/arc158/src/bin/b.rs
@@ -12,7 +12,32 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        mut xx: [isize; n],
     };
-    // println!("{rs}");
+    xx.sort_unstable();
+    let (pp, nn) = xx.iter().copied().partition::<Vec<_>, _>(|&x| 0 < x);
+    // それぞれの最小3個と最大3個の合計12要素から3要素を選ぶ
+    let mut yy = vec![];
+    let plen = pp.len();
+    for i in (0..3.min(plen))
+        .chain(plen.saturating_sub(3)..plen)
+        .unique()
+    {
+        yy.push(pp[i]);
+    }
+    let nlen = nn.len();
+    for i in (0..3.min(nlen))
+        .chain(nlen.saturating_sub(3)..nlen)
+        .unique()
+    {
+        yy.push(nn[i]);
+    }
+    let mut min = f64::INFINITY;
+    let mut max = f64::NEG_INFINITY;
+    for (a, b, c) in yy.into_iter().tuple_combinations() {
+        let v = (a + b + c) as f64 / (a * b * c) as f64;
+        min = min.min(v);
+        max = max.max(v);
+    }
+    println!("{min}\n{max}");
 }
