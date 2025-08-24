2c1d89825df4d7a1b154ceccbdfc124fcc194525
horyu <28817604+horyu@users.noreply.github.com>
2025-08-24 10:04:36 +0900

    abc414 d

--- atcoder2/abc414/src/bin/d.rs
+++ atcoder2/abc414/src/bin/d.rs
@@ -12,7 +12,30 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        mut xx: [usize; n],
     };
-    // println!("{rs}");
+    xx.sort_unstable();
+    xx.dedup();
+    let n = xx.len();
+    if n <= m {
+        println!("0");
+        return;
+    }
+    // 距離が近い順にまとめる
+    let mut bts = BTreeSet::new();
+    for (i, (xl, xr)) in xx.iter().tuple_windows().enumerate() {
+        bts.insert((xr - xl, i));
+    }
+    let mut dsu = ac_library::Dsu::new(n);
+    for (_d, i) in bts.into_iter().take(n - m) {
+        dsu.merge(i, i + 1);
+    }
+    let mut rs = 0;
+    for ii in dsu.groups() {
+        let l = xx[ii[0]];
+        let r = xx[ii[ii.len() - 1]];
+        rs += r - l;
+    }
+    println!("{rs}");
 }
