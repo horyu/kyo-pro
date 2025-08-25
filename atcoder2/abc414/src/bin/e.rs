a79990184fc97a82610f1b085d32cb1d96058c6a
horyu <28817604+horyu@users.noreply.github.com>
2025-08-25 22:32:38 +0900

    abc414 e

--- atcoder2/abc414/src/bin/e.rs
+++ atcoder2/abc414/src/bin/e.rs
@@ -9,10 +9,31 @@ use proconio::{input, marker::*};
 use std::cmp::{Ordering, Reverse as R};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 
+/*
+(b,c) a
+(2,1) 3,5,7 @ 9
+(3,1) 4,7 @ 10
+(3,2) 5 @ 8,11
+(4,1) 5 @ 9,13
+(4,2) 6 @ 10,14
+(4,3) 7 @ 11,15
+(5,1) 6 @ 11,14
+(5,2) 7 @ 12,17
+(5,3) @ 8,13,18
+(5,4) @ 9,14,19
+(6,1) 7 @ 13,19
+(6,2) @ 8,14,20
+(6,3) @ 9,15,21
+(6,4) @ 10,16,22
+(6,5) @ 11,17,23
+*/
+
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
     };
+    // a % b = c, c < b < a <= n
+
     // println!("{rs}");
 }
+
