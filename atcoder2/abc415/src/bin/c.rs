35d13bedb6899b0a9124189014c5b06b55614270
horyu <28817604+horyu@users.noreply.github.com>
2025-08-28 19:48:11 +0900

    abc415 c

--- atcoder2/abc415/src/bin/c.rs
+++ atcoder2/abc415/src/bin/c.rs
@@ -11,8 +11,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
+        nnss: [(usize, Chars); t],
     };
-    // println!("{rs}");
+    // for (i, &c) in nnss[0].1.iter().enumerate() {
+    //     let i = i + 1;
+    //     eprintln!("[{i}] {i:03b}: {}", if  c == '0' { 'o' } else { 'x' });
+    // }
+    'outer: for (n, mut s) in nnss {
+        let mut qq = VecDeque::new();
+        qq.push_back(0);
+        let ok = (1usize << n) - 1;
+        while let Some(i) = qq.pop_front() {
+            // if n == 3 {
+            //     eprintln!("{i:03b}");
+            // }
+            for j in 0..n {
+                let ii = i | (1 << j);
+                if ii == i || s[ii - 1] == '1' {
+                    continue;
+                }
+                if ii == ok {
+                    println!("Yes");
+                    continue 'outer;
+                }
+                qq.push_back(ii);
+                s[ii - 1] = '1';
+            }
+        }
+        println!("No");
+    }
 }
