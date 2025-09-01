3560dbad92225898fb64ea137229c95afe58141f
horyu <28817604+horyu@users.noreply.github.com>
2025-09-01 22:47:58 +0900

    abc415 e

--- atcoder2/abc415/src/bin/e.rs
+++ atcoder2/abc415/src/bin/e.rs
@@ -16,36 +16,20 @@ fn main() {
         aaa: [[usize; w]; h],
         pp: [usize; h + w - 1],
     };
-    let mut hm = HashMap::new();
-    hm.insert((0, 0), (0, 0));
-    for (k, p) in pp.iter().copied().enumerate() {
-        let mut new_hm = HashMap::new();
-        for ((i, j), (vp, vn)) in hm {
-            let a = aaa[i][j];
-            for (ii, jj) in chain!(
-                (i + 1 < h).then_some((i + 1, j)),
-                (j + 1 < w).then_some((i, j + 1)),
-                (i + j == h + w - 2).then_some((i, j))
-            ) {
-                let e = new_hm.entry((ii, jj)).or_insert((0, usize::MAX));
-                let vvp = (vp + a).saturating_sub(p);
-                let vvn = vn + p.saturating_sub(vp + a);
-                if vvn < e.1 || (vvn == e.1 && e.0 < vvp) {
-                    *e = (vvp, vvn);
-                    // if (h, w) == (2, 2) {
-                    //     eprintln!("[{k}] ({i},{j}) -> ({ii},{jj}): {a}+{vp}-{p}={vvp}, {vn}+{p}-{vp}-{a}={vvn}");
-                    // }
-                }
+    // https://atcoder.jp/contests/abc415/editorial/13490
+    let mut dp = vec![vec![1e18 as usize; w]; h];
+    dp[h - 1][w - 1] = 0;
+    for i in (0..h).rev() {
+        for j in (0..w).rev() {
+            if i + 1 < h {
+                dp[i][j] = dp[i][j].min(dp[i + 1][j]);
             }
+            if j + 1 < w {
+                dp[i][j] = dp[i][j].min(dp[i][j + 1]);
+            }
+            dp[i][j] = (dp[i][j] + pp[i + j]).saturating_sub(aaa[i][j]);
         }
-        hm = new_hm;
-        // if (h, w) == (2, 2) {
-        //     for (kk, v) in &hm {
-        //         eprintln!("[{k}] {kk:?}:{v:?}");
-        //     }
-        // }
     }
-    // dbg!(&hm);
-    let rs = hm.into_values().next().unwrap().1;
+    let rs = dp[0][0];
     println!("{rs}");
 }
