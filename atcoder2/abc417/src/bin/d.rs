d16a1ee6322eca52ea1cf5e656b93369e027d3ee
horyu <28817604+horyu@users.noreply.github.com>
2025-09-15 23:12:08 +0900

    abc417 d

--- atcoder2/abc417/src/bin/d.rs
+++ atcoder2/abc417/src/bin/d.rs
@@ -19,8 +19,20 @@ fn main() {
         n: usize,
         ppaabb: [(usize, usize, usize); n],
         q: usize,
-        xx: [usize; n],
+        xx: [usize; q],
     };
-    // テンション0..=1000の範囲で逆順にDP?
-    // println!("{rs}");
+    // テンション0..=1000の範囲で逆順にDP
+    // dp[i][j] = テンションjでi番目のプレゼントを処理したときの最終的な値
+    let mut memo = vec![vec![None::<usize>; 1001]; n + 1];
+    memo[n] = (0..=1000).map(Some).collect_vec();
+    let mut subs = vec![0; n + 1];
+    for (i, (p, a, b)) in ppaabb.iter().copied().enumerate() {
+        subs[i + 1] = subs[i] + b;
+    }
+    for x in xx {
+        let pos = subs.partition_point(|&s| 500 + s < x);
+        // TODO
+        // 処理しつつmemoに値があればそれを使う
+        // memoを更新
+    }
 }
