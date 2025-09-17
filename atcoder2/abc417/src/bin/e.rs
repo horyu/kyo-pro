1357224b78413a1c5846511ab35c61cb55ba7b93
horyu <28817604+horyu@users.noreply.github.com>
2025-09-17 22:33:57 +0900

    abc417 e

--- atcoder2/abc417/src/bin/e.rs
+++ atcoder2/abc417/src/bin/e.rs
@@ -16,8 +16,54 @@ macro_rules! eprintln {
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
-    };
-    // println!("{rs}");
+        t: usize,
+    }
+    for _ in 0..t {
+        input! {
+            n: usize,
+            m: usize,
+            x: Usize1,
+            y: Usize1,
+            uuvv: [(Usize1, Usize1); m],
+        };
+        let mut g = vec![vec![]; n];
+        for (u, v) in uuvv.iter().copied() {
+            g[u].push(v);
+            g[v].push(u);
+        }
+        // TODO: 多分xとyのパスの間にない一方通行の辺を消す
+        for vv in g.iter_mut() {
+            vv.sort_unstable();
+        }
+        let mut rrss = vec![x + 1];
+        let mut visited = vec![false; n];
+        visited[x] = true;
+        fn dfs(
+            g: &Vec<Vec<usize>>,
+            visited: &mut Vec<bool>,
+            rrss: &mut Vec<usize>,
+            v: usize,
+            y: usize,
+        ) -> bool {
+            if v == y {
+                return true;
+            }
+            for w in g[v].iter().copied() {
+                if visited[w] {
+                    continue;
+                }
+                visited[w] = true;
+                rrss.push(w + 1);
+                if dfs(g, visited, rrss, w, y) {
+                    return true;
+                }
+                rrss.pop();
+                visited[w] = false;
+            }
+            false
+        }
+        dfs(&g, &mut visited, &mut rrss, x, y);
+        let rs = rrss.iter().join(" ");
+        println!("{rs}");
+    }
 }
