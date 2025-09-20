c592bfb3ab430f9fc8231d6b327437882eb9c49e
horyu <28817604+horyu@users.noreply.github.com>
2025-09-20 23:21:06 +0900

    abc417 f

--- atcoder2/abc417/src/bin/f.rs
+++ atcoder2/abc417/src/bin/f.rs
@@ -14,10 +14,34 @@ macro_rules! eprintln {
     ($($tt:tt)*) => {};
 }
 
+use ac_library::{Additive, MapMonoid, Monoid};
+struct MM;
+impl MapMonoid for MM {
+    type M = Additive<i32>;
+    type F = i32;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
+        f + x
+    }
+
+    fn composition(f: &Self::F, g: &Self::F) -> Self::F {
+        f + g
+    }
+}
+
 fn main() {
     input! {
         n: usize,
+        m: usize,
         aa: [usize; n],
+        llrr: [(Usize1, Usize1); m],
     };
+    let mut ls = ac_library::LazySegtree::<MM>::new(n);
+    // TODO
+
     // println!("{rs}");
 }
