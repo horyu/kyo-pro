6e29e85e58110cbbdfe3b8652c18375eaba084a7
horyu <28817604+horyu@users.noreply.github.com>
2025-09-23 11:05:26 +0900

    abc418 c

--- atcoder2/abc418/src/bin/c.rs
+++ atcoder2/abc418/src/bin/c.rs
@@ -17,7 +17,30 @@ macro_rules! eprintln {
 fn main() {
     input! {
         n: usize,
+        q: usize,
         aa: [usize; n],
+        bb: [usize; q],
     };
-    // println!("{rs}");
+    let size = 1e6 as usize + 2;
+    // cc[i] := i以下のaaの要素数
+    let mut cc = vec![0usize; size];
+    // dd[i] := i以下のaaの要素和
+    let mut dd = vec![0usize; size];
+    for a in aa.iter().copied() {
+        cc[a] += 1;
+        dd[a] += a;
+    }
+    for i in 1..size {
+        cc[i] += cc[i - 1];
+        dd[i] += dd[i - 1];
+    }
+    for b in bb {
+        // b未満の要素和 + b以上の要素数 * (b - 1) + 1
+        let rs = dd[b - 1] + (cc[size - 1] - cc[b - 1]) * (b - 1) + 1;
+        if dd[size - 1] < rs {
+            println!("-1");
+            continue;
+        }
+        println!("{rs}");
+    }
 }
