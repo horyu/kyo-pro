be29eaeba9515211add275e2ba6068e0ee0932a1
horyu <28817604+horyu@users.noreply.github.com>
2025-09-24 23:06:17 +0900

    abc418 d

--- atcoder2/abc418/src/bin/d.rs
+++ atcoder2/abc418/src/bin/d.rs
@@ -14,10 +14,26 @@ macro_rules! eprintln {
     ($($tt:tt)*) => {};
 }
 
+/*
+100 -> 00,11 -> 1,1
+1100: [1][1],1[0]1 -> 1,01 -> 1,0
+*/
+
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        t: Chars,
     };
-    // println!("{rs}");
+    // https://atcoder.jp/contests/abc418/editorial/13622
+    // dp[i][j] = i文字目まで見た時、0を2m+j子含む部分文字列の個数
+    let mut dp = vec![[0; 2]; n + 1];
+    for (i, c) in t.iter().copied().enumerate() {
+        if c == '0' {
+            dp[i + 1] = [dp[i][1], dp[i][0] + 1];
+        } else {
+            dp[i + 1] = [dp[i][0] + 1, dp[i][1]];
+        }
+    }
+    let rs = dp.into_iter().map(|d| d[0]).sum::<usize>();
+    println!("{rs}");
 }
