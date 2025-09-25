aefda2ddb00e65de6a740406bf4c7d1e81d0eaa0
horyu <28817604+horyu@users.noreply.github.com>
2025-09-26 08:27:04 +0900

    abc419 ab

---
+++ atcoder2/abc419/src/bin/a.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables, unused_macros)]
+#![feature(int_roundings)]
+use itertools::{Itertools as _, chain, iproduct, izip};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[cfg(not(debug_assertions))]
+macro_rules! eprintln {
+    ($($tt:tt)*) => {};
+}
+
+fn main() {
+    input! {
+        s: String,
+    };
+    // red, blue, green => SSS, FFF, MMM
+    let rs = match s.as_str() {
+        "red" => "SSS",
+        "blue" => "FFF",
+        "green" => "MMM",
+        _ => "Unknown ",
+    };
+    println!("{rs}");
+}
