0009cb46bc48d8d7125f29eecd26162b6e635d1f
horyu <28817604+horyu@users.noreply.github.com>
2025-10-08 21:59:43 +0900

    abc421 c

--- atcoder2/abc421/src/bin/c.rs
+++ atcoder2/abc421/src/bin/c.rs
@@ -17,7 +17,32 @@ macro_rules! eprintln {
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        s: Chars,
     };
-    // println!("{rs}");
+    let mut rs = !0;
+    // A,Bが隣り合わないようにするのに必要な回数 = min(/AB/ループにする回数, /BA/ループにする回数)
+    for l in ['A', 'B'] {
+        let mut x = 0;
+        let mut y = 0;
+        let mut vv = vec![];
+        for c in s.iter().copied() {
+            if c == l {
+                vv.push(x * 2);
+                x += 1;
+            } else {
+                vv.push(y * 2 + 1);
+                y += 1;
+            }
+        }
+        // eprintln!("{vv:?}");
+        // BIT
+        let mut tmp = 0;
+        let mut ft = ac_library::FenwickTree::new(n * 2, 0usize);
+        for &v in vv.iter().rev() {
+            tmp += ft.sum(..=v);
+            ft.add(v, 1);
+        }
+        rs = rs.min(tmp);
+    }
+    println!("{rs}");
 }
