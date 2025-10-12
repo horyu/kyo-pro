14117f850a83b8e169ce4dcd32a4a0a838330456
horyu <28817604+horyu@users.noreply.github.com>
2025-10-12 20:18:35 +0900

    abc421 e

--- atcoder2/abc421/src/bin/e.rs
+++ atcoder2/abc421/src/bin/e.rs
@@ -20,6 +20,48 @@ fn main() {
         aa: [usize; 6],
     };
     // https://atcoder.jp/contests/abc421/submissions/69008500
-    // TODO
-    // println!("{rs}");
+    let rs = f(3, 0, [0; 5], &aa, &mut HashMap::new());
+    println!("{rs:.10}");
+}
+
+fn f(
+    k: usize,
+    l: usize,
+    vv: [usize; 5],
+    aa: &[usize],
+    memo: &mut HashMap<(usize, usize, [usize; 5]), f64>,
+) -> f64 {
+    if let Some(&rs) = memo.get(&(k, l, vv)) {
+        return rs;
+    }
+    let rest = 5 - l;
+    if k == 0 || rest == 0 {
+        let rs = vv
+            .into_iter()
+            .map(|v| aa[v])
+            .sorted_unstable()
+            .dedup_with_count()
+            .map(|(cnt, a)| cnt * a)
+            .max()
+            .unwrap() as f64;
+        memo.insert((k, l, vv), rs);
+        return rs;
+    }
+
+    let mut sum = 0f64;
+    for pp in (0..rest).map(|_| 0..6).multi_cartesian_product() {
+        let mut tpm = 0f64;
+        let mut ww = vv;
+        let iter = if k == 1 { rest..=rest } else { 0..=rest };
+        for size in iter {
+            for qq in pp.iter().copied().combinations(size) {
+                ww[l..(l + size)].copy_from_slice(&qq);
+                tpm = tpm.max(f(k - 1, l + size, ww, aa, memo));
+            }
+        }
+        sum += tpm;
+    }
+    let rs = sum / 6f64.powi(rest as i32);
+    memo.insert((k, l, vv), rs);
+    rs
 }
