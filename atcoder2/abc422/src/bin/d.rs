934a1df8385e3f780895ed394542973c9f23140a
horyu <28817604+horyu@users.noreply.github.com>
2025-10-20 23:20:38 +0900

    abc422 d

--- atcoder2/abc422/src/bin/d.rs
+++ atcoder2/abc422/src/bin/d.rs
@@ -14,19 +14,34 @@ macro_rules! eprintln {
     ($($tt:tt)*) => {};
 }
 
+/*
+可能な限り分散するようにkkを分配する
+2の冪乗ごとに分配するイメージ
+12345678
+1 3 2 4
+ 5 7 6 8
+01234567
+1       :0+[0]
+    2   :4+[0] -> [0,4]
+  3   4 :2+[0,4] -> [0,4,2,6]
+ 5 7 6 8:1+[0,4,2,6]
+
+1234567890123456
+1   3   2   4
+  5   7   6   8
+ 9   1   0   2
+   3   5   4   6
+0123456789012345
+*/
+
 fn main() {
     input! {
         n: usize,
         k: usize,
     };
-    let pow = 2usize.pow(n as u32);
-    let mut bb = vec![k / pow; pow];
-    let mut kk = k % pow;
-    // 可能な限り分散するようにkkを分配する
-    // 2の冪乗ごとに分配するイメージ
-    // 12345678
-    // 1 3 2 4
-    //  5 7 6 8
-    todo!();
-    // println!("{rs}");
+    let size = 2usize.pow(n as u32);
+    let mut bb = vec![k / size; size];
+    // TODO
+    let rs = bb.iter().join(" ");
+    println!("{rs}");
 }
