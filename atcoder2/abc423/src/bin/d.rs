ec04c3e78403b0bc95196ebe5fe0584035247d67
horyu <28817604+horyu@users.noreply.github.com>
2025-10-27 09:05:25 +0900

    abc423 d

--- atcoder2/abc423/src/bin/d.rs
+++ atcoder2/abc423/src/bin/d.rs
@@ -17,7 +17,26 @@ macro_rules! eprintln {
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        k: isize,
+        aabbcc: [(usize, usize, isize); n],
     };
-    // println!("{rs}");
+    let mut btm = BTreeMap::new();
+    let mut sum = 0isize;
+    let mut now = 0usize;
+    let mut rrss = vec![];
+    for (a, b, c) in aabbcc.iter().copied() {
+        while k < sum + c {
+            let (t, v) = btm.pop_first().unwrap();
+            now = t;
+            sum += v;
+        }
+        if now <= a {
+            now = a;
+        }
+        rrss.push(now);
+        sum += c;
+        *btm.entry(now + b).or_insert(0isize) -= c;
+    }
+    let rs = rrss.iter().join("\n");
+    println!("{rs}");
 }
