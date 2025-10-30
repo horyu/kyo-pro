901041371e5a6a7a5f9f8bdb7b8003666b7ce809
horyu <28817604+horyu@users.noreply.github.com>
2025-10-30 09:56:52 +0900

    abc423 e

--- atcoder2/abc423/src/bin/e.rs
+++ atcoder2/abc423/src/bin/e.rs
@@ -14,74 +14,56 @@ macro_rules! eprintln {
     ($($tt:tt)*) => {};
 }
 
-/*
-[1:1]
-1 = 0+(1) : 1+(0)
-[1:2]
-21
- 1
-22 = 1+(1,1) : 2+(0,0)
-[1:3]
-321
- 21
-  1
-343 = 2+(1,2,1) : 3+(0,1,0)
-[1:4]
-4321
- 321
-  21
-   1
-4664 = 3+(1,3,3,1) : 4+(0,2,2,0)
-[1:5]
-54321
- 4321
-  321
-   21
-    1
-58985 = 4+(1,4,5,4,1) : 5+(0,3,4,3,0)
-[1:6]
-654321
- 54321
-  4321
-   321
-    21
-     1
-602206 = 5+(1,5,7,7,5,1) : 6+(0,4,6,6,4,0)
+// https://atcoder.jp/contests/abc423/editorial/13869
+use ac_library::Monoid;
 
-ans(i:j) = ??
-*/
+#[derive(Clone, Debug)]
+struct S {
+    val: usize,
+    len: usize,
+    res: usize,
+}
+impl ac_library::Monoid for S {
+    type S = S;
+
+    fn identity() -> Self {
+        S {
+            val: 0,
+            len: 0,
+            res: 0,
+        }
+    }
+
+    fn binary_operation(l: &Self::S, r: &Self::S) -> Self::S {
+        S {
+            val: l.val + r.val,
+            len: l.len + r.len,
+            res: l.res + r.res + l.len * r.val - l.val * r.len,
+        }
+    }
+}
+
 fn main() {
     input! {
         n: usize,
         q: usize,
         aa: [usize; n],
-        llrr: [(Usize1, Usize1); q],
+        llrr: [(usize, usize); q],
     };
-    let mut memo = HashMap::new();
-    let mut ft = ac_library::FenwickTree::new(n, 0usize);
-    for (i, a) in aa.iter().copied().enumerate() {
-        ft.add(i, a);
+    let mut ss = vec![S::identity(); n + 1];
+    let mut cur = S {
+        val: 0,
+        len: 1,
+        res: 0,
+    };
+    ss[0] = cur.clone();
+    for i in 1..=n {
+        cur.val += aa[i - 1];
+        ss[i] = cur.clone();
     }
+    let st = ac_library::Segtree::<S>::from(ss);
     for (l, r) in llrr {
-        let rs = (r - l) * ft.sum(l..=r) + f(l, r, &aa, &mut memo);
+        let rs = st.prod((l - 1)..=r).res;
         println!("{rs}");
     }
-}
-
-fn f(
-    l: usize,
-    r: usize,
-    aa: &[usize],
-    memo: &mut HashMap<(usize, usize), usize>,
-) -> usize {
-    if let Some(&v) = memo.get(&(l, r)) {
-        return v;
-    }
-    let v = if l == r {
-        aa[l]
-    } else {
-        f(l, r - 1, aa, memo) + f(l + 1, r, aa, memo)
-    };
-    memo.insert((l, r), v);
-    v
 }
