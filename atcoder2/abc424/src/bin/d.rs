219295ae76001e94f48488f88d6b07836a6f968d
horyu <28817604+horyu@users.noreply.github.com>
2025-11-02 23:06:48 +0900

    abc424 d

--- atcoder2/abc424/src/bin/d.rs
+++ atcoder2/abc424/src/bin/d.rs
@@ -16,8 +16,29 @@ macro_rules! eprintln {
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
     };
-    // println!("{rs}");
+    for _ in 0..t {
+        input! {
+            h: usize,
+            w: usize,
+            mut ss: [Chars; h],
+        };
+        let mut rs = 0;
+        let tes = |(x, y): (usize, usize)| -> bool {
+            (0..2).all(|i| (0..2).all(|j| ss[x + i][y + j] == '#'))
+        };
+        for i in 0..(h - 1) {
+            for j in 0..(w - 1) {
+                let arr = [(i, j), (i + 1, j), (i, j + 1), (i + 1, j + 1)];
+                if arr.iter().all(|&(x, y)| ss[x][y] == '#') {
+                    let x = i + usize::from(i < h - 1);
+                    let y = j + 1;
+                    ss[x][y] = '.';
+                    rs += 1;
+                }
+            }
+        }
+        println!("{rs}");
+    }
 }
