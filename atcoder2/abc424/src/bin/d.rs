15c49fc4c2913db21cd22fbe0858032084fa7f1e
horyu <28817604+horyu@users.noreply.github.com>
2025-11-04 20:12:49 +0900

    abc424 d

--- atcoder2/abc424/src/bin/d.rs
+++ atcoder2/abc424/src/bin/d.rs
@@ -18,115 +18,48 @@ fn main() {
     input! {
         t: usize,
     };
-    let arr = |i: usize, j: usize| [(i, j), (i + 1, j), (i, j + 1), (i + 1, j + 1)];
+    // https://atcoder.jp/contests/abc424/editorial/13932
     for _ in 0..t {
         input! {
             h: usize,
             w: usize,
-            mut ss: [Chars; h],
+            ss: [Chars; h],
         };
-        let mut ccc = vec![vec![0; w]; h];
-        let mut hs = HashSet::new();
-        for i in 0..(h - 1) {
-            for j in 0..(w - 1) {
-                if arr(i, j).into_iter().all(|(x, y)| ss[x][y] == '#') {
-                    hs.insert((i, j));
-                    for &(x, y) in &arr(i, j) {
-                        ccc[x][y] += 1;
+        let k = 1usize << w;
+        let mut allow = vec![vec![true; k]; k];
+        for i in 0..k {
+            for j in 0..k {
+                for ii in 0..(w - 1) {
+                    if (i >> ii) & 3 == 3 && (j >> ii) & 3 == 3 {
+                        allow[i][j] = false;
+                        break;
                     }
                 }
             }
         }
-        // c=1 である端っこのマスから優先的に潰す
-        let mut rs = 0;
-        while !hs.is_empty() {
-            let mut new_hs = hs.clone();
-            let mut tmp1122 = None;
-            for (i, j) in hs.iter().copied() {
-                if arr(i, j).into_iter().any(|(x, y)| ss[x][y] == '.') {
-                    for (x, y) in arr(i, j) {
-                        new_hs.remove(&(x, y));
-                        ccc[x][y] -= 1;
-                    }
-                    continue;
+        const INF: usize = 1e11 as usize;
+        let mut dp = vec![INF; k];
+        dp[0] = 0;
+        for i in 0..h {
+            let mut state = 0;
+            for j in 0..w {
+                if ss[i][j] == '#' {
+                    state |= 1 << j;
                 }
-                let mut c = 0;
-                for (x, y) in arr(i, j) {
-                    if ccc[x][y] == 1 {
-                        c += 1;
-                    }
-                }
-                if c == 4 {
-                    // 11
-                    // 11
-                    // どこでも良い
-                    ss[i][j] = '.';
-                    for (x, y) in arr(i, j) {
-                        ccc[x][y] = 0;
-                        new_hs.remove(&(x, y));
-                    }
-                    rs += 1;
-                } else if c == 3 {
-                    // 1*
-                    // 11
-                    // 1ではない部分を潰す
-                    for (x, y) in arr(i, j) {
-                        if ccc[x][y] != 1 {
-                            ss[x][y] = '.';
+            }
+            let mut dp2 = vec![INF; k];
+            for j in 0..k {
+                if j | state == state {
+                    for jj in 0..k {
+                        if allow[jj][j] {
+                            dp2[j] = dp2[j].min(dp[jj] + (j ^ state).count_ones() as usize);
                         }
-                        ccc[x][y] -= 1;
-                        new_hs.remove(&(x, y));
                     }
-                    rs += 1;
-                } else if c == 2 {
-                    // 11; 22 だった場合には後回しにする
-                    if arr(i, j)
-                        .into_iter()
-                        .map(|(x, y)| ccc[x][y])
-                        .sorted_unstable()
-                        .collect_vec()
-                        == vec![1, 1, 2, 2]
-                    {
-                        tmp1122 = Some((i, j));
-                    }
-                } else if c == 1 {
-                    // **
-                    // *1
-                    // 一番大きい部分を潰す
-                    let Some((x, y)) = arr(i, j).into_iter().max_by_key(|&(x, y)| ccc[x][y]) else {
-                        unreachable!();
-                    };
-                    ss[x][y] = '.';
-                    for (x, y) in arr(i, j) {
-                        ccc[x][y] -= 1;
-                        new_hs.remove(&(x, y));
-                    }
-                    rs += 1;
                 }
             }
-            if hs.len() == new_hs.len() {
-                // 変化がなかった -> 1122 処理
-                if let Some((i, j)) = tmp1122 {
-                    // 2の部分を潰す
-                    let Some((x, y)) = arr(i, j)
-                        .into_iter()
-                        .find(|&(x, y)| ccc[x][y] == 2)
-                    else {
-                        unreachable!();
-                    };
-                    ss[x][y] = '.';
-                    for (x, y) in arr(i, j) {
-                        ccc[x][y] -= 1;
-                        new_hs.remove(&(x, y));
-                    }
-                    rs += 1;
-                } else {
-                    unreachable!();
-                }
-            }
-            hs = new_hs;
+            dp = dp2;
         }
-
+        let rs = dp.into_iter().min().unwrap();
         println!("{rs}");
     }
 }
