8714fd90aea24be9f7065288dbf84ada2f38ec11
horyu <28817604+horyu@users.noreply.github.com>
2025-11-10 23:14:43 +0900

    abc425 e

--- atcoder2/abc425/src/bin/e.rs
+++ atcoder2/abc425/src/bin/e.rs
@@ -16,8 +16,72 @@ macro_rules! eprintln {
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
+        m: usize,
     };
-    // println!("{rs}");
+    let mut memo = Vec::with_capacity(5001);
+    memo.push(counter::Counter::<usize>::new());
+    for i in 1..=5000 {
+        let mut counter = memo.last().unwrap().clone();
+        for (k, v) in factors(i) {
+            counter[&k] += v;
+        }
+        memo.push(counter);
+    }
+    let mut rs = String::new();
+    for _ in 0..t {
+        input! { n: usize, cc: [usize; n] };
+        let sum = cc.iter().sum::<usize>();
+        let mut counter = memo[sum].clone();
+        for c in cc {
+            for (k, v) in memo[c].iter() {
+                counter[k] -= v;
+                if counter[k] == 0 {
+                    counter.remove(k);
+                }
+            }
+        }
+        let v = counter
+            .into_map()
+            .into_iter()
+            .fold(1usize, |acc, (k, v)| acc * mod_pow(k, v, m) % m);
+        rs.push_str(&format!("{v}\n"));
+    }
+    print!("{rs}");
+}
+
+fn factors(mut n: usize) -> HashMap<usize, usize> {
+    let mut hm = HashMap::new();
+    if n <= 1 {
+        return hm;
+    }
+    while n.is_multiple_of(2) {
+        n /= 2;
+        *hm.entry(2).or_insert(0) += 1;
+    }
+    let mut i = 3;
+    while i * i <= n {
+        while n.is_multiple_of(i) {
+            n /= i;
+            *hm.entry(i).or_insert(0) += 1;
+        }
+        i += 2;
+    }
+    if n != 1 {
+        hm.insert(n, 1);
+    }
+    hm
+}
+
+fn mod_pow(mut x: usize, mut n: usize, m: usize) -> usize {
+    x %= m;
+    let mut ans = 1;
+    while n != 0 {
+        if n.is_odd() {
+            ans = ans * x % m;
+        }
+        x = x * x % m;
+        n >>= 1;
+    }
+    ans
 }
