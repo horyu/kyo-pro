ef60a6456873eb09db784ac1584ceda4f14e604c
horyu <28817604+horyu@users.noreply.github.com>
2025-11-13 09:04:46 +0900

    abc426 ab

---
+++ atcoder2/abc426/src/bin/a.rs
@@ -0,0 +1,28 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables, unused_macros)]
+#![feature(int_roundings)]
+use itertools::{Itertools as _, chain, iproduct, izip};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[cfg(not(debug_assertions))]
+macro_rules! eprintln {
+    ($($tt:tt)*) => {};
+}
+
+fn main() {
+    input! {
+        x: Chars,
+        y: Chars,
+    };
+    let arr = ['O', 'S', 'L'];
+    let i = arr.iter().position(|&a| a == x[0]).unwrap();
+    let j = arr.iter().position(|&a| a == y[0]).unwrap();
+    let tf = i >= j;
+    let rs = ["No", "Yes"][tf as usize];
+    println!("{rs}");
+}
