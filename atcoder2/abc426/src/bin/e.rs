9197ab6deb2d94544401d4882eebe3f8054c2860
horyu <28817604+horyu@users.noreply.github.com>
2025-11-17 23:30:04 +0900

    abc426 e

--- atcoder2/abc426/src/bin/e.rs
+++ atcoder2/abc426/src/bin/e.rs
@@ -16,8 +16,27 @@ macro_rules! eprintln {
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        t: usize,
+        // qq: [(f64, f64, f64, f64, f64, f64, f64, f64); t],
+        qq: [[(f64, f64, f64, f64); 2]; t],
     };
+    for mut fff in qq {
+        // 0 ~ t0(一方が止まる) ~ t1(両方止まる)
+        let mut tt = fff
+            .iter()
+            .copied()
+            .map(|(sx, sy, gx, gy)| (sx - gx).hypot(sy - gy))
+            .collect_vec();
+        if tt[1] < tt[0] {
+            fff.swap(0, 1);
+            tt.swap(0, 1);
+        }
+        // x = sx + (gx - sx) * u
+        // y = sy + (gy - sy) * u
+        // dx = (sx0-sx1) + (gx0-sx0 - (gx1-sx1)) * u
+        // dy = (sy0-sy1) + (gy0-sy0 - (gy1-sy1)) * u
+        // dx^2 + dy^2 が最小となるのは両端または極小点
+        // TODO
+    }
     // println!("{rs}");
 }
