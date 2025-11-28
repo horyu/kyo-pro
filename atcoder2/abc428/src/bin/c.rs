a85fedb54fef21e46ff45eb9e69a81e4a5474a8b
horyu <28817604+horyu@users.noreply.github.com>
2025-11-28 23:08:09 +0900

    abc428 c

--- atcoder2/abc428/src/bin/c.rs
+++ atcoder2/abc428/src/bin/c.rs
@@ -16,8 +16,41 @@ macro_rules! eprintln {
 
 fn main() {
     input! {
-        n: usize,
-        aa: [usize; n],
+        q: usize,
     };
-    // println!("{rs}");
+    let mut r2l = BTreeMap::new();
+    let mut btm = BTreeMap::new();
+    let mut s = vec![];
+    for _ in 0..q {
+        input! {t: usize};
+        if t == 1 {
+            input! {c: char};
+            let slen = s.len();
+            s.push(c);
+            if c == ')' {
+                if let Some((&pos, &cc)) = btm.last_key_value()
+                    && cc == '('
+                {
+                    btm.pop_last();
+                    r2l.insert(slen, pos);
+                } else {
+                    btm.insert(slen, c);
+                }
+            } else {
+                btm.insert(slen, c);
+            }
+        } else {
+            btm.pop_last();
+            let c = s.pop().unwrap();
+            if c == ')' {
+                let slen = s.len();
+                if let Some(pos) = r2l.remove(&slen) {
+                    btm.insert(pos, '(');
+                }
+            }
+        }
+        eprintln!("{s:?} {btm:?} {r2l:?}");
+        let rs = ["No", "Yes"][btm.is_empty() as usize];
+        println!("{rs}");
+    }
 }
