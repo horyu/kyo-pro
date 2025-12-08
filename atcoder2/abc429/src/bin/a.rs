250ca921c1a97084fa0b24a987e4a1fe386d7623
horyu <28817604+horyu@users.noreply.github.com>
2025-12-08 09:38:17 +0900

    abc429 ab

---
+++ atcoder2/abc429/src/bin/a.rs
@@ -0,0 +1,26 @@
+#![allow(clippy::many_single_char_names, clippy::needless_range_loop, clippy::collapsible_else_if)]
+#![allow(unused_imports, unused_variables, unused_macros)]
+#![feature(int_roundings)]
+use itertools::{Itertools as _, chain, iproduct, izip};
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::*};
+use std::cmp::{Ordering, Reverse as R};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[cfg(not(debug_assertions))]
+macro_rules! eprintln {
+    ($($tt:tt)*) => {};
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+    };
+    for i in 1..=n {
+        let rs = if i <= m { "OK" } else { "Too Many Requests" };
+        println!("{rs}");
+    }
+}
