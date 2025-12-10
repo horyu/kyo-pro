af4ea200ca7793192fc081361ede2552cb0b61b7
horyu <28817604+horyu@users.noreply.github.com>
2025-12-10 21:20:38 +0900

    abc429 d

--- atcoder2/abc429/src/bin/d.rs
+++ atcoder2/abc429/src/bin/d.rs
@@ -17,7 +17,41 @@ macro_rules! eprintln {
 fn main() {
     input! {
         n: usize,
-        aa: [usize; n],
+        m: usize,
+        c: usize,
+        mut aa: [usize; n],
     };
-    // println!("{rs}");
+    aa.sort_unstable();
+    aa.extend(chain!(
+        aa.clone().into_iter().map(|a| a + m),
+        aa.clone().into_iter().map(|a| a + 2 * m)
+    ));
+    let ccpp = aa.into_iter().dedup_with_count().collect_vec();
+    let len = ccpp.len();
+    let sspp = ccpp
+        .iter()
+        .scan(0usize, |acc, &(cnt, pos)| {
+            *acc += cnt;
+            Some((*acc, pos))
+        })
+        .collect_vec();
+    eprintln!("{ccpp:?}");
+    eprintln!("{sspp:?}");
+    let mut rs = 0usize;
+    let mut pre_i = 0usize;
+    for (i, (si, pi)) in sspp.iter().copied().enumerate().take(len) {
+        if pi == 0 {
+            continue;
+        }
+        let j = i + sspp[i..].partition_point(|(sj, pj)| sj - si < c);
+        // dbg!(si, sspp[j]);
+        eprintln!(
+            "[{i}: ({si},{pi})] {:?} -> diff:{} rs:{rs}",
+            sspp[j],
+            sspp[j].0 - si
+        );
+        rs += (sspp[j].0 - si) * (pi - pre_i);
+        pre_i = pi;
+    }
+    println!("{rs}");
 }
